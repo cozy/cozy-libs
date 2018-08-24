2d8a0c45f34ee75681b59a6d44d6b93eb5d5bcb1
Patrick Browne <pt.browne@gmail.com>
2018-08-24 09:32:50 +0200

    chore: relax the subject case rule

--- packages/commitlint-config/index.js
+++ packages/commitlint-config/index.js
@@ -4,11 +4,6 @@ module.exports = {
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 50],
     'scope-case': [0, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'always',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-    ],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
     'type-case': [2, 'always', 'lower-case'],
