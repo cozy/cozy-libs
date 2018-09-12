a2e5514a1d0c54dd83b64ca2b4cfbebe61dc875d
Patrick Browne <pt.browne@gmail.com>
2018-09-13 00:28:19 +0200

    test: Intent creation and iFrame insertion

--- packages/interapp/src/intents.spec.js
+++ packages/interapp/src/intents.spec.js
@@ -1,9 +1,140 @@
 import Intents from './intents'
+import { mockAPI, sleep } from './testUtils'
 
 describe('Interapp', () => {
+  let client,
+    intents,
+    api = mockAPI()
+
+  beforeEach(() => {
+    api.reset()
+  })
+
+  const pickFileIntentNoService = {
+    attributes: {
+      services: []
+    }
+  }
+
+  const serviceOrigin = 'http://service-mock'
+  const serviceURL = 'http://service-mock/custom-service'
+
+  const pickFileIntent = {
+    id: 'pickfile-intent-id',
+    meta: {
+      _rev: undefined,
+    },
+    attributes: {
+      action: 'PICK',
+      type: 'io.cozy.files',
+      permissions: ['GET'],
+      services: [
+        {
+          slug: 'files',
+          href: serviceURL
+        }
+      ]
+    }
+  }
+
+  beforeEach(() => {
+    client = {
+      client: {
+        fetchJSON: jest.fn()
+      }
+    }
+    intents = new Intents({ client })
+    client.client.fetchJSON.mockImplementation(api.fetch)
+  })
+
   it('should initialise with cozy client', () => {
-    const client = { client: { fetchJSON: jest.fn() } }
-    const intents = new Intents({ client })
     expect(intents.request.client).toEqual(client.client)
+  })
+
+  describe('creation', () => {
+    it('should resolve with created intent', async () => {
+      api.respond(
+        'POST',
+        '/intents',
+        {
+          data: pickFileIntent
+        },
+        body => body.data.attributes.action === 'EDIT'
+      )
+      const intent = await intents.create('EDIT', 'io.cozy.files')
+      expect(intent).toBe(pickFileIntent)
+    })
+  })
+
+  describe('unexisting service', () => {
+    it('should reject if no service found', async () => {
+      api.respond(
+        'POST',
+        '/intents',
+        {
+          data: pickFileIntentNoService
+        },
+        body => body.data.attributes.action === 'EDIT'
+      )
+      const element = document.createElement('div')
+      expect(
+        intents.create('EDIT', 'io.cozy.files').start(element)
+      ).rejects.toThrow('Unable to find a service')
+    })
+  })
+
+  describe('existing service', () => {
+    let intent, element, iframe, prom
+
+    const mkMessage = (type, data, _intent = intent) => {
+      const ev = new Event('message')
+      Object.assign(ev, {
+        data: {
+          type: `intent-${_intent.id}:${type}`,
+          ...data
+        },
+        origin: serviceOrigin,
+        source: window
+      })
+      return ev
+    }
+
+    beforeEach(async () => {
+      api.respond(
+        'POST',
+        '/intents',
+        {
+          data: pickFileIntent
+        },
+        body => body.data.attributes.action === 'EDIT'
+      )
+      element = document.createElement('div')
+      const promIntent = intents.create('EDIT', 'io.cozy.files', {
+        id: 'fileId'
+      })
+      intent = await promIntent
+      prom = promIntent.start(element)
+      await sleep(1)
+      iframe = element.querySelector('iframe')
+      iframe.postMessage = jest.fn()
+    })
+
+    afterEach(() => {
+      prom.stop()
+      jest.restoreAllMocks()
+    })
+
+    it('should have created an iframe', () => {
+      expect(iframe).not.toBeUndefined()
+      expect(iframe.getAttribute('src')).toBe(serviceURL)
+      expect(iframe.classList.contains('coz-intent')).toBe(true)
+    })
+
+    it('cannot handle message without handshake', async () => {
+      window.dispatchEvent(mkMessage('done', { document: 'hello' }))
+      expect(prom).rejects.toThrow(
+        'Unexpected handshake message from intent service'
+      )
+    })
   })
 })
