08db1c5fbe4f8e9ab6145e70703ba0b5f3661bbd
Grégory Le Garec <gregory.legarec@gmail.com>
2018-09-18 07:21:04 +0200

    feat(cozy-app-publish): Throw publish error ✨

--- packages/cozy-app-publish/lib/publish.js
+++ packages/cozy-app-publish/lib/publish.js
@@ -11,28 +11,33 @@ module.exports = async ({
   sha256Sum,
   appType
 }) => {
-  const response = await fetch(
-    `${registryUrl}/${spaceName ? spaceName + '/' : ''}registry/${appSlug}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Token ${registryToken}`
-      },
-      body: JSON.stringify({
-        editor: registryEditor,
-        version: appVersion,
-        url: appBuildUrl,
-        sha256: sha256Sum,
-        type: appType
-      })
-    }
-  )
+  let response
+  try {
+    response = await fetch(
+      `${registryUrl}/${spaceName ? spaceName + '/' : ''}registry/${appSlug}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Token ${registryToken}`
+        },
+        body: JSON.stringify({
+          editor: registryEditor,
+          version: appVersion,
+          url: appBuildUrl,
+          sha256: sha256Sum,
+          type: appType
+        })
+      }
+    )
+  } catch (error) {
+    throw error
+  }
 
   if (response.status !== 201) {
     const body = await response.json()
     throw new Error(`${response.status} ${response.statusText}: ${body.error}`)
   }
 
-  return true
+  return response
 }
