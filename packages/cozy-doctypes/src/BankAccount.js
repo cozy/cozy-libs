c19f0ce45d18dcbe41286aeadb2b0a165023c05f
Patrick Browne <pt.browne@gmail.com>
2018-10-12 11:35:37 +0200

    feat: Use reconciliationKey function to concat two attributes

--- packages/cozy-doctypes/src/BankAccount.js
+++ packages/cozy-doctypes/src/BankAccount.js
@@ -1,26 +1,46 @@
 const keyBy = require('lodash/keyBy')
-const some = require('lodash/some')
 const Document = require('./Document')
+const log = require('cozy-logger').namespace('BankAccount')
 
 class BankAccount extends Document {
   static reconciliate(fetchedAccounts, localAccounts) {
-    const numberAttr = BankAccount.numberAttr
-    const byAccountNumber = keyBy(localAccounts, numberAttr)
+    const byAccountNumber = keyBy(localAccounts, acc =>
+      BankAccount.reconciliationKey(acc)
+    )
     return fetchedAccounts.map(fetchedAccount => {
-      const matchedSavedAccount = byAccountNumber[fetchedAccount[numberAttr]]
+      const fetchedAccountKey = this.reconciliationKey(fetchedAccount)
+      const matchedSavedAccount = byAccountNumber[fetchedAccountKey]
       return Object.assign({}, fetchedAccount, {
         _id: matchedSavedAccount && matchedSavedAccount._id
       })
     })
   }
 
+  static reconciliationKey(account) {
+    if (this.numberAttr) {
+      log(
+        'warn',
+        'numberAttr is deprecated, use reconciliationAttributes instead'
+      )
+      return this[this.numberAttr]
+    } else if (this.reconciliationAttributes) {
+      const key = this.reconciliationAttributes
+        .map(attr => account[attr])
+        .join(' | ')
+      return key
+    } else {
+      throw new Error(
+        'Cannot make reconciliationKey without reconciliationAttributes'
+      )
+    }
+  }
 }
 
 BankAccount.doctype = 'io.cozy.bank.accounts'
 BankAccount.idAttributes = ['_id']
 BankAccount.version = 1
 BankAccount.checkedAttributes = null
-BankAccount.numberAttr = 'number'
+BankAccount.reconciliationAttributes = ['number', 'label']
 BankAccount.vendorIdAttr = 'vendorId'
 
 module.exports = BankAccount
