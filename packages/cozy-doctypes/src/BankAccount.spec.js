4526c7c3d1b5d8733bc1c05b8b796af7f576192e
Patrick Browne <pt.browne@gmail.com>
2018-11-09 09:35:22 +0100

    feat: Integrate find duplicates

--- packages/cozy-doctypes/src/BankAccount.spec.js
+++ packages/cozy-doctypes/src/BankAccount.spec.js
@@ -16,3 +16,26 @@ describe('account reconciliation', () => {
     expect(matchedAccounts.length).toBe(2)
   })
 })
+
+describe('deleteDuplicateBankAccountsWithNoOperations', () => {
+  it('should return duplicate with no operations', () => {
+    const res = BankAccount.findDuplicateAccountsWithNoOperations(
+      [
+        { _id: 'empty', label: 'Duplicate account' },
+        { _id: 'filled', label: 'Duplicate account' },
+        { _id: 'filled_not_duplicate', label: 'Account with ops' }
+      ],
+      [
+        { _id: 'op1', account: 'filled' },
+        { _id: 'op2', account: 'filled' },
+        { _id: 'op3', account: 'filled' },
+        { _id: 'op4', account: 'filled' },
+        { _id: 'op5', account: 'filled_not_duplicate' },
+        { _id: 'op6', account: 'filled_not_duplicate' },
+        { _id: 'op7', account: 'filled_not_duplicate' }
+      ]
+    )
+
+    expect(res.map(x => x._id)).toEqual(['empty'])
+  })
+})
