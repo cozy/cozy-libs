1bbb5f9a06deac8cd4d55d9f47c69c1d3795eade
Patrick Browne <pt.browne@gmail.com>
2019-01-23 11:01:06 +0100

    fix(doctypes): Only pass transactions from konn related account

--- packages/cozy-doctypes/src/banking/BankingReconciliator.spec.js
+++ packages/cozy-doctypes/src/banking/BankingReconciliator.spec.js
@@ -20,6 +20,9 @@ describe('banking reconciliator', () => {
     reconciliator = new BankingReconciliator({ BankAccount, BankTransaction })
   })
 
+  afterEach(() => {
+    jest.restoreAllMocks()
+  })
 
   const fmtCreateOrUpdateCall = call => {
     const doc = call[0]
@@ -101,7 +104,86 @@ describe('banking reconciliator', () => {
       ]
     )
     expect(Document.createOrUpdate).toHaveBeenCalledTimes(4)
-    expect(Document.createOrUpdate.mock.calls.map(fmtCreateOrUpdateCall)).toMatchSnapshot()
+    expect(
+      Document.createOrUpdate.mock.calls.map(fmtCreateOrUpdateCall)
+    ).toMatchSnapshot()
   })
+
+  it('should pass stack transactions belonging to reconciliated accounts to BankTransaction::reconciliate', async () => {
+    existingAccounts = [
+      {
+        vendorId: 1,
+        number: '1',
+        _id: 123,
+        balance: 2000,
+        label: 'Bank account 1'
+      },
+      {
+        vendorId: 1,
+        number: '1337',
+        _id: 124,
+        balance: 2000,
+        label: 'Bank account 2 from another konnector'
+      }
+    ]
+    existingTransactions = [
+      {
+        vendorAccountId: 1,
+        vendorId: 123,
+        date: '2018-06-25T00:00',
+        label: 'Debit 200',
+        amount: -200,
+        account: 123
+      },
+      {
+        vendorAccountId: 1,
+        vendorId: 124,
+        date: '2018-06-25T00:00',
+        label: 'Debit 200',
+        amount: -200,
+        account: 123
+      },
+      {
+        vendorAccountId: 2,
+        vendorId: 125,
+        date: '2018-06-25T00:00',
+        label: 'Debit 300 from another konnector',
+        amount: -300,
+        account: 124
+      }
+    ]
+    jest.spyOn(BankTransaction, 'reconciliate')
+    const fetchedAccounts = [
+      {
+        number: '1', // existing account
+        vendorId: 2, // from a new connector account
+        balance: 1000,
+        label: 'Bank account 1'
+      }
+    ]
+    const fetchedTransactions = [
+      {
+        amount: -200,
+        label: 'Debit 200',
+        vendorAccountId: 2,
+        date: '2018-06-24T00:00' // prior to split date and doesn't exist, saved
+      },
+      {
+        amount: -100,
+        label: 'Debit 100',
+        vendorAccountId: 2,
+        date: '2018-06-27T00:00'
+      }
+    ]
+    await reconciliator.save(fetchedAccounts, fetchedTransactions)
+    expect(BankTransaction.reconciliate).toHaveBeenCalledWith(
+      fetchedTransactions,
+      [existingTransactions[0], existingTransactions[1]],
+      {}
+    )
+    expect(Document.createOrUpdate).toHaveBeenCalledTimes(3)
+    expect(
+      Document.createOrUpdate.mock.calls.map(fmtCreateOrUpdateCall)
+    ).toMatchSnapshot()
   })
 })
