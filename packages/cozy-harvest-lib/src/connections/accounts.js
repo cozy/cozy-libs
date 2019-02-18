5ef0ce8469436b2ed70e9dcbe1edea29f16de79a
Grégory Le Garec <gregory.legarec@gmail.com>
2019-02-18 11:12:58 +0100

    feat(harvest): Get Account mutations from Cozy-Home ♻️

---
+++ packages/cozy-harvest-lib/src/connections/accounts.js
@@ -0,0 +1,118 @@
+const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
+const PERMISSIONS_DOCTYPE = 'io.cozy.permissions'
+
+/**
+ * Create an account for the given konnector.
+ * @param  {Object}  client     CozyClient
+ * @param  {Object}  konnector  io.cozy.konnectors document
+ * @param  {Object}  attributes Account attributes
+ */
+const createAccount = async (client, konnector, attributes) => {
+  const { data } = konnector.aggregator
+    ? await createChildAccount(client, konnector, attributes)
+    : await client.create(ACCOUNTS_DOCTYPE, attributes)
+  return data
+}
+
+/**
+ * Performs a query to find the given account
+ * @param  {Object}  client CozyClient
+ * @param  {string}  id     io.cozy.accounts document's id
+ * @return {Object}         Retrieved account
+ */
+const findAccount = async (client, id) => {
+  try {
+    const { data } = await client.get(ACCOUNTS_DOCTYPE, id)
+    return data
+  } catch (error) {
+    if (error.status === 404) {
+      return null
+    }
+    throw error
+  }
+}
+
+/**
+ * Creates an account for an konnector needing an aggregator account.
+ * Creates the aggregator account as well if it does not exist.
+ * @param  {Object}  client     CozyClient
+ * @param  {Object}  konnector  io.cozy.konnectors document
+ * @param  {Object}  attributes Account attibutes
+ * @return {Object}             The created io.cozy.accounts document
+ */
+const createChildAccount = async (client, konnector, attributes) => {
+  const { aggregator } = konnector
+
+  if (!aggregator || !aggregator.accountId)
+    throw new Error('Konnector does not provide aggregator account id')
+
+  const parentAccountId = aggregator.accountId
+
+  let parentAccount
+  try {
+    parentAccount = await findAccount(client, parentAccountId)
+  } catch (error) {
+    throw new Error(
+      `An error occurred when finding parent account ${parentAccountId} (${
+        error.message
+      })`
+    )
+  }
+
+  if (!parentAccount) {
+    try {
+      parentAccount = await client.create(ACCOUNTS_DOCTYPE, {
+        _id: parentAccountId
+      })
+    } catch (error) {
+      throw new Error(
+        `Cannot create parent account ${parentAccountId} (${error.message})`
+      )
+    }
+  }
+
+  try {
+    await client.collection(PERMISSIONS_DOCTYPE).add(konnector, {
+      aggregatorAccount: {
+        type: ACCOUNTS_DOCTYPE,
+        verbs: ['GET', 'PUT'],
+        values: [`${ACCOUNTS_DOCTYPE}.${parentAccountId}`]
+      }
+    })
+  } catch (error) {
+    throw new Error(
+      `Cannot set permission for account ${parentAccountId} (${error.message})`
+    )
+  }
+
+  return await client.create(ACCOUNTS_DOCTYPE, {
+    ...attributes,
+    relationships: {
+      parent: {
+        data: {
+          _id: parentAccountId,
+          _type: ACCOUNTS_DOCTYPE
+        }
+      }
+    }
+  })
+}
+
+/**
+ * Get accounts mutations
+ * @param  {Object} client CozyClient
+ * @return {Object}        Object containing accounts mutations
+ */
+export const accountsMutations = client => {
+  const updateAccount = async account => {
+    const { data } = await client.save(account)
+    return data
+  }
+
+  return {
+    createAccount: createAccount.bind(null, client),
+    updateAccount
+  }
+}
+
+export default accountsMutations
