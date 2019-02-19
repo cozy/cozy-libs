27650e6ec6cf1c99415bb131a3f4ad8d76b832f5
Grégory Le Garec <gregory.legarec@gmail.com>
2019-02-19 15:12:17 +0100

    feat(harvest): Add trigger mutations ✨

---
+++ packages/cozy-harvest-lib/src/connections/triggers.js
@@ -0,0 +1,77 @@
+import { subscribe } from 'cozy-realtime'
+
+const JOBS_DOCTYPE = 'io.cozy.jobs'
+const TRIGGERS_DOCTYPE = 'io.cozy.triggers'
+
+/**
+ * Creates a trigger with given attributes
+ * @param  {Object} client CozyClient
+ * @param  {Object}   attributes
+ * @return {Object}   Created trigger
+ */
+const createTrigger = async (client, attributes) => {
+  const { data } = await client.collection(TRIGGERS_DOCTYPE).create(attributes)
+  return data
+}
+
+/**
+ * Triggers job associated to given trigger
+ * @param  {Object} client CozyClient
+ * @param  {Object}  Trigger to launch
+ * @return {Object}  Job document
+ */
+const launchTrigger = async (client, trigger) => {
+  const { data } = await client.collection(TRIGGERS_DOCTYPE).launch(trigger)
+  return data
+}
+
+/**
+ * Waits for successful login. For now we are not able to know in real time
+ * if the login was successful or not. We consider that after a sufficient
+ * delay, the konnector has successfully logged in and is now in the data
+ * fetching phase.
+ * The default login delay is 8 seconds.
+ * In the future, the realtime login detection will be performed in this
+ * method.
+ * @param  {Object} client CozyClient
+ * @param  {Object}  job               io.cozy.jobs document
+ * @param  {Number}  [expectedSuccessDelay=8000] Delay, in ms, until the login is
+ * considered as sucessful.
+ * @return {Object}                    The executed job
+ */
+const waitForLoginSuccess = async (
+  client,
+  job,
+  expectedSuccessDelay = 8000
+) => {
+  const jobSubscription = await subscribe(
+    {
+      token: client.client.token.token,
+      domain: client.client.uri,
+      secure: window.location.protocol === 'https:'
+    },
+    JOBS_DOCTYPE,
+    job
+  )
+
+  return new Promise(resolve => {
+    const resolveJob = () => resolve(job)
+    setTimeout(resolveJob, expectedSuccessDelay)
+    jobSubscription.onUpdate(resolveJob)
+  })
+}
+
+/**
+ * Return triggers mutations
+ * @param  {Object} client CozyClient
+ * @return {Object}        Object containing mutations
+ */
+export const triggersMutations = client => {
+  return {
+    createTrigger: createTrigger.bind(null, client),
+    launchTrigger: launchTrigger.bind(null, client),
+    waitForLoginSuccess: waitForLoginSuccess.bind(null, client)
+  }
+}
+
+export default triggersMutations
---
+++ packages/cozy-harvest-lib/test/connections/triggers.spec.js
@@ -0,0 +1,118 @@
+/* eslint-env jest */
+import { triggersMutations } from 'connections/triggers'
+import client from 'cozy-client'
+import realtime from 'cozy-realtime'
+
+jest.mock('cozy-client', () => ({
+  collection: jest.fn().mockReturnValue({
+    create: jest.fn(),
+    launch: jest.fn()
+  }),
+  create: jest.fn(),
+  client: {
+    token: {
+      token: '1234abcd'
+    },
+    uri: 'cozy.tools:8080'
+  }
+}))
+
+jest.mock('cozy-realtime', () => ({
+  subscribe: jest.fn()
+}))
+
+const { createTrigger, launchTrigger, waitForLoginSuccess } = triggersMutations(
+  client
+)
+
+const fixtures = {
+  trigger: {
+    type: '@cron',
+    worker: 'konnector',
+    message: {
+      account: '8f75c33780e2487fa474e0965521dc32',
+      konnector: 'konnectest'
+    }
+  },
+  createdTrigger: {
+    _id: '42817ec169d047e68b912c6f7d7564a2',
+    _type: 'io.cozy.triggers',
+    type: '@cron',
+    worker: 'konnector',
+    message: {
+      account: '8f75c33780e2487fa474e0965521dc32',
+      konnector: 'konnectest'
+    }
+  },
+  launchedJob: {
+    _type: 'io.cozy.jobs',
+    _id: '2794bb7c3cd64712a427ca4454aef238',
+    state: 'queued'
+  }
+}
+
+describe('Trigger mutations', () => {
+  beforeAll(() => {
+    client
+      .collection()
+      .create.mockResolvedValue({ data: fixtures.createdTrigger })
+    client.collection().launch.mockResolvedValue({ data: fixtures.launchedJob })
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  afterAll(() => {
+    jest.restoreAllMocks()
+  })
+
+  describe('createTrigger', () => {
+    it('calls Cozy Client and returns trigger', async () => {
+      const result = await createTrigger(fixtures.trigger)
+      expect(client.collection().create).toHaveBeenCalledWith(fixtures.trigger)
+      expect(result).toEqual(fixtures.createdTrigger)
+    })
+  })
+
+  describe('launchTrigger', () => {
+    it('calls expected endpoint', async () => {
+      const result = await launchTrigger(fixtures.trigger)
+      expect(client.collection().launch).toHaveBeenCalledWith(fixtures.trigger)
+      expect(result).toEqual(fixtures.launchedJob)
+    })
+  })
+
+  describe('waitForLoginSuccess', () => {
+    const shortLoginResponseTime = 50
+    const longLoginResponseTime = 150
+    const jobSuccessResponseTime = 100
+
+    beforeAll(() => {
+      // Mock realtime to respond at 100ms
+      realtime.subscribe.mockImplementation(() => ({
+        onUpdate: fn => setTimeout(() => fn(), jobSuccessResponseTime)
+      }))
+    })
+
+    afterAll(() => {
+      realtime.subscribe.resetMock()
+    })
+
+    it('waits for the given delay', async () => {
+      const startTime = new Date().getTime()
+      await waitForLoginSuccess({}, shortLoginResponseTime)
+      const endTime = new Date().getTime()
+      expect(endTime - startTime).toBeGreaterThanOrEqual(shortLoginResponseTime)
+      expect(endTime - startTime).toBeLessThan(jobSuccessResponseTime)
+    })
+
+    it('handle job end before login delay', async () => {
+      const startTime = new Date().getTime()
+      await waitForLoginSuccess({}, longLoginResponseTime)
+      const endTime = new Date().getTime()
+      expect(endTime - startTime).toBeGreaterThanOrEqual(jobSuccessResponseTime)
+      expect(endTime - startTime).toBeLessThan(longLoginResponseTime)
+    })
+  })
+})
