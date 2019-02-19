79c9278c1af12c0cba0bdfd6fd1cd3a1480ea8bf
Grégory Le Garec <gregory.legarec@gmail.com>
2019-02-19 23:37:26 +0100

    feat(harvest): Enable form while editing with empty encrypted value 📝
    When form has initial values (editing), encrypted ones may be empty and
    the submit button still be enabled.

--- packages/cozy-harvest-lib/src/components/AccountForm.jsx
+++ packages/cozy-harvest-lib/src/components/AccountForm.jsx
@@ -165,10 +165,22 @@ export class AccountForm extends PureComponent {
     }
   }
 
-  validate = fields => vals => {
+  submitHandler(values, form) {
+    const { onSubmit } = this.props
+    // Reset form with new values to set back dirty to false
+    form.reset(values)
+    onSubmit(values)
+  }
+
+  validate = (fields, initialValues) => vals => {
     let errors = {}
     for (let name in fields)
-      if (fields[name].required && !vals[name])
+      if (
+        fields[name].required &&
+        !vals[name] &&
+        // Don't require value for empty encrypted fields with initial value
+        !initialValues[getEncryptedFieldName(name)]
+      )
         errors[name] = VALIDATION_ERROR_REQUIRED_FIELD
     return errors
   }
@@ -189,8 +201,8 @@ export class AccountForm extends PureComponent {
       <Form
         initialValues={initialAndDefaultValues}
         onSubmit={onSubmit}
-        validate={this.validate(sanitizedFields)}
-        render={({ values, valid }) => (
+        validate={this.validate(sanitizedFields, initialAndDefaultValues)}
+        render={({ dirty, form, values, valid }) => (
           <div
             ref={element => {
               container = element
@@ -206,10 +218,10 @@ export class AccountForm extends PureComponent {
             <Button
               busy={submitting}
               className="u-mt-2 u-mb-1-half"
-              disabled={submitting || !valid}
+              disabled={submitting || !valid || (initialValues && !dirty)}
               extension="full"
               label={t('accountForm.submit.label')}
-              onClick={() => onSubmit(values)}
+              onClick={() => this.submitHandler(values, form)}
             />
           </div>
         )}
