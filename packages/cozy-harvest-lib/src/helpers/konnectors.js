b71ac13cd20ea28c7abcf65ca37f0187b924a63b
Grégory Le Garec <gregory.legarec@gmail.com>
2019-03-04 22:04:19 +0100

    feat(harvest): Add KonnectorJobError ✨
    KonnectorJobError is a custom Error class aimed to handle
    Error returned by konnector jobs.

--- packages/cozy-harvest-lib/src/helpers/konnectors.js
+++ packages/cozy-harvest-lib/src/helpers/konnectors.js
@@ -1,5 +1,65 @@
 import has from 'lodash/has'
 
+// Type of errors returned by konnector
+const CHALLENGE_ASKED = 'CHALLENGE_ASKED'
+const DISK_QUOTA_EXCEEDED = 'DISK_QUOTA_EXCEEDED'
+const LOGIN_FAILED = 'LOGIN_FAILED'
+const MAINTENANCE = 'MAINTENANCE'
+const NOT_EXISTING_DIRECTORY = 'NOT_EXISTING_DIRECTORY'
+const TERMS_VERSION_MISMATCH = 'TERMS_VERSION_MISMATCH'
+const UNKNOWN_ERROR = 'UNKNOWN_ERROR'
+const USER_ACTION_NEEDED = 'USER_ACTION_NEEDED'
+const VENDOR_DOWN = 'VENDOR_DOWN'
+
+const KNOWN_ERRORS = [
+  CHALLENGE_ASKED,
+  DISK_QUOTA_EXCEEDED,
+  LOGIN_FAILED,
+  MAINTENANCE,
+  NOT_EXISTING_DIRECTORY,
+  TERMS_VERSION_MISMATCH,
+  USER_ACTION_NEEDED,
+  VENDOR_DOWN
+]
+
+/**
+ * Custom error to handle errors returnes by konnector.
+ * Konnectors are returning error codes in error messages.
+ * Those codes are known and we need to associate logic to them.
+ * For now, this logic only concerns login error.
+ * @extends Error
+ */
+export class KonnectorJobError extends Error {
+  constructor(...args) {
+    super(...args)
+    Error.captureStackTrace(this, KonnectorJobError)
+
+    /**
+     * Konnector job are throwing error with a message containing the error
+     * code.
+     * Example: LOGIN_FAILED, USER_ACTION_NEEDED,
+     *          USER_ACTION_NEEDED.PERMISSIONS_CHANGED
+     */
+    this.code = this.message
+
+    /**
+     * Some error codes are composed with several segments, and can be parsed.
+     * Example : USER_ACTION_NEEDED.PERMISSIONS_CHANGED
+     * The error type correspond to the first segment of the error.
+     */
+    const type = this.code.split('.')[0]
+    this.type = KNOWN_ERRORS.includes(type) ? type : UNKNOWN_ERROR
+  }
+
+  /**
+   * Test if the konnector error is a login error
+   * @return {Boolean}
+   */
+  isLoginError() {
+    return this.type === LOGIN_FAILED
+  }
+}
+
 /**
  * Indicates if the given konnector requires a folder to work properly.
  * This directly relies on the `fields.advancedFields.folderPath` from manifest.
@@ -28,6 +88,7 @@ export const buildFolderPermission = folder => {
 }
 
 export default {
+  KonnectorJobError,
   needsFolder,
   buildFolderPermission
 }
--- packages/cozy-harvest-lib/test/helpers/konnectors.spec.js
+++ packages/cozy-harvest-lib/test/helpers/konnectors.spec.js
@@ -1,4 +1,8 @@
-import { buildFolderPermission, needsFolder } from 'helpers/konnectors'
+import {
+  buildFolderPermission,
+  KonnectorJobError,
+  needsFolder
+} from 'helpers/konnectors'
 
 const fixtures = {
   folder: {
@@ -37,6 +41,54 @@ describe('Konnectors Helpers', () => {
           verbs: ['GET', 'PATCH', 'POST']
         }
       })
+    })
+  })
+
+  describe('KonnectorJobError', () => {
+    it('should parse error code and type', () => {
+      const error = new KonnectorJobError(
+        'USER_ACTION_NEEDED.PERMISSIONS_CHANGED'
+      )
+      expect(error.code).toBe('USER_ACTION_NEEDED.PERMISSIONS_CHANGED')
+      expect(error.type).toBe('USER_ACTION_NEEDED')
+    })
+
+    for (var message of [
+      'DISK_QUOTA_EXCEEDED',
+      'CHALLENGE_ASKED',
+      'LOGIN_FAILED',
+      'LOGIN_FAILED.NEEDS_SECRET',
+      'LOGIN_FAILED.TOO_MANY_ATTEMPTS',
+      'MAINTENANCE',
+      'NOT_EXISTING_DIRECTORY',
+      'USER_ACTION_NEEDED',
+      'USER_ACTION_NEEDED.OAUTH_OUTDATED',
+      'USER_ACTION_NEEDED.ACCOUNT_REMOVED',
+      'USER_ACTION_NEEDED.CHANGE_PASSWORD',
+      'USER_ACTION_NEEDED.PERMISSIONS_CHANGED',
+      'VENDOR_DOWN',
+      'VENDOR_DOWN.BANK_DOWN',
+      'VENDOR_DOWN.LINXO_DOWN'
+    ]) {
+      it(`should know ${message} error`, () => {
+        const error = new KonnectorJobError(message)
+        expect(error.type).not.toBe('UNKNOWN_ERROR')
+        expect(error.isLoginError()).toBe(
+          [
+            'LOGIN_FAILED',
+            'LOGIN_FAILED.NEEDS_SECRET',
+            'LOGIN_FAILED.TOO_MANY_ATTEMPTS'
+          ].includes(message)
+        )
+      })
+    }
+
+    it('should handle unknown konnector error', () => {
+      const error = new KonnectorJobError(
+        'NOT_YET_IMPLEMENTED.AT_LEAST_IN_HARVEST'
+      )
+      expect(error.code).toBe('NOT_YET_IMPLEMENTED.AT_LEAST_IN_HARVEST')
+      expect(error.type).toBe('UNKNOWN_ERROR')
     })
   })
 })
