dbd7e4975bd2431c5aeeab1e9a317603e094a6a7
Patrick Browne <pt.browne@gmail.com>
2019-03-09 20:02:35 +0100

    feat: Add presetEnv option
    This can be useful to configure presetEnv especially for libs that do
    not want to transpile modules (keep import/export for tree shaking).

--- packages/babel-preset-cozy-app/index.js
+++ packages/babel-preset-cozy-app/index.js
@@ -30,6 +30,10 @@ const optionConfigs = {
   transformRegenerator: {
     default: true,
     validator: isOfType('boolean')
+  },
+  presetEnv: {
+    default: {},
+    validator: isOfType('object')
   }
 }
 
@@ -49,13 +53,14 @@ module.exports = declare((api, options) => {
     throw e
   }
 
-  const { node, react, transformRegenerator } = presetOptions
+  const { node, react, transformRegenerator, presetEnv } = presetOptions
 
   const config = {}
 
   // Latest ECMAScript features on previous browsers versions
   const presetEnvOptions = {
-    ...(node ? presetEnvNodeOptions : presetEnvBrowserOptions)
+    ...(node ? presetEnvNodeOptions : presetEnvBrowserOptions),
+    ...presetEnv
   }
 
   config.presets = [
