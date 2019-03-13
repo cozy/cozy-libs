810e9ced5fa8552a2ffbe30d18289ebd678143f7
Patrick Browne <pt.browne@gmail.com>
2019-03-13 10:28:56 +0100

    feat(publish): Let npm warn when using old engine

--- packages/cozy-app-publish/cozy-app-publish.js
+++ packages/cozy-app-publish/cozy-app-publish.js
@@ -15,18 +15,6 @@ const MODES = {
   MANUAL: 'manual'
 }
 
-var currentNodeVersion = process.versions.node
-var semver = currentNodeVersion.split('.')
-var major = semver[0]
-
-if (major < 6) {
-  console.error(
-    colorize.red(`You are running Node v${currentNodeVersion}.
-    cozy-app-publish requires Node v6 minimum, please update you version of Node.`)
-  )
-  process.exit(1)
-}
-
 const program = new commander.Command(pkg.name)
   .version(pkg.version)
   .usage(`[options]`)
