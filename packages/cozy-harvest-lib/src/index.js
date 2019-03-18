c9c48bd340cbd8d8807d08f8bce7f83a79edde84
Grégory Le Garec <gregory.legarec@gmail.com>
2019-03-18 23:28:50 +0100

    feat(harvest): Export KonnectorJobError from module ✨

--- packages/cozy-harvest-lib/src/index.js
+++ packages/cozy-harvest-lib/src/index.js
@@ -3,6 +3,8 @@ import I18n from 'cozy-ui/react/I18n'
 import TranslatedAccountForm from './components/AccountForm'
 import TranslatedTriggerManager from './components/TriggerManager'
 
+export { KonnectorJobError } from './helpers/konnectors'
+
 const dictRequire = lang => require(`./locales/${lang}.json`)
 
 export const AccountForm = (props, context) => (
