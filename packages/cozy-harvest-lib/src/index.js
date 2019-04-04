a32df858739eb4cbdb7ad19557f187917ca9e85e
Grégory Le Garec <gregory.legarec@gmail.com>
2019-04-04 11:57:08 +0200

    feat(harvest): Stop exposing AccountForm, not used anymore 🔥

--- packages/cozy-harvest-lib/src/index.js
+++ packages/cozy-harvest-lib/src/index.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import I18n from 'cozy-ui/react/I18n'
-import TranslatedAccountForm from './components/AccountForm'
 import TranslatedTriggerManager from './components/TriggerManager'
 
 export { KonnectorJobError } from './helpers/konnectors'
 
 const dictRequire = lang => require(`./locales/${lang}.json`)
-
-export const AccountForm = (props, context) => (
-  <I18n dictRequire={dictRequire} lang={context.lang}>
-    <TranslatedAccountForm {...props} />
-  </I18n>
-)
 
 export const TriggerManager = (props, context) => (
   <I18n dictRequire={dictRequire} lang={context.lang}>
@@ -25,6 +18,5 @@ TriggerManager.contextTypes = {
 }
 
 export default {
-  AccountForm,
   TriggerManager
 }
