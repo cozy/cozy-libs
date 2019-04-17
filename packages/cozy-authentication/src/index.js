2fc8fb775f0e7eab77a74d033d6699022b0a7886
Patrick Browne <pt.browne@gmail.com>
2019-04-17 15:07:39 +0200

    feat: Export MobileRouter

--- packages/cozy-authentication/src/index.js
+++ packages/cozy-authentication/src/index.js
@@ -1,4 +1,5 @@
 export { Authentication, Revoked } from './Standalone'
+export { default as MobileRouter } from './MobileRouter'
 
 // stylesheet.css is created when transpiling
 import stylesheet from './stylesheet.css' // eslint-disable-line no-unused-vars
