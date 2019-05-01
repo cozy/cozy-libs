3fb99f8168beff7684fdb7902b7bc78954294285
Grégory Le Garec <gregory.legarec@gmail.com>
2019-05-01 12:04:34 +0200

    refactor(harvest): Create HOC to export components whit locales 🛠

--- packages/cozy-harvest-lib/src/index.js
+++ packages/cozy-harvest-lib/src/index.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import I18n from 'cozy-ui/react/I18n'
+
 import TranslatedTriggerManager from './components/TriggerManager'
 
 export { KonnectorJobError } from './helpers/konnectors'
 
 const dictRequire = lang => require(`./locales/${lang}.json`)
 
-export const TriggerManager = (props, context) => (
-  <I18n dictRequire={dictRequire} lang={context.lang}>
-    <TranslatedTriggerManager {...props} />
-  </I18n>
-)
+const withLocales = WrappedComponent =>
+  class ComponentWithLocales extends Component {
+    contextTypes = {
+      lang: PropTypes.string.isRequired
+    }
 
-TriggerManager.contextTypes = {
-  lang: PropTypes.string.isRequired
-}
+    render() {
+      const { lang } = this.props
+      return (
+        <I18n dictRequire={dictRequire} lang={lang}>
+          <WrappedComponent {...this.props} />
+        </I18n>
+      )
+    }
+  }
+
+export const TriggerManager = withLocales(TranslatedTriggerManager)
 
 export default {
   TriggerManager
