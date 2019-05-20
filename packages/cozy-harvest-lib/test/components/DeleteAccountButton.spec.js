ec1d050231979843955e79fd21a24fc29ae73ae5
CPatchane <code@patchane.com>
2019-05-20 14:12:15 +0200

    feat(harvest): :sparkles: Add AccountLogoutButton component

---
+++ packages/cozy-harvest-lib/test/components/DeleteAccountButton.spec.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { DeleteAccountButton } from 'components/DeleteAccountButton'
+
+const fixtures = {
+  account: {
+    _id: '61c683295560485db0f34b859197c581',
+    account_type: 'konnectest',
+    auth: {
+      username: 'foo',
+      passphrase: 'bar'
+    },
+    identifier: 'username'
+  }
+}
+
+const tMock = () => 'Mock button label'
+
+const props = {
+  t: tMock,
+  deleteAccount: jest.fn()
+}
+
+describe('DeleteAccountButton', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should render without listeners', () => {
+    const component = shallow(
+      <DeleteAccountButton account={fixtures.account} {...props} />
+    ).getElement()
+    expect(component).toMatchSnapshot()
+  })
+
+  it('should run deleteAccount on click', () => {
+    const component = shallow(
+      <DeleteAccountButton account={fixtures.account} {...props} />
+    )
+    component.simulate('click')
+    expect(props.deleteAccount).toHaveBeenCalledWith(fixtures.account)
+  })
+
+  it('should change state on click and handle onSuccess', done => {
+    const component = shallow(
+      <DeleteAccountButton account={fixtures.account} {...props} />
+    )
+    expect(component.state().deleting).toBe(false)
+
+    // async assertions
+    const onSuccessAssert = () => {
+      expect(component.state().deleting).toBe(true)
+      done()
+    }
+    component.setProps({ onSuccess: onSuccessAssert })
+
+    component.simulate('click')
+  })
+
+  it('should handle onError if provided', done => {
+    const mockError = new Error('Expected logout error test')
+    const deleteAccount = () => {
+      throw mockError
+    }
+    const onError = e => {
+      expect(e).toBe(mockError)
+      done()
+    }
+    const component = shallow(
+      <DeleteAccountButton
+        account={fixtures.account}
+        {...props}
+        deleteAccount={deleteAccount}
+        onError={onError}
+      />
+    )
+    component.simulate('click')
+    expect(component.state().deleting).toBe(false)
+  })
+})
