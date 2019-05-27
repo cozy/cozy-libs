36c7399380be3b1eb3668ed4f7c3d565efa1930f
Cédric Messiant <cedric.messiant@gmail.com>
2019-05-27 16:59:26 +0200

    feat(cozy-doctypes): Add move method to File model

--- packages/cozy-doctypes/src/File.js
+++ packages/cozy-doctypes/src/File.js
@@ -23,6 +23,55 @@ class CozyFile extends Document {
     const parentDirectoryPath = trimEnd(parentDir.path, '/')
     return `${parentDirectoryPath}/${name}`
   }
+
+  /**
+   * Move file to destination.
+   *
+   * @param   {string} fileId               - The file's id (required)
+   * @param   {object} destination
+   * @param   {string} destination.folderId - The destination folder's id (required)
+   * @param   {string} destination.path     - The file's path after the move (optional, used to optimize performance in case of conflict)
+   * @param   {string} force                - Whether we should overwrite the destination in case of conflict (defaults to false)
+   * @returns {Promise}                     - A promise that returns the move action response and the deleted file id (if any) if resolved or an Error if rejected
+   *
+   */
+  static async move(fileId, destination, force = false) {
+    const { folderId, path } = destination
+    const filesCollection = this.cozyClient.collection('io.cozy.files')
+    try {
+      const resp = await filesCollection.updateFileMetadata(fileId, {
+        dir_id: folderId
+      })
+
+      return {
+        moved: resp.data,
+        deleted: null
+      }
+    } catch (e) {
+      if (e.status === 409 && force) {
+        let destinationPath
+        if (path) {
+          destinationPath = path
+        } else {
+          const movedFile = await this.get(fileId)
+          const filename = movedFile.name
+          destinationPath = await this.getFullpath(folderId, filename)
+        }
+        const conflictResp = await filesCollection.statByPath(destinationPath)
+        await filesCollection.destroy(conflictResp.data)
+        const resp = await filesCollection.updateFileMetadata(fileId, {
+          dir_id: folderId
+        })
+
+        return {
+          moved: resp.data,
+          deleted: conflictResp.data.id
+        }
+      } else {
+        throw e
+      }
+    }
+  }
 }
 
 CozyFile.doctype = 'io.cozy.files'
