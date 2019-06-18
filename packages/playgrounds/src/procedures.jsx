2366a721370256fa72a50bf0e9f2274c41d0a117
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-18 10:28:15 +0200

    feat: Add cozy-ui css in playground

--- packages/playgrounds/src/procedures.jsx
+++ packages/playgrounds/src/procedures.jsx
@@ -5,6 +5,8 @@ import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './common/App'
 import client from './common/client'
+import 'cozy-ui/transpiled/react/stylesheet.css'
+import 'cozy-ui/dist/cozy-ui.min.css'
 // import Comp from 'cozy-procedures'
 import injectProcedureRoutes from '../../cozy-procedures/dist'
 
