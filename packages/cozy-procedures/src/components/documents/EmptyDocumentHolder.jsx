e671a27798fddfdb7329db6e121251cb98a65acc
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-19 14:42:47 +0200

    fix: Ghost style for document placeholder

--- packages/cozy-procedures/src/components/documents/EmptyDocumentHolder.jsx
+++ packages/cozy-procedures/src/components/documents/EmptyDocumentHolder.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { translate, Chip, Icon } from 'cozy-ui/transpiled/react/'
+import { translate, Button } from 'cozy-ui/transpiled/react/'
 
 const EmptyDocumentHolder = ({ t }) => (
-  <Chip variant="dashed" className="u-w-100">
-    <Icon icon="plus" size={16} className="u-mr-half u-p-half" />
-    <span className="u-flex-grow-1">{t('documents.import')}</span>
-  </Chip>
+  <Button
+    label={t('documents.import')}
+    icon="plus"
+    theme="ghost"
+    extension="full"
+    align="left"
+  />
 )
 
 EmptyDocumentHolder.propTypes = {
