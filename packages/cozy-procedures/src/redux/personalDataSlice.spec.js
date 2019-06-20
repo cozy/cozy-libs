76dfde79a2525454d171316c5c330d7dde8001ca
Cédric Messiant <cedric.messiant@gmail.com>
2019-06-20 15:09:59 +0200

    test(procedures): Add test for fetchMyself

--- packages/cozy-procedures/src/redux/personalDataSlice.spec.js
+++ packages/cozy-procedures/src/redux/personalDataSlice.spec.js
@@ -1,4 +1,4 @@
-import reducer, { init, update } from './personalDataSlice'
+import reducer, { init, update, fetchMyself } from './personalDataSlice'
 
 describe('Personal data', () => {
   it('should init fields with undefined values', () => {
@@ -38,5 +38,61 @@ describe('Personal data', () => {
       loading: false
     }
     expect(reducer(stateBefore, action)).toEqual(expectedState)
+  })
+})
+
+describe('fetchMyself action', () => {
+  const findSpy = jest.fn()
+  const fakeClient = {
+    collection: () => ({
+      find: findSpy
+    })
+  }
+  const dispatchSpy = jest.fn()
+  const contact = {
+    id: '123',
+    name: { givenName: 'Arya', familyName: 'Stark ' }
+  }
+
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should dispatch fetchMyselfSuccess with "myself" contact', async () => {
+    findSpy.mockResolvedValueOnce({ data: [contact] })
+    await fetchMyself(fakeClient)(dispatchSpy)
+    expect(dispatchSpy).toHaveBeenCalled()
+    expect(dispatchSpy).toHaveBeenNthCalledWith(1, {
+      type: 'personalData/fetchMyselfLoading',
+      payload: { loading: true }
+    })
+    expect(dispatchSpy).toHaveBeenNthCalledWith(2, {
+      type: 'personalData/fetchMyselfSuccess',
+      payload: { id: '123', name: { givenName: 'Arya', familyName: 'Stark ' } }
+    })
+    expect(dispatchSpy).toHaveBeenNthCalledWith(3, {
+      type: 'personalData/fetchMyselfLoading',
+      payload: { loading: false }
+    })
+  })
+
+  it('should dispatch fetchMyselfError if there is an error', async () => {
+    findSpy.mockRejectedValue({
+      message: 'Could not fetch myself'
+    })
+    await fetchMyself(fakeClient)(dispatchSpy)
+    expect(dispatchSpy).toHaveBeenCalled()
+    expect(dispatchSpy).toHaveBeenNthCalledWith(1, {
+      type: 'personalData/fetchMyselfLoading',
+      payload: { loading: true }
+    })
+    expect(dispatchSpy).toHaveBeenNthCalledWith(2, {
+      type: 'personalData/fetchMyselfError',
+      payload: { error: { message: 'Could not fetch myself' } }
+    })
+    expect(dispatchSpy).toHaveBeenNthCalledWith(3, {
+      type: 'personalData/fetchMyselfLoading',
+      payload: { loading: false }
+    })
   })
 })
