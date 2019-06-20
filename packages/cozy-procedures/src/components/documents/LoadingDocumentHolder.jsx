b5063378bf17e9c56905823497d91f2241839132
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-20 14:21:50 +0200

    fix: Accurate document holder colors

--- packages/cozy-procedures/src/components/documents/LoadingDocumentHolder.jsx
+++ packages/cozy-procedures/src/components/documents/LoadingDocumentHolder.jsx
@@ -5,7 +5,7 @@ import { translate, Chip, Icon, Spinner } from 'cozy-ui/transpiled/react/'
 const LoadingDocumentHolder = ({ t }) => (
   <Chip variant="dashed" className="u-w-100">
     <Spinner className="u-ml-0 u-mr-1" size="large" />
-    <span className="u-flex-grow-1">{t('documents.importing')}</span>
+    <span className="u-flex-grow-1 u-coolGrey">{t('documents.importing')}</span>
     <Icon icon="cross" size={16} className="u-pr-1" />
   </Chip>
 )
