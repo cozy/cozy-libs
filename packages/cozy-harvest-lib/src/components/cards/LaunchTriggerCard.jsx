0e634fd9e3d5476c6c42fcc64968b8305b8539ef
Grégory Le Garec <gregory.legarec@gmail.com>
2019-06-21 10:30:57 +0200

    feat(harvest): Add frequency in LaunchTriggerCard ✨

--- packages/cozy-harvest-lib/src/components/cards/LaunchTriggerCard.jsx
+++ packages/cozy-harvest-lib/src/components/cards/LaunchTriggerCard.jsx
@@ -5,8 +5,9 @@ import Button from 'cozy-ui/transpiled/react/Button'
 import Card from 'cozy-ui/transpiled/react/Card'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
-import { Text } from 'cozy-ui/transpiled/react/Text'
+import { Uppercase, Text } from 'cozy-ui/transpiled/react/Text'
 
+import * as triggers from '../../helpers/triggers'
 import TriggerLauncher from '../TriggerLauncher'
 
 export class LaunchTriggerCard extends PureComponent {
@@ -15,21 +16,44 @@ export class LaunchTriggerCard extends PureComponent {
     return (
       <Card className={className} {...rest}>
         <TriggerLauncher trigger={trigger}>
-          {({ error, launch, running }) => (
-            <div>
-              {error && (
-                <Text className="u-error">{t('card.launchTrigger.error')}</Text>
-              )}
-              <Button
-                label={t('card.launchTrigger.button.label')}
-                icon={<Icon focusable="false" icon="sync" spin={running} />}
-                className="u-mv-half u-mh-0"
-                disabled={running}
-                onClick={launch}
-                subtle
-              />
-            </div>
-          )}
+          {({ error, launch, running }) => {
+            return (
+              <div>
+                <ul className="u-nolist u-pl-0">
+                  <li>
+                    <Uppercase
+                      className="u-coolGrey u-mr-half u-fz-tiny"
+                      tag="span"
+                    >
+                      {t('card.launchTrigger.frequency.label')}
+                    </Uppercase>
+                    <Text className="u-fz-tiny" tag="span">
+                      {t(
+                        `card.launchTrigger.frequency.${triggers.getFrequency(
+                          trigger
+                        ) || 'undefined'}`
+                      )}
+                    </Text>
+                  </li>
+                </ul>
+                <div>
+                  {error && (
+                    <Text className="u-error">
+                      {t('card.launchTrigger.error')}
+                    </Text>
+                  )}
+                  <Button
+                    label={t('card.launchTrigger.button.label')}
+                    icon={<Icon focusable="false" icon="sync" spin={running} />}
+                    className="u-mv-half u-mh-0"
+                    disabled={running}
+                    onClick={launch}
+                    subtle
+                  />
+                </div>
+              </div>
+            )
+          }}
         </TriggerLauncher>
       </Card>
     )
