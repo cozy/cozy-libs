fb6d2853fc80b94256da5dffbf1feebd9cf1250a
Cédric Messiant <cedric.messiant@gmail.com>
2019-06-21 15:34:17 +0200

    refactor(procedures): Use get*Fields selectors in Overview

--- packages/cozy-procedures/src/containers/Overview.jsx
+++ packages/cozy-procedures/src/containers/Overview.jsx
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux'
 import context from '../redux/context'
 import Overview from '../components/Overview'
-import { getSlice } from '../redux/personalDataSlice'
+import { getCompletedFields, getTotalFields } from '../redux/personalDataSlice'
 
-const mapStateToProps = state => ({
-  personalData: getSlice(state) || {}
+export const mapStateToProps = state => ({
+  personalDataFieldsCompleted: getCompletedFields(state),
+  personalDataFieldsTotal: getTotalFields(state)
 })
 
 export default connect(
