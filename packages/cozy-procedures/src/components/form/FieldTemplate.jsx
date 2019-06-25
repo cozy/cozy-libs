760eccd9e2d9ac39cfaf251f5b162d4140ce2809
Cédric Messiant <cedric.messiant@gmail.com>
2019-06-25 09:26:34 +0200

    test(procedures): Add test for FieldTemplate

--- packages/cozy-procedures/src/components/form/FieldTemplate.jsx
+++ packages/cozy-procedures/src/components/form/FieldTemplate.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Label, translate } from 'cozy-ui/transpiled/react'
 
-const FieldTemplate = ({ children, label, id, t }) => (
+export const FieldTemplate = ({ children, label, id, t }) => (
   <div>
     {label && <Label htmlFor={id}>{t(label)}</Label>}
     {children}
@@ -12,7 +12,8 @@ const FieldTemplate = ({ children, label, id, t }) => (
 FieldTemplate.propTypes = {
   children: PropTypes.node,
   id: PropTypes.string.isRequired,
-  label: PropTypes.string
+  label: PropTypes.string,
+  t: PropTypes.func.isRequired
 }
 
 export default translate()(FieldTemplate)
