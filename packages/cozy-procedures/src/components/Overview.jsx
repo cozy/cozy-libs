dee8552d25f79e89159bc8c0882cb0cf988d2bac
Cédric Messiant <cedric.messiant@gmail.com>
2019-06-26 17:27:47 +0200

    feat(procedures): Submit the procedure

--- packages/cozy-procedures/src/components/Overview.jsx
+++ packages/cozy-procedures/src/components/Overview.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router'
-import Topbar from './Topbar'
+
 import { creditApplicationTemplate } from 'cozy-procedures'
+import { withClient } from 'cozy-client'
+import { AdministrativeProcedure } from 'cozy-doctypes'
+import CozyRealtime from 'cozy-realtime'
 import {
+  Alerter,
   Title,
   SubTitle,
   Caption,
@@ -12,7 +16,17 @@ import {
 } from 'cozy-ui/transpiled/react'
 import InlineCard from 'cozy-ui/transpiled/react/InlineCard'
 
+import Topbar from './Topbar'
+
 class Overview extends React.Component {
+  realtime = null
+
+  componentWillUnmount() {
+    if (this.realtime) {
+      this.realtime.unsubscribeAll()
+    }
+  }
+
   navigateTo = view => {
     const { location, router } = this.props
     const rootPath = location.pathname
@@ -20,24 +34,97 @@ class Overview extends React.Component {
     router.push(`${rootPath}${separator}${view}`)
   }
 
+  ensureRealtime = () => {
+    if (!this.realtime) {
+      this.realtime = new CozyRealtime({ client: this.props.client })
+    }
+  }
+
+  handleZipChanged = jsonFile => job => {
+    const { client, t } = this.props
+    if (job.state === 'done') {
+      Alerter.success(t('overview.zip_ready'))
+      client.collection('io.cozy.files').destroy(jsonFile)
+    }
+  }
+
+  getDestinationId = template => {
+    const { client } = this.props
+    return client
+      .collection('io.cozy.files')
+      .ensureDirectoryExists(template.pathToSave)
+  }
+
+  submitProcedure = async () => {
+    const { client } = this.props
+    const template = creditApplicationTemplate
+    try {
+      const destinationId = await this.getDestinationId(template)
+      const datetime = new Date().toISOString()
+      const baseFilename = `${template.type}-${datetime}`
+      const jsonFilename = `${baseFilename}.json`
+      const administrativeProcedure = AdministrativeProcedure.create(
+        this.props.data,
+        creditApplicationTemplate
+      )
+      const response = await client.create(
+        'io.cozy.procedures.administratives',
+        administrativeProcedure
+      )
+      const jsonData = AdministrativeProcedure.createJson(response.data)
+
+      const file = new File([jsonData], jsonFilename)
+      const resp = await client
+        .collection('io.cozy.files')
+        .createFile(file, { dirId: destinationId })
+
+      const jsonFile = resp.data
+      const params = {
+        files: {
+          [jsonFilename]: jsonFile.id
+          // TODO: use real files from redux state
+          // 'documents/some.jpg': 'fd3b80df50114b26a723700b7006deda'
+        },
+        dir_id: destinationId,
+        filename: `${baseFilename}.zip`
+      }
+      this.ensureRealtime()
+      const zipResp = await client
+        .collection('io.cozy.jobs')
+        .create('zip', params)
+
+      await this.realtime.subscribe(
+        'updated',
+        'io.cozy.jobs',
+        zipResp.data.id,
+        this.handleZipChanged(jsonFile)
+      )
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
   render() {
     const {
       personalDataFieldsCompleted,
       personalDataFieldsTotal,
-      amount,
-      duration,
+      data,
       t
     } = this.props
+    const { amount, duration } = data.procedureData
     return (
       <div>
         <Topbar title={creditApplicationTemplate.name} />
         <Title className="u-mb-2">{t('overview.subtitle')}</Title>
+        <Alerter />
         <section className="u-mb-2">
           <SubTitle className="u-mb-1">{t('overview.request')}</SubTitle>
           <div className="u-flex u-flex-items-center">
             {amount !== null ? (
               <InlineCard onClick={() => this.navigateTo('amount')}>
-                {t('overview.amountUnit', { smart_count: amount })}
+                {t('overview.amountUnit', {
+                  smart_count: amount
+                })}
               </InlineCard>
             ) : (
               <Button
@@ -49,7 +136,9 @@ class Overview extends React.Component {
             <span className="u-ph-half">{t('overview.over')}</span>
             {duration !== null ? (
               <InlineCard onClick={() => this.navigateTo('duration')}>
-                {t('overview.durationUnit', { smart_count: duration })}
+                {t('overview.durationUnit', {
+                  smart_count: duration
+                })}
               </InlineCard>
             ) : (
               <Button
@@ -83,7 +172,11 @@ class Overview extends React.Component {
           />
         </section>
         <Caption className="u-mb-1">{t('overview.notice')}</Caption>
-        <Button label={t('overview.button')} extension="full" />
+        <Button
+          label={t('overview.button')}
+          extension="full"
+          onClick={this.submitProcedure}
+        />
       </div>
     )
   }
@@ -111,4 +204,4 @@ Overview.defaultProps = {
   }
 }
 
-export default translate()(withRouter(Overview))
+export default translate()(withRouter(withClient(Overview)))
--- packages/cozy-procedures/src/containers/Overview.jsx
+++ packages/cozy-procedures/src/containers/Overview.jsx
@@ -1,15 +1,24 @@
 import { connect } from 'react-redux'
 import context from '../redux/context'
 import Overview from '../components/Overview'
-import { getCompletedFields, getTotalFields } from '../redux/personalDataSlice'
+import {
+  getCompletedFields,
+  getTotalFields,
+  getData as getPersonalData
+} from '../redux/personalDataSlice'
 import { getSlice as getAmount } from '../redux/amountSlice'
 import { getSlice as getDuration } from '../redux/durationSlice'
 
 export const mapStateToProps = state => ({
   personalDataFieldsCompleted: getCompletedFields(state),
   personalDataFieldsTotal: getTotalFields(state),
-  amount: getAmount(state),
-  duration: getDuration(state)
+  data: {
+    procedureData: {
+      amount: getAmount(state),
+      duration: getDuration(state)
+    },
+    personalData: getPersonalData(state)
+  }
 })
 
 export default connect(
