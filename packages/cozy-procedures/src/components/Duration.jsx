246a61a7ec89a62dba1687b2f85f86b611d6dc38
Yannick Lohse <contact@yannick-lohse.fr>
2019-06-27 11:25:41 +0200

    feat: Dumb duration view

--- packages/cozy-procedures/src/components/Duration.jsx
+++ packages/cozy-procedures/src/components/Duration.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Topbar from './Topbar'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { translate, Title, SubTitle, Label } from 'cozy-ui/transpiled/react'
+import Card from 'cozy-ui/transpiled/react/Card'
+import Slider from '@material-ui/lab/Slider'
 
 class Duration extends React.Component {
   render() {
@@ -9,7 +11,17 @@ class Duration extends React.Component {
     return (
       <div>
         <Topbar title={t('duration.title')} />
-        Duration
+        <Title>{t('duration.subtitle')}</Title>
+
+        <Label>{t('duration.label')}</Label>
+        <Card>
+          <SubTitle>24 mois</SubTitle>
+          <div>
+            {t('duration.reimbursement')} : -25,83 {t('duration.unit')}
+          </div>
+          <div>{t('duration.rate')} : 3,390%</div>
+        </Card>
+        <Slider min={2} max={60} value={24} aria-label={t('duration.label')} />
       </div>
     )
   }
