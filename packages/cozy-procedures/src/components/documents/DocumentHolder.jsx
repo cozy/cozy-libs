1145e6215faae73f8f1023d9ed2092fd2e1e0394
Quentin Valmori <quentin.valmori@gmail.com>
2019-07-03 19:29:35 +0200

    feat: Display a modal before detaching a doc

--- packages/cozy-procedures/src/components/documents/DocumentHolder.jsx
+++ packages/cozy-procedures/src/components/documents/DocumentHolder.jsx
@@ -1,33 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { translate, Chip, Icon } from 'cozy-ui/transpiled/react/'
+import {
+  translate,
+  Chip,
+  Icon,
+  Modal,
+  SubTitle,
+  Text
+} from 'cozy-ui/transpiled/react/'
 import { CozyFile } from 'cozy-doctypes'
 
 import DocumentsDataFormContainer from '../../containers/DocumentsDataForm'
 
-const DocumentHolder = ({ document, unlinkDocument, documentId }) => {
-  const splittedName = CozyFile.splitFilename(document)
-  return (
-    <Chip variant="dashed" className="u-w-100">
-      <Icon icon={`file-type-${document.class}`} size={24} className="u-mr-1" />
-      <span className="u-flex-grow-1 u-ellipsis">
-        {splittedName.filename}
-        <span className="u-coolGrey">{splittedName.extension}</span>
-      </span>
-      <Icon
-        icon="cross"
-        size={16}
-        className="u-pr-1 u-c-pointer"
-        onClick={() => unlinkDocument({ document, documentId })}
-      />
-    </Chip>
-  )
+class DocumentHolder extends Component {
+  state = {
+    isModalOpened: false
+  }
+
+  render() {
+    const { document, unlinkDocument, documentId, t } = this.props
+    const { isModalOpened } = this.state
+
+    const splittedName = CozyFile.splitFilename(document)
+    return (
+      <>
+        {isModalOpened && (
+          <Modal
+            primaryText={t('documents.unlink.unlink')}
+            primaryAction={() => unlinkDocument({ document, documentId })}
+            secondaryText={t('cancel')}
+            secondaryAction={() => this.setState({ isModalOpened: false })}
+            dismissAction={() => this.setState({ isModalOpened: false })}
+            overflowHidden={true}
+            title={t('documents.unlink.title')}
+            description={
+              <>
+                <SubTitle>{t('documents.unlink.subtitle')}</SubTitle>
+                <Text className="u-mt-1">{t('documents.unlink.text')}</Text>
+              </>
+            }
+          />
+        )}
+        <Chip variant="dashed" className="u-w-100">
+          <Icon
+            icon={`file-type-${document.class}`}
+            size={24}
+            className="u-mr-1"
+          />
+          <span className="u-flex-grow-1 u-ellipsis">
+            {splittedName.filename}
+            <span className="u-coolGrey">{splittedName.extension}</span>
+          </span>
+          <Icon
+            icon="cross"
+            size={16}
+            className="u-pr-1 u-c-pointer"
+            onClick={() => this.setState({ isModalOpened: true })}
+          />
+        </Chip>
+      </>
+    )
+  }
 }
 
 DocumentHolder.propTypes = {
   unlinkDocument: PropTypes.func.isRequired,
   documentId: PropTypes.string.isRequired,
   //io.cozy.files
-  document: PropTypes.object.isRequired
+  document: PropTypes.object.isRequired,
+  t: PropTypes.func.isRequired
 }
 export default translate()(DocumentsDataFormContainer(DocumentHolder))
