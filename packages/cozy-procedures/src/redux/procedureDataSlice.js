885d1700941131a91305ab8e0fcc293e863bee31
Cédric Messiant <cedric.messiant@gmail.com>
2019-07-03 15:03:44 +0200

    refactor(procedures): Move initialized state to procedure slice

--- packages/cozy-procedures/src/redux/procedureDataSlice.js
+++ packages/cozy-procedures/src/redux/procedureDataSlice.js
@@ -3,29 +3,40 @@ import { createSlice } from 'redux-starter-kit'
 
 const procedureDataSlice = createSlice({
   initialState: {
-    amount: null,
-    duration: null
+    data: {
+      amount: null,
+      duration: null
+    },
+    ui: {
+      initialized: false
+    }
   },
   slice: 'procedureData',
   reducers: {
     updateAmount: (state, action) => {
-      state.amount = action.payload
+      state.data.amount = action.payload
     },
     updateDuration: (state, action) => {
-      state.duration = action.payload
+      state.data.duration = action.payload
+    },
+    initializationSuccess: state => {
+      state.ui.initialized = true
     }
   }
 })
 
 const { actions, reducer } = procedureDataSlice
 
-export const { updateAmount, updateDuration } = actions
+export const { updateAmount, updateDuration, initializationSuccess } = actions
 
 const selectors = {
-  getAmount: state => get(state, ['procedureData', 'amount']),
-  getDuration: state => get(state, ['procedureData', 'duration'])
+  getAmount: state => get(state, [procedureDataSlice.slice, 'data', 'amount']),
+  getDuration: state =>
+    get(state, [procedureDataSlice.slice, 'data', 'duration']),
+  getInitialized: state =>
+    get(state, [procedureDataSlice.slice, 'ui', 'initialized'], false)
 }
 
-export const { getAmount, getDuration } = selectors
+export const { getAmount, getDuration, getInitialized } = selectors
 
 export default reducer
