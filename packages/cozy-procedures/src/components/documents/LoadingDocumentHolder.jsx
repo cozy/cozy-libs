50e428a51a40100161cbfcd1059d2d17faf776b0
Yannick Lohse <contact@yannick-lohse.fr>
2019-07-04 14:54:08 +0200

    fix: Use card instead of chip for documents

--- packages/cozy-procedures/src/components/documents/LoadingDocumentHolder.jsx
+++ packages/cozy-procedures/src/components/documents/LoadingDocumentHolder.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { translate, Chip, Icon, Spinner } from 'cozy-ui/transpiled/react/'
+import { translate, Card, Icon, Spinner } from 'cozy-ui/transpiled/react/'
 
 const LoadingDocumentHolder = ({ t }) => (
-  <Chip variant="dashed" className="u-w-100">
+  <Card className="u-flex u-flex-row u-flex-items-center">
     <Spinner className="u-ml-0 u-mr-1" size="large" />
     <span className="u-flex-grow-1 u-coolGrey">{t('documents.importing')}</span>
     <Icon icon="cross" size={16} className="u-pr-1" />
-  </Chip>
+  </Card>
 )
 
 LoadingDocumentHolder.propTypes = {
