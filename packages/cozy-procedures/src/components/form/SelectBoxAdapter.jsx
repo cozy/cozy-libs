5629d0911838560e6a4fc707776ae19e518be6be
Cédric Messiant <cedric.messiant@gmail.com>
2019-07-05 10:50:28 +0200

    fix(procedures): Use direct import for SelectBox

--- packages/cozy-procedures/src/components/form/SelectBoxAdapter.jsx
+++ packages/cozy-procedures/src/components/form/SelectBoxAdapter.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { SelectBox, translate } from 'cozy-ui/transpiled/react'
+import SelectBox from 'cozy-ui/transpiled/react/SelectBox'
+import { translate } from 'cozy-ui/transpiled/react'
 
 import getInputProps from './getInputProps'
 
