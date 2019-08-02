56ba59d43e5a2e82a82675e585a5503257787593
Quentin Valmori <quentin.valmori@gmail.com>
2019-08-02 15:26:58 +0200

    feat(cozy-doctypes): Add Account model

--- packages/cozy-doctypes/src/Account.js
+++ packages/cozy-doctypes/src/Account.js
@@ -2,7 +2,34 @@ const Document = require('./Document')
 
 const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
 
-class Account extends Document {}
+// Order matters
+export const probableLoginFieldNames = [
+  'login',
+  'identifier',
+  'new_identifier',
+  'email'
+]
+
+class Account extends Document {
+  static getAccountName = account => {
+    if (!account) return null
+    if (account.auth) {
+      return (
+        account.auth.accountName || this.getAccountLogin(account) || account._id
+      )
+    } else {
+      return account._id
+    }
+  }
+
+  static getAccountLogin = account => {
+    if (account && account.auth) {
+      for (const fieldName of probableLoginFieldNames) {
+        if (account.auth[fieldName]) return account.auth[fieldName]
+      }
+    }
+  }
+}
 
 Account.schema = {
   doctype: ACCOUNTS_DOCTYPE,
