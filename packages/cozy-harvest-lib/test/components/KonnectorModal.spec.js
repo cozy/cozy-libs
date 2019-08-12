be67f5347c900b69b740ebaf09b043af0d58c6e2
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-12 14:07:26 +0200

    test: Refactor using utility functions

--- packages/cozy-harvest-lib/test/components/KonnectorModal.spec.js
+++ packages/cozy-harvest-lib/test/components/KonnectorModal.spec.js
@@ -12,24 +12,42 @@ const findAccountMock = jest.fn().mockImplementation(() => ({
 }))
 
 describe('KonnectorModal', () => {
-  const mockKonnector = {
-    slug: 'mock',
-    name: 'Mock',
-    triggers: {
-      data: [{ _id: 784, doctype: 'io.cozy.triggers' }]
+  let mockKonnector, props, shallowOptions
+
+  beforeEach(() => {
+    mockKonnector = {
+      slug: 'mock',
+      name: 'Mock',
+      triggers: {
+        data: [{ _id: 784, doctype: 'io.cozy.triggers' }]
+      }
     }
-  }
-  const props = {
-    findAccount: findAccountMock,
-    dismissAction: jest.fn(),
-    onSuccess: jest.fn(),
-    konnector: mockKonnector,
-    t
-  }
-  const shallowOptions = {
-    context: {
-      client: {}
+    props = {
+      findAccount: findAccountMock,
+      dismissAction: jest.fn(),
+      onSuccess: jest.fn(),
+      konnector: mockKonnector,
+      t
     }
+    shallowOptions = {
+      context: {
+        client: {}
+      }
+    }
+  })
+
+  const getMountedComponent = async extraProps => {
+    const finalProps = {
+      ...props,
+      ...extraProps
+    }
+    const component = shallow(
+      <KonnectorModal {...finalProps} />,
+      shallowOptions
+    )
+    await component.instance().componentDidMount()
+    component.update()
+    return component
   }
 
   it('should show a spinner while loading', () => {
@@ -44,87 +62,59 @@ describe('KonnectorModal', () => {
   })
 
   it('should show an error view', async () => {
-    const failingProps = {
-      ...props,
+    const component = await getMountedComponent({
       findAccount: () => {
         throw new Error('nope')
       }
-    }
-    const component = shallow(
-      <KonnectorModal {...failingProps} />,
-      shallowOptions
-    )
-    await component.instance().componentDidMount()
-    component.update()
+    })
 
     const content = component.dive().find('ModalContent')
     expect(content.getElement()).toMatchSnapshot()
   })
 
   it('should show the configuration view of a single account', async () => {
-    const component = shallow(<KonnectorModal {...props} />, shallowOptions)
-    await component.instance().componentDidMount()
-    component.update()
+    const component = await getMountedComponent()
 
     const content = component.dive().find('ModalContent')
     expect(content.getElement()).toMatchSnapshot()
   })
 
   it('should show the list of accounts', async () => {
-    const dataBackup = mockKonnector.triggers.data
     mockKonnector.triggers.data = [
       { _id: '784', doctype: 'io.cozy.triggers' },
       { _id: '872', doctype: 'io.cozy.triggers' }
     ]
-    const component = shallow(<KonnectorModal {...props} />, shallowOptions)
-    await component.instance().componentDidMount()
-    component.update()
+    const component = await getMountedComponent()
 
     const content = component.dive().find('ModalContent')
     expect(content.getElement()).toMatchSnapshot()
-    mockKonnector.triggers.data = dataBackup
   })
 
   it('should render the selected account via a prop', async () => {
-    const dataBackup = mockKonnector.triggers.data
     mockKonnector.triggers.data = [
       { _id: '784', doctype: 'io.cozy.triggers' },
       { _id: '872', doctype: 'io.cozy.triggers' }
     ]
-    const propsWithSelectedAccount = {
-      ...props,
+    const component = await getMountedComponent({
       accountId: '123'
-    }
-    const component = shallow(
-      <KonnectorModal {...propsWithSelectedAccount} />,
-      shallowOptions
-    )
-    await component.instance().componentDidMount()
-    component.update()
+    })
 
     const content = component.dive().find('ModalContent')
     expect(content.getElement()).toMatchSnapshot()
-    mockKonnector.triggers.data = dataBackup
   })
 
   describe('adding an account', () => {
-    it('should call the parent when controlled by props', () => {
+    it('should call the parent when controlled by props', async () => {
       const createAction = jest.fn()
-      const propsWithCreation = {
-        ...props,
+      const component = await getMountedComponent({
         createAction
-      }
-      const component = shallow(
-        <KonnectorModal {...propsWithCreation} />,
-        shallowOptions
-      )
+      })
       component.instance().requestAccountCreation()
       expect(createAction).toHaveBeenCalled()
     })
 
     it('should render the form when controlled by state', async () => {
-      const component = shallow(<KonnectorModal {...props} />, shallowOptions)
-      await component.instance().componentDidMount()
+      const component = await getMountedComponent()
       component.instance().requestAccountCreation()
 
       const content = component.dive().find('ModalContent')
@@ -133,26 +123,20 @@ describe('KonnectorModal', () => {
   })
 
   describe('switching account', () => {
-    it('should call the parent when controlled by props', () => {
+    it('should call the parent when controlled by props', async () => {
       const onAccountChange = jest.fn()
-      const propsWithSwitch = {
-        ...props,
-        onAccountChange
-      }
-      const component = shallow(
-        <KonnectorModal {...propsWithSwitch} />,
-        shallowOptions
-      )
       const account = { _id: '456' }
       const trigger = { _id: 'abc' }
+      const component = await getMountedComponent({
+        onAccountChange
+      })
+
       component.instance().requestAccountChange(account, trigger)
       expect(onAccountChange).toHaveBeenCalledWith(account)
     })
 
     it('should show the add account view when controlled by state', async () => {
-      const component = shallow(<KonnectorModal {...props} />, shallowOptions)
-      await component.instance().componentDidMount()
-      component.update()
+      const component = await getMountedComponent()
 
       const account = { _id: '456' }
       const trigger = { _id: 'abc' }
