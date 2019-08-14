118aa63b7810f764ee4b51fc5c9dfcb13c034400
Quentin Valmori <quentin.valmori@gmail.com>
2019-08-14 11:15:24 +0200

    fix(cozy-harvest-lib): Fix weird behavior on mobile device

--- packages/cozy-harvest-lib/src/components/AccountSelectBox/CreateAccountButton.jsx
+++ packages/cozy-harvest-lib/src/components/AccountSelectBox/CreateAccountButton.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { translate, Button } from 'cozy-ui/transpiled/react/'
 
+/**
+ * onClick is not called when we are on mobile device.
+ * It seems that react-select captures this event before us
+ * and when we clicked on this button, the selectbox closed itself
+ *
+ * Using touchEnd, seems to fix the issue on mobile device (Android & iOS)
+ */
 const CreateAccount = translate()(({ createAction, t }) => {
   return (
     <Button
@@ -8,6 +15,7 @@ const CreateAccount = translate()(({ createAction, t }) => {
       size={'small'}
       className={'u-m-half'}
       onClick={createAction}
+      onTouchEnd={createAction}
       label={t('modal.addAccount.button')}
     />
   )
