a4a5889d231e92a31323d68e2b390f27a1dd8f6f
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-19 14:10:03 +0200

    fix: Handle long acount names

--- packages/cozy-harvest-lib/src/components/AccountSelectBox/AccountSelectControl.jsx
+++ packages/cozy-harvest-lib/src/components/AccountSelectBox/AccountSelectControl.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Text, Icon } from 'cozy-ui/transpiled/react'
+import MidEllipsis from 'cozy-ui/transpiled/react/MidEllipsis'
+import palette from 'cozy-ui/transpiled/react/palette'
 
 const AccountSelectControl = ({ name }) => {
   return (
     <Text className="u-slateGrey u-flex u-flex-items-center u-c-pointer">
-      {name} <Icon icon={'bottom-select'} size="12" className="u-ml-half" />
+      <MidEllipsis text={name} className="u-maw-4" />
+      <Icon
+        icon="bottom"
+        size="12"
+        className="u-ml-half"
+        color={palette['coolGrey']}
+      />
     </Text>
   )
 }
