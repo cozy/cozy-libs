7986708608e5254868db19d8d84d88cbd45407df
Quentin Valmori <quentin.valmori@gmail.com>
2019-08-22 14:51:19 +0200

    feat: Add account route

--- packages/cozy-harvest-lib/src/Routes.jsx
+++ packages/cozy-harvest-lib/src/Routes.jsx
@@ -1,7 +1,9 @@
+import React from 'react'
 import { Route, Redirect, withRouter } from 'react-router-dom'
 import trimEnd from 'lodash/trimEnd'
 import KonnectorAccounts from './components/KonnectorAccounts'
 import AccountsListModal from './components/AccountsListModal'
+import AccountModal from './components/AccountModal'
 import KonnectorModal from './components/KonnectorModal'
 
 const Wrapper = ({ konnector }) => {
@@ -21,29 +23,41 @@ const Wrapper = ({ konnector }) => {
   )
 }
 
-const Routes = ({ base, konnector, location, history }) => (
+const Routes = ({ konnectorRoot, konnector, location, onDismiss }) => (
   <KonnectorAccounts konnector={konnector}>
     {accounts => (
       <>
         <Route
-          path={`${base}/`}
+          path={`${konnectorRoot}/`}
           exact
           component={() => (
-            <AccountsListModal konnector={konnector} accounts={accounts} />
+            <AccountsListModal
+              konnector={konnector}
+              accounts={accounts}
+              onDismiss={onDismiss}
+            />
           )}
         />
         <Route
-          path={`${base}/accounts/:accountId`}
+          path={`${konnectorRoot}/accounts/:accountId`}
           exact
-          component={() => <Wrapper konnector={konnector} />}
+          component={({ match }) => (
+            <AccountModal
+              konnector={konnector}
+              accountId={match.params.accountId}
+              accounts={accounts}
+              onDismiss={onDismiss}
+            />
+          )}
         />
         <Route
-          path={`${base}/new`}
+          path={`${konnectorRoot}/new`}
           exact
           component={() => <Wrapper konnector={konnector} />}
         />
+        {/* TODO redirect render twice the component */}
         <Redirect
-          from={base}
+          from={konnectorRoot}
           to={trimEnd(location.pathname, '/') + '/'}
           exact
         />
