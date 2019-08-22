5e1018903a1ab1b718d3134555b6e4d0cc1e675a
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-22 19:06:18 +0200

    feat: New account modal

--- packages/cozy-harvest-lib/src/components/NewAccountModal.jsx
+++ packages/cozy-harvest-lib/src/components/NewAccountModal.jsx
@@ -1,21 +1,39 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router'
-
+import Modal, {
+  ModalContent,
+  ModalHeader
+} from 'cozy-ui/transpiled/react/Modal'
 import TriggerManager from '../components/TriggerManager'
+import KonnectorIcon from './KonnectorIcon'
 
 class NewAccountModal extends Component {
   render() {
-    const { konnector, history } = this.props
+    const { konnector, history, onDismiss } = this.props
     return (
-      <TriggerManager
-        konnector={konnector}
-        onLoginSuccess={(trigger, account) =>
-          history.push(`../accounts/${account._id}`)
-        }
-        onSuccess={(trigger, account) =>
-          history.push(`../accounts/${account._id}`)
-        }
-      />
+      <Modal dismissAction={onDismiss} mobileFullscreen size="small">
+        <ModalHeader className="u-pr-2">
+          <div className="u-flex u-flex-row u-w-100 u-flex-items-center">
+            <div className="u-w-3 u-h-3 u-mr-half">
+              <KonnectorIcon konnector={konnector} />
+            </div>
+            <div className="u-flex-grow-1 u-mr-half">
+              <h3 className="u-title-h3 u-m-0">{konnector.name}</h3>
+            </div>
+          </div>
+        </ModalHeader>
+        <ModalContent>
+          <TriggerManager
+            konnector={konnector}
+            onLoginSuccess={(trigger, account) =>
+              history.push(`../accounts/${account._id}`)
+            }
+            onSuccess={(trigger, account) =>
+              history.push(`../accounts/${account._id}`)
+            }
+          />
+        </ModalContent>
+      </Modal>
     )
   }
 }
