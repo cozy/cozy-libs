149e2b4659ace2c4c476958f941a9a3bca1d80e0
Yannick Lohse <contact@yannick-lohse.fr>
2019-08-30 14:04:13 +0200

    feat: Save credentials in vault

--- packages/cozy-harvest-lib/src/components/Routes.jsx
+++ packages/cozy-harvest-lib/src/components/Routes.jsx
@@ -8,56 +8,59 @@ import AccountsListModal from './AccountsListModal'
 import AccountModal from './AccountModal'
 import NewAccountModal from './NewAccountModal'
 import EditAccountModal from './EditAccountModal'
+import HarvestVaultProvider from './HarvestVaultProvider'
 
 const Routes = ({ konnectorRoot, konnector, location, onDismiss }) => (
   <Modal dismissAction={onDismiss} mobileFullscreen size="small">
-    <KonnectorAccounts konnector={konnector}>
-      {accounts => (
-        <>
-          <Route
-            path={`${konnectorRoot}/`}
-            exact
-            render={() => (
-              <AccountsListModal konnector={konnector} accounts={accounts} />
-            )}
-          />
-          <Route
-            path={`${konnectorRoot}/accounts/:accountId`}
-            exact
-            render={({ match }) => (
-              <AccountModal
-                konnector={konnector}
-                accountId={match.params.accountId}
-                accounts={accounts}
-                onDismiss={onDismiss}
-              />
-            )}
-          />
-          <Route
-            path={`${konnectorRoot}/accounts/:accountId/edit`}
-            exact
-            render={({ match }) => (
-              <EditAccountModal
-                konnector={konnector}
-                accountId={match.params.accountId}
-                accounts={accounts}
-              />
-            )}
-          />
-          <Route
-            path={`${konnectorRoot}/new`}
-            exact
-            render={() => <NewAccountModal konnector={konnector} />}
-          />
-          {/* TODO redirect render twice the component */}
-          <Redirect
-            from={konnectorRoot}
-            to={trimEnd(location.pathname, '/') + '/'}
-            exact
-          />
-        </>
-      )}
-    </KonnectorAccounts>
+    <HarvestVaultProvider>
+      <KonnectorAccounts konnector={konnector}>
+        {accounts => (
+          <>
+            <Route
+              path={`${konnectorRoot}/`}
+              exact
+              render={() => (
+                <AccountsListModal konnector={konnector} accounts={accounts} />
+              )}
+            />
+            <Route
+              path={`${konnectorRoot}/accounts/:accountId`}
+              exact
+              render={({ match }) => (
+                <AccountModal
+                  konnector={konnector}
+                  accountId={match.params.accountId}
+                  accounts={accounts}
+                  onDismiss={onDismiss}
+                />
+              )}
+            />
+            <Route
+              path={`${konnectorRoot}/accounts/:accountId/edit`}
+              exact
+              render={({ match }) => (
+                <EditAccountModal
+                  konnector={konnector}
+                  accountId={match.params.accountId}
+                  accounts={accounts}
+                />
+              )}
+            />
+            <Route
+              path={`${konnectorRoot}/new`}
+              exact
+              render={() => <NewAccountModal konnector={konnector} />}
+            />
+            {/* TODO redirect render twice the component */}
+            <Redirect
+              from={konnectorRoot}
+              to={trimEnd(location.pathname, '/') + '/'}
+              exact
+            />
+          </>
+        )}
+      </KonnectorAccounts>
+    </HarvestVaultProvider>
   </Modal>
 )
 
