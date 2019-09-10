f4cf684ec318fab125ee930a7baebf5ba5185774
Cyrille Perois <cperois@outlook.com>
2019-09-10 10:47:30 +0200

    refactor: Extract CiphersListItem and OtherAccountListItem

---
+++ packages/cozy-harvest-lib/src/components/VaultCiphersList/OtherAccountListItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import cx from 'classnames'
+
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+
+import withLocales from '../hoc/withLocales'
+
+const OtherAccountListItem = props => {
+  const { t, onClick, className, ...rest } = props
+  return (
+    <ListItem
+      onClick={onClick}
+      className={cx(
+        {
+          'u-c-pointer': onClick
+        },
+        className
+      )}
+      {...rest}
+    >
+      <ListItemText primaryText={t('Depuis un autre compte…')} />
+    </ListItem>
+  )
+}
+
+export default withLocales(OtherAccountListItem)
