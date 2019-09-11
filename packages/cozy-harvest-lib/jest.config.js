873904825c2ce48c5da2b0e7b331bab5e419e578
Cyrille Perois <cperois@outlook.com>
2019-09-11 11:10:18 +0200

    chore(jest): Remove styles from moduleNameMapper

--- packages/cozy-harvest-lib/jest.config.js
+++ packages/cozy-harvest-lib/jest.config.js
@@ -8,8 +8,6 @@ module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'json', 'styl'],
   moduleDirectories: ['src', 'node_modules'],
   moduleNameMapper: {
-    // identity-obj-proxy module is installed by cozy-scripts
-    styles: 'identity-obj-proxy',
     '^cozy-logger$': 'cozy-logger/dist/index.js',
     '\\.(png|gif|jpe?g|svg)$': '<rootDir>/test/__mocks__/fileMock.js',
     '^cozy-client$': 'cozy-client/dist/index.js'
