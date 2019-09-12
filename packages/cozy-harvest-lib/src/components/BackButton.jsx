d61a3f749b2946d25d02f9cc6197edac735a684a
Cyrille Perois <cperois@outlook.com>
2019-09-12 16:08:38 +0200

    feat: Add locale for back button

--- packages/cozy-harvest-lib/src/components/BackButton.jsx
+++ packages/cozy-harvest-lib/src/components/BackButton.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import cx from 'classnames'
-import withLocales from './hoc/withLocales'
 
 const BackButton = props => {
-  const { className, t, ...rest } = props
+  const { className, children, ...rest } = props
 
   return (
     <button
@@ -15,9 +14,9 @@ const BackButton = props => {
       {...rest}
     >
       <Icon icon="left" className="u-mr-half" />
-      {t('Revenir à la liste')}
+      {children}
     </button>
   )
 }
 
-export default withLocales(BackButton)
+export default BackButton
