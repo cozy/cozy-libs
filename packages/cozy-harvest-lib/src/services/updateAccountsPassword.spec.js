331bf0994799269c7dd539c2b37df22fa8c0fa85
Yannick Lohse <contact@yannick-lohse.fr>
2019-09-19 16:43:33 +0200

    feat: Update accounts script

---
+++ packages/cozy-harvest-lib/src/services/updateAccountsPassword.spec.js
@@ -0,0 +1,123 @@
+import updateAccountsPassword from './updateAccountsPassword'
+
+jest.mock('cozy-keys-lib/transpiled/SymmetricCryptoKey', () => {
+  return class {
+    constructor(key, encType) {
+      return { key, encType }
+    }
+  }
+})
+
+describe('update accounts password function', () => {
+  const mockVaultClient = {
+    Utils: {
+      fromB64ToArray: str => str
+    },
+    cryptoService: {
+      aesDecryptToUtf8: jest.fn(str => str)
+    }
+  }
+  const mockCozyClient = {
+    getStackClient: () => mockCozyClient,
+    find: () => mockCozyClient,
+    where: () => mockCozyClient,
+    indexFields: () => mockCozyClient,
+    query: jest.fn(),
+    save: jest.fn(),
+    fetchJSON: jest.fn()
+  }
+
+  it('should fail when the stack provides no org key', async () => {
+    mockCozyClient.fetchJSON.mockRejectedValue({
+      error: 'No org key'
+    })
+    expect.assertions(1)
+    try {
+      await updateAccountsPassword(mockCozyClient, mockVaultClient, {})
+    } catch (err) {
+      expect(err).toEqual({
+        error: 'No org key'
+      })
+    }
+  })
+
+  it('should decrypt the password', async () => {
+    const encType = 2
+    const iv = 'iv123'
+    const mac = 'mac456'
+    const data = 'super_secret'
+    const orgKey = '123'
+
+    const encryptedPassword = `${encType}.${iv}|${data}|${mac}`
+    mockCozyClient.fetchJSON.mockResolvedValue({
+      organizationKey: orgKey
+    })
+    mockCozyClient.query.mockResolvedValue({
+      data: []
+    })
+    await updateAccountsPassword(mockCozyClient, mockVaultClient, {
+      login: {
+        password: encryptedPassword
+      }
+    })
+    expect(mockVaultClient.cryptoService.aesDecryptToUtf8).toHaveBeenCalledWith(
+      encType,
+      data,
+      iv,
+      mac,
+      {
+        key: orgKey,
+        encType
+      }
+    )
+  })
+
+  it('should update accounts', async () => {
+    mockCozyClient.fetchJSON.mockResolvedValue({
+      organizationKey: '123'
+    })
+    mockCozyClient.save.mockResolvedValue()
+    mockVaultClient.cryptoService.aesDecryptToUtf8.mockResolvedValue(
+      'new_password'
+    )
+    mockCozyClient.query.mockResolvedValue({
+      data: [
+        {
+          auth: {
+            credentials_encrypted: 'abc123',
+            login: 'A'
+          }
+        },
+        {
+          auth: {
+            credentials_encrypted: 'abc123',
+            login: 'B',
+            extra_field: 'stays'
+          }
+        }
+      ]
+    })
+    await updateAccountsPassword(mockCozyClient, mockVaultClient, {
+      login: {
+        password: 'yolo'
+      }
+    })
+
+    expect(mockCozyClient.save).toHaveBeenCalledTimes(2)
+    expect(mockCozyClient.save).toHaveBeenCalledWith({
+      _type: 'io.cozy.accounts',
+      auth: {
+        login: 'A',
+        password: 'new_password'
+      }
+    })
+    expect(mockCozyClient.save).toHaveBeenCalledWith({
+      _type: 'io.cozy.accounts',
+      auth: {
+        login: 'B',
+        password: 'new_password',
+        extra_field: 'stays'
+      }
+    })
+  })
+})
