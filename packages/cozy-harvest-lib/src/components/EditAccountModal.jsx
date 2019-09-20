a9d04ec824c7050c520340c999a74b5b379eeb85
Cyrille Perois <cperois@outlook.com>
2019-09-20 09:46:09 +0200

    fix: Don't redirect to account on vault dismiss when editing an account
    Unlocking the vault redirected to the account modal everytime, so we
    couldn't edit the account at all

--- packages/cozy-harvest-lib/src/components/EditAccountModal.jsx
+++ packages/cozy-harvest-lib/src/components/EditAccountModal.jsx
@@ -132,7 +132,6 @@ export class EditAccountModal extends Component {
               konnector={konnector}
               initialTrigger={trigger}
               onSuccess={this.redirectToAccount}
-              onVaultDismiss={this.redirectToAccount}
               showError={true}
             />
           )}
