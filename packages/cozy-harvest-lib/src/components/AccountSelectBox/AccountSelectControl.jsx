8dae8c7fad078c090c1dfcb756aca3fe1810fb37
Yannick Lohse <contact@yannick-lohse.fr>
2019-10-07 10:09:45 +0200

    fix: Make account selection control larger

--- packages/cozy-harvest-lib/src/components/AccountSelectBox/AccountSelectControl.jsx
+++ packages/cozy-harvest-lib/src/components/AccountSelectBox/AccountSelectControl.jsx
@@ -5,7 +5,7 @@ import palette from 'cozy-ui/transpiled/react/palette'
 const AccountSelectControl = ({ name }) => {
   return (
     <Text className="u-slateGrey u-flex u-flex-items-center u-c-pointer">
-      <Text className="u-maw-4 u-ellipsis">{name}</Text>
+      <Text className="u-maw-5 u-ellipsis">{name}</Text>
       <Icon
         icon="bottom"
         size="12"
