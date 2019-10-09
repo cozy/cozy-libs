2c0919a9af5208cd69cec6da4d9404877ea4e2a8
Yannick Lohse <contact@yannick-lohse.fr>
2019-10-09 17:30:18 +0200

    fix: Avoid double-render with Redirect

--- packages/cozy-harvest-lib/src/components/Routes.jsx
+++ packages/cozy-harvest-lib/src/components/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Redirect, withRouter } from 'react-router'
+import { Route, withRouter } from 'react-router'
 import trimEnd from 'lodash/trimEnd'
 import Modal from 'cozy-ui/transpiled/react/Modal'
 
@@ -10,83 +10,85 @@ import NewAccountModal from './NewAccountModal'
 import EditAccountModal from './EditAccountModal'
 import KonnectorSuccess from './KonnectorSuccess'
 
-const Routes = ({ konnectorRoot, konnector, location, history, onDismiss }) => (
-  <Modal dismissAction={onDismiss} mobileFullscreen size="small">
-    <KonnectorAccounts konnector={konnector}>
-      {accounts => (
-        <>
-          <Route
-            path={`${konnectorRoot}/`}
-            exact
-            render={() => {
-              if (accounts.length === 0) {
-                history.push('./new')
-                return null
-              } else if (accounts.length === 1) {
-                history.push(`./accounts/${accounts[0].account._id}`)
-                return null
-              } else {
-                return (
-                  <AccountsListModal
-                    konnector={konnector}
-                    accounts={accounts}
-                  />
-                )
-              }
-            }}
-          />
-          <Route
-            path={`${konnectorRoot}/accounts/:accountId`}
-            exact
-            render={({ match }) => (
-              <AccountModal
-                konnector={konnector}
-                accountId={match.params.accountId}
-                accounts={accounts}
-                onDismiss={onDismiss}
-              />
-            )}
-          />
-          <Route
-            path={`${konnectorRoot}/accounts/:accountId/edit`}
-            exact
-            render={({ match }) => (
-              <EditAccountModal
-                konnector={konnector}
-                accountId={match.params.accountId}
-                accounts={accounts}
-              />
-            )}
-          />
-          <Route
-            path={`${konnectorRoot}/new`}
-            exact
-            render={() => <NewAccountModal konnector={konnector} />}
-          />
-          <Route
-            path={`${konnectorRoot}/accounts/:accountId/success`}
-            exact
-            render={({ match }) => {
-              return (
-                <KonnectorSuccess
+const Routes = ({ konnectorRoot, konnector, location, history, onDismiss }) => {
+  // we need to make sure the path ends with a / for relative links to work
+  if (/\/$/.test(location.pathname) === false) {
+    history.replace(trimEnd(location.pathname, '/') + '/')
+    return null
+  }
+
+  return (
+    <Modal dismissAction={onDismiss} mobileFullscreen size="small">
+      <KonnectorAccounts konnector={konnector}>
+        {accounts => (
+          <>
+            <Route
+              path={`${konnectorRoot}/`}
+              exact
+              render={() => {
+                if (accounts.length === 0) {
+                  history.push('./new')
+                  return null
+                } else if (accounts.length === 1) {
+                  history.push(`./accounts/${accounts[0].account._id}`)
+                  return null
+                } else {
+                  return (
+                    <AccountsListModal
+                      konnector={konnector}
+                      accounts={accounts}
+                    />
+                  )
+                }
+              }}
+            />
+            <Route
+              path={`${konnectorRoot}/accounts/:accountId`}
+              exact
+              render={({ match }) => (
+                <AccountModal
                   konnector={konnector}
                   accountId={match.params.accountId}
                   accounts={accounts}
                   onDismiss={onDismiss}
                 />
-              )
-            }}
-          />
-          {/* TODO redirect render twice the component */}
-          <Redirect
-            from={konnectorRoot}
-            to={trimEnd(location.pathname, '/') + '/'}
-            exact
-          />
-        </>
-      )}
-    </KonnectorAccounts>
-  </Modal>
-)
+              )}
+            />
+            <Route
+              path={`${konnectorRoot}/accounts/:accountId/edit`}
+              exact
+              render={({ match }) => (
+                <EditAccountModal
+                  konnector={konnector}
+                  accountId={match.params.accountId}
+                  accounts={accounts}
+                />
+              )}
+            />
+            <Route
+              path={`${konnectorRoot}/new`}
+              exact
+              render={() => <NewAccountModal konnector={konnector} />}
+            />
+            <Route
+              path={`${konnectorRoot}/accounts/:accountId/success`}
+              exact
+              render={({ match }) => {
+                return (
+                  <KonnectorSuccess
+                    konnector={konnector}
+                    accountId={match.params.accountId}
+                    accounts={accounts}
+                    onDismiss={onDismiss}
+                  />
+                )
+              }}
+            />
+          </>
+        )}
+      </KonnectorAccounts>
+    </Modal>
+  )
+}
 
 export default withRouter(Routes)
