d19d5bc8ddc5e14932d3dd42b582afd9a42b317d
Cyrille Perois <cperois@outlook.com>
2019-10-22 14:57:26 +0200

    feat(cozy-harvest-lib): Make TriggerManager usable standalone
    In cozy-home intents, we use the TriggerManager in a standalone manner
    (ie outside of harvest routes). So it must embed the providers (like
    HarvestVaultProvider and MuiCozyTheme) that are required for it to work.

--- packages/cozy-harvest-lib/src/components/Routes.jsx
+++ packages/cozy-harvest-lib/src/components/Routes.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router'
 import Modal from 'cozy-ui/transpiled/react/Modal'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
-import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme'
 
 import KonnectorAccounts from './KonnectorAccounts'
 import AccountModal from './AccountModal'
 import NewAccountModal from './NewAccountModal'
 import EditAccountModal from './EditAccountModal'
 import KonnectorSuccess from './KonnectorSuccess'
-import HarvestVaultProvider from './HarvestVaultProvider'
 import HarvestModalRoot from './HarvestModalRoot'
 import { MountPointProvider } from './MountPointContext'
 
@@ -17,77 +15,67 @@ const Routes = ({ konnectorRoot, konnector, onDismiss }) => {
   return (
     <MountPointProvider baseRoute={konnectorRoot}>
       <Modal dismissAction={onDismiss} mobileFullscreen size="small">
-        <MuiCozyTheme>
-          <HarvestVaultProvider>
-            <KonnectorAccounts konnector={konnector}>
-              {accounts => (
-                <Switch>
-                  <Route
-                    path={`${konnectorRoot}/`}
-                    exact
-                    render={() => (
-                      <HarvestModalRoot
-                        accounts={accounts}
-                        konnector={konnector}
-                      />
-                    )}
+        <KonnectorAccounts konnector={konnector}>
+          {accounts => (
+            <Switch>
+              <Route
+                path={`${konnectorRoot}/`}
+                exact
+                render={() => (
+                  <HarvestModalRoot accounts={accounts} konnector={konnector} />
+                )}
+              />
+              <Route
+                path={`${konnectorRoot}/accounts/:accountId`}
+                exact
+                render={({ match }) => (
+                  <AccountModal
+                    konnector={konnector}
+                    accountId={match.params.accountId}
+                    accounts={accounts}
+                    onDismiss={onDismiss}
                   />
-                  <Route
-                    path={`${konnectorRoot}/accounts/:accountId`}
-                    exact
-                    render={({ match }) => (
-                      <AccountModal
-                        konnector={konnector}
-                        accountId={match.params.accountId}
-                        accounts={accounts}
-                        onDismiss={onDismiss}
-                      />
-                    )}
+                )}
+              />
+              <Route
+                path={`${konnectorRoot}/accounts/:accountId/edit`}
+                exact
+                render={({ match }) => (
+                  <EditAccountModal
+                    konnector={konnector}
+                    accountId={match.params.accountId}
+                    accounts={accounts}
                   />
-                  <Route
-                    path={`${konnectorRoot}/accounts/:accountId/edit`}
-                    exact
-                    render={({ match }) => (
-                      <EditAccountModal
-                        konnector={konnector}
-                        accountId={match.params.accountId}
-                        accounts={accounts}
-                      />
-                    )}
+                )}
+              />
+              <Route
+                path={`${konnectorRoot}/new`}
+                exact
+                render={() => (
+                  <NewAccountModal
+                    konnector={konnector}
+                    onDismiss={onDismiss}
                   />
-                  <Route
-                    path={`${konnectorRoot}/new`}
-                    exact
-                    render={() => (
-                      <NewAccountModal
-                        konnector={konnector}
-                        onDismiss={onDismiss}
-                      />
-                    )}
-                  />
-                  <Route
-                    path={`${konnectorRoot}/accounts/:accountId/success`}
-                    exact
-                    render={({ match }) => {
-                      return (
-                        <KonnectorSuccess
-                          konnector={konnector}
-                          accountId={match.params.accountId}
-                          accounts={accounts}
-                          onDismiss={onDismiss}
-                        />
-                      )
-                    }}
-                  />
-                  <Redirect
-                    from={`${konnectorRoot}/*`}
-                    to={`${konnectorRoot}/`}
-                  />
-                </Switch>
-              )}
-            </KonnectorAccounts>
-          </HarvestVaultProvider>
-        </MuiCozyTheme>
+                )}
+              />
+              <Route
+                path={`${konnectorRoot}/accounts/:accountId/success`}
+                exact
+                render={({ match }) => {
+                  return (
+                    <KonnectorSuccess
+                      konnector={konnector}
+                      accountId={match.params.accountId}
+                      accounts={accounts}
+                      onDismiss={onDismiss}
+                    />
+                  )
+                }}
+              />
+              <Redirect from={`${konnectorRoot}/*`} to={`${konnectorRoot}/`} />
+            </Switch>
+          )}
+        </KonnectorAccounts>
       </Modal>
     </MountPointProvider>
   )
