d895d33d884f92bfbcdfb0d7caddb5c63b4e25f6
Quentin Valmori <quentin.valmori@gmail.com>
2019-10-28 11:30:12 +0100

    refactor: Since we removed stylus, no need for custom babel jest

--- packages/cozy-scanner/babel-transformer.js
+++
@@ -1,9 +0,0 @@
-/**
- * Since we are using a custom babel config
- * we need to create a Transformer to tell
- * jest to use it
- */
-
-const config = require('./babel.config')
-const { createTransformer } = require('babel-jest')
-module.exports = createTransformer(config)
