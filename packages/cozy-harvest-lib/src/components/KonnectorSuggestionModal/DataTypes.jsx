d5d742e34b9bd253d448eec8eafe8d1e42425465
Yannick Lohse <contact@yannick-lohse.fr>
2019-11-20 17:06:47 +0100

    refactor: Explicit icon loading

--- packages/cozy-harvest-lib/src/components/KonnectorSuggestionModal/DataTypes.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorSuggestionModal/DataTypes.jsx
@@ -3,7 +3,74 @@ import PropTypes from 'prop-types'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
 import Text from 'cozy-ui/transpiled/react/Text'
 import CompositeRow from 'cozy-ui/transpiled/react/CompositeRow'
-import Icon from 'cozy-ui/transpiled/react/Icon'
+
+import activityIcon from '../../assets/datatypes/icon-activity.svg'
+import appointmentIcon from '../../assets/datatypes/icon-appointment.svg'
+import bankTransactionsIcon from '../../assets/datatypes/icon-bankTransactions.svg'
+import bankAccountsIcon from '../../assets/datatypes/icon-bankAccounts.svg'
+import billIcon from '../../assets/datatypes/icon-bill.svg'
+import bloodPressureIcon from '../../assets/datatypes/icon-bloodPressure.svg'
+import calendarIcon from '../../assets/datatypes/icon-calendar.svg'
+import certificateIcon from '../../assets/datatypes/icon-certificate.svg'
+import commitIcon from '../../assets/datatypes/icon-commit.svg'
+import consumptionIcon from '../../assets/datatypes/icon-consumption.svg'
+import contactIcon from '../../assets/datatypes/icon-contact.svg'
+import contractIcon from '../../assets/datatypes/icon-contract.svg'
+import courseMaterialIcon from '../../assets/datatypes/icon-courseMaterial.svg'
+import documentIcon from '../../assets/datatypes/icon-document.svg'
+import eventIcon from '../../assets/datatypes/icon-event.svg'
+import familyIcon from '../../assets/datatypes/icon-family.svg'
+import geopointIcon from '../../assets/datatypes/icon-geopoint.svg'
+import heartbeatIcon from '../../assets/datatypes/icon-heartbeat.svg'
+import homeIcon from '../../assets/datatypes/icon-home.svg'
+import phonecommunicationlogIcon from '../../assets/datatypes/icon-phonecommunicationlog.svg'
+import pictureIcon from '../../assets/datatypes/icon-picture.svg'
+import podcastIcon from '../../assets/datatypes/icon-podcast.svg'
+import profileIcon from '../../assets/datatypes/icon-profile.svg'
+import refundIcon from '../../assets/datatypes/icon-refund.svg'
+import sinisterIcon from '../../assets/datatypes/icon-sinister.svg'
+import sleepTimeIcon from '../../assets/datatypes/icon-sleepTime.svg'
+import stepsNumberIcon from '../../assets/datatypes/icon-stepsNumber.svg'
+import temperatureIcon from '../../assets/datatypes/icon-temperature.svg'
+import travelDateIcon from '../../assets/datatypes/icon-travelDate.svg'
+import tweetIcon from '../../assets/datatypes/icon-tweet.svg'
+import videostreamIcon from '../../assets/datatypes/icon-videostream.svg'
+import weightIcon from '../../assets/datatypes/icon-weight.svg'
+
+const icons = {
+  activity: activityIcon,
+  appointment: appointmentIcon,
+  bankTransactions: bankTransactionsIcon,
+  bankAccounts: bankAccountsIcon,
+  bill: billIcon,
+  bloodPressure: bloodPressureIcon,
+  calendar: calendarIcon,
+  certificate: certificateIcon,
+  commit: commitIcon,
+  consumption: consumptionIcon,
+  contact: contactIcon,
+  contract: contractIcon,
+  courseMaterial: courseMaterialIcon,
+  document: documentIcon,
+  event: eventIcon,
+  family: familyIcon,
+  geopoint: geopointIcon,
+  heartbeat: heartbeatIcon,
+  home: homeIcon,
+  phonecommunicationlog: phonecommunicationlogIcon,
+  picture: pictureIcon,
+  podcast: podcastIcon,
+  profile: profileIcon,
+  refund: refundIcon,
+  sinister: sinisterIcon,
+  sleepTime: sleepTimeIcon,
+  stepsNumber: stepsNumberIcon,
+  temperature: temperatureIcon,
+  travelDate: travelDateIcon,
+  tweet: tweetIcon,
+  videostream: videostreamIcon,
+  weight: weightIcon
+}
 
 const DataTypes = ({ t, dataTypes, konnectorName }) => {
   return dataTypes.length > 0 ? (
@@ -12,23 +79,17 @@ const DataTypes = ({ t, dataTypes, konnectorName }) => {
         {t('suggestions.data', { name: konnectorName })}
       </Text>
       <ul className="u-w-100 u-nolist u-mt-0 u-ph-0 u-mb-half">
-        {dataTypes.map(dataType => (
-          <li key={dataType}>
-            <CompositeRow
-              primaryText={t(`dataType.${dataType}`)}
-              image={
-                <Icon
-                  size={32}
-                  icon={
-                    require('!svg-sprite-loader!../../assets/datatypes/icon-' +
-                      dataType.toLowerCase() +
-                      '.svg').default
-                  }
-                />
-              }
-            />
-          </li>
-        ))}
+        {dataTypes.map(dataType => {
+          const TypeIcon = icons[dataType]
+          return (
+            <li key={dataType}>
+              <CompositeRow
+                primaryText={t(`dataType.${dataType}`)}
+                image={TypeIcon ? <TypeIcon className="u-w-2 u-h-2" /> : null}
+              />
+            </li>
+          )
+        })}
       </ul>
     </>
   ) : null
