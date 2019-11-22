a823922f1b736ff6d0fb194f939ecb0f09ebfec1
Yannick Lohse <contact@yannick-lohse.fr>
2019-11-22 11:17:05 +0100

    refactor: Use MUI Dialog

--- packages/cozy-harvest-lib/src/components/KonnectorSuggestionModal/index.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorSuggestionModal/index.jsx
@@ -1,25 +1,33 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { withClient } from 'cozy-client'
-import get from 'lodash/get'
 
 import { translate } from 'cozy-ui/transpiled/react/I18n'
-import ExperimentalModal from 'cozy-ui/transpiled/react/Labs/ExperimentalModal'
 import AppLinker, { generateWebLink } from 'cozy-ui/transpiled/react/AppLinker'
 import { Button, ButtonLink } from 'cozy-ui/transpiled/react/Button'
 import { Title, Caption } from 'cozy-ui/transpiled/react/Text'
+
+import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme'
+import DialogCloseButton from 'cozy-ui/transpiled/react/MuiCozyTheme/Dialog/DialogCloseButton'
 
 import Illustration from './Illustration'
 import DataTypes from './DataTypes'
 import { getDataTypes, getKonnectorName } from '../../helpers/manifest'
 import { getSuggestionReason } from '../../helpers/appSuggestions'
 
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import withMobileDialog from '@material-ui/core/withMobileDialog'
+
 const KonnectorSuggestionModal = ({
   t,
   client,
   konnectorAppSuggestion,
   konnectorManifest,
-  closeModal
+  closeModal,
+  fullScreen
 }) => {
   const { slug } = konnectorAppSuggestion
   const cozyURL = new URL(client.getStackClient().uri)
@@ -43,59 +51,71 @@ const KonnectorSuggestionModal = ({
   }
 
   return (
-    <ExperimentalModal
-      description={
-        <div className="u-flex u-flex-column u-flex-items-center">
-          <Illustration
-            alt={t('suggestions.illustration')}
-            iconAlt={t('app.logo.alt', { name })}
-            app={slug}
-          />
-          <Title className="u-mb-half">
-            {t('suggestions.title', { name })}
-          </Title>
-          <DataTypes dataTypes={dataTypes} konnectorName={name} />
-        </div>
-      }
-      descriptionFooter={
-        <>
-          {reason === 'FOUND_TRANSACTION' && (
-            <Caption className="u-mb-1">
-              {t('suggestions.why', { name })}
-              <br />
-              {t('suggestions.reason_bank', { name })}
-            </Caption>
-          )}
-          <AppLinker
-            slug={storeAppName}
-            nativePath={nativePath}
-            href={generateWebLink({
-              cozyUrl: cozyURL.origin,
-              slug: storeAppName,
-              nativePath,
-              subDomainType
-            })}
-          >
-            {({ onClick, href }) => (
-              <ButtonLink
-                onClick={onClick}
-                href={href}
-                label={t('suggestions.install')}
-                extension="full"
-                className="u-mb-half"
+    <MuiCozyTheme>
+      <Dialog
+        open
+        onClose={closeModal}
+        fullScreen={fullScreen}
+        fullWidth
+        maxWidth="sm"
+        scroll="body"
+      >
+        <DialogCloseButton onClick={closeModal} />
+        <DialogContent>
+          <DialogContentText>
+            <div className="u-flex u-flex-column u-flex-items-center">
+              <Illustration
+                alt={t('suggestions.illustration')}
+                iconAlt={t('app.logo.alt', { name })}
+                app={slug}
               />
+              <Title className="u-mb-half">
+                {t('suggestions.title', { name })}
+              </Title>
+              <DataTypes dataTypes={dataTypes} konnectorName={name} />
+            </div>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <div className="u-flex u-flex-column u-flex-items-center">
+            {reason === 'FOUND_TRANSACTION' && (
+              <Caption className="u-mb-1">
+                {t('suggestions.why', { name })}
+                <br />
+                {t('suggestions.reason_bank', { name })}
+              </Caption>
             )}
-          </AppLinker>
-          <Button
-            theme="secondary"
-            label={t('suggestions.silence')}
-            onClick={silenceSuggestion}
-            busy={isSilencing}
-            extension="full"
-          />
-        </>
-      }
-    />
+            <AppLinker
+              slug={storeAppName}
+              nativePath={nativePath}
+              href={generateWebLink({
+                cozyUrl: cozyURL.origin,
+                slug: storeAppName,
+                nativePath,
+                subDomainType
+              })}
+            >
+              {({ onClick, href }) => (
+                <ButtonLink
+                  onClick={onClick}
+                  href={href}
+                  label={t('suggestions.install')}
+                  extension="full"
+                  className="u-mb-half"
+                />
+              )}
+            </AppLinker>
+            <Button
+              theme="secondary"
+              label={t('suggestions.silence')}
+              onClick={silenceSuggestion}
+              busy={isSilencing}
+              extension="full"
+            />
+          </div>
+        </DialogActions>
+      </Dialog>
+    </MuiCozyTheme>
   )
 }
 
@@ -117,4 +137,6 @@ KonnectorSuggestionModal.propTypes = {
   closeModal: PropTypes.func.isRequired
 }
 
-export default withClient(translate()(KonnectorSuggestionModal))
+export default withClient(
+  translate()(withMobileDialog()(KonnectorSuggestionModal))
+)
