60385261ac6f8f9d59937b3750a3259975c2cd00
Yannick Lohse <contact@yannick-lohse.fr>
2019-11-25 12:03:35 +0100

    fix: Replace faulty illustration

--- packages/cozy-harvest-lib/src/components/KonnectorSuggestionModal/Illustration.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorSuggestionModal/Illustration.jsx
@@ -2,24 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import AppIcon from 'cozy-ui/transpiled/react/AppIcon'
 
-import preinstallIllustration from '../../assets/preinstall.svg'
+import PreinstallIllustration from '../../assets/preinstall.svg'
 
-const Illustration = ({ alt, iconAlt, app }) => (
+const Illustration = ({ app, alt }) => (
   <div className="u-pos-relative u-mb-half">
-    <img src={preinstallIllustration} alt={alt} />
+    <PreinstallIllustration />
     <div
       className="u-w-2 u-h-2 u-pos-absolute"
       style={{ top: '18px', left: '39px' }}
     >
-      <AppIcon alt={iconAlt} app={app} />
+      <AppIcon alt={alt} app={app} />
     </div>
   </div>
 )
 
 Illustration.propTypes = {
-  alt: PropTypes.string,
-  iconAlt: PropTypes.string,
-  app: PropTypes.string.isRequired
+  app: PropTypes.string.isRequired,
+  alt: PropTypes.string
 }
 
 export default Illustration
