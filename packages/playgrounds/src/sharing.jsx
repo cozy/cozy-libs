3f405282085795d2fd631cbe6e4b6973c692ebc6
Éric D <15271+edas@users.noreply.github.com>
2019-11-29 15:28:49 +0100

    feat: Adds cozy-sharing to playgrounds

---
+++ packages/playgrounds/src/sharing.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import App from './common/App'
+import client from './common/client'
+import { Route } from 'react-router'
+import 'cozy-ui/transpiled/react/stylesheet.css'
+import 'cozy-ui/dist/cozy-ui.min.css'
+
+import { queryConnect } from 'cozy-client'
+
+import SharingProvider, { ShareButton } from 'cozy-sharing'
+import withLocales from 'cozy-sharing/dist/withLocales'
+
+const reducer = combineReducers({
+  cozy: client.reducer()
+})
+
+// get a directory
+// const FileCollection = client.getStackClient().collection('io.cozy.files')
+//const rootDir = FileCollection.get('io.cozy.files.root-dir')
+
+//console.log(rootDir)
+//console.log(client)
+
+const store = createStore(reducer)
+const LocalizedSharingProvider = withLocales(SharingProvider)
+
+const MySharing = props => {
+  const file = props.file
+  const docId = file.id
+  return <ShareButton docId={docId} />
+}
+
+export const WithFirstChild = (props) => {
+  console.log(props)
+  if (props.file.fetchStatus == 'loaded') {
+    const Component = props.component
+    return <Component file={props.file.data.relationships.contents.data[0]} {...props} />
+  } else {
+    return <p>{props.file.fetchStatus}</p>
+  }
+}
+
+export const WithFile = queryConnect({
+  file: {
+    query: client => client.all('io.cozy.files').getById( 'io.cozy.files.root-dir'),
+    as: 'file'
+  }
+})(WithFirstChild)
+
+const MyComponent = () => {
+  return (
+    <LocalizedSharingProvider doctype="io.cozy.files" documentType="Files">
+      <WithFile component={MySharing} />
+    </LocalizedSharingProvider>
+  )
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App client={client} existingStore={store}>
+      <Route path="/" component={MyComponent} />
+    </App>
+  </Provider>,
+  document.querySelector('#app')
+)
