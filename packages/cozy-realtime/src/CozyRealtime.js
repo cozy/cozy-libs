c5da980b3d15e5123b1781b641949e765b7d241f
Patrick Browne <pt.browne@gmail.com>
2020-01-09 17:34:02 +0100

    fix: Correctly check that window exists

--- packages/cozy-realtime/src/CozyRealtime.js
+++ packages/cozy-realtime/src/CozyRealtime.js
@@ -2,7 +2,7 @@ import MicroEE from 'microee'
 import Socket from './Socket'
 import minilog_ from 'minilog'
 
-const minilog = (typeof window !== undefined && window.minilog) || minilog_
+const minilog = (typeof window !== 'undefined' && window.minilog) || minilog_
 const logger = minilog('cozy-realtime')
 minilog.suggest.deny('cozy-realtime', 'info')
 
