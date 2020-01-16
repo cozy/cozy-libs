ee314d1b43624a7e04be4f3359974ad3344caee2
Quentin Valmori <quentin.valmori@gmail.com>
2020-01-16 17:38:37 +0100

    refactor: Do not use while

--- packages/cozy-sharing/src/fetchNextPermissions.js
+++ packages/cozy-sharing/src/fetchNextPermissions.js
@@ -8,12 +8,10 @@ import { addSharingLink } from './state'
  */
 export const fetchNextPermissions = async (permissions, dispatch, client) => {
   if (permissions.links && permissions.links.next) {
-    let resp = { links: { next: true } }
-    while (resp && resp.links.next) {
-      resp = await client
-        .getStackClient()
-        .fetchJSON('GET', permissions.links.next)
-      dispatch(addSharingLink(resp.data))
-    }
+    const resp = await client
+      .getStackClient()
+      .fetchJSON('GET', permissions.links.next)
+    dispatch(addSharingLink(resp.data))
+    return fetchNextPermissions(resp, dispatch, client)
   }
 }
