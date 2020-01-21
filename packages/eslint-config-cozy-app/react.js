6bea38bda1c5c1c5d5d41293f70cad5385a29360
Cyrille Perois <cperois@outlook.com>
2020-01-21 16:37:31 +0100

    feat(eslint-config-cozy-app): Add react-hooks plugin

--- packages/eslint-config-cozy-app/react.js
+++ packages/eslint-config-cozy-app/react.js
@@ -3,13 +3,15 @@
 const basics = require('./basics')
 
 module.exports = {
-  plugins: basics.plugins,
+  plugins: basics.plugins.concat(['react-hooks']),
   extends: basics.extends.concat(['plugin:react/recommended']),
   parser: basics.parser,
   parserOptions: { ecmaFeatures: { jsx: true } },
   env: basics.env,
   settings: { react: { version: 'latest' } },
   rules: Object.assign({}, basics.rules, {
-    'react/prop-types': 0
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1
   })
 }
