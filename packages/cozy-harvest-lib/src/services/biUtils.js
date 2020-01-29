8f6b7da376a41ecc8f5d355e3d67eda356f39069
Patrick Browne <pt.browne@gmail.com>
2020-01-29 09:14:43 +0100

    docs: Comment on potentially mutualizing BI sdk code

--- packages/cozy-harvest-lib/src/services/biUtils.js
+++ packages/cozy-harvest-lib/src/services/biUtils.js
@@ -1,3 +1,14 @@
+/**
+ * This is meant to be a subset of low level functions that are needed
+ * to talk with Budget-Insight when adding a budget-insight connection.
+ *
+ * Much more of the API is supported in the banking konnector and it
+ * might make sense to mutualize the code under a bi-sdk package if
+ * we ever need more of it here.
+ *
+ * See https://github.com/cozy/cozy-libs/pull/929#discussion_r372238690
+ */
+
 import assert from '../assert'
 import biPublicKeyProd from './bi-public-key-prod.json'
 
