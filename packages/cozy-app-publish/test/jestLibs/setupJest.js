691e9af819553d1bfd6b234751a46cbbeb1514a8
Patrick Browne <pt.browne@gmail.com>
2020-01-31 11:46:34 +0100

    refactor: Use logger instead of console.log to mock it out in tests

--- packages/cozy-app-publish/test/jestLibs/setupJest.js
+++ packages/cozy-app-publish/test/jestLibs/setupJest.js
@@ -2,3 +2,9 @@
 
 const fetch = require('jest-fetch-mock')
 jest.doMock('node-fetch', () => fetch.mockResponse({ status: 201 }))
+
+jest.mock('../../utils/logger', () => ({
+  log: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn()
+}))
