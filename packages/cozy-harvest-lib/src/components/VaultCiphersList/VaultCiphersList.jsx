7c37ac1964a826d31b6a00ef1e760ac25749db31
Yannick Lohse <contact@yannick-lohse.fr>
2020-01-31 12:36:54 +0100

    fix: Remove extra translation context

--- packages/cozy-harvest-lib/src/components/VaultCiphersList/VaultCiphersList.jsx
+++ packages/cozy-harvest-lib/src/components/VaultCiphersList/VaultCiphersList.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Title } from 'cozy-ui/transpiled/react/Text'
-import I18n from 'cozy-ui/transpiled/react/I18n'
 
 import List from 'cozy-ui/transpiled/react/MuiCozyTheme/RaisedList'
 
@@ -11,7 +10,7 @@ import OtherAccountListItem from './OtherAccountListItem'
 
 export const DumbVaultCiphersList = ({ konnector, onSelect, ciphers, t }) => {
   return (
-    <I18n lang="en" dictRequire={() => {}}>
+    <>
       <Title className="u-ta-center u-mb-2">
         {t('vaultCiphersList.title')}
       </Title>
@@ -27,7 +26,7 @@ export const DumbVaultCiphersList = ({ konnector, onSelect, ciphers, t }) => {
 
         <OtherAccountListItem onClick={() => onSelect(null)} />
       </List>
-    </I18n>
+    </>
   )
 }
 
