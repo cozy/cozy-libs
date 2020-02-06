0ec0ca2c304fd7d2c9671a940eb4bd225c18291d
Patrick Browne <pt.browne@gmail.com>
2020-02-06 11:37:59 +0100

    refactor: Extract flag.enable

--- packages/cozy-flags/src/flag.js
+++ packages/cozy-flags/src/flag.js
@@ -23,8 +23,35 @@ export const resetFlags = () => {
   listFlags().forEach(name => store.remove(name))
 }
 
+/**
+ * Enables a list of flags
+ *
+ * Supports passing either
+ * - an array containing flag names (in this case, flag values will be true)
+ * - or an object flagName -> flagValue
+ *
+ * @param {string[]|Object} flagsToEnable
+ */
+export const enable = flagsToEnable => {
+  let flagNameToValue
+  if (Array.isArray(flagsToEnable)) {
+    flagNameToValue = flagsToEnable.map(flagName => [flagName, true])
+  } else if (typeof flagsToEnable === 'object') {
+    flagNameToValue = Object.entries(flagsToEnable)
+  }
+
+  if (!flagNameToValue) {
+    return
+  }
+
+  for (const [flagName, flagValue] of flagNameToValue) {
+    flag(flagName, flagValue)
+  }
+}
+
 flag.store = store
 flag.list = listFlags
 flag.reset = resetFlags
+flag.enable = enable
 
 export default flag
