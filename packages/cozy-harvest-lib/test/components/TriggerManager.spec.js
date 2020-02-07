cozy/cozy-libs

commit 15e64e071d7d8ac2183b9b941155a98561de2cc3

fix(cozy-harvest-lib): Back button on account form
When the user selected a cipher, he should be able to go back to the
ciphers list by clicking on the back button on the left modal corner.
This was broken, so this PR fixes it.

packages/cozy-harvest-lib/src/components/TriggerManager.jsx
@@ -419,12 +419,13 @@ export class DumbTriggerManager extends Component {
   }
 
   showCiphersList(ciphers) {
-    this.setState(prevState => {
-      return {
-        step: prevState.step === 'accountForm' ? 'accountForm' : 'ciphersList',
-        ciphers: ciphers ? ciphers : prevState.ciphers
-      }
-    })
+    const newState = { step: 'ciphersList' }
+
+    if (ciphers) {
+      newState.ciphers = ciphers
+    }
+
+    this.setState(newState)
   }
 
   async handleVaultUnlock() {
@@ -448,7 +449,11 @@ export class DumbTriggerManager extends Component {
       if (ciphers.length === 0) {
         this.showAccountForm()
       } else {
-        this.showCiphersList(ciphers)
+        if (this.step === 'accountForm') {
+          this.setState({ ciphers })
+        } else {
+          this.showCiphersList(ciphers)
+        }
       }
     } catch (err) {
       logger.error(

packages/cozy-harvest-lib/test/components/TriggerManager.spec.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 import React from 'react'
 import { shallow } from 'enzyme'
-import { render } from '@testing-library/react'
+import { render, fireEvent, cleanup } from '@testing-library/react'
 
 import { DumbTriggerManager as TriggerManager } from 'components/TriggerManager'
 import cronHelpers from 'helpers/cron'
@@ -296,10 +296,16 @@ describe('TriggerManager', () => {
     mockVaultClient.createNewCozySharedCipher.mockResolvedValue({
       id: 'cipher-id-1'
     })
+    tMock.mockImplementation(key => key)
   })
 
   afterEach(() => {
     jest.resetAllMocks()
+
+    // This should be done automatically by react-testing-library, but
+    // not calling it manually leads to errors because the dom has not
+    // been cleaned up between some test cases
+    cleanup()
   })
 
   describe('when given an oauth konnector', () => {
@@ -327,13 +333,16 @@ describe('TriggerManager', () => {
         mockVaultClient.getAll.mockResolvedValue([])
         mockVaultClient.getAllDecrypted.mockResolvedValue([])
 
-        const { findByLabelText } = render(<TriggerManager {...props} />)
+        const { findByLabelText, findByTitle } = render(
+          <TriggerManager {...props} />
+        )
 
         const usernameField = await findByLabelText('username')
         const passwordField = await findByLabelText('passphrase')
 
         expect(usernameField).toBeDefined()
         expect(passwordField).toBeDefined()
+        expect(findByTitle('back')).rejects.toThrow()
       })
     })
 
@@ -356,6 +365,40 @@ describe('TriggerManager', () => {
 
         expect(cipherItem).toBeDefined()
       })
+
+      describe('when selecting a cipher without password', () => {
+        it('should show the account form with only password field editable', async () => {
+          mockVaultClient.getAll.mockResolvedValue([{ id: 'cipher1' }])
+          mockVaultClient.getAllDecrypted.mockResolvedValue([
+            {
+              id: 'cipher1',
+              name: fixtures.konnector.name,
+              login: {
+                username: 'Isabelle'
+              }
+            }
+          ])
+
+          const { findByText, findByLabelText, findByTitle } = render(
+            <TriggerManager {...props} />
+          )
+
+          const cipherItem = await findByText('Isabelle')
+
+          fireEvent.click(cipherItem)
+
+          const passwordField = await findByLabelText('passphrase')
+          const backButton = await findByTitle('back')
+
+          expect(findByLabelText('username')).rejects.toThrow()
+          expect(passwordField).toBeDefined()
+          expect(backButton).toBeDefined()
+
+          fireEvent.click(backButton)
+
+          expect(findByText('Isabelle')).resolves.toBeDefined()
+        })
+      })
     })
   })
 
@@ -363,48 +406,6 @@ describe('TriggerManager', () => {
     it('should render correctly', () => {
       const component = shallowWithAccount().getElement()
       expect(component).toMatchSnapshot()
-    })
-
-    describe('when the vault contains ciphers', () => {
-      it('should show the account form', async () => {
-        mockVaultClient.getAll.mockResolvedValue([])
-        mockVaultClient.getAllDecrypted.mockResolvedValue([])
-
-        const { findByLabelText } = render(
-          <TriggerManager {...propsWithAccount} />
-        )
-
-        const usernameField = await findByLabelText('username')
-        const passwordField = await findByLabelText('passphrase')
-
-        expect(usernameField).toBeDefined()
-        expect(passwordField).toBeDefined()
-      })
-    })
-
-    describe('when the vault does not contain ciphers', () => {
-      it('should show the account form', async () => {
-        mockVaultClient.getAll.mockResolvedValue([{ id: 'cipher1' }])
-        mockVaultClient.getAllDecrypted.mockResolvedValue([
-          {
-            id: 'cipher1',
-            name: fixtures.konnector.name,
-            login: {
-              username: 'Isabelle'
-            }
-          }
-        ])
-
-        const { findByLabelText } = render(
-          <TriggerManager {...propsWithAccount} />
-        )
-
-        const usernameField = await findByLabelText('username')
-        const passwordField = await findByLabelText('passphrase')
-
-        expect(usernameField).toBeDefined()
-        expect(passwordField).toBeDefined()
-      })
     })
   })
 

commit a0dd97fcce2500435fd80a4f16288dfb16211d5b

fix(cozy-harvest-lib): Show account form on account edition
This is quite similar to https://github.com/cozy/cozy-libs/pull/949.
Actually I broke it in https://github.com/cozy/cozy-libs/pull/952
because I reverted a commit that contained tests. Thus the tests
disappeared, but I thought they were passing.

So here I bring back the tests from the reverted commit and really fix
the issue...

packages/cozy-harvest-lib/src/components/TriggerManager.jsx
@@ -449,7 +449,7 @@ export class DumbTriggerManager extends Component {
       if (ciphers.length === 0) {
         this.showAccountForm()
       } else {
-        if (this.step === 'accountForm') {
+        if (this.state.step === 'accountForm') {
           this.setState({ ciphers })
         } else {
           this.showCiphersList(ciphers)

packages/cozy-harvest-lib/test/components/TriggerManager.spec.js
@@ -300,7 +300,16 @@ describe('TriggerManager', () => {
   })
 
   afterEach(() => {
-    jest.resetAllMocks()
+    // We reset mocks one by one instead of doing jest.resetAllMocks
+    // because otherwise some tests fail because of incorrect mocks and I
+    // couldn't find why
+    tMock.mockReset()
+    saveAccountMock.mockReset()
+    createDirectoryByPathMock.mockReset()
+    addPermissionMock.mockReset()
+    addReferencesToMock.mockReset()
+    createTriggerMock.mockReset()
+    statDirectoryByPathMock.mockReset()
 
     // This should be done automatically by react-testing-library, but
     // not calling it manually leads to errors because the dom has not
@@ -322,32 +331,31 @@ describe('TriggerManager', () => {
     })
   })
 
-  describe('when given no account', () => {
+  describe('account creation', () => {
     it('should render correctly', () => {
       const component = shallowWithoutAccount().getElement()
       expect(component).toMatchSnapshot()
     })
 
     describe('when the vault does not contain ciphers', () => {
-      it('should show the new account form', async () => {
+      beforeEach(() => {
         mockVaultClient.getAll.mockResolvedValue([])
         mockVaultClient.getAllDecrypted.mockResolvedValue([])
+      })
 
+      it('should show the new account form', async () => {
         const { findByLabelText, findByTitle } = render(
           <TriggerManager {...props} />
         )
 
-        const usernameField = await findByLabelText('username')
-        const passwordField = await findByLabelText('passphrase')
-
-        expect(usernameField).toBeDefined()
-        expect(passwordField).toBeDefined()
+        expect(findByLabelText('username')).resolves.toBeDefined()
+        expect(findByLabelText('passphrase')).resolves.toBeDefined()
         expect(findByTitle('back')).rejects.toThrow()
       })
     })
 
     describe('when the vault contains ciphers', () => {
-      it('should show the ciphers list', async () => {
+      beforeEach(() => {
         mockVaultClient.getAll.mockResolvedValue([{ id: 'cipher1' }])
         mockVaultClient.getAllDecrypted.mockResolvedValue([
           {
@@ -358,7 +366,9 @@ describe('TriggerManager', () => {
             }
           }
         ])
+      })
 
+      it('should show the ciphers list', async () => {
         const { findByText } = render(<TriggerManager {...props} />)
 
         const cipherItem = await findByText('Isabelle')
@@ -367,7 +377,7 @@ describe('TriggerManager', () => {
       })
 
       describe('when selecting a cipher without password', () => {
-        it('should show the account form with only password field editable', async () => {
+        beforeEach(() => {
           mockVaultClient.getAll.mockResolvedValue([{ id: 'cipher1' }])
           mockVaultClient.getAllDecrypted.mockResolvedValue([
             {
@@ -378,7 +388,9 @@ describe('TriggerManager', () => {
               }
             }
           ])
+        })
 
+        it('should show the account form with only password field editable', async () => {
           const { findByText, findByLabelText, findByTitle } = render(
             <TriggerManager {...props} />
           )
@@ -402,10 +414,56 @@ describe('TriggerManager', () => {
     })
   })
 
-  describe('when given an account', () => {
+  describe('account update', () => {
     it('should render correctly', () => {
       const component = shallowWithAccount().getElement()
       expect(component).toMatchSnapshot()
+    })
+
+    describe('when the vault contains ciphers', () => {
+      beforeEach(() => {
+        mockVaultClient.getAll.mockResolvedValue([])
+        mockVaultClient.getAllDecrypted.mockResolvedValue([])
+      })
+
+      it('should show the account form', async () => {
+        const { findByLabelText } = render(
+          <TriggerManager {...propsWithAccount} />
+        )
+
+        const usernameField = await findByLabelText('username')
+        const passwordField = await findByLabelText('passphrase')
+
+        expect(usernameField).toBeDefined()
+        expect(passwordField).toBeDefined()
+      })
+    })
+
+    describe('when the vault does not contain ciphers', () => {
+      beforeEach(() => {
+        mockVaultClient.getAll.mockResolvedValue([{ id: 'cipher1' }])
+        mockVaultClient.getAllDecrypted.mockResolvedValue([
+          {
+            id: 'cipher1',
+            name: fixtures.konnector.name,
+            login: {
+              username: 'Isabelle'
+            }
+          }
+        ])
+      })
+
+      it('should show the account form', async () => {
+        const { findByLabelText } = render(
+          <TriggerManager {...propsWithAccount} />
+        )
+
+        const usernameField = await findByLabelText('username')
+        const passwordField = await findByLabelText('passphrase')
+
+        expect(usernameField).toBeDefined()
+        expect(passwordField).toBeDefined()
+      })
     })
   })
 

packages/cozy-harvest-lib/test/components/__snapshots__/TriggerManager.spec.js.snap
@@ -1,5 +1,85 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
+exports[`TriggerManager account creation should render correctly 1`] = `
+<KonnectorVaultUnlocker
+  konnector={
+    Object {
+      "fields": Object {
+        "passphrase": Object {
+          "type": "password",
+        },
+        "username": Object {
+          "type": "text",
+        },
+      },
+      "slug": "konnectest",
+    }
+  }
+  onDismiss={[MockFunction]}
+  onUnlock={[Function]}
+>
+  <div
+    id="coz-harvest-modal-place"
+  />
+</KonnectorVaultUnlocker>
+`;
+
+exports[`TriggerManager account update should render correctly 1`] = `
+<KonnectorVaultUnlocker
+  konnector={
+    Object {
+      "fields": Object {
+        "passphrase": Object {
+          "type": "password",
+        },
+        "username": Object {
+          "type": "text",
+        },
+      },
+      "slug": "konnectest",
+    }
+  }
+  onDismiss={[MockFunction]}
+  onUnlock={[Function]}
+>
+  <div
+    id="coz-harvest-modal-place"
+  />
+  <React.Fragment>
+    <withI18n(withLocales(withKonnectorLocales(AccountForm))
+      account={
+        Object {
+          "_id": "existing-account-id",
+          "account_type": "konnectest",
+          "auth": Object {
+            "passphrase": "bar",
+            "username": "foo",
+          },
+          "identifier": "username",
+        }
+      }
+      konnector={
+        Object {
+          "fields": Object {
+            "passphrase": Object {
+              "type": "password",
+            },
+            "username": Object {
+              "type": "text",
+            },
+          },
+          "slug": "konnectest",
+        }
+      }
+      onBack={[Function]}
+      onSubmit={[Function]}
+      readOnlyIdentifier={false}
+      submitting={false}
+    />
+  </React.Fragment>
+</KonnectorVaultUnlocker>
+`;
+
 exports[`TriggerManager handleError should render error 1`] = `
 <KonnectorVaultUnlocker
   konnector={
@@ -57,62 +137,6 @@ exports[`TriggerManager handleError should render error 1`] = `
 </KonnectorVaultUnlocker>
 `;
 
-exports[`TriggerManager when given an account should render correctly 1`] = `
-<KonnectorVaultUnlocker
-  konnector={
-    Object {
-      "fields": Object {
-        "passphrase": Object {
-          "type": "password",
-        },
-        "username": Object {
-          "type": "text",
-        },
-      },
-      "slug": "konnectest",
-    }
-  }
-  onDismiss={[MockFunction]}
-  onUnlock={[Function]}
->
-  <div
-    id="coz-harvest-modal-place"
-  />
-  <React.Fragment>
-    <withI18n(withLocales(withKonnectorLocales(AccountForm))
-      account={
-        Object {
-          "_id": "existing-account-id",
-          "account_type": "konnectest",
-          "auth": Object {
-            "passphrase": "bar",
-            "username": "foo",
-          },
-          "identifier": "username",
-        }
-      }
-      konnector={
-        Object {
-          "fields": Object {
-            "passphrase": Object {
-              "type": "password",
-            },
-            "username": Object {
-              "type": "text",
-            },
-          },
-          "slug": "konnectest",
-        }
-      }
-      onBack={[Function]}
-      onSubmit={[Function]}
-      readOnlyIdentifier={false}
-      submitting={false}
-    />
-  </React.Fragment>
-</KonnectorVaultUnlocker>
-`;
-
 exports[`TriggerManager when given an oauth konnector should redirect to OAuthForm 1`] = `
 <withI18n(OAuthForm)
   konnector={
@@ -125,28 +149,4 @@ exports[`TriggerManager when given an oauth konnector should redirect to OAuthFo
   onSuccess={[Function]}
   submitting={false}
 />
-`;
-
-exports[`TriggerManager when given no account should render correctly 1`] = `
-<KonnectorVaultUnlocker
-  konnector={
-    Object {
-      "fields": Object {
-        "passphrase": Object {
-          "type": "password",
-        },
-        "username": Object {
-          "type": "text",
-        },
-      },
-      "slug": "konnectest",
-    }
-  }
-  onDismiss={[MockFunction]}
-  onUnlock={[Function]}
->
-  <div
-    id="coz-harvest-modal-place"
-  />
-</KonnectorVaultUnlocker>
 `;