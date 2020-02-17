6f5a1bddf41f7170901384d8100916fae75ea522
Yannick Lohse <contact@yannick-lohse.fr>
2020-02-17 11:07:52 +0100

    refactor: Use functions instead of classes

--- packages/cozy-harvest-lib/src/components/KonnectorConfiguration/Success/BanksLink.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorConfiguration/Success/BanksLink.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import AppLinker from 'cozy-ui/transpiled/react/AppLinker'
 import { ButtonLink } from 'cozy-ui/transpiled/react/Button'
@@ -8,48 +8,46 @@ import { Application } from 'cozy-doctypes'
 
 import withLocales from '../../hoc/withLocales'
 
-class BanksLinkRedirectStore extends Component {
-  render() {
-    const { apps, t } = this.props
-    if (apps.fetchStatus === 'loaded') {
-      const banksApp = {
-        slug: 'banks'
-      }
-      let url = ''
-      const banksInstalled = Application.isInstalled(apps.data, banksApp)
-      if (banksInstalled) {
-        url = Application.getUrl(banksInstalled)
-      } else {
-        url = Application.getStoreInstallationURL(apps.data, banksApp)
-      }
-
-      return (
-        <AppLinker slug="banks" href={url}>
-          {({ href, name }) => (
-            <ButtonLink
-              icon="openwith"
-              href={href}
-              label={t('account.success.banksLinkText', {
-                appName: name
-              })}
-              subtle
-            />
-          )}
-        </AppLinker>
-      )
+const BanksLinkRedirectStore = ({ apps, t }) => {
+  if (apps.fetchStatus === 'loaded') {
+    const banksApp = {
+      slug: 'banks'
     }
+    let url = ''
+    const banksInstalled = Application.isInstalled(apps.data, banksApp)
+    if (banksInstalled) {
+      url = Application.getUrl(banksInstalled)
+    } else {
+      url = Application.getStoreInstallationURL(apps.data, banksApp)
+    }
+
     return (
-      <ButtonLink
-        icon="openwith"
-        label={t('account.success.banksLinkText', {
-          appName: name
-        })}
-        busy
-        subtle
-      />
+      <AppLinker slug="banks" href={url}>
+        {({ href, name }) => (
+          <ButtonLink
+            icon="openwith"
+            href={href}
+            label={t('account.success.banksLinkText', {
+              appName: name
+            })}
+            subtle
+          />
+        )}
+      </AppLinker>
     )
   }
+  return (
+    <ButtonLink
+      icon="openwith"
+      label={t('account.success.banksLinkText', {
+        appName: name
+      })}
+      busy
+      subtle
+    />
+  )
 }
+
 BanksLinkRedirectStore.propTypes = {
   apps: PropTypes.array.isRequired,
   t: PropTypes.func.isRequired
--- packages/cozy-harvest-lib/src/components/KonnectorConfiguration/Success/DriveLink.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorConfiguration/Success/DriveLink.jsx
@@ -1,35 +1,33 @@
-import React, { PureComponent } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import { ButtonLink } from 'cozy-ui/transpiled/react/Button'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
 import { queryConnect, Q } from 'cozy-client'
 
-export class DriveLink extends PureComponent {
-  render() {
-    const { folderId, driveQuery, t } = this.props
-    let hasDrive = false
-    if (driveQuery.fetchStatus === 'loaded') {
-      if (driveQuery.data.length > 0) {
-        hasDrive = true
-      }
+const DriveLink = memo(({ folderId, driveQuery, t }) => {
+  let hasDrive = false
+  if (driveQuery.fetchStatus === 'loaded') {
+    if (driveQuery.data.length > 0) {
+      hasDrive = true
     }
-    const href = hasDrive
-      ? `${driveQuery.data[0].links.related}#/files/${folderId}`
-      : undefined
-
-    return (
-      <ButtonLink
-        href={href}
-        target="_parent"
-        disabled={!hasDrive}
-        subtle
-        icon="openwith"
-        label={t('account.success.driveLinkText')}
-      />
-    )
   }
-}
+  const href = hasDrive
+    ? `${driveQuery.data[0].links.related}#/files/${folderId}`
+    : undefined
 
+  return (
+    <ButtonLink
+      href={href}
+      target="_parent"
+      disabled={!hasDrive}
+      subtle
+      icon="openwith"
+      label={t('account.success.driveLinkText')}
+    />
+  )
+})
+
+DriveLink.displayName = 'DriveLink'
 DriveLink.propTypes = {
   t: PropTypes.func.isRequired,
   folderId: PropTypes.string.isRequired,
