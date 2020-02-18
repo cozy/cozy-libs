8b610fe9b431e947fe00fcac40dcd1799597a630
Quentin Valmori <quentin.valmori@gmail.com>
2020-02-18 09:00:40 +0100

    feat: Serve all html files and add link to procedure

--- packages/playgrounds/src/common/Nav.jsx
+++ packages/playgrounds/src/common/Nav.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Nav = () => (
   <div>
     <a href="existing-store.html">Existing store</a>
+    <br />
     <a href="sharing.html">Sharing widget</a>
+    <br />
+    <a href="procedures.html">Cozy-Procedures</a>
   </div>
 )
 
