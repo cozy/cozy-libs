5b7fbbf18ccc21e7b6694c488a813270a525b1c6
Yannick Lohse <contact@yannick-lohse.fr>
2020-02-18 10:04:54 +0100

    fix: Make DriveLink similar to BankLink

--- packages/cozy-harvest-lib/src/components/KonnectorConfiguration/Success/DriveLink.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorConfiguration/Success/DriveLink.jsx
@@ -1,28 +1,43 @@
 import React, { memo } from 'react'
 import PropTypes from 'prop-types'
+import AppLinker from 'cozy-ui/transpiled/react/AppLinker'
 import { ButtonLink } from 'cozy-ui/transpiled/react/Button'
 import { translate } from 'cozy-ui/transpiled/react/I18n'
 import { withClient } from 'cozy-client'
 import useAppLinkWithStoreFallback from '../../hooks/useAppLinkWithStoreFallback'
 
 const DriveLink = memo(({ folderId, client, t }) => {
-  const { fetchStatus, url, isInstalled } = useAppLinkWithStoreFallback(
-    'drive',
-    client,
-    `#/files/${folderId}`
-  )
-  const href = fetchStatus === 'loaded' && isInstalled ? url : undefined
+  const slug = 'drive'
+  const path = `#/files/${folderId}`
+  const { fetchStatus, url } = useAppLinkWithStoreFallback(slug, client, path)
 
-  return (
-    <ButtonLink
-      href={href}
-      target="_parent"
-      disabled={!isInstalled}
-      subtle
-      icon="openwith"
-      label={t('account.success.driveLinkText')}
-    />
-  )
+  if (fetchStatus === 'loaded') {
+    return (
+      <AppLinker slug={slug} href={url} nativePath={path}>
+        {({ href, name }) => (
+          <ButtonLink
+            icon="openwith"
+            href={href}
+            label={t('account.success.driveLinkText', {
+              appName: name
+            })}
+            subtle
+          />
+        )}
+      </AppLinker>
+    )
+  } else {
+    return (
+      <ButtonLink
+        icon="openwith"
+        label={t('account.success.banksLinkText', {
+          appName: name
+        })}
+        busy
+        subtle
+      />
+    )
+  }
 })
 
 DriveLink.displayName = 'DriveLink'
