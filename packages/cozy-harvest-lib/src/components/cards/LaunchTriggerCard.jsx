c6a6dbdd3d36a286e94ffc9b4814307e2b953070
Patrick Browne <pt.browne@gmail.com>
2020-02-29 09:58:21 +0100

    refactor: Separate LaunchTriggerCard into two components
    First step towards having a LaunchTriggerCard that can accept a flow
    from above

--- packages/cozy-harvest-lib/src/components/cards/LaunchTriggerCard.jsx
+++ packages/cozy-harvest-lib/src/components/cards/LaunchTriggerCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import pick from 'lodash/pick'
 
@@ -10,83 +10,73 @@ import Uppercase, { Text } from 'cozy-ui/transpiled/react/Text'
 import * as triggers from '../../helpers/triggers'
 import FlowProvider, { FlowProvider as DumbFlowProvider } from '../FlowProvider'
 
+const DumbLaunchTriggerCard = ({ flow, className, f, t, disabled }) => {
+  const flowState = flow.getState()
+  const launch = flow.launch
+  const trigger = flow.trigger
+  const running = flowState.running
+  const lastSuccessDate = triggers.getLastSuccessDate(trigger)
+  return (
+    <Card className={className}>
+      <div className="u-flex u-flex-column-s">
+        <ul className="u-nolist u-m-0 u-mr-1 u-pl-0 u-flex-grow-1">
+          <li className="u-mb-half">
+            <Uppercase tag="span" className="u-coolGrey u-mr-half u-fz-tiny">
+              {t('card.launchTrigger.lastSync.label')}
+            </Uppercase>
+            <Text className="u-fz-tiny" tag="span">
+              {running
+                ? t('card.launchTrigger.lastSync.syncing')
+                : lastSuccessDate
+                ? f(lastSuccessDate, t('card.launchTrigger.lastSync.format'))
+                : t('card.launchTrigger.lastSync.unknown')}
+            </Text>
+          </li>
+          <li className="u-mb-half">
+            <Uppercase className="u-coolGrey u-mr-half u-fz-tiny" tag="span">
+              {t('card.launchTrigger.frequency.label')}
+            </Uppercase>
+            <Text className="u-fz-tiny" tag="span">
+              {t(
+                `card.launchTrigger.frequency.${triggers.getFrequency(
+                  trigger
+                ) || 'undefined'}`
+              )}
+            </Text>
+          </li>
+        </ul>
+        <div>
+          {/* TODO: Extract this directly in Cozy-UI
+            (either with an utility class or a Button prop) */}
+          <Button
+            label={t('card.launchTrigger.button.label')}
+            icon={<Icon focusable="false" icon="sync" spin={running} />}
+            className="u-mh-0 u-mv-0"
+            disabled={running || disabled}
+            onClick={() => launch(trigger)}
+            subtle
+            style={{ lineHeight: '1.4' }}
+          />
+        </div>
+      </div>
+    </Card>
+  )
+}
+
 /**
  * Shows the state of the trigger and provides the ability to
  * relaunch a trigger
  *
  * - Will follow the connection flow and re-render in case of change
  */
-export class LaunchTriggerCard extends Component {
-  render() {
-    const { className, f, t, disabled } = this.props
-    return (
-      <Card className={className}>
-        <FlowProvider
-          {...pick(this.props, Object.keys(DumbFlowProvider.propTypes))}
-        >
-          {({ flow }) => {
-            const flowState = flow.getState()
-            const launch = flow.launch
-            const trigger = flow.trigger
-            const running = flowState.running
-            const lastSuccessDate = triggers.getLastSuccessDate(trigger)
-            return (
-              <div className="u-flex u-flex-column-s">
-                <ul className="u-nolist u-m-0 u-mr-1 u-pl-0 u-flex-grow-1">
-                  <li className="u-mb-half">
-                    <Uppercase
-                      tag="span"
-                      className="u-coolGrey u-mr-half u-fz-tiny"
-                    >
-                      {t('card.launchTrigger.lastSync.label')}
-                    </Uppercase>
-                    <Text className="u-fz-tiny" tag="span">
-                      {running
-                        ? t('card.launchTrigger.lastSync.syncing')
-                        : lastSuccessDate
-                        ? f(
-                            lastSuccessDate,
-                            t('card.launchTrigger.lastSync.format')
-                          )
-                        : t('card.launchTrigger.lastSync.unknown')}
-                    </Text>
-                  </li>
-                  <li className="u-mb-half">
-                    <Uppercase
-                      className="u-coolGrey u-mr-half u-fz-tiny"
-                      tag="span"
-                    >
-                      {t('card.launchTrigger.frequency.label')}
-                    </Uppercase>
-                    <Text className="u-fz-tiny" tag="span">
-                      {t(
-                        `card.launchTrigger.frequency.${triggers.getFrequency(
-                          trigger
-                        ) || 'undefined'}`
-                      )}
-                    </Text>
-                  </li>
-                </ul>
-                <div>
-                  <Button
-                    label={t('card.launchTrigger.button.label')}
-                    icon={<Icon focusable="false" icon="sync" spin={running} />}
-                    className="u-mh-0 u-mv-0"
-                    disabled={running || disabled}
-                    onClick={() => launch(trigger)}
-                    subtle
-                    // TODO: Extract this directly in Cozy-UI
-                    // (either with an utility class or a Button prop)
-                    style={{ lineHeight: '1.4' }}
-                  />
-                </div>
-              </div>
-            )
-          }}
-        </FlowProvider>
-      </Card>
-    )
-  }
+const LaunchTriggerCard = props => {
+  return (
+    <FlowProvider {...pick(props, Object.keys(DumbFlowProvider.propTypes))}>
+      {({ flow }) => {
+        return <DumbLaunchTriggerCard {...props} flow={flow} />
+      }}
+    </FlowProvider>
+  )
 }
 
 LaunchTriggerCard.propTypes = {
