c74f066d36223b363321c0b502f2ba337d9ea83a
Quentin Valmori <quentin.valmori@gmail.com>
2020-03-23 14:59:26 +0100

    refactor: Extract get or create contact logic

---
+++ packages/cozy-sharing/src/helpers/contacts.js
@@ -0,0 +1,41 @@
+import { validateEmail } from '../helpers/email'
+
+// Return the contact object if present
+// Else check if a contact exist with this email address, if yes return it
+// if not create it
+export const getOrCreateFromArray = async (client, contacts, createContact) => {
+  return await Promise.all(
+    contacts.map(async contact => {
+      if (contact.id) {
+        return contact
+      } else if (validateEmail(contact.email)) {
+        const matchedContact = await client.collection('io.cozy.contacts').find(
+          {
+            email: {
+              $elemMatch: {
+                address: contact.email
+              }
+            },
+            id: {
+              $gt: null
+            }
+          },
+          {
+            indexedFields: ['id']
+          }
+        )
+
+        if (matchedContact.data.length > 0) {
+          //We take the shortcut that if we have sevaral contacts
+          //with the same address, we take the first one for now
+          return matchedContact.data[0]
+        } else {
+          const resp = await createContact({
+            email: [{ address: contact.email, primary: true }]
+          })
+          return resp.data
+        }
+      }
+    })
+  )
+}
