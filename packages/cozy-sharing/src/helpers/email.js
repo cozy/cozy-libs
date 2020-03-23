634bca1ac637e05eff1ba4f615d8a7183cc05a6f
Quentin Valmori <quentin.valmori@gmail.com>
2020-03-23 18:08:21 +0100

    refactor: More readable regex

--- packages/cozy-sharing/src/helpers/email.js
+++ packages/cozy-sharing/src/helpers/email.js
@@ -1,11 +1,10 @@
 // Extract emails address from a string
 export const extractEmails = str => {
-  if (typeof str === 'string')
-    return str.match(/([a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi)
+  if (typeof str === 'string') return str.match(/([\w.+-]+@[\w.-]+\.[\w-]+)/gi)
   return null
 }
 // Validate if a string is an email address or not
 export const validateEmail = str => {
-  const reg = /([a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/i
+  const reg = /([\w.+-]+@[\w.-]+\.[\w-]+)/i
   return reg.test(String(str).toLowerCase())
 }
