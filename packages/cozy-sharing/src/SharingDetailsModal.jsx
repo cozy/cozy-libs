9096d1512f0e5a7ada217d9df5a278c26fef6e8f
Quentin Valmori <quentin.valmori@gmail.com>
2020-04-28 09:25:27 +0200

    refactor: No need to use Component here.
    Remove context usage
    Fix PropTypes issue

--- packages/cozy-sharing/src/SharingDetailsModal.jsx
+++ packages/cozy-sharing/src/SharingDetailsModal.jsx
@@ -1,80 +1,88 @@
 import styles from './share.styl'
+import React from 'react'
 import PropTypes from 'prop-types'
-import React, { Component } from 'react'
-import { UserAvatar as Owner } from './components/Recipient'
-import WhoHasAccess from './components/WhoHasAccess'
 
+import { translate } from 'cozy-ui/transpiled/react/I18n'
 import Modal, { ModalContent } from 'cozy-ui/transpiled/react/Modal'
 
+import { UserAvatar as Owner } from './components/Recipient'
+import WhoHasAccess from './components/WhoHasAccess'
 import { Contact } from './models'
 
-export class SharingDetailsModal extends Component {
-  static contextTypes = {
-    t: PropTypes.func.isRequired,
-    f: PropTypes.object.isRequired
-  }
-  render() {
-    const { t, f } = this.context
-    const {
-      onClose,
-      sharingType,
-      owner,
-      recipients,
-      document,
-      documentType = 'Document',
-      onRevoke,
-      onRevokeSelf
-    } = this.props
-    return (
-      <Modal
-        title={t(`${documentType}.share.details.title`)}
-        secondaryAction={onClose}
-        className={styles['share-modal']}
-        into="body"
-        size="small"
-        mobileFullscreen
-      >
-        <ModalContent className={styles['share-modal-content']}>
-          <div className={styles['share-details']}>
-            <Owner
-              name={t(`${documentType}.share.sharedBy`, {
-                name: Contact.getDisplayName(owner)
-              })}
-              url={owner.instance}
-            />
-            <div className={styles['share-details-created']}>
-              {t(`${documentType}.share.details.createdAt`, {
-                date: f(document.created_at || null, 'Do MMMM YYYY')
-              })}
-            </div>
-            <div className={styles['share-details-perm']}>
-              {t(
-                `${documentType}.share.details.${
-                  sharingType === 'one-way' ? 'ro' : 'rw'
-                }`
-              )}
-            </div>
-            <div className={styles['share-details-perm-desc']}>
-              {t(
-                `${documentType}.share.details.desc.${
-                  sharingType === 'one-way' ? 'ro' : 'rw'
-                }`
-              )}
-            </div>
-          </div>
-          <hr className={styles['divider']} />
-          <WhoHasAccess
-            title={t('Share.recipients.accessCount', {
-              count: recipients.length
+export const SharingDetailsModal = ({
+  onClose,
+  sharingType,
+  owner,
+  recipients,
+  document,
+  documentType = 'Document',
+  onRevoke,
+  onRevokeSelf,
+  t,
+  f
+}) => {
+  return (
+    <Modal
+      title={t(`${documentType}.share.details.title`)}
+      dismissAction={onClose}
+      className={styles['share-modal']}
+      into="body"
+      size="small"
+      mobileFullscreen
+    >
+      <ModalContent className={styles['share-modal-content']}>
+        <div className={styles['share-details']}>
+          <Owner
+            name={t(`${documentType}.share.sharedBy`, {
+              name: Contact.getDisplayName(owner)
             })}
-            recipients={recipients}
-            document={document}
-            documentType={documentType}
-            onRevoke={onRevoke}
-            onRevokeSelf={onRevokeSelf}
+            url={owner.instance}
           />
-        </ModalContent>
-      </Modal>
-    )
-  }
+          <div className={styles['share-details-created']}>
+            {t(`${documentType}.share.details.createdAt`, {
+              date: f(document.created_at || null, 'Do MMMM YYYY')
+            })}
+          </div>
+          <div className={styles['share-details-perm']}>
+            {t(
+              `${documentType}.share.details.${
+                sharingType === 'one-way' ? 'ro' : 'rw'
+              }`
+            )}
+          </div>
+          <div className={styles['share-details-perm-desc']}>
+            {t(
+              `${documentType}.share.details.desc.${
+                sharingType === 'one-way' ? 'ro' : 'rw'
+              }`
+            )}
+          </div>
+        </div>
+        <hr className={styles['divider']} />
+        <WhoHasAccess
+          title={t('Share.recipients.accessCount', {
+            count: recipients.length
+          })}
+          recipients={recipients}
+          document={document}
+          documentType={documentType}
+          onRevoke={onRevoke}
+          onRevokeSelf={onRevokeSelf}
+        />
+      </ModalContent>
+    </Modal>
+  )
 }
+
+SharingDetailsModal.propTypes = {
+  onClose: PropTypes.func,
+  sharingType: PropTypes.string,
+  owner: PropTypes.object.isRequired,
+  document: PropTypes.object.isRequired,
+  documentType: PropTypes.string.isRequired,
+  onRevoke: PropTypes.func.isRequired,
+  onRevokeSelf: PropTypes.func,
+  f: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired
+}
+export default translate()(SharingDetailsModal)
--- packages/cozy-sharing/src/components/WhoHasAccess.jsx
+++ packages/cozy-sharing/src/components/WhoHasAccess.jsx
@@ -1,47 +1,49 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import { translate } from 'cozy-ui/transpiled/react/I18n'
+
 import Recipient from './Recipient'
 import { SubTitle } from 'cozy-ui/transpiled/react'
 
-class WhoHasAccess extends PureComponent {
-  static contextTypes = {
-    t: PropTypes.func.isRequired
-  }
-  render() {
-    const {
-      isOwner = false,
-      recipients,
-      document,
-      documentType,
-      onRevoke,
-      className,
-      onRevokeSelf
-    } = this.props
-    const { t } = this.context
-
-    return (
-      <div className={className}>
-        {recipients.length > 1 && (
-          <SubTitle>
-            {t(`${documentType}.share.whoHasAccess.title`, {
-              smart_count: recipients.length
-            })}
-          </SubTitle>
-        )}
-        {recipients.map((recipient, index) => (
-          <Recipient
-            {...recipient}
-            key={`key_r_${index}`}
-            isOwner={isOwner}
-            document={document}
-            documentType={documentType}
-            onRevoke={onRevoke}
-            onRevokeSelf={onRevokeSelf}
-          />
-        ))}
-      </div>
-    )
-  }
+const WhoHasAccess = ({
+  isOwner = false,
+  recipients,
+  document,
+  documentType,
+  onRevoke,
+  className,
+  onRevokeSelf,
+  t
+}) => {
+  return (
+    <div className={className}>
+      {recipients.length > 1 && (
+        <SubTitle>
+          {t(`${documentType}.share.whoHasAccess.title`, {
+            smart_count: recipients.length
+          })}
+        </SubTitle>
+      )}
+      {recipients.map((recipient, index) => (
+        <Recipient
+          {...recipient}
+          key={`key_r_${index}`}
+          isOwner={isOwner}
+          document={document}
+          documentType={documentType}
+          onRevoke={onRevoke}
+          onRevokeSelf={onRevokeSelf}
+        />
+      ))}
+    </div>
+  )
 }
-
-export default WhoHasAccess
+WhoHasAccess.propTypes = {
+  isOwner: PropTypes.bool,
+  recipients: PropTypes.array.isRequired,
+  document: PropTypes.object.isRequired,
+  documentType: PropTypes.string.isRequired,
+  onRevoke: PropTypes.func.isRequired,
+  onRevokeSelf: PropTypes.func
+}
+export default translate()(WhoHasAccess)
