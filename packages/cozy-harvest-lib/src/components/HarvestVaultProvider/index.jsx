8c596623c12ed27449a3f744e914354afece2ef5
Patrick Browne <pt.browne@gmail.com>
2020-06-07 21:02:59 +0200

    feat: Remove undocumented local.config.json
    Introduced in d0a119a09bd8bf79b26a088e66ed9b040b49123a, I am not sure
    that this is needed anymore. It triggers a warning in cozy-home since
    local.config.json does not exist.

--- packages/cozy-harvest-lib/src/components/HarvestVaultProvider/index.jsx
+++ packages/cozy-harvest-lib/src/components/HarvestVaultProvider/index.jsx
@@ -3,35 +3,13 @@ import PropTypes from 'prop-types'
 import { withClient } from 'cozy-client'
 import { VaultProvider } from 'cozy-keys-lib'
 
-let localConfig
-try {
-  localConfig = require('./local.config.json')
-} catch (error) {
-  // void
-}
-
 class HarvestVaultProvider extends React.PureComponent {
   render() {
     const { children, client } = this.props
 
-    let vaultInstanceUrl
-    let useUnsafeStorage
-    if (localConfig) {
-      vaultInstanceUrl = localConfig.keysInstance
-      useUnsafeStorage = true
-    } else {
-      vaultInstanceUrl = client.getStackClient().uri
-      useUnsafeStorage = false
-    }
+    const vaultInstanceUrl = client.getStackClient().uri
 
-    return (
-      <VaultProvider
-        instance={vaultInstanceUrl}
-        unsafeStorage={useUnsafeStorage}
-      >
-        {children}
-      </VaultProvider>
-    )
+    return <VaultProvider instance={vaultInstanceUrl}>{children}</VaultProvider>
   }
 }
 
