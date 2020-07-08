673029b396e71266e636417d9c8d056e2f240bf4
Patrick Browne <pt.browne@gmail.com>
2020-07-08 14:34:04 +0200

    fix: Make sure the initializing promise is reset on logout

--- packages/cozy-flags/src/tests.js
+++ packages/cozy-flags/src/tests.js
@@ -1,4 +1,5 @@
 import CozyClient from 'cozy-client'
+import util from 'util'
 
 export default function testFlagAPI(flag) {
   afterEach(() => {
@@ -111,6 +112,23 @@ export default function testFlagAPI(flag) {
         expect(flag('from_remote')).toBe(true)
         expect(flag('number_of_foos')).toBe(10)
         expect(flag('bar_config')).toEqual({ qux: 'quux' })
+      })
+
+      it('should reset the initializing promise on logout', async () => {
+        const { client } = setup()
+        client.isLogged = true
+        client.registerPlugin(flag.plugin)
+
+        const initializing1 = client.plugins.flags.initializing
+        const state = util.inspect(initializing1)
+        expect(state).toContain('pending')
+        await client.plugins.flags.initializing
+        const state2 = util.inspect(initializing1)
+        expect(state2).not.toContain('pending')
+        await client.logout()
+        const initializing2 = client.plugins.flags.initializing
+        expect(initializing2).not.toBe(initializing1)
+        expect(util.inspect(initializing2)).toContain('pending')
       })
     })
 
