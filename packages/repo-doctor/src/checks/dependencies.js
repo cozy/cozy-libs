ad3d6f1fa7227ff70ffe88299841d62db79557ff
Patrick Browne <pt.browne@gmail.com>
2020-07-10 09:18:09 +0200

    refactor: Remove unused code

--- packages/repo-doctor/src/checks/dependencies.js
+++ packages/repo-doctor/src/checks/dependencies.js
@@ -10,14 +10,6 @@ const severityByDiffType = {
 const checkDependencies = function*(repositoryInfo, { dependencyInfos }) {
   for (const dependency of repositoryInfo.dependencies) {
     const depInfo = dependencyInfos[dependency.name]
-    if (!depInfo.lastVersion) {
-      console.log(`No version for ${dependencyName}`)
-      continue
-    }
-    if (!dependency.version) {
-      console.log(`No version for ${dependencyName}`)
-      continue
-    }
     const diffType = semverDiff(
       dependency.version.replace('^', ''),
       depInfo.lastVersion
