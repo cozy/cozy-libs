29fd4581796ad856a47364fd38c128e9636d37b4
Patrick Browne <pt.browne@gmail.com>
2020-07-17 16:03:11 +0200

    refactor: Remove double wrapping in withLocales
    - Authentication and Revoked were already exported wrapped in
    with withLocales so the second wrapping inside Standalone was
    superfluous
    
    - withLocales has been implemented in cozy-ui so we can use
    it from there

--- packages/cozy-authentication/src/withLocales.jsx
+++ packages/cozy-authentication/src/withLocales.jsx
@@ -1,18 +1,8 @@
-import React from 'react'
-import { I18n, translate } from 'cozy-ui/transpiled/react/I18n'
+import withLocales from 'cozy-ui/transpiled/react/I18n/withLocales'
 
 const locales = {
   en: require(`./locales/en.json`),
   fr: require(`./locales/fr.json`)
 }
 
-const withLocales = Component =>
-  translate()(props => {
-    return (
-      <I18n dictRequire={localeCode => locales[localeCode]} lang={props.lang}>
-        <Component {...props} />
-      </I18n>
-    )
-  })
-
-export default withLocales
+export default withLocales(locales)
