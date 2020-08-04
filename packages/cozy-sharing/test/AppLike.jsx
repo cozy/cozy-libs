272658cc108f73419338775034bc3c53ca8f2936
Yannick Lohse <contact@yannick-lohse.fr>
2020-08-04 11:33:44 +0200

    test: Switch recipient tests to testing-lib

--- packages/cozy-sharing/test/AppLike.jsx
+++ packages/cozy-sharing/test/AppLike.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import { I18n } from 'cozy-ui/transpiled/react'
+import { CozyProvider, createMockClient } from 'cozy-client'
 import langEn from '../locales/en.json'
 
-export const TestI18n = ({ children }) => {
+export const TestI18n = ({ children, client }) => {
   return (
     <I18n lang={'en'} dictRequire={() => langEn}>
-      {children}
+      <CozyProvider client={client || createMockClient({})}>
+        {children}
+      </CozyProvider>
     </I18n>
   )
 }
 
-const AppLike = ({ children }) => <TestI18n>{children}</TestI18n>
+const AppLike = ({ children, client }) => (
+  <TestI18n client={client}>{children}</TestI18n>
+)
 
 export default AppLike
