97ed0abc1e56c0b5453c39ce40426485517b8dc8
Patrick Browne <pt.browne@gmail.com>
2020-08-20 14:00:52 +0200

    feat: Use List components for ConfigurationTab
    Since we will have the contracts section, we prefer to use List
    components instead of a Card

---
+++ packages/cozy-harvest-lib/src/components/KonnectorConfiguration/ConfigurationTab/index.jsx
@@ -0,0 +1,104 @@
+import React, { useContext } from 'react'
+import PropTypes from 'prop-types'
+
+import { Account } from 'cozy-doctypes'
+import Button from 'cozy-ui/transpiled/react/Button'
+import Spinner from 'cozy-ui/transpiled/react/Spinner'
+import palette from 'cozy-ui/transpiled/react/palette'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+
+import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
+import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemIcon'
+import ListSubHeader from 'cozy-ui/transpiled/react/MuiCozyTheme/ListSubHeader'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import ListItemSecondaryAction from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemSecondaryAction'
+
+import TriggerErrorInfo from '../../infos/TriggerErrorInfo'
+import DeleteAccountButton from '../../DeleteAccountButton'
+import { MountPointContext } from '../../MountPointContext'
+import tabSpecs from '../tabSpecs'
+
+import { useI18n } from "cozy-ui/transpiled/react";
+
+const ConfigurationTab = ({
+  konnector,
+  account,
+  addAccount,
+  onAccountDeleted,
+  flow
+}) => {
+  const { t } = useI18n()
+  const { pushHistory } = useContext(MountPointContext)
+  const flowState = flow.getState()
+  const { error, running } = flowState
+  const shouldDisplayError = tabSpecs.configuration.errorShouldBeDisplayed(
+    error,
+    flowState
+  )
+  const hasLoginError = error && error.isLoginError()
+
+  return (
+    <div className="u-stack-m">
+      {shouldDisplayError && hasLoginError && (
+        <TriggerErrorInfo
+          className="u-mb-2"
+          error={error}
+          konnector={konnector}
+        />
+      )}
+      {!konnector.oauth ? (
+        <div>
+          <ListSubHeader>{t('modal.updateAccount.title')}</ListSubHeader>
+          <List dense className="u-pt-0">
+            <ListItem
+              className="u-mt-half u-c-pointer"
+              onClick={() => pushHistory(`/accounts/${account._id}/edit`)}
+            >
+              <ListItemIcon>
+                <Icon icon="lock" color={palette['coolGrey']} />
+              </ListItemIcon>
+              <ListItemText
+                primaryText={konnector.name}
+                secondaryText={Account.getAccountName(account)}
+              />
+              <ListItemSecondaryAction>
+                {running && <Spinner />}
+              </ListItemSecondaryAction>
+              <ListItemSecondaryAction>
+                <Icon
+                  className="u-mr-1"
+                  icon="right"
+                  color={palette['coolGrey']}
+                />
+              </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+        </div>
+      ) : null}
+      <div className="u-flex u-flex-row">
+        <DeleteAccountButton
+          account={account}
+          disabled={running}
+          onSuccess={onAccountDeleted}
+        />
+        <Button
+          onClick={addAccount}
+          label={t('modal.addAccount.button')}
+          theme="ghost"
+        />
+      </div>
+    </div>
+  )
+}
+
+ConfigurationTab.propTypes = {
+  konnector: PropTypes.object.isRequired,
+  account: PropTypes.object.isRequired,
+  error: PropTypes.object,
+  addAccount: PropTypes.func.isRequired,
+  onAccountDeleted: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired
+}
+
+export default ConfigurationTab;
