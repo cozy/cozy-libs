04d954a5ffdbe2320949146e641d610fefef5979
Patrick Browne <pt.browne@gmail.com>
2020-08-21 17:45:11 +0200

    test: Deactivate cozy-logger during tests

--- packages/cozy-sharing/jestHelpers/setup.js
+++ packages/cozy-sharing/jestHelpers/setup.js
@@ -1,4 +1,7 @@
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
+jest.mock('cozy-logger', () => ({
+  namespace: () => () => {}
+}))
 Enzyme.configure({ adapter: new Adapter() })
