b4533715582a9cb655685e205909ce7f921aab99
Patrick Browne <pt.browne@gmail.com>
2020-08-25 14:34:18 +0200

    test: Ignore componentWillMount and componentWillUpdate warnings from react-final-form

--- packages/cozy-harvest-lib/test/jest.setup.js
+++ packages/cozy-harvest-lib/test/jest.setup.js
@@ -8,3 +8,22 @@ minilog.suggest.deny('harvest', 'warn')
 minilog.suggest.deny('harvest', 'error')
 
 configure({ adapter: new Adapter() })
+
+const originalConsoleWarn = console.warn // eslint-disable-line no-console
+// eslint-disable-next-line no-console
+console.warn = function(message) {
+  if (
+    /**
+     * We cannot control ReactFinalForm, thus we ignore the warning, this can be
+     * removed when ReactFinalForm is updated and does not use componentWillMount
+     * and componentWillUpdate
+     */
+    (message.startsWith('Warning: componentWillMount has been renamed') ||
+      message.startsWith('Warning: componentWillUpdate has been renamed')) &&
+    message.endsWith('Please update the following components: ReactFinalForm')
+  ) {
+    return
+  } else {
+    return originalConsoleWarn.apply(this, arguments)
+  }
+}
