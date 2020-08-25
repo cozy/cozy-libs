d49ce557c08d868f0e941d26ab47659ba9198ce8
Patrick Browne <pt.browne@gmail.com>
2020-08-25 14:19:11 +0200

    feat: Deactivate logging for harvest during tests

--- packages/cozy-harvest-lib/test/jest.setup.js
+++ packages/cozy-harvest-lib/test/jest.setup.js
@@ -1,5 +1,10 @@
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import '@testing-library/jest-dom/extend-expect'
+import 'isomorphic-fetch'
+import minilog from 'minilog'
+
+minilog.suggest.deny('harvest', 'warn')
+minilog.suggest.deny('harvest', 'error')
 
 configure({ adapter: new Adapter() })
