428726181fb3d8a1bad629e72113113594854603
Yannick Lohse <contact@yannick-lohse.fr>
2020-08-27 15:36:26 +0200

    refactor: Simplify AppLike test component

--- packages/cozy-sharing/test/AppLike.jsx
+++ packages/cozy-sharing/test/AppLike.jsx
@@ -3,18 +3,12 @@ import { I18n } from 'cozy-ui/transpiled/react'
 import { CozyProvider, createMockClient } from 'cozy-client'
 import langEn from '../locales/en.json'
 
-export const TestI18n = ({ children, client }) => {
-  return (
-    <I18n lang={'en'} dictRequire={() => langEn}>
-      <CozyProvider client={client || createMockClient({})}>
-        {children}
-      </CozyProvider>
-    </I18n>
-  )
-}
-
 const AppLike = ({ children, client }) => (
-  <TestI18n client={client}>{children}</TestI18n>
+  <I18n lang={'en'} dictRequire={() => langEn}>
+    <CozyProvider client={client || createMockClient({})}>
+      {children}
+    </CozyProvider>
+  </I18n>
 )
 
 export default AppLike
