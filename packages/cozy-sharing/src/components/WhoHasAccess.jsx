732260ed94a51fe30832bdb7fb1b084f7216d78c
Yannick Lohse <contact@yannick-lohse.fr>
2020-08-27 11:58:25 +0200

    refactor: Class component to function

--- packages/cozy-sharing/src/components/WhoHasAccess.jsx
+++ packages/cozy-sharing/src/components/WhoHasAccess.jsx
@@ -1,36 +1,31 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import Recipient from './Recipient'
 
-class WhoHasAccess extends PureComponent {
-  render() {
-    const {
-      isOwner = false,
-      recipients,
-      document,
-      documentType,
-      onRevoke,
-      className,
-      onRevokeSelf
-    } = this.props
+const WhoHasAccess = ({
+  isOwner = false,
+  recipients,
+  document,
+  documentType,
+  onRevoke,
+  className,
+  onRevokeSelf
+}) => (
+  <div className={className}>
+    {recipients.map(recipient => (
+      <Recipient
+        {...recipient}
+        key={`key_r_${recipient.index}`}
+        isOwner={isOwner}
+        document={document}
+        documentType={documentType}
+        onRevoke={onRevoke}
+        onRevokeSelf={onRevokeSelf}
+      />
+    ))}
+  </div>
+)
 
-    return (
-      <div className={className}>
-        {recipients.map(recipient => (
-          <Recipient
-            {...recipient}
-            key={`key_r_${recipient.index}`}
-            isOwner={isOwner}
-            document={document}
-            documentType={documentType}
-            onRevoke={onRevoke}
-            onRevokeSelf={onRevokeSelf}
-          />
-        ))}
-      </div>
-    )
-  }
-}
 WhoHasAccess.propTypes = {
   isOwner: PropTypes.bool,
   recipients: PropTypes.array.isRequired,
