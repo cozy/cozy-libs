fcf0c827db9fbfe9a607e85614c7799442f2f9c6
Patrick Browne <pt.browne@gmail.com>
2020-09-02 13:16:04 +0200

    feat: Make the KonnectorModalHeader smaller when there is no children
    Since there is not enough content to the right to balance its size,
    we need to reduce the image's size

--- packages/cozy-harvest-lib/src/components/KonnectorModalHeader.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorModalHeader.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import cx from 'classnames'
 import { ModalHeader } from 'cozy-ui/transpiled/react/Modal'
 import KonnectorIcon from './KonnectorIcon'
 import { Media, Img } from 'cozy-ui/transpiled/react/Media'
@@ -8,7 +9,12 @@ const KonnectorModalHeader = ({ konnector, children }) => {
   return (
     <ModalHeader className="u-pr-2">
       <Media>
-        <Img className="u-w-3 u-h-3 u-mr-1">
+        <Img
+          className={cx('u-mr-1', {
+            'u-w-3 u-h-3': children,
+            'u-w-2 u-h-2': !children
+          })}
+        >
           <KonnectorIcon konnector={konnector} />
         </Img>
         <div className="u-flex-grow-1 u-mr-1">
