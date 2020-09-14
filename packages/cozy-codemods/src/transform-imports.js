d4986af31b6f250f1fd56938ab9ff7eaea72422b
Patrick Browne <pt.browne@gmail.com>
2020-09-14 10:29:01 +0200

    docs: Add docstrings for transformImports and mergeImports

--- packages/cozy-codemods/src/transform-imports.js
+++ packages/cozy-codemods/src/transform-imports.js
@@ -1,6 +1,26 @@
 const groupBy = require('lodash/groupBy')
 const flatten = require('lodash/flatten')
 
+/**
+ * Merge imports with the same source.
+ *
+ * @param  {Object} j - The jscodeshift API
+ * @param  {Object} root - The jscodeshift root node
+ *
+ * @example
+ * Before:
+ *
+ * ```
+ * import { A } from 'my-package'
+ * import { B } from 'my-package'
+ * ```
+ *
+ * After:
+ *
+ * ```
+ * import { A, B } from 'my-package'
+ * ```
+ */
 const mergeImports = (j, root) => {
   const imports = root.find(j.ImportDeclaration).paths()
   const groupedBySource = groupBy(imports, path => path.node.source.value)
@@ -16,6 +36,49 @@ const mergeImports = (j, root) => {
   }
 }
 
+/**
+ * Transform import statements so that they match paths passed in options.
+ *
+ * @param  {Object} j - The jscodeshift API
+ * @param  {Object} root - The jscodeshift root node
+ * @param  {Object} options
+ *
+ * @example
+ *
+ * ```jsx
+ * // See https://github.com/facebook/jscodeshift
+ * transformImports(j, root, {
+ *   imports: {
+ *     Dialog: {
+ *       importPath: 'cozy-ui/transpiled/react/Dialog',
+ *       defaultImport: true
+ *     },
+ *     DialogFile: {
+ *       importPath: 'cozy-ui/transpiled/react/Dialog',
+ *       defaultImport: false
+ *     },
+ *     DialogContent: {
+ *       importPath: 'cozy-ui/transpiled/react/Dialog',
+ *       defaultImport: false
+ *     }
+ *   }
+ * })
+ * ```
+ *
+ * Before:
+ *
+ * ```
+ * import DialogFile from 'cozy-ui/transpiled/react/DialogFile'
+ * import DialogContent from 'cozy-ui/transpiled/react/DialogContent'
+ * import Dialog from 'cozy-ui/transpiled/react/Dialog'
+ * ```
+ *
+ * After:
+ *
+ * ```
+ * import Dialog, { DialogFile, DialogContent } from "cozy-ui/transpiled/react/Dialog";
+ * ```
+ */
 const transformImports = (j, root, options) => {
   const imports = options.imports
   const identifiers = Object.keys(imports)
