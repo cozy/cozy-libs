257c37a4b2f2bc8cca9937eb6d19d09072f636cc
doubleface <christophe@cozycloud.cc>
2020-09-29 16:47:08 +0200

    fix(Harvest): Display disconnect button for OAuth connectors

---
+++ packages/cozy-harvest-lib/src/components/KonnectorConfiguration/ConfigurationTab/index.spec.js
@@ -0,0 +1,63 @@
+/* eslint no-console: off */
+
+import { render } from '@testing-library/react'
+import ConfigurationTab from './index'
+import React from 'react'
+import I18n from 'cozy-ui/transpiled/react/I18n'
+import enLocale from '../../../locales/en.json'
+import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
+import { MountPointProvider } from '../../../components/MountPointContext'
+
+describe('ConfigurationTab', () => {
+  let originalWarn
+  beforeEach(() => {
+    originalWarn = console.warn
+    console.warn = function(msg) {
+      if (msg && msg.includes && msg.includes('componentWillReceiveProps')) {
+        return
+      }
+      return originalWarn.apply(this, arguments)
+    }
+  })
+
+  afterEach(() => {
+    console.warn = originalWarn
+  })
+
+  function setup({ konnector = {} } = {}) {
+    const account = {}
+    const addAccount = jest.fn()
+    const onAccountDeleted = jest.fn()
+    const flow = {
+      getState: jest.fn().mockReturnValue({
+        error: null,
+        running: true
+      })
+    }
+    const root = render(
+      <MountPointProvider baseRoute="/">
+        <BreakpointsProvider>
+          <I18n lang="en" dictRequire={() => enLocale}>
+            <ConfigurationTab
+              konnector={konnector}
+              account={account}
+              addAccount={addAccount}
+              onAccountDeleted={onAccountDeleted}
+              flow={flow}
+            />
+          </I18n>
+        </BreakpointsProvider>
+      </MountPointProvider>
+    )
+    return { root }
+  }
+
+  it('should render', () => {
+    const { root } = setup()
+    expect(root.getByText('Identifiers')).toBeTruthy()
+  })
+  it('should not render identifiers for oauth konnectors', () => {
+    const { root } = setup({ konnector: { oauth: true } })
+    expect(root.queryByText('Identifiers')).toBe(null)
+  })
+})
