a79355f3f2578175308efad9c1651a8433e9169f
Quentin Valmori <quentin.valmori@gmail.com>
2020-11-03 18:30:31 +0100

    fix: Split Dialog in two
    One that allow cozy to cozy sharing and sharing by link
    One for a cozy by link only (needed for Albums and Notes for instance)
    
    Since they don't have the same content, we don't have them to behave
    the same way.
    
    For cozybylink only, we have a Dialog without actions

---
+++ packages/cozy-sharing/src/components/ShareDialogOnlyByLink.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+
+import { default as DumbShareByLink } from './ShareByLink'
+
+const ShareDialogOnlyByLink = ({
+  onClose,
+  documentType,
+  document,
+  permissions,
+  link,
+  onShareByLink,
+  onRevokeLink,
+  onUpdateShareLinkPermissions
+}) => {
+  const { t } = useI18n()
+  return (
+    <Dialog
+      opened={true}
+      onClose={onClose}
+      title={t(`${documentType}.share.title`, { name: document.name })}
+      content={
+        <DumbShareByLink
+          document={document}
+          permissions={permissions}
+          documentType={documentType}
+          checked={link !== null}
+          link={link}
+          onEnable={onShareByLink}
+          onDisable={onRevokeLink}
+          onChangePermissions={onUpdateShareLinkPermissions}
+        />
+      }
+    />
+  )
+}
+export default ShareDialogOnlyByLink
