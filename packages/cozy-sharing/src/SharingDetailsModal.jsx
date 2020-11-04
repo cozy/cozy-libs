789ad0b3af6837d0e0f32bfff5623bf44e054725
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-11-04 11:17:50 +0100

    refactor: SharingDetailsModal use new CozyDialogs

--- packages/cozy-sharing/src/SharingDetailsModal.jsx
+++ packages/cozy-sharing/src/SharingDetailsModal.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
-import Modal, { ModalContent } from 'cozy-ui/transpiled/react/Modal'
+import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import { CardDivider } from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
 
 import { OwnerIdentity } from './components/Recipient'
 import WhoHasAccess from './components/WhoHasAccess'
@@ -20,56 +21,56 @@ export const SharingDetailsModal = ({
   onRevokeSelf
 }) => {
   const { t, f } = useI18n()
+
   return (
-    <Modal
-      title={t(`${documentType}.share.details.title`)}
-      dismissAction={onClose}
+    <Dialog
+      open={true}
+      onClose={onClose}
       className={styles['share-modal']}
-      into="body"
-      size="small"
-      mobileFullscreen
-    >
-      <ModalContent className={styles['share-modal-content']}>
-        <div className={styles['share-details']}>
-          <OwnerIdentity
-            name={t(`${documentType}.share.sharedBy`, {
-              name: getDisplayName(owner)
+      title={t(`${documentType}.share.details.title`)}
+      content={
+        <div className={styles['share-modal-content']}>
+          <div className={styles['share-details']}>
+            <OwnerIdentity
+              name={t(`${documentType}.share.sharedBy`, {
+                name: getDisplayName(owner)
+              })}
+              url={owner.instance}
+            />
+            <div className={styles['share-details-created']}>
+              {t(`${documentType}.share.details.createdAt`, {
+                date: f(document.created_at || null, 'Do MMMM YYYY')
+              })}
+            </div>
+            <div className={styles['share-details-perm']}>
+              {t(
+                `${documentType}.share.details.${
+                  sharingType === 'one-way' ? 'ro' : 'rw'
+                }`
+              )}
+            </div>
+            <div className={styles['share-details-perm-desc']}>
+              {t(
+                `${documentType}.share.details.desc.${
+                  sharingType === 'one-way' ? 'ro' : 'rw'
+                }`
+              )}
+            </div>
+          </div>
+          <CardDivider />
+          <WhoHasAccess
+            title={t('Share.recipients.accessCount', {
+              count: recipients.length
             })}
-            url={owner.instance}
+            recipients={recipients}
+            document={document}
+            documentType={documentType}
+            onRevoke={onRevoke}
+            onRevokeSelf={onRevokeSelf}
           />
-          <div className={styles['share-details-created']}>
-            {t(`${documentType}.share.details.createdAt`, {
-              date: f(document.created_at || null, 'Do MMMM YYYY')
-            })}
-          </div>
-          <div className={styles['share-details-perm']}>
-            {t(
-              `${documentType}.share.details.${
-                sharingType === 'one-way' ? 'ro' : 'rw'
-              }`
-            )}
-          </div>
-          <div className={styles['share-details-perm-desc']}>
-            {t(
-              `${documentType}.share.details.desc.${
-                sharingType === 'one-way' ? 'ro' : 'rw'
-              }`
-            )}
-          </div>
         </div>
-        <hr className={styles['divider']} />
-        <WhoHasAccess
-          title={t('Share.recipients.accessCount', {
-            count: recipients.length
-          })}
-          recipients={recipients}
-          document={document}
-          documentType={documentType}
-          onRevoke={onRevoke}
-          onRevokeSelf={onRevokeSelf}
-        />
-      </ModalContent>
-    </Modal>
+      }
+    />
   )
 }
 
