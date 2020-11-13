3f40ef5e2892ce01d096bea8edc4a11924105004
doubleface <christophe@cozycloud.cc>
2020-11-13 15:36:26 +0100

    feat(Harvest): Ignore soft deleted ciphers

--- packages/cozy-harvest-lib/src/components/VaultCiphersList/VaultCiphersList.spec.jsx
+++ packages/cozy-harvest-lib/src/components/VaultCiphersList/VaultCiphersList.spec.jsx
@@ -6,7 +6,7 @@ import I18n from 'cozy-ui/transpiled/react/I18n'
 jest.mock('cozy-ui/transpiled/react/AppIcon', () => () => null)
 
 describe('when there is some ciphers', () => {
-  it('should handler cipher select', async () => {
+  it('should handle cipher select', async () => {
     const onSelect = jest.fn()
     const ciphers = [
       {
@@ -33,6 +33,40 @@ describe('when there is some ciphers', () => {
     fireEvent.click(node)
 
     expect(onSelect).toHaveBeenCalledWith(ciphers[0])
+  })
+
+  it('should ignore trashed ciphers', async () => {
+    const onSelect = jest.fn()
+    const ciphers = [
+      {
+        id: 'alan',
+        name: 'Alan',
+        login: {
+          username: 'isabelledurand'
+        }
+      },
+      {
+        id: 'trashed',
+        name: 'trash',
+        login: {
+          username: 'toignore'
+        },
+        deletedDate: '2020-11-03'
+      }
+    ]
+    const { queryByText } = render(
+      <I18n lang="en" dictRequire={() => {}}>
+        <DumbVaultCiphersList
+          konnector={{ vendor_link: 'https://alan.com' }}
+          ciphers={ciphers}
+          onSelect={onSelect}
+          t={key => key}
+        />
+      </I18n>
+    )
+
+    const node = await queryByText('toignore')
+    expect(node).toBe(null)
   })
 
   it('should handle "other account" select', async () => {
