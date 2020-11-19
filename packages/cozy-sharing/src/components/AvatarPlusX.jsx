b2b71ff421edb21f6c440bce4ca42c3c48db2d1b
JF-Cozy <julien.ficheux@cozycloud.cc>
2020-11-19 12:26:10 +0100

    fix: Tooltip on sharingAvatars show only the row recipients

--- packages/cozy-sharing/src/components/AvatarPlusX.jsx
+++ packages/cozy-sharing/src/components/AvatarPlusX.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import uniqueId from 'lodash/uniqueId'
 
 import Avatar from 'cozy-ui/transpiled/react/Avatar'
 
 import { SharingTooltip, TooltipRecipientList } from './Tooltip'
 
-const AvatarPlusX = ({ className, size, extraRecipients = [] }) => (
-  <span data-tip data-for="extra-recipients-avatar" className="u-db">
-    <Avatar
-      className={className}
-      size={size}
-      text={`+${Math.min(extraRecipients.length, 99)}`}
-      style={{
-        backgroundColor: 'var(--genericRecipientBackground)',
-        color: 'var(--genericRecipientColor)'
-      }}
-    />
-    <SharingTooltip id="extra-recipients-avatar">
-      <TooltipRecipientList recipientNames={extraRecipients} />
-    </SharingTooltip>
-  </span>
-)
+const AvatarPlusX = ({ className, size, extraRecipients = [] }) => {
+  const sharingTooltipId = uniqueId('extra-recipients-avatar-')
+
+  return (
+    <span data-tip data-for={sharingTooltipId} className="u-db">
+      <Avatar
+        className={className}
+        size={size}
+        text={`+${Math.min(extraRecipients.length, 99)}`}
+        style={{
+          backgroundColor: 'var(--genericRecipientBackground)',
+          color: 'var(--genericRecipientColor)'
+        }}
+      />
+      <SharingTooltip id={sharingTooltipId}>
+        <TooltipRecipientList recipientNames={extraRecipients} />
+      </SharingTooltip>
+    </span>
+  )
+}
 
 AvatarPlusX.propTypes = {
   extraRecipients: PropTypes.arrayOf(PropTypes.string),
