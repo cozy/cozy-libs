6e9536017ee6f5139747b9413e2823e5862418d8
Patrick Browne <pt.browne@gmail.com>
2020-11-24 17:49:51 +0100

    test: Use @testing-library/react to test TwoFAModal
    - @testing-library/react is now the preferred way of testing components
    at Cozy since it encourages tests that are less focused on the
    implementation of the components
    - Here since we do not have to setState, we can import the TwoFAModal
    that is exported by default, thus we test the withLocales behavior
    (previously, t was injected by hand in the component during tests)

--- packages/cozy-harvest-lib/src/components/TwoFAModal.spec.jsx
+++ packages/cozy-harvest-lib/src/components/TwoFAModal.spec.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
-import { mount } from 'enzyme'
-import { TwoFAModal } from './TwoFAModal'
+import { render, fireEvent } from '@testing-library/react'
+import I18n from 'cozy-ui/transpiled/react/I18n'
+
+import TwoFAModal from './TwoFAModal'
 import ConnectionFlow from '../models/ConnectionFlow'
-import { Text, Field } from 'cozy-ui/transpiled/react'
-import en from '../locales/en.json'
-import Polyglot from 'node-polyglot'
 
 jest.mock('./KonnectorIcon', () => () => null)
 
 describe('TwoFAModal', () => {
-  const polyglot = new Polyglot()
-  polyglot.extend(en)
-
   const setup = ({ konnectorSlug, account }) => {
     const client = {
       on: jest.fn()
@@ -23,26 +19,21 @@ describe('TwoFAModal', () => {
     flow.getAccount = () => account
     flow.getKonnectorSlug = () => konnectorSlug
 
-    const root = mount(
-      <TwoFAModal
-        dismissAction={jest.fn()}
-        flow={flow}
-        t={polyglot.t.bind(polyglot)}
-        breakpoints={{ isMobile: true }}
-        account={account}
-        client={client}
-      />
+    const root = render(
+      <I18n dictRequire={() => {}} lang="en">
+        <TwoFAModal
+          dismissAction={jest.fn()}
+          flow={flow}
+          breakpoints={{ isMobile: true }}
+          account={account}
+          client={client}
+        />
+      </I18n>
     )
     return { root, flow }
   }
 
-  const getDesc = root => root.find(Text).text()
-  const getFieldLabel = root => root.find(Field).props().label
-  const getInputValue = root =>
-    root
-      .find('input')
-      .props()
-      .value.toString()
+  const getInputValue = root => root.getByPlaceholderText('').value
 
   it('should work even with unknown 2FA', () => {
     const opts = {
@@ -54,10 +45,10 @@ describe('TwoFAModal', () => {
     const { root } = setup(opts)
 
     // show an input by default
-    expect(root.find('input').length).toBe(1)
-    expect(root.text()).toContain(
-      'This code enables you to finish your connexion.'
-    )
+    expect(root.getByPlaceholderText('')).toBeTruthy()
+    expect(
+      root.getByText('This code enables you to finish your connexion.')
+    ).toBeTruthy()
   })
 
   it('should work', () => {
@@ -69,15 +60,12 @@ describe('TwoFAModal', () => {
     }
     const { root } = setup(opts)
     // no input for app two fa
-    expect(root.find('input').length).toBe(0)
-    expect(root.text()).toContain(
-      `You need to open your provider's app and/or click on a notification to confirm your authentication.`
+    expect(root.queryByPlaceholderText('')).toBeFalsy()
+    expect(
+      root.getByText(
+        `You need to open your provider's app and/or click on a notification to confirm your authentication.`
+      )
     )
-  })
-
-  it('should correctly unmount', () => {
-    const { root } = setup({})
-    expect(() => root.instance().componentWillUnmount()).not.toThrow()
   })
 
   it('should work for several two fa requests', () => {
@@ -88,39 +76,42 @@ describe('TwoFAModal', () => {
       konnectorSlug: 'boursoma83'
     }
     const { root, flow } = setup(opts)
-    const inp = root.find('input')
 
-    expect(inp.length).toBe(1)
-    expect(getInputValue(root)).toBe('')
-    expect(getDesc(root)).toBe(
-      'This code enables you to finish your connexion.'
-    )
-    expect(getFieldLabel(root)).toContain('code')
-    root.setState({ twoFACode: 'abcd' })
-    root.update()
-    expect(getInputValue(root)).toBe('abcd')
+    const input = root.getByPlaceholderText('')
+    expect(input).toBeTruthy()
+
+    // expect(inp.length).toBe(1)
+    // expect((root)).toBe('')
+    expect(
+      root.getByText('This code enables you to finish your connexion.')
+    ).toBeTruthy()
+
+    expect(root.getByText('code')).toBeTruthy()
+
+    fireEvent.change(input, { target: { value: 'abcd' } })
 
     // 2nd 2FA request
     flow.emit('twoFARequest')
-    root.update()
+
     expect(getInputValue(root)).toBe('')
-    expect(getDesc(root)).toBe(
-      'The second code received on your mobile phone or by email enables you to finalize your connexion.'
-    )
-    expect(getFieldLabel(root)).toBe('Second code')
+    expect(
+      root.getByText(
+        'The second code received on your mobile phone or by email enables you to finalize your connexion.'
+      )
+    ).toBeTruthy()
+
+    expect(root.getByText('Second code')).toBeTruthy()
     flow.emit('twoFARequest')
-    root.update()
 
     // 3rd 2FA (should not happen, hypothetical case)
     flow.emit('twoFARequest')
-    root.update()
+
     expect(getInputValue(root)).toBe('')
     // First attempt translations are re-used
-    expect(getDesc(root)).toContain(
-      'This code enables you to finish your connexion.'
-    )
-    expect(getFieldLabel(root)).toBe('code (4)')
+    expect(
+      root.getByText('This code enables you to finish your connexion.')
+    ).toBeTruthy()
+    expect(root.getByText('code (4)')).toBeTruthy()
     flow.emit('twoFARequest')
-    root.update()
   })
 })
