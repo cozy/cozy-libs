ba7b783e48c57e40d04045c0b918093755348251
Patrick Browne <pt.browne@gmail.com>
2020-11-26 10:33:58 +0100

    feat: Use svgr icons supports ButtonLink and Avatar

--- packages/cozy-codemods/src/transforms/replace-icons-svgr.js
+++ packages/cozy-codemods/src/transforms/replace-icons-svgr.js
@@ -11,32 +11,42 @@ export default function replaceSvgrIcons(file, api) {
   const j = api.jscodeshift
   const root = j(file.source)
 
+  const transform = path => {
+    const iconAttr = path.node.attributes.find(
+      attr => attr.name && attr.name.name === 'icon'
+    )
+
+    if (iconAttr && iconAttr.value.type === 'Literal') {
+      const componentName = iconNameToComponentName(iconAttr.value.value)
+
+      imports.ensure(
+        root,
+        {
+          default: `${componentName}Icon`
+        },
+        `cozy-ui/transpiled/react/Icons/${componentName}`
+      )
+      iconAttr.value = j.jsxExpressionContainer(
+        j.jsxIdentifier(`${componentName}Icon`)
+      )
+    }
+  }
+
+  root
+    .find(j.JSXOpeningElement, {
+      name: {
+        name: 'Avatar'
+      }
+    })
+    .forEach(transform)
+
   root
     .find(j.JSXOpeningElement, {
       name: {
         name: 'Icon'
       }
     })
-    .forEach(path => {
-      const iconAttr = path.node.attributes.find(
-        attr => attr.name.name === 'icon'
-      )
-
-      if (iconAttr.value.type === 'Literal') {
-        const componentName = iconNameToComponentName(iconAttr.value.value)
-
-        imports.ensure(
-          root,
-          {
-            default: `${componentName}Icon`
-          },
-          `cozy-ui/transpiled/react/Icons/${componentName}`
-        )
-        iconAttr.value = j.jsxExpressionContainer(
-          j.jsxIdentifier(`${componentName}Icon`)
-        )
-      }
-    })
+    .forEach(transform)
 
   root
     .find(j.JSXOpeningElement, {
@@ -44,31 +54,15 @@ export default function replaceSvgrIcons(file, api) {
         name: 'Button'
       }
     })
-    .forEach(path => {
-      const iconAttr = path.node.attributes.find(
-        attr => attr.name && attr.name.name === 'icon'
-      )
+    .forEach(transform)
 
-      if (iconAttr && iconAttr.value.type === 'Literal') {
-        const componentName = iconNameToComponentName(iconAttr.value.value)
-
-        imports.ensure(
-          root,
-          {
-            default: `${componentName}Icon`
-          },
-          `cozy-ui/transpiled/react/Icons/${componentName}`
-        )
-        imports.ensure(
-          root,
-          {
-            default: `Icon`
-          },
-          `cozy-ui/transpiled/react/Icon`
-        )
-        iconAttr.value = `{<Icon icon={${componentName}Icon} />}`
+  root
+    .find(j.JSXOpeningElement, {
+      name: {
+        name: 'ButtonLink'
       }
     })
+    .forEach(transform)
 
   return root.toSource()
 }
