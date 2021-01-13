ae9853643abfe130ae2ec8a65afbf4b292b78925
Patrick Browne <pt.browne@gmail.com>
2021-01-13 09:07:59 +0100

    refactor: Use hook to access client

--- packages/cozy-harvest-lib/src/components/HarvestVaultProvider/index.jsx
+++ packages/cozy-harvest-lib/src/components/HarvestVaultProvider/index.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { withClient } from 'cozy-client'
+import { useClient } from 'cozy-client'
 import { VaultProvider } from 'cozy-keys-lib'
 
-class HarvestVaultProvider extends React.PureComponent {
-  render() {
-    const { children, client } = this.props
-
-    const vaultInstanceUrl = client.getStackClient().uri
-
-    return <VaultProvider instance={vaultInstanceUrl}>{children}</VaultProvider>
-  }
+const HarvestVaultProvider = props => {
+  const client = useClient()
+  const { children } = props
+  const vaultInstanceUrl = client.getStackClient().uri
+  return <VaultProvider instance={vaultInstanceUrl}>{children}</VaultProvider>
 }
 
-HarvestVaultProvider.propTypes = {
-  client: PropTypes.object
-}
-
-export default withClient(HarvestVaultProvider)
+export default React.memo(HarvestVaultProvider)
