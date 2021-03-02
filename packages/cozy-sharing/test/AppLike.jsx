0d644b8656318df3afbed9f9bf00ce95b20fbdf3
JF-Cozy <julien.ficheux@cozycloud.cc>
2021-03-02 15:49:18 +0100

    feat: Update to muiv4 for cozy-sharing
    BREAKING CHANGE: The application needs to use MUI v4 to works properly with this version of cozy-sharing

--- packages/cozy-sharing/test/AppLike.jsx
+++ packages/cozy-sharing/test/AppLike.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+
+import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import { I18n } from 'cozy-ui/transpiled/react'
 import { CozyProvider, createMockClient } from 'cozy-client'
+
 import langEn from '../locales/en.json'
 
 const AppLike = ({ children, client }) => (
-  <I18n lang={'en'} dictRequire={() => langEn}>
-    <CozyProvider client={client || createMockClient({})}>
-      {children}
-    </CozyProvider>
-  </I18n>
+  <BreakpointsProvider>
+    <I18n lang={'en'} dictRequire={() => langEn}>
+      <CozyProvider client={client || createMockClient({})}>
+        {children}
+      </CozyProvider>
+    </I18n>
+  </BreakpointsProvider>
 )
 
 export default AppLike
