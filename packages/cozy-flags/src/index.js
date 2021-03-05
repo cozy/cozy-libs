cb506f14f4761cbd2d9b20652c45c77587444c96
Paul Tran-Van <paul@cozycloud.cc>
2021-03-05 17:59:02 +0100

    fix: Remove useless block for node
    See https://github.com/cozy/cozy-libs/issues/1272

--- packages/cozy-flags/src/index.js
+++ packages/cozy-flags/src/index.js
@@ -2,11 +2,6 @@
 
 import flag from './flag'
 
-if (typeof window !== 'undefined') {
-  flag.connect = require('./connect').default
-  flag.FlagSwitcher = require('./FlagSwitcher').default
-}
-
 if (typeof __ENABLED_FLAGS__ !== 'undefined') {
   flag.enable(__ENABLED_FLAGS__)
 }
