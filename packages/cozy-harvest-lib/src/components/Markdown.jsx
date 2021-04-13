f193cb074cc9889d7fd6858227c51e421840c790
Patrick Browne <pt.browne@gmail.com>
2021-04-13 15:25:32 +0200

    fix: Open external links in new page
    React markdown no longer supports Link renderer but has linkTarget prop

--- packages/cozy-harvest-lib/src/components/Markdown.jsx
+++ packages/cozy-harvest-lib/src/components/Markdown.jsx
@@ -3,17 +3,15 @@ import ReactMarkdown from 'react-markdown'
 
 export const reactMarkdownRendererOptions = {
   // eslint-disable-next-line react/display-name
-  Link: props => (
-    <a href={props.href} rel="noopener noreferrer" target="_blank">
-      {props.children}
-    </a>
-  ),
-  // eslint-disable-next-line react/display-name
   paragraph: props => <span className="u-db u-mv-0">{props.children}</span>
 }
 
 export const Markdown = ({ source }) => (
-  <ReactMarkdown source={source} renderers={reactMarkdownRendererOptions} />
+  <ReactMarkdown
+    source={source}
+    linkTarget="_blank"
+    renderers={reactMarkdownRendererOptions}
+  />
 )
 
 export default Markdown
