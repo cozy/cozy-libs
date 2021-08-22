8435795f77b783f20b87caafb7688594546274bd
Yannick Chiron <yannick.chiron@gmail.com>
2021-08-22 16:53:24 +0200

    feat: Implement twoStepsConfirmation in `WhoHasAccess`
    When `WhoHasAccess` receives a list of  recipients that needs
    confirmation for their sharing, then it should display a warning to the
    user and pass those recipients' confirmation data to the corresponding
    `Recipients`

--- packages/cozy-sharing/src/components/WhoHasAccess.jsx
+++ packages/cozy-sharing/src/components/WhoHasAccess.jsx
@@ -1,37 +1,74 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Recipient from './Recipient'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+
+/**
+ * Displays a warning if some contacts are waiting for confirmation of their sharing
+ *
+ * This may happen in two steps confirmation scenario like when sharing passwords
+ */
+const RecipientWaitingForConfirmationAlert = ({ recipientsToBeConfirmed }) => {
+  const { t } = useI18n()
+
+  if (recipientsToBeConfirmed.length > 0) {
+    return t(`Share.twoStepsConfirmation.contactAreWaitingForConfirmation`, {
+      smart_count: recipientsToBeConfirmed.length
+    })
+  }
+
+  return null
+}
 
 const WhoHasAccess = ({
   isOwner = false,
   recipients,
+  recipientsToBeConfirmed,
   document,
   documentType,
   onRevoke,
   className,
-  onRevokeSelf
+  onRevokeSelf,
+  rejectRecipient,
+  confirmRecipient
 }) => (
   <div className={className}>
-    {recipients.map(recipient => (
-      <Recipient
-        {...recipient}
-        key={`key_r_${recipient.index}`}
-        isOwner={isOwner}
-        document={document}
-        documentType={documentType}
-        onRevoke={onRevoke}
-        onRevokeSelf={onRevokeSelf}
-      />
-    ))}
+    <RecipientWaitingForConfirmationAlert
+      recipientsToBeConfirmed={recipientsToBeConfirmed}
+    />
+
+    {recipients.map(recipient => {
+      const recipientConfirmationData = recipientsToBeConfirmed.find(
+        user => user.email === recipient.email
+      )
+
+      return (
+        <Recipient
+          {...recipient}
+          key={`key_r_${recipient.index}`}
+          isOwner={isOwner}
+          document={document}
+          documentType={documentType}
+          onRevoke={onRevoke}
+          onRevokeSelf={onRevokeSelf}
+          recipientConfirmationData={recipientConfirmationData}
+          rejectRecipient={rejectRecipient}
+          confirmRecipient={confirmRecipient}
+        />
+      )
+    })}
   </div>
 )
 
 WhoHasAccess.propTypes = {
   isOwner: PropTypes.bool,
   recipients: PropTypes.array.isRequired,
+  recipientsToBeConfirmed: PropTypes.array,
   document: PropTypes.object.isRequired,
   documentType: PropTypes.string.isRequired,
   onRevoke: PropTypes.func.isRequired,
-  onRevokeSelf: PropTypes.func
+  onRevokeSelf: PropTypes.func,
+  confirmRecipient: PropTypes.func,
+  rejectRecipient: PropTypes.func
 }
 export default WhoHasAccess
