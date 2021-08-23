bcdcd7a28df185604de04f9da1308a8ab9caaeb2
Yannick Chiron <yannick.chiron@gmail.com>
2021-08-23 15:08:23 +0200

    feat: Implement `CozyPassFingerprintDialogContent`
    When calling `ShareModal` from `cozy-pass-web` we would need to pass a
    dialog content that is specific to passwords
    
    `CozyPassFingerprintDialogContent` can be used to displays this
    specific content
    
    `snarkdown` node modules has been added in the project to handle
    markdown formatting it `CozyPassFingerprintDialogContent`

---
+++ packages/cozy-sharing/src/components/CozyPassFingerprintDialogContent.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Paper from 'cozy-ui/transpiled/react/Paper'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import snarkdown from 'snarkdown'
+
+import cx from 'classnames'
+import styles from './CozyPassFingerprintDialogContent.styl'
+
+/**
+ * CozyPassFingerprintDialogContent can be use as a content for ShareModal when
+ * using 2 steps confirmation
+ *
+ * This should be the case when ShareModal is called from `cozy-pass-web`
+ */
+export const CozyPassFingerprintDialogContent = ({
+  recipientConfirmationData
+}) => {
+  const { t } = useI18n()
+
+  const instruction1 = snarkdown(
+    t(`Organizations.fingerprint.instruction`, {
+      userName: recipientConfirmationData.name,
+      userEmail: recipientConfirmationData.email
+    })
+  )
+
+  const instruction2 = snarkdown(t(`Organizations.fingerprint.instruction2`))
+
+  return (
+    <div className={cx(styles['share-modal-content'])}>
+      <p dangerouslySetInnerHTML={{ __html: instruction1 }}></p>
+      <Paper
+        className={cx(styles['cozy-pass-fingerprint-modal__fingerprint'])}
+        elevation={1}
+      >
+        <Typography variant="body1">
+          {recipientConfirmationData.fingerprintPhrase}
+        </Typography>
+      </Paper>
+      <p dangerouslySetInnerHTML={{ __html: instruction2 }}></p>
+    </div>
+  )
+}
+
+CozyPassFingerprintDialogContent.propTypes = {
+  recipientConfirmationData: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    fingerprintPhrase: PropTypes.string.isRequired
+  })
+}
