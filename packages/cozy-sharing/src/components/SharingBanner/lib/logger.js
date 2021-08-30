b2c63a8332354f47ef53e92f7f0558a6387a0830
Antonin Cezard <acezard@gmail.com>
2021-08-30 10:14:00 +0200

    feat: Reorganize directories

--- packages/cozy-sharing/src/components/SharingBanner/lib/logger.js
+++
@@ -1,9 +0,0 @@
-import minilog from '@cozy/minilog'
-
-const logger = minilog(`cozy-${window.__APP_SLUG__ || 'app'}`)
-minilog.enable()
-
-minilog.suggest.allow(`cozy-${window.__APP_SLUG__ || 'app'}`, 'log')
-minilog.suggest.allow(`cozy-${window.__APP_SLUG__ || 'app'}`, 'info')
-
-export default logger
--- packages/cozy-sharing/src/components/SharingBanner/lib/react-cozy-helpers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-
-import modalReducer from './ModalManager'
-
-export default combineReducers({ modal: modalReducer })
-
-export { ModalManager, showModal } from './ModalManager'
-
-export { default as getQueryParameter } from './QueryParameter'
