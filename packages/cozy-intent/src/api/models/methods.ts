6e78ceb6122c1d41c3f0713324800bed18615107
Antonin Cezard <acezard@gmail.com>
2022-02-04 10:04:09 +0100

    feat: Add new method signature

--- packages/cozy-intent/src/api/models/methods.ts
+++ packages/cozy-intent/src/api/models/methods.ts
@@ -3,4 +3,5 @@ import { AppManifest } from './applications'
 export type NativeMethodsRegister = {
   logout: () => Promise<void>
   openApp: (href: string, app: AppManifest) => Promise<void>
+  backToHome: () => void
 }
