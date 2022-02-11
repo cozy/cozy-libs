d70d5abfc88ca3afed54eadd7faed1cfc437098d
Antonin Cezard <acezard@gmail.com>
2022-02-11 16:18:00 +0100

    chore: Add jest coverage config

---
+++ packages/cozy-intent/src/tests/jest.config.js
@@ -0,0 +1,19 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  collectCoverage: true,
+  collectCoverageFrom: ['./**/*.{ts,tsx}'],
+  coverageDirectory: './tests/coverage',
+  rootDir: '../',
+  testMatch: ['./**/*.spec.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+}
+
+module.exports = config
