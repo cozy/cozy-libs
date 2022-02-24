226dc4074cceea55c0a759b5d54ef7753b313754
Antonin Cezard <acezard@gmail.com>
2022-02-24 17:08:43 +0100

    feat(cozy-intent): Add new method signatures
    Will be exposed by the native application to
    allow changing interface color

--- packages/cozy-intent/src/api/models/methods.ts
+++ packages/cozy-intent/src/api/models/methods.ts
@@ -1,7 +1,9 @@
 import { AppManifest } from './applications'
 
 export type NativeMethodsRegister = {
+  backToHome: () => void
   logout: () => Promise<void>
   openApp: (href: string, app: AppManifest) => Promise<void>
-  backToHome: () => void
+  setNavBarColor: (color: string) => void
+  setStatusBarColor: (color: string) => void
 }
