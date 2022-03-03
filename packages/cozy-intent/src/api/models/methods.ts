214ad1e50139300881c16cc0a06bd8862a62d439
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-03-03 15:43:16 +0100

    feat(intent): Add  hide and show SplashScreen native methods in types

--- packages/cozy-intent/src/api/models/methods.ts
+++ packages/cozy-intent/src/api/models/methods.ts
@@ -6,4 +6,6 @@ export type NativeMethodsRegister = {
   openApp: (href: string, app: AppManifest) => Promise<void>
   setNavBarColor: (color: string) => void
   setStatusBarColor: (color: string) => void
+  hideSplashScreen: () => void
+  showSplashScreen: () => void
 }
