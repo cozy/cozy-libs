800bc0b6b5604cc08e46a636c5ffd3ccc933b91c
Merkur39 <merkkur39@gmail.com>
2022-03-28 11:55:09 +0200

    refactor: Replace stylus by JS styles
    as we don't compile them in cozy-libs

--- packages/cozy-mespapiers-lib/src/components/CompositeHeader/CompositeHeader.jsx
+++ packages/cozy-mespapiers-lib/src/components/CompositeHeader/CompositeHeader.jsx
@@ -2,20 +2,51 @@ import React, { useState, useEffect, isValidElement } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 import isArray from 'lodash/isArray'
+import { makeStyles } from '@material-ui/core/styles'
 
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import Icon, { iconPropType } from 'cozy-ui/transpiled/react/Icon'
 
-// import './styles.styl'
+const useStyles = makeStyles(() => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+    height: '100%',
+    maxWidth: '100%',
+    margin: '2rem 0 1rem',
+    '&.is-focused': {
+      height: 'initial'
+    },
+    '& img': {
+      width: 'fit-content',
+      margin: '0 auto'
+    }
+  },
+  image: {
+    '&--small': {
+      height: '4rem'
+    },
+    '&--medium': {
+      height: '6rem'
+    },
+    '&--large': {
+      height: '8rem'
+    }
+  }
+}))
 
 const CompositeHeaderImage = ({ isBitmap, src, iconSize }) => {
+  const styles = useStyles()
+
   return isBitmap ? (
     <img src={src} alt="illustration" />
   ) : (
     <div
       className={cx('u-pb-1', {
-        [`composite-header-img--${iconSize}`]: iconSize
+        [`${styles.image}--${iconSize}`]: iconSize
       })}
     >
       <Icon icon={src} size={'100%'} />
@@ -35,6 +66,8 @@ const CompositeHeader = ({
 }) => {
   const [imgScr, setImgSrc] = useState(null)
   const { isMobile } = useBreakpoints()
+  const styles = useStyles()
+
   const isPNG = icon && icon.endsWith('.png')
   const subFolder = isPNG ? 'images' : 'icons'
 
@@ -53,7 +86,7 @@ const CompositeHeader = ({
   }, [icon, fallbackIcon, subFolder])
 
   return (
-    <div className={cx('composite-header-container', className)} {...restProps}>
+    <div className={cx(styles.container, className)} {...restProps}>
       {imgScr && (
         <CompositeHeaderImage
           isBitmap={isPNG}
