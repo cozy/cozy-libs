54b6ffff9f8275804246e0033124871c5fb59ff9
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-03-30 13:14:48 +0200

    feat: Remove useless HomeWrapper

--- packages/cozy-mespapiers-lib/src/index.js
+++ packages/cozy-mespapiers-lib/src/index.js
@@ -1,3 +1,2 @@
 export { default } from './components/MesPapiersLib'
-export { default as Home } from './components/Home/HomeWrapper'
 export { default as PapersFab } from './components/PapersFab/PapersFab'
