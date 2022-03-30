01f1905cbd31975e71a3769b422f9dfe4a4b07f8
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-03-30 12:40:42 +0200

    feat: Remove Fab from the Home and upgrade its API
    Now we need to import the Fab inside the application. To be able
    to use its API and adjust its position for example. We add
    withLocales HOC and apply it on the Fab, because Fab use locales
    from the lib, not from the app.

--- packages/cozy-mespapiers-lib/src/components/PapersFab/PapersFab.jsx
+++ packages/cozy-mespapiers-lib/src/components/PapersFab/PapersFab.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { makeStyles } from '@material-ui/styles'
+import cx from 'classnames'
 
 import { useLocation } from 'react-router-dom'
-import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Fab from 'cozy-ui/transpiled/react/Fab'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import PlusIcon from 'cozy-ui/transpiled/react/Icons/Plus'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
+
+import withLocales from '../../locales/withLocales'
 
 const useStyles = makeStyles(() => ({
   fab: {
@@ -18,8 +20,7 @@ const useStyles = makeStyles(() => ({
   }
 }))
 
-export const PapersFab = () => {
-  const { t } = useI18n()
+const PapersFab = ({ t, className, ...props }) => {
   const { isDesktop } = useBreakpoints()
   const classes = useStyles(isDesktop)
   const history = useHistory()
@@ -33,15 +34,16 @@ export const PapersFab = () => {
   }
 
   return (
-    <>
-      <Fab
-        color="primary"
-        aria-label={t('Home.Fab.ariaLabel')}
-        className={classes.fab}
-        onClick={handleClick}
-      >
-        <Icon icon={PlusIcon} />
-      </Fab>
-    </>
+    <Fab
+      color="primary"
+      aria-label={t('Home.Fab.ariaLabel')}
+      className={cx(classes.fab, className)}
+      onClick={handleClick}
+      {...props}
+    >
+      <Icon icon={PlusIcon} />
+    </Fab>
   )
 }
+
+export default withLocales(PapersFab)
--- packages/cozy-mespapiers-lib/src/index.js
+++ packages/cozy-mespapiers-lib/src/index.js
@@ -1,2 +1,3 @@
 export { default } from './components/MesPapiersLib'
 export { default as Home } from './components/Home/HomeWrapper'
+export { default as PapersFab } from './components/PapersFab/PapersFab'
