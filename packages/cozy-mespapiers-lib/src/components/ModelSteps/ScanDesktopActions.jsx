7c840859dcd9070c079e372f7d17371b689e65d1
Merkur39 <merkkur39@gmail.com>
2022-04-07 10:22:45 +0200

    feat: Add listener on Scan step
    On the Scan step,
    we have added the possibility to trigger file picker(native)
    with the Enter key

--- packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanDesktopActions.jsx
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanDesktopActions.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, createRef } from 'react'
 
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
@@ -6,11 +6,24 @@ import FileInput from 'cozy-ui/transpiled/react/FileInput'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import FolderMoveto from 'cozy-ui/transpiled/react/Icons/FolderMoveto'
 import PhoneUpload from 'cozy-ui/transpiled/react/Icons/PhoneUpload'
+import useEventListener from 'cozy-ui/transpiled/react/hooks/useEventListener'
 
 const styleBtn = { color: 'var(--primaryTextColor)' }
 
 const ScanDesktopActions = ({ openFilePickerModal, onChangeFile }) => {
   const { t } = useI18n()
+  const buttonRef = createRef()
+
+  const handleKeyDown = useCallback(
+    ({ key }) => {
+      if (key === 'Enter' && buttonRef.current) {
+        buttonRef.current.click()
+      }
+    },
+    [buttonRef]
+  )
+
+  useEventListener(window, 'keydown', handleKeyDown)
 
   return (
     <>
@@ -32,6 +45,7 @@ const ScanDesktopActions = ({ openFilePickerModal, onChangeFile }) => {
         <Button
           startIcon={<Icon icon={PhoneUpload} />}
           component={'a'}
+          ref={buttonRef}
           className={'u-w-100 u-m-0 u-mb-1'}
           label={t('Scan.importPicFromDesktop')}
         />
