cozy/cozy-libs

commit 7d06e05cf9cb3647a92c6ab85f2b2262946971c6

feat: AppLinkCard does not wait for fetch before showing
Prevent content from jumping

packages/cozy-harvest-lib/src/components/cards/AppLinkCard.jsx
@@ -25,8 +25,6 @@ const AppLinkCard = ({ slug, path, icon, iconColor }) => {
     path
   )
 
-  if (fetchStatus !== 'loaded') return null
-
   return (
     <Card>
       <Stack>
@@ -44,7 +42,7 @@ const AppLinkCard = ({ slug, path, icon, iconColor }) => {
         <AppLinker slug={slug} nativePath={path} href={url}>
           {({ onClick, href }) => (
             <ButtonLink
-              onClick={onClick}
+              onClick={fetchStatus !== 'loaded' ? onClick : null}
               href={href}
               icon={
                 isInstalled ? (

commit 64312c488002808a9420471f248db1f34ecb3950

fix(cozy-harvest-lib): Correctly handle onClick on AppLinkCard
Refactoring from 7d06e05cf9cb3647a92c6ab85f2b2262946971c6 introduced a
bug as the `fetchStatus` condition should have been reversed

We didn't notice this error as most of our apps can rely on the
button's `href`

But in ReactNative context, we rely on the onClick behavior in order to
send the correct `openApp` message to the native side

packages/cozy-harvest-lib/src/components/cards/AppLinkCard.jsx
@@ -28,7 +28,7 @@ export const AppLinkButton = ({ slug, path }) => {
     <AppLinker app={{ slug }} nativePath={path} href={url || '#'}>
       {({ onClick, href }) => (
         <ButtonLink
-          onClick={fetchStatus !== 'loaded' ? onClick : null}
+          onClick={fetchStatus === 'loaded' ? onClick : null}
           href={href}
           icon={
             isInstalled ? (