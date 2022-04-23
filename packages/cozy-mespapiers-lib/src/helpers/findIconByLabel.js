c8a1fa33769f0f1662e104d04a1c32b137665992
Merkur39 <merkkur39@gmail.com>
2022-04-23 13:50:04 +0200

    refactor: Remove useless helper

--- packages/cozy-mespapiers-lib/src/helpers/findIconByLabel.js
+++
@@ -1,7 +0,0 @@
-import People from 'cozy-ui/transpiled/react/Icons/People'
-
-const identityLabels = ['national_id_card', 'passport']
-
-export const findIconByLabel = label => {
-  if (identityLabels.includes(label)) return People
-}
