9b468e52e284051208eee5b2632530beacf3155f
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-04-28 15:42:49 +0200

    feat: Add hasItemByLabel helper
    will be moved in cozy-client after feature validation

--- packages/cozy-mespapiers-lib/src/components/Home/helpers.js
+++ packages/cozy-mespapiers-lib/src/components/Home/helpers.js
@@ -1,7 +1,11 @@
+// should be in cozy-client : https://github.com/cozy/cozy-client/blob/master/packages/cozy-client/src/models/document/documentTypeDataHelpers.js
+export const hasItemByLabel = (theme, label) =>
+  theme.items.some(item => item.label === label)
+
 export const filterPapersByTheme = (files, theme) => {
   if (!theme) return files
 
   return files.filter(file =>
-    theme.items.some(item => item.label === file.metadata.qualification.label)
+    hasItemByLabel(theme, file.metadata.qualification.label)
   )
 }
