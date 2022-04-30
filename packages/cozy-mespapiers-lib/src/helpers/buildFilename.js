e63e4bc2a71a6a17f1eb5e808b6119c977cb40d5
Merkur39 <merkkur39@gmail.com>
2022-05-01 00:38:38 +0200

    fix: The buildFilename function needs all the contacts
    This function must manage several contacts

--- packages/cozy-mespapiers-lib/src/helpers/buildFilename.js
+++ packages/cozy-mespapiers-lib/src/helpers/buildFilename.js
@@ -1,43 +1,66 @@
-/**
- * @typedef {object} BuildPaperNameParam
- * @property {string} qualificationName - Name of the paper qualification
- * @property {Array<string>} [filenameModel] - Array of key for build filename
- * @property {object} [metadata] - Object with data of Information input
- * @property {string} [pageName] - Name of page (eg Front)
- * @property {string} [contactName] - Fullname of contact
- * @property {string} [formatedDate] - Date already formated
- */
+import { models } from 'cozy-client'
+
+const {
+  contact: { getFullname }
+} = models
 
 /**
  * Builded Paper name with qualification name & without use filename original
- * @param {BuildPaperNameParam} opts
+ * @param {string} contacts - Array of contacts
+ * @param {string} qualificationName - Name of the paper qualification
+ * @param {Array<string>} [filenameModel] - Array of key for build filename
+ * @param {object} [metadata] - Object with data of Information input
+ * @param {string} [pageName] - Name of page (eg Front)
+ * @param {string} [formatedDate] - Date already formated
  * @returns {string} Paper name with PDF extension
  */
-export const buildFilename = ({ metadata, filenameModel, ...opts }) => {
+export const buildFilename = ({
+  contacts,
+  qualificationName,
+  filenameModel,
+  metadata,
+  pageName,
+  formatedDate
+}) => {
   /*
     Calling the stack's file creation method would trigger a `status: "422", title: "Invalid Parameter"` error if filename contains`/`.
     So we need to remove any occurrence of this character from the filename.
   */
-  const safeFileName = opts.qualificationName.replaceAll('/', '_')
+  const safeFileName = qualificationName.replaceAll('/', '_')
 
-  const pageName = opts.pageName ? ` - ${opts.pageName}` : ''
-  const contactName = opts.contactName ? ` - ${opts.contactName}` : ''
-  const formatedDate = opts.formatedDate ? ` - ${opts.formatedDate}` : ''
+  const filename = []
+  let contactName = ''
+  if (contacts.length > 0) {
+    contactName += `${getFullname(contacts[0])}`
+    if (contacts.length > 1) {
+      contactName += `, ${getFullname(contacts[1])}`
+      if (contacts.length > 2) contactName += ', ... '
+    }
+  }
+
+  filename.push(safeFileName)
+  if (pageName) filename.push(pageName)
+  if (contactName) filename.push(contactName)
+  if (formatedDate) filename.push(formatedDate)
 
   if (filenameModel?.length > 0) {
-    const result = filenameModel
+    const filenameWithModel = filenameModel
       .map(el => {
         if (el === 'label') return safeFileName
-        if (el === 'contactName') return opts.contactName || ''
-        if (el === 'featureDate') return opts.formatedDate || ''
+        if (el === 'contactName') return contactName || ''
+        if (el === 'featureDate') return formatedDate || ''
         return metadata?.[el] ? metadata[el] : null
       })
       .filter(Boolean)
 
-    if (result.length > 0) {
-      return `${result.join(' - ')}.pdf`
+    if (filenameWithModel.length > 0) {
+      return `${filenameWithModel.join(' - ')}.pdf`
     }
   }
 
-  return `${safeFileName}${pageName}${contactName}${formatedDate}.pdf`
+  if (filename.length > 1) {
+    return `${filename.join(' - ')}.pdf`
+  }
+
+  return `${filename.join()}.pdf`
 }
--- packages/cozy-mespapiers-lib/src/helpers/buildFilename.spec.js
+++ packages/cozy-mespapiers-lib/src/helpers/buildFilename.spec.js
@@ -1,8 +1,28 @@
 import { buildFilename } from './buildFilename'
 
+const mockSameFamilyNameContacts = [
+  { _id: 'contactId01', name: { givenName: 'Bob', familyName: 'Durand' } },
+  { _id: 'contactId02', name: { givenName: 'Alice', familyName: 'Durand' } }
+]
+const mockOneContact = [
+  { _id: 'contactId01', name: { givenName: 'Bernard', familyName: 'Chabert' } }
+]
+const mockTwoContacts = [
+  { _id: 'contactId01', name: { givenName: 'Bernard', familyName: 'Chabert' } },
+  { _id: 'contactId02', name: { givenName: 'Clair', familyName: 'Guillot' } }
+]
+const mockThreeContacts = [
+  { _id: 'contactId01', name: { givenName: 'Bernard', familyName: 'Chabert' } },
+  { _id: 'contactId02', name: { givenName: 'Clair', familyName: 'Guillot' } },
+  { _id: 'contactId03', name: { givenName: 'Jean', familyName: 'Rossi' } }
+]
+
+// TODO Build le nom comme "buildSubheaderName" & merger dans le commit "25efe0de6bbabb633da3a8d10c98dab5a35c97b6"
+
 describe('buildFilename', () => {
   it('should replace "/" by "_" in qualificationName', () => {
     const paperName = buildFilename({
+      contacts: [],
       qualificationName: 'passport/test'
     })
 
@@ -14,69 +34,77 @@ describe('buildFilename', () => {
     ${{
   qualificationName: 'passport',
   formatedDate: '2022.01.01',
-  contactName: 'Bob',
+  contacts: mockOneContact,
   filenameModel: ['labelGivenByUser', 'contactName', 'featureDate'],
   metadata: { labelGivenByUser: 'Mon fichier' }
-}} | ${'Mon fichier - Bob - 2022.01.01.pdf'}
+}} | ${'Mon fichier - Bernard Chabert - 2022.01.01.pdf'}
     ${{
   qualificationName: 'passport',
   formatedDate: '2022.01.01',
+  contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate'],
   metadata: { labelGivenByUser: 'Mon fichier' }
 }} | ${'Mon fichier - 2022.01.01.pdf'}
     ${{
   qualificationName: 'passport',
   formatedDate: '2022.01.01',
+  contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate'],
   metadata: { labelGivenByUser: '' }
 }} | ${'2022.01.01.pdf'}
     ${{
   qualificationName: 'passport',
   formatedDate: '2022.01.01',
+  contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate']
 }} | ${'2022.01.01.pdf'}
     ${{
   qualificationName: 'passport',
   formatedDate: '2022.01.01',
+  contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate'],
   metadata: {}
 }} | ${'2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport'
+  qualificationName: 'passport',
+  contacts: []
 }} | ${'passport.pdf'}
     ${{
   qualificationName: 'passport',
+  contacts: [],
   pageName: 'front'
 }} | ${'passport - front.pdf'}
     ${{
   qualificationName: 'passport',
-  contactName: 'Bob'
-}} | ${'passport - Bob.pdf'}
+  contacts: mockOneContact
+}} | ${'passport - Bernard Chabert.pdf'}
     ${{
   qualificationName: 'passport',
+  contacts: [],
   formatedDate: '2022.01.01'
 }} | ${'passport - 2022.01.01.pdf'}
     ${{
   qualificationName: 'passport',
   pageName: 'front',
-  contactName: 'Bob'
-}} | ${'passport - front - Bob.pdf'}
+  contacts: mockOneContact
+}} | ${'passport - front - Bernard Chabert.pdf'}
     ${{
   qualificationName: 'passport',
   pageName: 'front',
+  contacts: [],
   formatedDate: '2022.01.01'
 }} | ${'passport - front - 2022.01.01.pdf'}
     ${{
   qualificationName: 'passport',
-  contactName: 'Bob',
+  contacts: mockOneContact,
   formatedDate: '2022.01.01'
-}} | ${'passport - Bob - 2022.01.01.pdf'}
+}} | ${'passport - Bernard Chabert - 2022.01.01.pdf'}
     ${{
   qualificationName: 'passport',
   pageName: 'front',
-  contactName: 'Bob',
+  contacts: mockOneContact,
   formatedDate: '2022.01.01'
-}} | ${'passport - front - Bob - 2022.01.01.pdf'}
+}} | ${'passport - front - Bernard Chabert - 2022.01.01.pdf'}
   `(`should return $result with "$opts" parameters`, ({ opts, result }) => {
     expect(buildFilename(opts)).toEqual(result)
   })
