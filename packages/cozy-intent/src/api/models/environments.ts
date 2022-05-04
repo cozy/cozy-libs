73e882c739d643b72d5c1da4c4a510761c5678ea
Yannick Chiron <yannick.chiron@gmail.com>
2022-05-04 21:47:04 +0200

    fix(cozy-intent): Allow to register webviews with baseUrl and no uri
    ReactNative webviews can be registered using two forms of sources:
    - `source={{ uri }}`
    - `source={{ html, baseUrl }}`
    
    Previous code implementation was able to support only the first form
    and would crash when a webview is registered with a baseUrl
    
    Not that react-native-webview priorize check on html+baseUrl over uri,
    so we chose to implement only `isWebviewSourceBaseUrl` to prevent a
    wrong check
    
    More details:
    - https://github.com/react-native-webview/react-native-webview/blob/a36394f598944c1ffe288f739b7441830249dc1e/apple/RNCWebView.m#L679-L686
    - https://github.com/react-native-webview/react-native-webview/blob/a36394f598944c1ffe288f739b7441830249dc1e/android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java#L513-L518

--- packages/cozy-intent/src/api/models/environments.ts
+++ packages/cozy-intent/src/api/models/environments.ts
@@ -2,12 +2,35 @@ import { Connection, EventsType, MethodsType } from 'post-me'
 
 import { NativeMethodsRegister } from '../../api'
 
+export interface WebviewSourceUri {
+  uri: string
+
+  baseUrl?: never
+  html?: never
+}
+
+export interface WebiewSourceBaseUrl {
+  baseUrl: string
+  html: string
+
+  uri?: never
+}
+
+export type WebviewSource = WebviewSourceUri | WebiewSourceBaseUrl
+
+export function isWebviewSourceBaseUrl(
+  webviewSource: WebviewSource
+): webviewSource is WebiewSourceBaseUrl {
+  if ((webviewSource as WebiewSourceBaseUrl).html) {
+    return true
+  }
+  return false
+}
+
 export interface WebviewRef {
   injectJavaScript: (data: string) => void
   props: {
-    source: {
-      uri: string
-    }
+    source: WebviewSource
   }
 }
 
