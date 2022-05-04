f8db24b1ef048168d31b6ee1299757b90b56ff6d
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-05-04 15:54:17 +0200

    feat: Add search input to filter files by name

--- packages/cozy-mespapiers-lib/src/components/Home/helpers.js
+++ packages/cozy-mespapiers-lib/src/components/Home/helpers.js
@@ -1,11 +1,36 @@
-// should be in cozy-client : https://github.com/cozy/cozy-client/blob/master/packages/cozy-client/src/models/document/documentTypeDataHelpers.js
-export const hasItemByLabel = (theme, label) =>
-  theme.items.some(item => item.label === label)
+import Fuse from 'fuse.js'
 
-export const filterPapersByTheme = (files, theme) => {
-  if (!theme) return files
+const fuse = new Fuse([], {
+  findAllMatches: true,
+  threshold: 0.3,
+  ignoreLocation: true,
+  ignoreFieldNorm: true,
+  keys: [
+    {
+      name: 'name'
+    }
+  ]
+})
 
-  return files.filter(file =>
-    hasItemByLabel(theme, file.metadata.qualification.label)
-  )
+// TODO: hasItemByLabel should be in cozy-client : https://github.com/cozy/cozy-client/blob/master/packages/cozy-client/src/models/document/documentTypeDataHelpers.js
+export const hasItemByLabel = (theme, label) => {
+  if (!theme) return true
+  return theme.items.some(item => item.label === label)
+}
+
+export const filterPapersByThemeAndSearchValue = ({ files, theme, search }) => {
+  let filteredFiles = files
+
+  if (search) {
+    fuse.setCollection(files)
+    filteredFiles = fuse.search(search).map(result => result.item)
+  }
+
+  if (theme) {
+    filteredFiles = filteredFiles.filter(file =>
+      hasItemByLabel(theme, file.metadata.qualification.label)
+    )
+  }
+
+  return filteredFiles
 }
