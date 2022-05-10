ddb8fb59a8c45731a9e2bea28b5dfeb847ef0978
Merkur39 <merkkur39@gmail.com>
2022-05-10 16:34:32 +0200

    refactor: Move helpers functions in ActionsItems to utils file

--- packages/cozy-mespapiers-lib/src/components/Actions/utils.js
+++ packages/cozy-mespapiers-lib/src/components/Actions/utils.js
@@ -7,6 +7,54 @@ import { FILES_DOCTYPE } from '../../doctypes'
 import { getSharingLink } from '../../utils/getSharingLink'
 import { download, forward } from './Actions'
 
+export const getActionName = actionObject => {
+  return Object.keys(actionObject)[0]
+}
+
+// We need to clean Actions since action has a displayable
+// conditions and we can't know from the begining what the
+// behavior will be. For instance, we can't know that
+// hr will be the latest action in the sharing views for a
+// folder.
+// Or we can't know that we'll have two following hr if the
+// display condition for the actions between are true or false
+export const getOnlyNeededActions = (actions, file) => {
+  let previousAction = ''
+  const displayableActions = actions.filter(actionObject => {
+    const actionDefinition = Object.values(actionObject)[0]
+
+    return (
+      !actionDefinition.displayCondition ||
+      actionDefinition.displayCondition([file])
+    )
+  })
+
+  return (
+    displayableActions
+      // We do not want to display the same 2 actions in a row
+      .map(actionObject => {
+        const actionName = getActionName(actionObject)
+
+        if (previousAction === actionName) {
+          previousAction = actionName
+          return null
+        } else {
+          previousAction = actionName
+        }
+
+        return actionObject
+      })
+      .filter(Boolean)
+      // We don't want to have an hr as the latest actions available
+      .filter((cleanedAction, idx, cleanedActions) => {
+        return !(
+          getActionName(cleanedAction) === 'hr' &&
+          idx === cleanedActions.length - 1
+        )
+      })
+  )
+}
+
 /**
  * Make array of actions for ActionsItems component
  *
