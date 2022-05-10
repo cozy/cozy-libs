c326164e4a0a7d03c77faaf19dbb3fec858911bb
Merkur39 <merkkur39@gmail.com>
2022-05-10 16:34:32 +0200

    refactor: Move helpers functions in ActionsItems to utils file

--- packages/cozy-mespapiers-lib/src/components/Actions/ActionsItems.jsx
+++ packages/cozy-mespapiers-lib/src/components/Actions/ActionsItems.jsx
@@ -1,61 +1,9 @@
-/* This code is copy/pasted from Drive */
 import React, { useMemo } from 'react'
 import cx from 'classnames'
 
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import { getActionName, getOnlyNeededActions } from './utils'
 
-export const getActionName = actionObject => {
-  return Object.keys(actionObject)[0]
-}
-
-// We need to clean Actions since action has a displayable
-// conditions and we can't know from the begining what the
-// behavior will be. For instance, we can't know that
-// hr will be the latest action in the sharing views for a
-// folder.
-// Or we can't know that we'll have two following hr if the
-// display condition for the actions between are true or false
-export const getOnlyNeededActions = (actions, file) => {
-  let previousAction = ''
-  const displayableActions = actions.filter(actionObject => {
-    const actionDefinition = Object.values(actionObject)[0]
-
-    return (
-      !actionDefinition.displayCondition ||
-      actionDefinition.displayCondition([file])
-    )
-  })
-
-  return (
-    displayableActions
-      .map(actionObject => {
-        const actionName = getActionName(actionObject)
-
-        if (previousAction === actionName) {
-          previousAction = actionName
-          return null
-        } else {
-          previousAction = actionName
-        }
-
-        return actionObject
-      })
-      .filter(Boolean)
-      // We don't want to have an hr as the latest actions available
-      .filter((cleanedAction, idx, cleanedActions) => {
-        return !(
-          getActionName(cleanedAction) === 'hr' &&
-          idx === cleanedActions.length - 1
-        )
-      })
-  )
-}
-
-/**
- *
- * ActionsItems only shows `actions` that are  suitable for the given
- * `file`.
- */
 export const ActionsItems = ({ actions, file, onClose }) => {
   const { t } = useI18n()
   const cleanedActions = useMemo(
