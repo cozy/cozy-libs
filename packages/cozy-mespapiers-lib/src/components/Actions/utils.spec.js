bf5c7fc365be29cb3d90a57bcc78a242b1d4f328
Merkur39 <merkkur39@gmail.com>
2022-05-10 15:54:08 +0200

    refactor: Move makeActions to utils file

--- packages/cozy-mespapiers-lib/src/components/Actions/utils.spec.js
+++ packages/cozy-mespapiers-lib/src/components/Actions/utils.spec.js
@@ -1,18 +1,57 @@
-import { makeActionVariant } from './utils'
+import { makeActions, makeActionVariant } from './utils'
 import { download, forward } from './Actions'
 
 jest.mock('./Actions')
 
-describe('makeActionVariant', () => {
-  it('should have "download" action on desktop', () => {
-    global.navigator.share = null
+describe('Actions utils', () => {
+  fdescribe('makeActions', () => {
+    it('should have empty actions array', () => {
+      const actions = makeActions()
 
-    expect(makeActionVariant()).toStrictEqual([download])
+      expect(actions).toStrictEqual([])
+    })
+
+    it('should have object with key named with name property of the object returned by the function and value is the full object returned by the function', () => {
+      const mockFuncActionWithName = jest.fn(() => ({
+        name: 'mockFuncActionWithName name'
+      }))
+
+      const actions = makeActions([mockFuncActionWithName])
+
+      expect(actions).toStrictEqual([
+        {
+          'mockFuncActionWithName name': {
+            name: 'mockFuncActionWithName name'
+          }
+        }
+      ])
+    })
+
+    it('should have object with key named with name of function passed and value is the full object returned by the function', () => {
+      const mockFuncActionWithoutName = jest.fn(() => ({
+        propA: 0,
+        propB: 1
+      }))
+
+      const actions = makeActions([mockFuncActionWithoutName])
+
+      expect(actions).toStrictEqual([
+        { mockConstructor: { propA: 0, propB: 1 } }
+      ])
+    })
   })
 
-  it('should have "download" & "Forward" action (in this order) on mobile', () => {
-    global.navigator.share = () => {}
+  describe('makeActionVariant', () => {
+    it('should have "download" action on desktop', () => {
+      global.navigator.share = null
 
-    expect(makeActionVariant()).toStrictEqual([download, forward])
+      expect(makeActionVariant()).toStrictEqual([download])
+    })
+
+    it('should have "download" & "Forward" action (in this order) on mobile', () => {
+      global.navigator.share = () => {}
+
+      expect(makeActionVariant()).toStrictEqual([download, forward])
+    })
   })
 })
