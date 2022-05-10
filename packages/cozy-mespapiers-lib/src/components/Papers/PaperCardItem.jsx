67910002f27a5aff24f285c350e96623a0dd7d06
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-05-10 14:50:42 +0200

    feat: Create PaperCardItem

---
+++ packages/cozy-mespapiers-lib/src/components/Papers/PaperCardItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import CrossCircleIcon from 'cozy-ui/transpiled/react/icons/CrossCircle'
+import Paper from 'cozy-ui/transpiled/react/Paper'
+
+import PaperItem from '../Papers/PaperItem'
+
+const PaperCardItem = ({ paper, divider }) => {
+  return (
+    <Paper className="u-mh-1">
+      <PaperItem paper={paper} divider={divider}>
+        <IconButton color="secondary" onClick={() => {}}>
+          <Icon icon={CrossCircleIcon} />
+        </IconButton>
+      </PaperItem>
+    </Paper>
+  )
+}
+
+PaperCardItem.propTypes = {
+  paper: PropTypes.object.isRequired,
+  divider: PropTypes.bool
+}
+
+export default PaperCardItem
