a593d17c0df3b3e1a5a6626ee20c60c8179eb78b
Merkur39 <merkkur39@gmail.com>
2022-05-13 19:44:56 +0200

    feat: Add functions to MultiSelectViewActions component

--- packages/cozy-mespapiers-lib/src/components/Multiselect/MultiselectViewActions.jsx
+++ packages/cozy-mespapiers-lib/src/components/Multiselect/MultiselectViewActions.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
 
+import { useClient } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 
+import { downloadFiles, forwardFile } from '../Actions/utils'
+import { useMultiSelection } from '../Hooks/useMultiSelection'
+
 const MultiselectViewActions = () => {
   const { t } = useI18n()
+  const client = useClient()
+  const { multiSelectionFiles } = useMultiSelection()
+
+  const download = async () => {
+    await downloadFiles(client, multiSelectionFiles)
+  }
+
+  const forward = async () => {
+    await forwardFile(client, multiSelectionFiles, t)
+  }
 
   return (
     <>
-      <Button variant="secondary" label={t('action.download')} />
-      <Button variant="secondary" label={t('action.forward')} />
+      <Button
+        variant="secondary"
+        label={t('action.download')}
+        onClick={download}
+        disabled={multiSelectionFiles.length === 0}
+      />
+      {navigator.share && (
+        <Button
+          variant="secondary"
+          label={t('action.forward')}
+          onClick={forward}
+          disabled={multiSelectionFiles.length === 0}
+        />
+      )}
     </>
   )
 }
