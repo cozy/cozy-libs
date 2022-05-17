db595c54ab1d80c790acc762a6e549c0e05b7a44
Merkur39 <merkkur39@gmail.com>
2022-05-17 16:10:03 +0200

    feat: Improve PaperCardItem
    Add the `className` & `square` props
    needed to properly manage the effect of the `hover`
    on the component

--- packages/cozy-mespapiers-lib/src/components/Papers/PaperCardItem.jsx
+++ packages/cozy-mespapiers-lib/src/components/Papers/PaperCardItem.jsx
@@ -4,13 +4,27 @@ import PropTypes from 'prop-types'
 import IconButton from 'cozy-ui/transpiled/react/IconButton'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import Paper from 'cozy-ui/transpiled/react/Paper'
+import makeStyles from 'cozy-ui/transpiled/react/helpers/makeStyles'
 
 import PaperItem from '../Papers/PaperItem'
 
-const PaperCardItem = ({ paper, divider }) => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    // This values corresponds to the value of the property `border-radius` of the component Paper of MUI
+    borderRadius: square => (square ? 0 : theme.shape.borderRadius),
+    height: '100%'
+  },
+  container: {
+    height: '100%'
+  }
+}))
+
+const PaperCardItem = ({ paper, divider, className, square = false }) => {
+  const classes = useStyles(square)
+
   return (
-    <Paper className="u-mh-1">
-      <PaperItem paper={paper} divider={divider}>
+    <Paper className={`u-h-3 ${className}`} square={square}>
+      <PaperItem paper={paper} divider={divider} classes={classes}>
         <IconButton color="secondary" onClick={() => {}}>
           <Icon icon="cross-circle" />
         </IconButton>
@@ -21,7 +35,9 @@ const PaperCardItem = ({ paper, divider }) => {
 
 PaperCardItem.propTypes = {
   paper: PropTypes.object.isRequired,
-  divider: PropTypes.bool
+  divider: PropTypes.bool,
+  className: PropTypes.string,
+  square: PropTypes.bool
 }
 
 export default PaperCardItem
