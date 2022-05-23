9790a77cf62afec5832e27ac7a9b67ec9ada97d9
Merkur39 <merkkur39@gmail.com>
2022-05-23 16:05:56 +0200

    feat(cozy-codemods): Add helper for make JSX element

--- packages/cozy-codemods/src/__mocks__/imports.js
+++ packages/cozy-codemods/src/__mocks__/imports.js
@@ -13,3 +13,96 @@ import 'side-effect-package'
 import Dialog, { DialogFile, DialogContent } from "cozy-ui/transpiled/react/Dialog";
 import Tabs, { TabPanels, TabPanel } from 'cozy-ui/transpiled/react/Tabs'
 `
+
+export const JSXElementWithoutClosingElement = {
+  openingElement: expect.any(Object),
+  closingElement: null,
+  attributes: [],
+  children: [],
+  name: expect.objectContaining({
+    name: 'SomeElement',
+    type: 'JSXIdentifier'
+  }),
+  selfClosing: true,
+  type: 'JSXElement'
+}
+
+export const JSXElementWithClosingElement = {
+  openingElement: expect.any(Object),
+  closingElement: expect.any(Object),
+  attributes: [],
+  children: [],
+  name: expect.objectContaining({
+    name: 'SomeElement',
+    type: 'JSXIdentifier'
+  }),
+  selfClosing: false,
+  type: 'JSXElement'
+}
+
+export const JSXElementWithChildren = {
+  openingElement: expect.any(Object),
+  closingElement: expect.any(Object),
+  attributes: [],
+  children: expect.arrayContaining([
+    expect.objectContaining({
+      openingElement: expect.any(Object),
+      closingElement: null,
+      attributes: [],
+      children: [],
+      name: expect.objectContaining({
+        name: 'Child',
+        type: 'JSXIdentifier'
+      }),
+      selfClosing: true,
+      type: 'JSXElement'
+    })
+  ]),
+  name: expect.objectContaining({
+    name: 'SomeElement',
+    type: 'JSXIdentifier'
+  }),
+  selfClosing: false,
+  type: 'JSXElement'
+}
+
+export const JSXElementWithAttribute = {
+  attributes: expect.arrayContaining([
+    expect.objectContaining({
+      name: expect.objectContaining({
+        name: 'attr0',
+        type: 'JSXIdentifier'
+      }),
+      type: 'JSXAttribute',
+      value: expect.objectContaining({
+        regex: null,
+        type: 'Literal',
+        value: '0'
+      })
+    })
+  ]),
+  children: [],
+  closingElement: null,
+  name: expect.objectContaining({
+    name: 'SomeElement',
+    type: 'JSXIdentifier'
+  }),
+  openingElement: expect.objectContaining({
+    attributes: expect.arrayContaining([
+      expect.objectContaining({
+        name: expect.objectContaining({
+          name: 'attr0',
+          type: 'JSXIdentifier'
+        }),
+        value: expect.objectContaining({
+          regex: null,
+          type: 'Literal',
+          value: '0'
+        }),
+        type: 'JSXAttribute'
+      })
+    ])
+  }),
+  selfClosing: true,
+  type: 'JSXElement'
+}
--- packages/cozy-codemods/src/imports.spec.js
+++ packages/cozy-codemods/src/imports.spec.js
@@ -1,29 +1,77 @@
 const j = require('jscodeshift')
-const { simplify } = require('./imports')
-const { before, after } = require('./__mocks__/imports')
+const { simplify, makeJSXElement, makeAttribute } = require('./imports')
+const {
+  before,
+  after,
+  JSXElementWithoutClosingElement,
+  JSXElementWithClosingElement,
+  JSXElementWithChildren,
+  JSXElementWithAttribute
+} = require('./__mocks__/imports')
 require('@testing-library/jest-dom')
 
 describe('transform imports', () => {
-  it('should transform imports according to options', () => {
-    const root = j(before)
+  describe('simplify', () => {
+    it('should transform imports according to options', () => {
+      const root = j(before)
 
-    simplify(root, {
-      imports: {
-        Dialog: {
-          importPath: 'cozy-ui/transpiled/react/Dialog',
-          defaultImport: true
-        },
-        DialogFile: {
-          importPath: 'cozy-ui/transpiled/react/Dialog',
-          defaultImport: false
-        },
-        DialogContent: {
-          importPath: 'cozy-ui/transpiled/react/Dialog',
-          defaultImport: false
+      simplify(root, {
+        imports: {
+          Dialog: {
+            importPath: 'cozy-ui/transpiled/react/Dialog',
+            defaultImport: true
+          },
+          DialogFile: {
+            importPath: 'cozy-ui/transpiled/react/Dialog',
+            defaultImport: false
+          },
+          DialogContent: {
+            importPath: 'cozy-ui/transpiled/react/Dialog',
+            defaultImport: false
+          }
         }
-      }
-    })
+      })
 
-    expect(root.toSource()).toEqual(after)
+      expect(root.toSource()).toEqual(after)
+    })
+  })
+
+  describe('makeJSXElement', () => {
+    const child = makeJSXElement({ name: 'Child' })
+    const mockAttr = makeAttribute({ name: 'attr0', value: '0' })
+
+    it.each`
+      attrs                                                     | result
+      ${{ name: 'SomeElement' }}                                | ${JSXElementWithoutClosingElement}
+      ${{ name: 'SomeElement', children: [] }}                  | ${JSXElementWithoutClosingElement}
+      ${{ name: 'SomeElement', autoClose: true }}               | ${JSXElementWithoutClosingElement}
+      ${{ name: 'SomeElement', children: [], autoClose: true }} | ${JSXElementWithoutClosingElement}
+    `(
+      `should make a JSX element that close automatically when passed argument: $attrs`,
+      ({ attrs, result }) => {
+        expect(makeJSXElement(attrs)).toEqual(expect.objectContaining(result))
+      }
+    )
+
+    it.each`
+      attrs                                                           | result
+      ${{ name: 'SomeElement', autoClose: false }}                    | ${JSXElementWithClosingElement}
+      ${{ name: 'SomeElement', children: [child] }}                   | ${JSXElementWithChildren}
+      ${{ name: 'SomeElement', children: [child], autoClose: true }}  | ${JSXElementWithChildren}
+      ${{ name: 'SomeElement', children: [child], autoClose: false }} | ${JSXElementWithChildren}
+    `(
+      `should make a JSX element that does not close automatically when passed argument: $attrs`,
+      ({ attrs, result }) => {
+        expect(makeJSXElement(attrs)).toEqual(expect.objectContaining(result))
+      }
+    )
+
+    it('should make a JSX element with attributes when passed "attributes" argument', () => {
+      const attrs = { name: 'SomeElement', attributes: [mockAttr] }
+
+      expect(makeJSXElement(attrs)).toEqual(
+        expect.objectContaining(JSXElementWithAttribute)
+      )
+    })
   })
 })
