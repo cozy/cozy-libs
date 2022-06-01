935d86b5f08f12aaaf4cf4ced982e3fde9ac4a1a
Merkur39 <merkkur39@gmail.com>
2022-06-01 09:27:40 +0200

    feat: Change process for forward multiple files

--- packages/cozy-mespapiers-lib/src/components/Multiselect/MultiselectViewActions.jsx
+++ packages/cozy-mespapiers-lib/src/components/Multiselect/MultiselectViewActions.jsx
@@ -2,23 +2,49 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { useClient, models } from 'cozy-client'
+import useRealtime from 'cozy-realtime/dist/useRealtime'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Button from 'cozy-ui/transpiled/react/Buttons'
-import useRealtime from 'cozy-realtime/dist/useRealtime'
+import Backdrop from 'cozy-ui/transpiled/react/Backdrop'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+import { LinearProgress } from 'cozy-ui/transpiled/react/Progress'
+import makeStyles from 'cozy-ui/transpiled/react/helpers/makeStyles'
 
-import { downloadFiles, forwardFile, makeZipFolder } from '../Actions/utils'
+import { downloadFiles, makeZipFolder } from '../Actions/utils'
 import { useMultiSelection } from '../Hooks/useMultiSelection'
 import { FILES_DOCTYPE } from '../../doctypes'
 import { fetchCurrentUser } from '../../helpers/fetchCurrentUser'
 import getOrCreateAppFolderWithReference from '../../helpers/getFolderWithReference'
+import ForwardModal from './ForwardModal'
 
 const { getDisplayName } = models.contact
+
+const useStyles = makeStyles(theme => ({
+  backdropRoot: {
+    zIndex: 'var(--zIndex-modal)'
+  },
+  barText: {
+    color: 'var(--primaryContrastTextColor)'
+  },
+  bar: {
+    borderRadius: theme.shape.borderRadius
+  },
+  barBackgroundColorPrimary: {
+    backgroundColor: 'var(--secondaryTextColor)'
+  },
+  barBackgroundActiveColorPrimary: {
+    backgroundColor: 'var(--primaryContrastTextColor)'
+  }
+}))
 
 const MultiselectViewActions = ({ onClose }) => {
   const { t, f } = useI18n()
   const client = useClient()
+  const classes = useStyles()
   const { multiSelectionFiles } = useMultiSelection()
   const [zipFolder, setZipFolder] = useState({ name: '', dirId: '' })
+  const [isTransferModalOpen, setIsTransferModalOpen] = useState(false)
+  const [isBackdropOpen, setIsBackdropOpen] = useState(false)
 
   const onFileCreate = async file => {
     if (
@@ -26,9 +52,9 @@ const MultiselectViewActions = ({ onClose }) => {
       file.name === zipFolder.name &&
       file.dir_id === zipFolder.dirId
     ) {
-      await forwardFile(client, [file], t)
+      setIsBackdropOpen(false)
+      setIsTransferModalOpen(true)
     }
-    onClose()
   }
 
   useRealtime(client, {
@@ -43,6 +69,8 @@ const MultiselectViewActions = ({ onClose }) => {
   }
 
   const forward = async () => {
+    setIsBackdropOpen(true)
+
     const currentUser = await fetchCurrentUser(client)
     const defaultZipFolderName = t('Multiselect.folderZipName', {
       contactName: getDisplayName(currentUser),
@@ -65,18 +93,41 @@ const MultiselectViewActions = ({ onClose }) => {
 
   return (
     <>
+      <Backdrop classes={{ root: classes.backdropRoot }} open={isBackdropOpen}>
+        <div className="u-w-100 u-mh-2 u-ta-center">
+          <Typography classes={{ root: classes.barText }} className="u-mb-1">
+            {t('Multiselect.backdrop')}
+          </Typography>
+          <LinearProgress
+            classes={{
+              root: classes.bar,
+              colorPrimary: classes.barBackgroundColorPrimary,
+              barColorPrimary: classes.barBackgroundActiveColorPrimary
+            }}
+          />
+        </div>
+      </Backdrop>
+
       <Button
         variant="secondary"
         label={t('action.download')}
         onClick={download}
         disabled={multiSelectionFiles.length === 0}
       />
+
       {navigator.share && (
         <Button
           variant="secondary"
           label={t('action.forward')}
           onClick={forward}
           disabled={multiSelectionFiles.length === 0}
+        />
+      )}
+
+      {isTransferModalOpen && (
+        <ForwardModal
+          onClose={() => setIsTransferModalOpen(false)}
+          onForward={onClose}
         />
       )}
     </>
