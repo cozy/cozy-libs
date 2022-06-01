0cb4fe5bbdc143f5ff569fcef743bbd52ce9527d
doubleface <christophe@cozycloud.cc>
2022-06-01 11:15:57 +0200

    refactor: Mulitple fixes after review + more unit test

--- packages/cozy-harvest-lib/src/components/KonnectorConfiguration/ConfigurationTab/BIContractActivationWindow.spec.jsx
+++ packages/cozy-harvest-lib/src/components/KonnectorConfiguration/ConfigurationTab/BIContractActivationWindow.spec.jsx
@@ -16,53 +16,99 @@ jest.mock('cozy-ui/transpiled/react/Popup', () => {
     return null
   })
 })
+jest.mock('../../InAppBrowser', () => {
+  return jest.fn().mockImplementation(({ onClose }) => {
+    setTimeout(onClose, 1)
+    return null
+  })
+})
+
+jest.mock('cozy-device-helper')
 import { findKonnectorPolicy } from '../../../konnector-policies'
 import Popup from 'cozy-ui/transpiled/react/Popup'
+import InAppBrowser from '../../InAppBrowser'
+import { isFlagshipApp } from 'cozy-device-helper'
 findKonnectorPolicy.mockImplementation(() => ({
   fetchContractSynchronizationUrl,
   refreshContracts
 }))
 
+const mockKonnector = { slug: 'mockkonnector' }
+const mockAccount = {}
+
+const setup = () => {
+  const client = new CozyClient({})
+  return render(
+    <AppLike client={client}>
+      <BIContractActivationWindow
+        konnector={mockKonnector}
+        account={mockAccount}
+      />
+    </AppLike>
+  )
+}
+
 describe('BIContractActivationWindow', () => {
-  const mockKonnector = { slug: 'mockkonnector' }
-  const mockAccount = {}
-  const setup = () => {
-    const client = new CozyClient({})
-    const root = render(
-      <AppLike client={client}>
-        <BIContractActivationWindow
-          konnector={mockKonnector}
-          account={mockAccount}
-        />
-      </AppLike>
-    )
-    return { root }
-  }
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  afterAll(() => {
+    jest.resetAllMocks()
+  })
 
   it('should display popup with url from policy and update contract after popup is closed', async () => {
+    isFlagshipApp.mockImplementation(() => false)
     fetchContractSynchronizationUrl.mockResolvedValue('bi url')
-    let root
+    const { getByRole } = setup()
     await act(async () => {
-      const res = setup()
-      root = res.root
-      const button = root.getByRole('button')
       await waitFor(() => {
-        expect(button.getAttribute('class')).not.toContain('Mui-disabled')
+        expect(getByRole('button').getAttribute('class')).not.toContain(
+          'Mui-disabled'
+        )
       })
     })
 
     await act(async () => {
-      const button = root.getByRole('button')
-      fireEvent.click(button)
+      fireEvent.click(getByRole('button'))
       await waitFor(() => {
         expect(refreshContracts).toHaveBeenCalled()
       })
     })
 
-    await expect(fetchContractSynchronizationUrl).toHaveBeenCalled()
-    await expect(Popup).toHaveBeenCalledWith(
+    expect(fetchContractSynchronizationUrl).toHaveBeenCalled()
+    expect(refreshContracts).toHaveBeenCalledTimes(1)
+    expect(Popup).toHaveBeenCalledWith(
       expect.objectContaining({
         initialUrl: 'bi url'
+      }),
+      expect.anything()
+    )
+  })
+  it('should call InAppBrowser display if in flagship app context', async () => {
+    isFlagshipApp.mockImplementation(() => true)
+    fetchContractSynchronizationUrl.mockResolvedValue('bi url')
+    const { getByRole } = setup()
+    await act(async () => {
+      await waitFor(() => {
+        expect(getByRole('button').getAttribute('class')).not.toContain(
+          'Mui-disabled'
+        )
+      })
+    })
+
+    await act(async () => {
+      fireEvent.click(getByRole('button'))
+      await waitFor(() => {
+        expect(refreshContracts).toHaveBeenCalled()
+      })
+    })
+
+    expect(fetchContractSynchronizationUrl).toHaveBeenCalled()
+    expect(refreshContracts).toHaveBeenCalledTimes(1)
+    expect(InAppBrowser).toHaveBeenCalledWith(
+      expect.objectContaining({
+        url: 'bi url'
       }),
       expect.anything()
     )
