1666939390a8a3e3721f7a4894d729f7a7468a5c
Merkur39 <merkkur39@gmail.com>
2022-06-03 10:14:49 +0200

    refactor(cozy-mespapiers-lib): Move logic to PlaceholderListModal
    Move all logic and Dialog components to PlaceholderListModal,
    so that PlaceholderThemeList only manages the list of themes

--- packages/cozy-mespapiers-lib/src/components/Placeholders/PlaceholderThemesList.jsx
+++ packages/cozy-mespapiers-lib/src/components/Placeholders/PlaceholderThemesList.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useCallback, useMemo } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import { models } from 'cozy-client'
-import { FixedDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import IconStack from 'cozy-ui/transpiled/react/IconStack'
 import Icon from 'cozy-ui/transpiled/react/Icon'
-import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
 import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
 import ListItemIcon, {
@@ -17,100 +15,49 @@ const {
   themes: { themesList }
 } = models.document
 
-import PlaceholdersList from '../Placeholders/PlaceholdersList'
 import { useScannerI18n } from '../Hooks/useScannerI18n'
 
-const PlaceholderThemesList = ({ title, onClose }) => {
-  const { t } = useI18n()
+const PlaceholderThemesList = ({ setQualifByTheme }) => {
   const scannerT = useScannerI18n()
-  const defaultState = useMemo(
-    () => ({
-      onBack: false,
-      currentQualifItems: [],
-      qualificationLabel: ''
-    }),
-    []
-  )
-  const [state, setState] = useState(defaultState)
 
-  const resetCurrentQualif = useCallback(() => {
-    setState({ ...defaultState, onBack: true })
-  }, [defaultState])
-
-  const setQualifByTheme = useCallback(theme => {
-    setState(prev => ({
-      ...prev,
-      currentQualifItems: theme.items,
-      qualificationLabel: theme.label
-    }))
-  }, [])
-
-  return state.currentQualifItems.length === 0 ? (
-    <FixedDialog
-      onClose={onClose}
-      title={title}
-      transitionDuration={state.onBack ? 0 : undefined}
-      disableGutters
-      open
-      content={
-        <List className="u-mv-half">
-          {themesList.map((theme, idx) => {
-            return (
-              <ListItem
-                button
-                key={idx}
-                onClick={() => setQualifByTheme(theme)}
-              >
-                <ListItemIcon>
-                  <IconStack
-                    backgroundIcon={
-                      <Icon
-                        icon={'file-duotone'}
-                        color="var(--primaryColor)"
-                        size={largeSize}
-                      />
-                    }
-                    foregroundIcon={
-                      <Icon
-                        icon={theme.icon}
-                        color="var(--primaryColor)"
-                        size={smallSize}
-                      />
-                    }
+  return (
+    <List className="u-mv-half">
+      {themesList.map((theme, idx) => {
+        return (
+          <ListItem button key={idx} onClick={() => setQualifByTheme(theme)}>
+            <ListItemIcon>
+              <IconStack
+                backgroundIcon={
+                  <Icon
+                    icon={'file-duotone'}
+                    color="var(--primaryColor)"
+                    size={largeSize}
                   />
-                </ListItemIcon>
-                <ListItemText primary={scannerT(`themes.${theme.label}`)} />
-                <Icon
-                  icon={'right'}
-                  size={smallSize}
-                  color={'var(--secondaryTextColor)'}
-                />
-              </ListItem>
-            )
-          })}
-        </List>
-      }
-    />
-  ) : (
-    <FixedDialog
-      onClose={onClose}
-      onBack={resetCurrentQualif}
-      transitionDuration={0}
-      title={t('PlaceholdersList.title', {
-        name: ` - ${scannerT(`themes.${state.qualificationLabel}`)}`
+                }
+                foregroundIcon={
+                  <Icon
+                    icon={theme.icon}
+                    color="var(--primaryColor)"
+                    size={smallSize}
+                  />
+                }
+              />
+            </ListItemIcon>
+            <ListItemText primary={scannerT(`themes.${theme.label}`)} />
+            <Icon
+              icon={'right'}
+              size={smallSize}
+              color={'var(--secondaryTextColor)'}
+            />
+          </ListItem>
+        )
       })}
-      disableGutters
-      open
-      content={
-        <PlaceholdersList currentQualifItems={state.currentQualifItems} />
-      }
-    />
+    </List>
   )
 }
 
 PlaceholderThemesList.propTypes = {
-  title: PropTypes.string.isRequired,
-  onClose: PropTypes.func.isRequired
+  setQualifByTheme: PropTypes.func.isRequired
 }
 
 export default PlaceholderThemesList
