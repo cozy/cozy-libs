a9ceb98894e61ce1e2246123115dcf291da3d692
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-06-07 11:15:52 +0200

    feat(cozy-intent): Throw error when hooks are used without providers
    for useNativeIntent and useWebviewIntent

--- packages/cozy-intent/src/view/hooks/useNativeIntent.ts
+++ packages/cozy-intent/src/view/hooks/useNativeIntent.ts
@@ -3,5 +3,11 @@ import { useContext } from 'react'
 import { NativeContext } from '../../view'
 import { NativeService } from '../../api'
 
-export const useNativeIntent = (): void | NativeService =>
-  useContext(NativeContext)
+export const useNativeIntent = (): void | NativeService => {
+  const context = useContext(NativeContext)
+
+  if (!context) {
+    throw new Error('NativeContext must be used within a NativeIntentProvider')
+  }
+  return context
+}
--- packages/cozy-intent/src/view/hooks/useWebviewIntent.ts
+++ packages/cozy-intent/src/view/hooks/useWebviewIntent.ts
@@ -3,5 +3,13 @@ import { useContext } from 'react'
 import { WebviewContext } from '../../view'
 import { WebviewService } from '../../api'
 
-export const useWebviewIntent = (): WebviewService | undefined =>
-  useContext(WebviewContext)
+export const useWebviewIntent = (): WebviewService | undefined => {
+  const context = useContext(WebviewContext)
+
+  if (!context) {
+    throw new Error(
+      'useWebviewIntent must be used within a WebviewIntentProvider'
+    )
+  }
+  return context
+}
