dac954344ee7886d8515cc271f6e57ffb5274e1a
Merkur39 <merkkur39@gmail.com>
2022-06-22 16:43:45 +0200

    fix(cozy-sharing): Use `react-router-dom` instead `react-router`

--- packages/cozy-sharing/src/SharingBanner/test/RouterContext.jsx
+++ packages/cozy-sharing/src/SharingBanner/test/RouterContext.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from 'react'
-import { withRouter } from 'react-router'
+import { useLocation, useParams } from 'react-router-dom'
 
 export const RouterContext = React.createContext()
 
-export const RouterContextProvider = withRouter(
-  ({ children, router, params, location, routes }) => {
-    return (
-      <RouterContext.Provider value={{ router, params, location, routes }}>
-        {children}
-      </RouterContext.Provider>
-    )
-  }
-)
+export const RouterContextProvider = ({ children }) => {
+  const location = useLocation()
+  const params = useParams()
+
+  return (
+    <RouterContext.Provider value={{ params, location }}>
+      {children}
+    </RouterContext.Provider>
+  )
+}
 
 export const useRouter = () => {
   return useContext(RouterContext)
