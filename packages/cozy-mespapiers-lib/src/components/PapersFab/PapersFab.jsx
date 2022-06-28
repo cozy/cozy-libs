aef283dab521599eeb22a89a7282740ee0b8acc4
Merkur39 <merkkur39@gmail.com>
2022-06-28 11:24:35 +0200

    feat: Update PaperFab
    PaperFab open an BottomDrawer with 2 choices:
    - Open creation of paper
    - Open multiselect for forward files

--- packages/cozy-mespapiers-lib/src/components/PapersFab/PapersFab.jsx
+++ packages/cozy-mespapiers-lib/src/components/PapersFab/PapersFab.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useState, useRef } from 'react'
 import cx from 'classnames'
 
+import { useClient } from 'cozy-client'
 import makeStyles from 'cozy-ui/transpiled/react/helpers/makeStyles'
-import { useLocation } from 'react-router-dom'
 import Fab from 'cozy-ui/transpiled/react/Fab'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
 import withLocales from '../../locales/withLocales'
+import ActionMenuWrapper from '../Actions/ActionMenuWrapper'
+import { makeActions } from '../Actions/utils'
+import { ActionsItems } from '../Actions/ActionsItems'
+import { select } from '../Actions/Items/select'
+import { createPaper } from '../Actions/Items/createPaper'
 
 const useStyles = makeStyles(() => ({
   fab: {
@@ -20,28 +24,37 @@ const useStyles = makeStyles(() => ({
 }))
 
 const PapersFab = ({ t, className, ...props }) => {
+  const [generalOptions, setGeneralOptions] = useState(false)
+  const actionBtnRef = useRef()
   const { isDesktop } = useBreakpoints()
   const classes = useStyles(isDesktop)
-  const history = useHistory()
-  const { pathname } = useLocation()
+  const client = useClient()
 
-  const handleClick = () => {
-    history.push({
-      pathname: `/paper/create`,
-      search: `backgroundPath=${pathname}`
-    })
-  }
+  const hideActionsMenu = () => setGeneralOptions(false)
+  const toggleActionsMenu = () => setGeneralOptions(prev => !prev)
+  const actions = makeActions([createPaper, select], {
+    client,
+    hideActionsMenu
+  })
 
   return (
-    <Fab
-      color="primary"
-      aria-label={t('Home.Fab.ariaLabel')}
-      className={cx(classes.fab, className)}
-      onClick={handleClick}
-      {...props}
-    >
-      <Icon icon="plus" />
-    </Fab>
+    <>
+      <Fab
+        color="primary"
+        aria-label={t('Home.Fab.ariaLabel')}
+        className={cx(classes.fab, className)}
+        onClick={toggleActionsMenu}
+        {...props}
+      >
+        <Icon icon="plus" />
+      </Fab>
+
+      {generalOptions && (
+        <ActionMenuWrapper onClose={hideActionsMenu} ref={actionBtnRef}>
+          <ActionsItems actions={actions} />
+        </ActionMenuWrapper>
+      )}
+    </>
   )
 }
 
