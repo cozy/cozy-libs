81ee3c7bb7e926eb1e82cb8ce4a8414d754fcc9b
Merkur39 <merkkur39@gmail.com>
2022-06-30 10:16:48 +0200

    feat: Change style of Multiselect components

--- packages/cozy-mespapiers-lib/src/components/Multiselect/MultiselectView.jsx
+++ packages/cozy-mespapiers-lib/src/components/Multiselect/MultiselectView.jsx
@@ -3,27 +3,13 @@ import { useHistory, useLocation } from 'react-router-dom'
 
 import { FixedDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
-import makeStyles from 'cozy-ui/transpiled/react/helpers/makeStyles'
 
 import MultiselectContent from './MultiselectContent'
 import MultiselectViewActions from './MultiselectViewActions'
 import { useMultiSelection } from '../Hooks/useMultiSelection'
 
-const useStyles = makeStyles({
-  paper: {
-    '& .dialogContentInner': {
-      padding: 0,
-      margin: 0,
-      height: '100%',
-      display: 'flex',
-      alignItems: 'center'
-    }
-  }
-})
-
 const MultiselectView = () => {
   const { t } = useI18n()
-  const classes = useStyles()
   const history = useHistory()
   const location = useLocation()
   const { setIsMultiSelectionActive } = useMultiSelection()
@@ -44,7 +30,6 @@ const MultiselectView = () => {
     <FixedDialog
       open
       transitionDuration={0}
-      classes={classes}
       onClose={handleClose}
       title={t('Multiselect.title')}
       content={<MultiselectContent />}
