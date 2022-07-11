faf891970977b2d3a988ef1e63c37b9d0042e18c
doubleface <christophe@cozycloud.cc>
2022-07-11 18:04:27 +0200

    fix: Revert f3b37dc37b63df591fcafe691020f23c4bea460c
    This caused some regressions in home application. The AccountModal
    component would be rerendered infinitely and cause multiple requests
    when only updating a trigger

--- packages/cozy-harvest-lib/src/components/AccountModal.jsx
+++ packages/cozy-harvest-lib/src/components/AccountModal.jsx
@@ -58,24 +58,7 @@ export class AccountModal extends Component {
   }
 
   async componentDidUpdate(prevProps) {
-    const { accountId, accountsAndTriggers } = this.props
-    const matchingTrigger = get(
-      accountsAndTriggers.find(
-        accountAndTrigger => accountAndTrigger.account._id === accountId
-      ),
-      'trigger'
-    )
-
-    const hasTriggerStatusChanged =
-      matchingTrigger &&
-      this.state.trigger &&
-      matchingTrigger?.current_state?.status !==
-        this.state.trigger?.current_state?.status
-
-    if (
-      this.props.accountId !== prevProps.accountId ||
-      hasTriggerStatusChanged
-    ) {
+    if (this.props.accountId !== prevProps.accountId) {
       return await this.loadSelectedAccountId()
     }
   }
