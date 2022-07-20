a405e86d71151f76f927c5e045e71591b82bed6c
Merkur39 <merkkur39@gmail.com>
2022-07-20 19:16:48 +0200

    feat: Add Onboarding component in the possible components

--- packages/cozy-mespapiers-lib/src/helpers/defaultComponent.js
+++ packages/cozy-mespapiers-lib/src/helpers/defaultComponent.js
@@ -1,14 +1,20 @@
 import PapersFab from '../components/PapersFab/PapersFab'
+import Onboarding from '../components/Onboarding/Onboarding'
 
 const defaultComponents = {
-  PapersFab
+  PapersFab,
+  Onboarding
+}
+
+const getComponentByName = (components, name) => {
+  return components?.[name] || components?.[name] === null
+    ? components[name]
+    : defaultComponents[name]
 }
 
 export const getComponents = components => {
   return {
-    PapersFab:
-      components?.PapersFab || components?.PapersFab === null
-        ? components.PapersFab
-        : defaultComponents.PapersFab
+    PapersFab: getComponentByName(components, 'PapersFab'),
+    Onboarding: getComponentByName(components, 'Onboarding')
   }
 }
