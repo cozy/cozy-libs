9023793881740ed362588fce0e8fb4d4ee9b8122
Merkur39 <merkkur39@gmail.com>
2022-09-18 18:17:31 +0200

    feat(mespapiers): Add InformationEditWrapper component
    This component allows to display a modal
    with a input corresponding to the metadata
    you want to modify (text/number or date),
    and their respective constraints

---
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/Edit/InformationEditWrapper.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from 'react'
+import { useParams, useNavigate, Navigate } from 'react-router-dom'
+import cx from 'classnames'
+
+import { isIOS } from 'cozy-device-helper'
+import { useClient } from 'cozy-client'
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
+import { makeStyles } from 'cozy-ui/transpiled/react/styles'
+import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import Spinner from 'cozy-ui/transpiled/react/Spinner'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+
+import { FILES_DOCTYPE } from '../../../doctypes'
+import { useScannerI18n } from '../../Hooks/useScannerI18n'
+import CompositeHeaderImage from '../../CompositeHeader/CompositeHeaderImage'
+import IlluGenericInputText from '../../../assets/icons/IlluGenericInputText.svg'
+import { getInputsInformationStep } from '../../../helpers/getInputsInformationStep'
+import { useCurrentEditInformation } from './useCurrentEditInformation'
+import { isInformationEditPermitted, updateFileMetadata } from './helpers'
+
+const useStyles = makeStyles(() => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+    margin: '3rem 1rem',
+    '&.is-focused': {
+      height: 'initial'
+    },
+    '& img': {
+      width: 'fit-content',
+      margin: '0 auto'
+    }
+  }
+}))
+
+const InformationEditWrapper = () => {
+  const { fileId } = useParams()
+  const client = useClient()
+  const { t } = useI18n()
+  const styles = useStyles()
+  const { isMobile } = useBreakpoints()
+  const scannerT = useScannerI18n()
+  const navigate = useNavigate()
+
+  const [value, setValue] = useState({})
+  const [validInput, setValidInput] = useState({})
+  const [isFocus, setIsFocus] = useState(false)
+  const [isBusy, setIsBusy] = useState(false)
+
+  const currentEditInformation = useCurrentEditInformation(fileId)
+
+  const onClose = () => {
+    navigate(currentEditInformation.searchParams.backgroundPath)
+  }
+
+  const onConfirm = async () => {
+    setIsBusy(true)
+    let newMetadata = updateFileMetadata({
+      file: currentEditInformation.file,
+      type: currentEditInformation.currentStep.attributes[0].type,
+      metadataName: currentEditInformation.searchParams.metadataName,
+      value
+    })
+
+    await client
+      .collection(FILES_DOCTYPE)
+      .updateMetadataAttribute(fileId, newMetadata)
+
+    navigate(currentEditInformation.searchParams.backgroundPath || '/paper')
+  }
+
+  const currentAttributes = currentEditInformation?.currentStep?.attributes
+
+  const { Component, attrs } =
+    makeInputsInformationStep(currentAttributes)[0] || {}
+
+  const dialogTitle = currentEditInformation?.paperDef?.label
+    ? scannerT(`items.${currentEditInformation.paperDef.label}`)
+    : ''
+
+  if (
+    !currentEditInformation.isLoading &&
+    !isInformationEditPermitted(currentEditInformation)
+  ) {
+    return <Navigate to={currentEditInformation.searchParams.backgroundPath} />
+  }
+
+  return (
+    <Dialog
+      open
+      onClose={onClose}
+      title={dialogTitle}
+      content={
+        <div
+          className={cx(styles.container, {
+            'is-focused': isFocus && isIOS()
+          })}
+        >
+          {currentEditInformation.isLoading ? (
+            <Spinner size="xlarge" />
+          ) : (
+            <>
+              <CompositeHeaderImage
+                icon={currentEditInformation.currentStep?.illustration}
+                fallbackIcon={IlluGenericInputText}
+                iconSize="medium"
+              />
+              <div
+                className={cx('u-mt-1', {
+                  'u-mh-1': !isMobile
+                })}
+              >
+                {Component && (
+                  <Component
+                    attrs={attrs}
+                    defaultValue={
+                      currentEditInformation.file.metadata[
+                        currentEditInformation.searchParams.metadataName
+                      ]
+                    }
+                    setValue={setValue}
+                    setValidInput={setValidInput}
+                    setIsFocus={setIsFocus}
+                    idx={0}
+                  />
+                )}
+              </div>
+            </>
+          )}
+        </div>
+      }
+      actions={
+        <Button
+          label={t('common.apply')}
+          onClick={onConfirm}
+          fullWidth
+          disabled={!validInput[0]}
+          busy={isBusy}
+        />
+      }
+    />
+  )
+}
+
+export default InformationEditWrapper
