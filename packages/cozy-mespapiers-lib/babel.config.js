d8c7f15d9505e6262d3c65b029b207a3dafd8012
Merkur39 <merkkur39@gmail.com>
2022-09-18 16:17:36 +0200

    feat(mespapiers): Add stylus configuration
    BREAKING CHANGE: In order to have the css styles specific to some components of `cozy-mespapiers-lib`,
    you need to add the following import to the base of your application:
    `import 'cozy-mespapiers-lib/dist/stylesheet.css'`

--- packages/cozy-mespapiers-lib/babel.config.js
+++ packages/cozy-mespapiers-lib/babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['cozy-app']
+  presets: ['cozy-app'],
+  plugins: [
+    [
+      'css-modules-transform',
+      {
+        extensions: ['.styl'],
+        preprocessCss: './preprocess',
+        extractCss: './dist/stylesheet.css',
+        generateScopedName:
+          process.env['NODE_ENV'] == 'test'
+            ? '[local]'
+            : '[name]__[local]___[hash:base64:5]'
+      }
+    ]
+  ]
 }
