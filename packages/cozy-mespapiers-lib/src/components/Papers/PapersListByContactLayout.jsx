9126007882acd4d0b41ead2195965a8c4a436c9e
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-11-17 14:38:44 +0100

    refactor(mespapiers): Change PapersListToolbar API to be more standalone

--- packages/cozy-mespapiers-lib/src/components/Papers/PapersListByContactLayout.jsx
+++ packages/cozy-mespapiers-lib/src/components/Papers/PapersListByContactLayout.jsx
@@ -1,33 +1,18 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-import { useMultiSelection } from '../Hooks/useMultiSelection'
-import PapersListToolbar from '../Papers/PapersListToolbar'
-import { useScannerI18n } from '../Hooks/useScannerI18n'
-import PapersListByContact from '../Papers/PapersListByContact'
 import { Spinner } from 'cozy-ui/transpiled/react/Spinner'
+
+import PapersListToolbar from '../Papers/PapersListToolbar'
+import PapersListByContact from '../Papers/PapersListByContact'
 
 const PapersListByContactLayout = ({
   currentFileTheme,
   paperslistByContact
 }) => {
-  const navigate = useNavigate()
-  const { setIsMultiSelectionActive, isMultiSelectionActive } =
-    useMultiSelection()
-  const scannerT = useScannerI18n()
-  const themeLabel = scannerT(`items.${currentFileTheme}`)
-
   return (
     <>
-      {!isMultiSelectionActive && (
-        <PapersListToolbar
-          title={themeLabel}
-          onBack={() => navigate('/paper')}
-          onClose={() => setIsMultiSelectionActive(false)}
-        />
-      )}
-
+      <PapersListToolbar currentFileTheme={currentFileTheme} />
       {paperslistByContact.length > 0 ? (
         <PapersListByContact paperslistByContact={paperslistByContact} />
       ) : (
