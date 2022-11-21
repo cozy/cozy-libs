65c58be4cff64e21ee87d0dc48effc20cd1b8bd1
doubleface <christophe@cozycloud.cc>
2022-11-21 19:56:08 +0100

    feat: Use independant OAuth window
    Creation of a new OAuthService class to handle all OAuth communiction
    without depending on React component tree
    
    We had to transform Popup.jsx and OAuthWindow.jsx into functions to
    avoid to be blocked by browser when opening a popup (Too many useEffect
    with render tend to block the popup)

---
+++ packages/cozy-harvest-lib/src/components/OAuthService.js
@@ -0,0 +1,331 @@
+// @ts-check
+import { isFlagshipApp } from 'cozy-device-helper'
+import CozyRealtime from 'cozy-realtime'
+import {
+  prepareOAuth,
+  checkOAuthData,
+  terminateOAuth,
+  OAUTH_REALTIME_CHANNEL
+} from '../helpers/oauth'
+import logger from '../logger'
+import { openWindow } from './PopupService'
+
+const OAUTH_POPUP_HEIGHT = 800
+const OAUTH_POPUP_WIDTH = 800
+
+/**
+ * Monitor URL changes for mobile apps and in app browsers
+ * @param  {import('cozy-client/types/types').KonnectorsDoctype} konnector
+ *
+ * The provider redirect us to something like : oauthcallback.mycozy.cloud/?account=A&state=b
+ * So we listen to the URL change with these informations, and we try to handle it.
+ * It works well on iOS or when we are logged on the same device on the web, but it will
+ * fail on Android if we're ne logged in the browser. Why ?
+ * Our oauthcallback.mycozy.cloud/?account=A&state=b checks if we're logged, if not the server
+ * sends an http redirect.
+ * On Android, the 'loadstart' event is not dispatched by the browser when it get a redirect.
+ * The inAppBrowser follows the URL and arrive on :
+ * https://my.mycozy.cloud/auth?redirect=https://home.cozy.cloud/?account=a&state=b
+ * So if we don't have account & state searchParams we have to check if we've a redirect searchParams
+ * init it and search if we have inside this url an account and state params
+ */
+function handleUrlChange(konnector) {
+  return resolve => {
+    return event => {
+      const { url } = event
+      const oAuthData = extractOAuthDataFromUrl(url)
+      if (oAuthData) {
+        return handleOAuthData(oAuthData, konnector, resolve)
+      }
+      const redirect = url.searchParams.get('redirect')
+      if (redirect) {
+        const redirectOAuthData = extractOAuthDataFromUrl(new URL(redirect))
+        if (redirectOAuthData) {
+          return handleOAuthData(redirectOAuthData, konnector, resolve)
+        }
+      }
+    }
+  }
+}
+
+/**
+ * Handles OAuth data. OAuth data may be provided by different way:
+ * * realtime message from web apps (see handleMessage)
+ * * url changes from mobile apps
+ *
+ * @param {OAuthData} data
+ * @param {import('cozy-client/types/types').KonnectorsDoctype} konnector
+ * @param {Function} resolve - function which will resolve the openOAuthWindow promise
+ * @returns <void>
+ */
+function handleOAuthData(data, konnector, resolve) {
+  if (!checkOAuthData(konnector, data)) return
+
+  if (data.error) {
+    logger.info('OAuthWindow: oauth error message', data.error)
+    resolve({
+      result: 'CANCEL',
+      error: data.error
+    })
+    return
+  }
+
+  resolve({
+    result: 'OK',
+    key: data.key,
+    data
+  })
+}
+
+/**
+ * @typedef OAuthData
+ * @property  {String} key - `io.cozy.accounts` id The created OAuth account
+ * @property  {String} [error] - error message
+ * @property  {String} oAuthStateKey - key for localStorage
+ * @property  {String} [finalLocation] - final query string added to the redirected url
+ */
+
+/**
+ * Extract OAuthData from url search params if any
+ *
+ * @param {URL} url
+ * @returns {OAuthData|null} OAuthData
+ */
+function extractOAuthDataFromUrl(url) {
+  const account = url.searchParams.get('account')
+  const state = url.searchParams.get('state')
+  const error = url.searchParams.get('error')
+  if (account && state) {
+    const oAuthData = { key: account, oAuthStateKey: state }
+    if (error) {
+      oAuthData.error = error
+    }
+    return oAuthData
+  }
+  return null
+}
+
+/**
+ * @typedef OAuthServiceResult
+ * @property {"OK"|"CLOSED"|"DISMISSED"|"ERROR"} result
+ * @property {OAuthData} [data]
+ */
+
+/**
+ * Opens given url in the webview intent inAppBrowser
+ *
+ * @param {Object} options
+ * @param {String} options.oAuthUrl - final url to open in webview intent inAppBrowser
+ * @param {import('cozy-intent').WebviewService} options.webviewIntent - WebviewService object to use
+ * @param {Function} options.registerRealtime
+ * @returns {Promise<OAuthServiceResult>}
+ */
+async function openWebviewIntentInAppBrowser({
+  oAuthUrl,
+  webviewIntent,
+  registerRealtime
+}) {
+  return new Promise((resolve, reject) => {
+    const doExec = async () => {
+      if (!webviewIntent) {
+        return reject('No webviewIntent available')
+      }
+
+      try {
+        registerRealtime(data => {
+          webviewIntent.call('closeInAppBrowser')
+          return resolve(data)
+        })
+        logger.debug('url at the beginning: ', oAuthUrl)
+        const sessionCode = await webviewIntent.call('fetchSessionCode')
+        logger.debug('got session code', sessionCode)
+        const iabUrl = new URL(oAuthUrl)
+        // @ts-ignore wrong return type associated to webviewIntent.call
+        iabUrl.searchParams.append('session_code', sessionCode)
+        // we need to decodeURIComponent since toString() encodes URL
+        // but native browser will also encode them.
+        const urlToOpen = decodeURIComponent(iabUrl.toString())
+        logger.debug('url to open: ', urlToOpen)
+        const result = await webviewIntent.call('showInAppBrowser', {
+          url: urlToOpen
+        })
+        // @ts-ignore wrong return type associated to webviewIntent.call
+        if (result?.type !== 'dismiss' && result?.type !== 'cancel') {
+          // @ts-ignore wrong return type associated to webviewIntent.call
+          resolve({ result: 'ERROR', error: result })
+          return
+        }
+
+        webviewIntent.call('closeInAppBrowser')
+        resolve({ result: 'CLOSED' })
+        return
+      } catch (err) {
+        webviewIntent.call('closeInAppBrowser')
+        resolve({ result: 'ERROR', error: err })
+        return
+      }
+    }
+    doExec()
+  })
+}
+
+/**
+ * @typedef IntentsApi
+ * @property {Function} fetchSessionCode
+ * @property {Function} showInAppBrowser
+ * @property {Function} closeInAppBrowser
+ * @property {String} tokenParamName
+ */
+
+/**
+ * Opens given url in the intentsApi inAppBrowser
+ *
+ * @param {Object} options
+ * @param {String} options.oAuthUrl - final url to open in the intentsApi inAppBrowser
+ * @param {IntentsApi} options.intentsApi - WebviewService object to use
+ * @param {Function} options.registerRealtime
+ * @returns {Promise<OAuthServiceResult>}
+ */
+function openIntentsApiInAppBrowser({
+  oAuthUrl,
+  registerRealtime,
+  intentsApi
+}) {
+  return new Promise((resolve, reject) => {
+    const doExec = async () => {
+      const {
+        fetchSessionCode,
+        showInAppBrowser,
+        closeInAppBrowser,
+        tokenParamName = 'session_code'
+      } = intentsApi
+
+      const isReady = Boolean(
+        intentsApi?.fetchSessionCode &&
+          intentsApi?.showInAppBrowser &&
+          intentsApi?.closeInAppBrowser
+      )
+
+      if (!isReady) {
+        return reject('Missing method in intentsApi')
+      }
+      try {
+        registerRealtime(data => {
+          closeInAppBrowser()
+          return resolve(data)
+        })
+        const sessionCode = await fetchSessionCode()
+        logger.debug('got session code', sessionCode)
+        const iabUrl = new URL(oAuthUrl)
+        iabUrl.searchParams.append(tokenParamName, sessionCode)
+        // we need to decodeURIComponent since toString() encodes URL
+        // but native browser will also encode them.
+        const urlToOpen = decodeURIComponent(iabUrl.toString())
+        logger.debug('url to open: ', urlToOpen)
+        const result = await showInAppBrowser(urlToOpen)
+        if (result?.state !== 'dismiss' && result?.state !== 'cancel') {
+          return resolve({ result: 'ERROR', error: result })
+        }
+        return resolve({ result: 'CLOSED' })
+      } catch (err) {
+        return resolve({ result: 'ERROR', error: err })
+      }
+    }
+    doExec()
+  })
+}
+
+/**
+ * Register to realtime events
+ *
+ * @param {Object} options
+ * @param {Object} options.client
+ * @param {import('cozy-client/types/types').KonnectorsDoctype} options.konnector
+ * @returns  {Function}
+ */
+function registerRealtime({ client, konnector }) {
+  return resolve => {
+    const realtime = new CozyRealtime({ client })
+    realtime.subscribe(
+      'notified',
+      'io.cozy.accounts',
+      OAUTH_REALTIME_CHANNEL,
+      handleRealtime(konnector, resolve)
+    )
+    return () => realtime.unsubscribeAll()
+  }
+}
+
+function handleRealtime(konnector, resolve) {
+  return event => {
+    return handleOAuthData(event.data, konnector, resolve)
+  }
+}
+
+/**
+ * Open an OAuth window in popup or inAppBrowser
+ *
+ * @param {Object} options
+ * @param {Object} options.client - CozyClient instance
+ * @param {import('cozy-client/types/types').KonnectorsDoctype} options.konnector - current manifest of the connector
+ * @param {String}  [options.redirectSlug] - slug of the app to redirect to after OAuth
+ * @param {Object} options.extraParams - any extra parameters to pass in the oauth url
+ * @param {Boolean} [options.reconnect] - if this is a reconnection or not
+ * @param {Boolean} [options.manage] - if we try to manage an existing oauth connection
+ * @param {String} [options.title] - title of the resulting popup
+ * @param {import('cozy-intent').WebviewService} [options.webviewIntent] - WebviewService object to use
+ * @param {import('cozy-client/types/types').AccountsDoctype} options.account - associated account (useful if in reconnection or manage mode)
+ * @param {IntentsApi} options.intentsApi - Intentsapi object to use to communicate with the host native application
+ * @returns {Promise<OAuthServiceResult>}
+ */
+export function openOAuthWindow({
+  client,
+  konnector,
+  redirectSlug,
+  extraParams,
+  reconnect,
+  manage,
+  title,
+  webviewIntent,
+  account,
+  intentsApi
+}) {
+  const { oAuthStateKey, oAuthUrl } = prepareOAuth({
+    client,
+    konnector,
+    redirectSlug,
+    extraParams,
+    reconnect,
+    manage,
+    account
+  })
+
+  let promise
+  if (intentsApi) {
+    promise = openIntentsApiInAppBrowser({
+      oAuthUrl,
+      intentsApi,
+      registerRealtime: registerRealtime({ konnector, client })
+    })
+  } else if (isFlagshipApp() && webviewIntent) {
+    promise = openWebviewIntentInAppBrowser({
+      oAuthUrl,
+      webviewIntent,
+      registerRealtime: registerRealtime({ konnector, client })
+    })
+  } else {
+    promise = openWindow({
+      url: oAuthUrl,
+      title: title || '',
+      width: OAUTH_POPUP_WIDTH,
+      height: OAUTH_POPUP_HEIGHT,
+      handleUrlChange: handleUrlChange(konnector),
+      registerRealtime: registerRealtime({ konnector, client })
+    })
+  }
+
+  return promise.then(result => {
+    terminateOAuth(oAuthStateKey)
+    return result
+  })
+}
---
+++ packages/cozy-harvest-lib/src/components/OAuthService.spec.js
@@ -0,0 +1,258 @@
+import MicroEE from 'microee'
+import util from 'util'
+import { openOAuthWindow } from 'components/OAuthService'
+import { isMobileApp, isFlagshipApp } from 'cozy-device-helper'
+
+import PopupMock from '../helpers/windowWrapperMocks'
+
+import { OAUTH_REALTIME_CHANNEL, terminateOAuth } from '../helpers/oauth'
+
+jest.mock('../helpers/oauth', () => {
+  return {
+    ...jest.requireActual('../helpers/oauth'),
+    terminateOAuth: jest.fn()
+  }
+})
+
+const mockPopup = new PopupMock()
+jest.mock('../helpers/windowWrapper', () => ({
+  windowOpen: jest.fn().mockImplementation(() => {
+    return mockPopup
+  })
+}))
+jest.mock('uuid/v4', () => {
+  return () => 'someuuid'
+})
+
+function CozyRealtimeMock() {
+  this.subscribe = jest
+    .fn()
+    .mockImplementation((eventType, doctype, channel, fn) => {
+      this.on(eventType + doctype + channel, fn)
+    })
+  this.unsubscribeAll = jest.fn().mockImplementation(() => {
+    this.removeAllListeners()
+  })
+
+  this.emitRealtimeEvent = (eventType, doctype, channel, event) => {
+    this.emit(eventType + doctype + channel, event)
+  }
+
+  this.clear = () => {
+    this.removeAllListeners()
+    this.subscribe.mockClear()
+    this.unsubscribeAll.mockClear()
+  }
+}
+MicroEE.mixin(CozyRealtimeMock)
+
+const mockCozyRealtime = new CozyRealtimeMock()
+jest.mock('cozy-realtime', () => {
+  return function CozyRealtime() {
+    return mockCozyRealtime
+  }
+})
+
+const client = {
+  stackClient: {
+    uri: 'https://claud.mycozy.cloud'
+  },
+  appMetadata: { slug: 'home' }
+}
+
+jest.mock('cozy-device-helper', () => ({
+  isMobileApp: jest.fn(),
+  isFlagshipApp: jest.fn()
+}))
+
+describe('OAuthService', () => {
+  describe('openOAuthWindow', () => {
+    beforeEach(() => {
+      mockPopup.clear()
+      mockCozyRealtime.clear()
+    })
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    describe('on Flagship app', () => {
+      beforeEach(() => {
+        isFlagshipApp.mockReturnValue(true)
+      })
+
+      it('should render', async () => {
+        const webviewIntent = {
+          call: jest.fn().mockImplementation(fnName => {
+            if (fnName === 'fetchSessionCode') {
+              return Promise.resolve('somesessioncode')
+            } else if (fnName === 'showInAppBrowser') {
+              return Promise.resolve({ result: 'dismiss' })
+            } else if (fnName === 'closeInAppBrowser') {
+              return Promise.resolve()
+            }
+          })
+        }
+        const promise = openOAuthWindow({
+          client,
+          konnector: { slug: 'ameli' },
+          redirectSlug: 'home',
+          extraParams: { extraParam1: 'extraParamValue' },
+          title: 'popup title',
+          account: { _id: 'someaccountid' },
+          webviewIntent
+        })
+
+        expect(isPending(promise)).toBe(true)
+        expect(terminateOAuth).not.toHaveBeenCalled()
+
+        mockCozyRealtime.emitRealtimeEvent(
+          'notified',
+          'io.cozy.accounts',
+          OAUTH_REALTIME_CHANNEL,
+          {
+            _id: 'oauth-popup',
+            data: {
+              error: null,
+              finalLocation: 'connection_id=194&state=someuuid',
+              key: null,
+              oAuthStateKey: 'someuuid'
+            }
+          }
+        )
+        const result = await promise
+        expect(result).toStrictEqual({
+          result: 'OK',
+          key: null,
+          data: {
+            error: null,
+            finalLocation: 'connection_id=194&state=someuuid',
+            key: null,
+            oAuthStateKey: 'someuuid'
+          }
+        })
+
+        expect(terminateOAuth).toHaveBeenCalled()
+      })
+    })
+
+    describe('with intentsApi', () => {
+      beforeEach(() => {
+        isMobileApp.mockReturnValue(true)
+      })
+
+      it('should render', async () => {
+        let closePromise = new Promise(() => {})
+        const intentsApi = {
+          fetchSessionCode: jest.fn().mockResolvedValue('somesessioncode'),
+          showInAppBrowser: jest.fn().mockImplementation(() => {
+            return closePromise
+          }),
+          closeInAppBrowser: jest.fn().mockResolvedValue()
+        }
+        const promise = openOAuthWindow({
+          client,
+          konnector: { slug: 'ameli' },
+          redirectSlug: 'home',
+          extraParams: { extraParam1: 'extraParamValue' },
+          title: 'popup title',
+          account: { _id: 'someaccountid' },
+          intentsApi
+        })
+
+        expect(isPending(promise)).toBe(true)
+        expect(terminateOAuth).not.toHaveBeenCalled()
+
+        await sleep(100) // lets time to show inAppBrowser before realtime event occurs
+
+        mockCozyRealtime.emitRealtimeEvent(
+          'notified',
+          'io.cozy.accounts',
+          OAUTH_REALTIME_CHANNEL,
+          {
+            _id: 'oauth-popup',
+            data: {
+              error: null,
+              finalLocation: 'connection_id=194&state=someuuid',
+              key: null,
+              oAuthStateKey: 'someuuid'
+            }
+          }
+        )
+        const result = await promise
+        expect(result).toStrictEqual({
+          result: 'OK',
+          key: null,
+          data: {
+            error: null,
+            finalLocation: 'connection_id=194&state=someuuid',
+            key: null,
+            oAuthStateKey: 'someuuid'
+          }
+        })
+
+        expect(terminateOAuth).toHaveBeenCalled()
+        expect(intentsApi.fetchSessionCode).toHaveBeenCalledTimes(1)
+        expect(intentsApi.showInAppBrowser).toHaveBeenCalled() // TODO test url mock nonce
+        expect(intentsApi.closeInAppBrowser).toHaveBeenCalled()
+      })
+    })
+
+    describe('on web', () => {
+      beforeEach(() => {
+        isMobileApp.mockReturnValue(false)
+        isFlagshipApp.mockReturnValue(false)
+      })
+
+      it('should render', async () => {
+        const promise = openOAuthWindow({
+          client,
+          konnector: { slug: 'ameli' },
+          redirectSlug: 'home',
+          extraParams: { extraParam1: 'extraParamValue' },
+          title: 'popup title',
+          account: { _id: 'someaccountid' }
+        })
+
+        expect(isPending(promise)).toBe(true)
+        expect(terminateOAuth).not.toHaveBeenCalled()
+
+        mockCozyRealtime.emitRealtimeEvent(
+          'notified',
+          'io.cozy.accounts',
+          OAUTH_REALTIME_CHANNEL,
+          {
+            _id: 'oauth-popup',
+            data: {
+              error: null,
+              finalLocation: 'connection_id=194&state=someuuid',
+              key: null,
+              oAuthStateKey: 'someuuid'
+            }
+          }
+        )
+        const result = await promise
+        expect(result).toStrictEqual({
+          result: 'OK',
+          key: null,
+          data: {
+            error: null,
+            finalLocation: 'connection_id=194&state=someuuid',
+            key: null,
+            oAuthStateKey: 'someuuid'
+          }
+        })
+
+        expect(terminateOAuth).toHaveBeenCalled()
+      })
+    })
+  })
+})
+
+const isPending = promise => {
+  return util.inspect(promise).includes('pending')
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
