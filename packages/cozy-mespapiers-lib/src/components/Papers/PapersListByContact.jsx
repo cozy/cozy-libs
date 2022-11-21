1d185f064f316856af1bd3b6267ed77b53032185
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-11-21 11:40:02 +0100

    refactor(mespapiers): Move logic from PapersList to PapersListByContact
    to separate queries and data manipulation

--- packages/cozy-mespapiers-lib/src/components/Papers/PapersListByContact.jsx
+++ packages/cozy-mespapiers-lib/src/components/Papers/PapersListByContact.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
+import { useParams } from 'react-router-dom'
 
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
 import ListSubheader from 'cozy-ui/transpiled/react/MuiCozyTheme/ListSubheader'
 
+import { DEFAULT_MAX_FILES_DISPLAYED } from '../../constants/const'
+import { usePapersDefinitions } from '../Hooks/usePapersDefinitions'
+import { buildFilesByContacts, getCurrentFileTheme } from '../Papers/helpers'
 import PapersList from '../Papers/PapersList'
 
-const PapersListByContact = ({ paperslistByContact }) => {
+const PapersListByContact = ({ selectedThemeLabel, files, contacts }) => {
+  const params = useParams()
+  const { t } = useI18n()
+  const { papersDefinitions } = usePapersDefinitions()
+
+  const currentFileTheme = getCurrentFileTheme(params, selectedThemeLabel)
+  const currentDefinition = useMemo(
+    () =>
+      papersDefinitions.find(paperDef => paperDef.label === currentFileTheme),
+    [papersDefinitions, currentFileTheme]
+  )
+
+  const paperslistByContact = useMemo(
+    () =>
+      buildFilesByContacts({
+        files,
+        contacts,
+        maxDisplay:
+          currentDefinition?.maxDisplay || DEFAULT_MAX_FILES_DISPLAYED,
+        t
+      }),
+    [contacts, currentDefinition, files, t]
+  )
+
   return paperslistByContact.map(({ withHeader, contact, papers }, idx) => (
     <List
       subheader={withHeader && <ListSubheader>{contact}</ListSubheader>}
@@ -18,7 +46,9 @@ const PapersListByContact = ({ paperslistByContact }) => {
 }
 
 PapersListByContact.propTypes = {
-  paperslistByContact: PropTypes.arrayOf(PropTypes.object)
+  selectedThemeLabel: PropTypes.arrayOf(PropTypes.object),
+  files: PropTypes.arrayOf(PropTypes.object),
+  contacts: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default PapersListByContact
