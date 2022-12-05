cd7e22bdb9c5f7addacb1230e788b4a3a03c1126
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-12-05 11:50:24 +0100

    fix(harvest): Use correct button to close CozyConfirmDialog

--- packages/cozy-harvest-lib/src/components/CozyConfirmDialogProvider/index.jsx
+++ packages/cozy-harvest-lib/src/components/CozyConfirmDialogProvider/index.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState } from 'react'
-import Buttons from 'cozy-ui/transpiled/react/MuiCozyTheme/Buttons'
+
+import Button from 'cozy-ui/transpiled/react/Buttons'
 import { ConfirmDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 
 export const CozyConfirmDialogContext = createContext()
@@ -19,12 +20,14 @@ export const useCozyConfirmDialog = () => {
 export const CozyConfirmDialogProvider = React.memo(props => {
   const [dialogs, setDialogs] = useState([])
   const { children } = props
+
   const onClose = dialogId => {
     dialogs.find(dialog => dialog.id === dialogId).callback()
     setDialogs(prevDialogs =>
       prevDialogs.filter(dialog => dialog.id !== dialogId)
     )
   }
+
   const context = {
     showDialog: ({ title, closeLabel, description }) => {
       return new Promise(resolve => {
@@ -43,29 +46,25 @@ export const CozyConfirmDialogProvider = React.memo(props => {
       })
     }
   }
+
   return (
     <CozyConfirmDialogContext.Provider value={context}>
       {dialogs &&
         dialogs.map(dialog => {
           return (
             <ConfirmDialog
+              open
               key={dialog.id}
-              open={true}
               title={dialog.title}
               content={dialog.description}
-              onClose={() => onClose(dialog.id)}
               actions={
-                <>
-                  <Buttons
-                    variant="text"
-                    color="primary"
-                    aria-label="Close dialog"
-                    onClick={() => onClose(dialog.id)}
-                  >
-                    {dialog.closeLabel}
-                  </Buttons>
-                </>
+                <Button
+                  label={dialog.closeLabel}
+                  aria-label="Close dialog"
+                  onClick={() => onClose(dialog.id)}
+                />
               }
+              onClose={() => onClose(dialog.id)}
             />
           )
         })}
