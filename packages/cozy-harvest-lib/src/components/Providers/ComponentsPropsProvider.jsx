406a0739df1f2a90fb202348bc2ff8ef4bac1d21
Merkur39 <merkkur39@gmail.com>
2022-12-15 12:32:18 +0100

    feat(harvest): Add OverloadComponentsProvider component
    This is a very basic implementation,
    which only works with adding content to the
    `CannotConnectModal` component (via `extraContent`)

---
+++ packages/cozy-harvest-lib/src/components/Providers/ComponentsPropsProvider.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const ComponentsPropsContext = React.createContext()
+
+/**
+ * @typedef {object} ComponentsProps
+ * @property {object} CannotConnectModal - Name of component
+ * @property {object} CannotConnectModal.extraContent - Additional content
+ */
+
+/**
+ * @param {object} param
+ * @param {JSX.Element} param.children
+ * @param {ComponentsProps} param.ComponentsProps
+ * @returns
+ */
+const ComponentsPropsProvider = ({ children, ComponentsProps }) => {
+  return (
+    <ComponentsPropsContext.Provider value={{ ComponentsProps }}>
+      {children}
+    </ComponentsPropsContext.Provider>
+  )
+}
+
+export const useComponentsProps = () => {
+  const componentsProps = React.useContext(ComponentsPropsContext)
+  if (!componentsProps) {
+    throw new Error(
+      'ComponentsPropsContext must be used within a ComponentsPropsProvider'
+    )
+  }
+
+  return componentsProps
+}
+
+export default ComponentsPropsProvider
