525af210caa45b77bb79abf8206b14767edb9c52
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-12-19 12:33:14 +0100

    refactor(harvest): Extract routes v4 from Routes

---
+++ packages/cozy-harvest-lib/src/components/Routes/RoutesV4.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import { ViewerModal } from '../../datacards/ViewerModal'
+import AccountModal from '../AccountModal'
+import NewAccountModal from '../NewAccountModal'
+import EditAccountModal from '../EditAccountModal'
+import KonnectorSuccess from '../KonnectorSuccess'
+import HarvestModalRoot from '../HarvestModalRoot'
+
+const RoutesV4 = ({
+  konnectorRoot,
+  konnectorWithTriggers,
+  accountsAndTriggers,
+  onDismiss
+}) => {
+  return (
+    <Switch>
+      <Route
+        path={`${konnectorRoot}/`}
+        exact
+        render={() => (
+          <HarvestModalRoot
+            accounts={accountsAndTriggers}
+            konnector={konnectorWithTriggers}
+          />
+        )}
+      />
+      <Route
+        path={`${konnectorRoot}/accounts/:accountId`}
+        exact
+        render={({ match }) => (
+          <AccountModal
+            konnector={konnectorWithTriggers}
+            accountId={match.params.accountId}
+            accountsAndTriggers={accountsAndTriggers}
+            onDismiss={onDismiss}
+            showNewAccountButton={!konnectorWithTriggers.clientSide}
+            showAccountSelection={!konnectorWithTriggers.clientSide}
+          />
+        )}
+      />
+      <Route
+        path={`${konnectorRoot}/accounts/:accountId/edit`}
+        exact
+        render={({ match }) => (
+          <EditAccountModal
+            konnector={konnectorWithTriggers}
+            accountId={match.params.accountId}
+            accounts={accountsAndTriggers}
+          />
+        )}
+      />
+      <Route
+        path={`${konnectorRoot}/viewer/:accountId/:folderToSaveId/:fileIndex`}
+        exact
+        render={routeComponentProps => <ViewerModal {...routeComponentProps} />}
+      />
+      <Route
+        path={`${konnectorRoot}/new`}
+        exact
+        render={() => (
+          <NewAccountModal
+            konnector={konnectorWithTriggers}
+            onDismiss={onDismiss}
+          />
+        )}
+      />
+      <Route
+        path={`${konnectorRoot}/accounts/:accountId/success`}
+        exact
+        render={({ match }) => {
+          return (
+            <KonnectorSuccess
+              konnector={konnectorWithTriggers}
+              accountId={match.params.accountId}
+              accounts={accountsAndTriggers}
+              onDismiss={onDismiss}
+            />
+          )
+        }}
+      />
+      <Redirect from={`${konnectorRoot}/*`} to={`${konnectorRoot}/`} />
+    </Switch>
+  )
+}
+
+export default RoutesV4
