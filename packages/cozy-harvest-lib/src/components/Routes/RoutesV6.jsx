9927d782bd1608225537c4062090cb104e40ee27
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-12-20 16:23:38 +0100

    feat(harvest): Add routes v6

---
+++ packages/cozy-harvest-lib/src/components/Routes/RoutesV6.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import { Routes, Route, Navigate, useParams } from 'react-router-dom'
+
+import { ViewerModal } from '../../datacards/ViewerModal'
+import AccountModal from '../AccountModal'
+import NewAccountModal from '../NewAccountModal'
+import EditAccountModal from '../EditAccountModal'
+import KonnectorSuccess from '../KonnectorSuccess'
+import HarvestModalRoot from '../HarvestModalRoot'
+
+const HarvestParamsWrapper = props => {
+  const params = useParams()
+  return props.children(params)
+}
+
+const RoutesV6 = ({
+  konnectorWithTriggers,
+  accountsAndTriggers,
+  onDismiss
+}) => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <HarvestModalRoot
+            accounts={accountsAndTriggers}
+            konnector={konnectorWithTriggers}
+          />
+        }
+      />
+
+      <Route
+        path="new"
+        element={
+          <NewAccountModal
+            konnector={konnectorWithTriggers}
+            onDismiss={onDismiss}
+          />
+        }
+      />
+
+      <Route
+        path="accounts/:accountId"
+        element={
+          <HarvestParamsWrapper>
+            {params => (
+              <AccountModal
+                konnector={konnectorWithTriggers}
+                accountId={params.accountId}
+                accountsAndTriggers={accountsAndTriggers}
+                onDismiss={onDismiss}
+                showNewAccountButton={!konnectorWithTriggers.clientSide}
+                showAccountSelection={!konnectorWithTriggers.clientSide}
+              />
+            )}
+          </HarvestParamsWrapper>
+        }
+      />
+      <Route
+        path="accounts/:accountId/edit"
+        element={
+          <HarvestParamsWrapper>
+            {params => (
+              <EditAccountModal
+                konnector={konnectorWithTriggers}
+                accountId={params.accountId}
+                accounts={accountsAndTriggers}
+              />
+            )}
+          </HarvestParamsWrapper>
+        }
+      />
+      <Route
+        path="accounts/:accountId/success"
+        element={
+          <HarvestParamsWrapper>
+            {params => (
+              <KonnectorSuccess
+                konnector={konnectorWithTriggers}
+                accountId={params.accountId}
+                accounts={accountsAndTriggers}
+                onDismiss={onDismiss}
+              />
+            )}
+          </HarvestParamsWrapper>
+        }
+      />
+
+      <Route
+        path="viewer/:accountId/:folderToSaveId/:fileIndex"
+        element={routeComponentProps => (
+          <ViewerModal {...routeComponentProps} />
+        )}
+      />
+
+      {/* Redirections */}
+      <Route path="*" element={<Navigate replace to="/" />} />
+    </Routes>
+  )
+}
+
+export default RoutesV6
