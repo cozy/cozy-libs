0056a9c5ce8ac90e9df49633e154c6537aeff16e
Merkur39 <merkkur39@gmail.com>
2022-12-20 16:38:02 +0100

    feat(Harvest): See the different states of a connector
    Update the `makeLabel` function to return a different label
    if the synchronization was done less than 5 minutes ago

--- packages/cozy-harvest-lib/src/components/cards/helpers.js
+++ packages/cozy-harvest-lib/src/components/cards/helpers.js
@@ -1,13 +1,38 @@
+import { formatLocallyDistanceToNow } from 'cozy-ui/transpiled/react/I18n/format'
+
+const DEFAULT_TIME = 300_000 // To milliseconds (5 minutes)
+
+const getDifferenceInMinutes = date => {
+  return Date.now() - new Date(date).getTime()
+}
+
+/**
+ * @param {object} options
+ * @param {object} options.t - i18n function
+ * @param {object} options.running - If the connector is running
+ * @param {object} options.expectingTriggerLaunch - If the trigger is waiting to be launched
+ * @param {object} options.lastSuccessDate - The last date when the trigger was successfully executed
+ * @returns {string}
+ */
 export const makeLabel = ({
   t,
-  f,
   running,
   expectingTriggerLaunch,
   lastSuccessDate
 }) => {
-  return running || expectingTriggerLaunch
-    ? t('card.launchTrigger.lastSync.syncing')
-    : lastSuccessDate
-    ? f(lastSuccessDate, t('card.launchTrigger.lastSync.format'))
-    : t('card.launchTrigger.lastSync.unknown')
+  if (running || expectingTriggerLaunch) {
+    return t('card.launchTrigger.lastSync.syncing')
+  }
+
+  if (lastSuccessDate) {
+    if (getDifferenceInMinutes(lastSuccessDate) < DEFAULT_TIME) {
+      return t('card.launchTrigger.lastSync.justNow')
+    }
+
+    return t('card.launchTrigger.lastSync.afterSomeTimes', {
+      times: formatLocallyDistanceToNow(lastSuccessDate)
+    })
+  }
+
+  return t('card.launchTrigger.lastSync.unknown')
 }
