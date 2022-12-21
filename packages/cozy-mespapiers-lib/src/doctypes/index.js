2848ab206df8f0154cb50bd8e76e1ba4dc7fb750
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-12-21 11:47:59 +0100

    feat(mespapiers): Add harvest routes

--- packages/cozy-mespapiers-lib/src/doctypes/index.js
+++ packages/cozy-mespapiers-lib/src/doctypes/index.js
@@ -4,6 +4,8 @@ export const FILES_DOCTYPE = 'io.cozy.files'
 export const PERMISSIONS_DOCTYPE = 'io.cozy.permissions'
 export const SETTINGS_DOCTYPE = 'io.cozy.mespapiers.settings'
 export const JOBS_DOCTYPE = 'io.cozy.jobs'
+export const TRIGGERS_DOCTYPE = 'io.cozy.triggers'
+export const KONNECTORS_DOCTYPE = 'io.cozy.konnectors'
 
 // the documents schema, necessary for CozyClient
 export default {
