e60c1911885208dc01d3e22d8b1fba5a9dbc37b6
Théo Poizat <hello@zatteo.com>
2022-12-29 10:53:11 +0100

    refactor: ShareByLink from class to functional component

--- packages/cozy-sharing/src/components/ShareByLink.jsx
+++ packages/cozy-sharing/src/components/ShareByLink.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import copy from 'copy-text-to-clipboard'
 import { models } from 'cozy-client'
@@ -10,7 +10,7 @@ import Icon from 'cozy-ui/transpiled/react/Icon'
 import Radio from 'cozy-ui/transpiled/react/Radio'
 import DropdownButton from 'cozy-ui/transpiled/react/DropdownButton'
 import ActionMenu, { ActionMenuItem } from 'cozy-ui/transpiled/react/ActionMenu'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import get from 'lodash/get'
 import logger from '../logger'
 import styles from '../share.styl'
@@ -35,67 +35,56 @@ const checkIsReadOnlyPermissions = permissions => {
   )
 }
 
-class ShareByLink extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: false,
-      menuIsOpen: false
-    }
+const ShareByLink = ({
+  link,
+  documentType,
+  document,
+  permissions,
+  onChangePermissions,
+  onEnable,
+  onDisable,
+  checked,
+  popperOptions
+}) => {
+  const { t } = useI18n()
+  const [loading, setLoading] = useState(false)
+  const [menuIsOpen, setMenuIsOpen] = useState(false)
 
-    this.containerRef = React.createRef()
-  }
+  const containerRef = useRef()
 
-  toggleShareLink = checked => {
-    if (checked) {
-      this.createShareLink()
-    } else {
-      this.deleteShareLink()
-    }
-  }
-
-  copyLinkToClipboard = ({ isAutomaticCopy }) => {
-    if (copy(this.props.link))
-      Alerter.success(
-        this.props.t(`${this.props.documentType}.share.shareByLink.copied`)
-      )
+  const copyLinkToClipboard = ({ isAutomaticCopy }) => {
+    if (copy(link))
+      Alerter.success(t(`${documentType}.share.shareByLink.copied`))
     else if (!isAutomaticCopy)
       // In case of automatic copy, the browser can block the copy request. This is not shown to the user since it is expected and can be circumvented by clicking directly on the copy link
-      Alerter.error(
-        this.props.t(`${this.props.documentType}.share.shareByLink.failed`)
-      )
+      Alerter.error(t(`${documentType}.share.shareByLink.failed`))
   }
 
-  async createShareLink() {
+  const createShareLink = async () => {
     try {
-      this.setState(state => ({ ...state, loading: true }))
-      await this.props.onEnable(this.props.document)
+      setLoading(true)
+      await onEnable(document)
     } catch (e) {
-      Alerter.error(
-        this.props.t(`${this.props.documentType}.share.error.generic`)
-      )
+      Alerter.error(t(`${documentType}.share.error.generic`))
       logger.log(e)
     } finally {
-      this.setState(state => ({ ...state, loading: false }))
+      setLoading(false)
     }
   }
 
-  async deleteShareLink() {
+  const deleteShareLink = async () => {
     try {
-      this.setState(state => ({ ...state, loading: true }))
-      await this.props.onDisable(this.props.document)
+      setLoading(true)
+      await onDisable(document)
     } catch (e) {
-      Alerter.error(
-        this.props.t(`${this.props.documentType}.share.error.revoke`)
-      )
+      Alerter.error(t(`${documentType}.share.error.revoke`))
       logger.log(e)
     } finally {
-      this.setState(state => ({ ...state, loading: false }))
+      setLoading(false)
     }
   }
 
-  updateLinkPermissions({ isReadOnly }) {
-    const { document, documentType, onChangePermissions, t } = this.props
+  const updateLinkPermissions = ({ isReadOnly }) => {
     const verbs = isReadOnly ? ['GET'] : ['GET', 'POST', 'PUT', 'PATCH']
     try {
       onChangePermissions(document, verbs)
@@ -105,156 +94,151 @@ class ShareByLink extends React.Component {
     }
   }
 
-  toggleMenu = () => {
-    this.setState(state => ({ ...state, menuIsOpen: !state.menuIsOpen }))
+  const toggleMenu = () => {
+    setMenuIsOpen(!menuIsOpen)
   }
 
-  render() {
-    const { loading, menuIsOpen } = this.state
-    const { checked, documentType, permissions, t, popperOptions } = this.props
+  const hasReadOnlyPermissions = checkIsReadOnlyPermissions(permissions)
 
-    const hasReadOnlyPermissions = checkIsReadOnlyPermissions(permissions)
-
-    return (
-      <div className="u-w-100">
-        <div className="u-flex u-flex-row u-flex-justify-between u-flex-items-center">
-          <Typography variant="body1">
-            {t(`${documentType}.share.shareByLink.subtitle`)}
-          </Typography>
-          {!checked && (
-            <Button
-              style={{ position: 'initial' }} // fix z-index bug on iOS when under a BottomDrawer due to relative position
-              theme="text"
-              className={styles['aligned-dropdown-button']}
-              onClick={async () => {
-                await this.createShareLink()
-                this.copyLinkToClipboard({ isAutomaticCopy: true })
-              }}
-              busy={loading}
-              label={t(`${documentType}.share.shareByLink.create`)}
-            />
-          )}
-        </div>
-        {checked && (
-          <CompositeRow
-            className="u-ph-0"
-            primaryText={
-              <Typography
-                onClick={this.copyLinkToClipboard}
-                className="u-c-pointer"
-                variant="body1"
-              >
-                {t(`${documentType}.share.shareByLink.activated`)}
-              </Typography>
-            }
-            secondaryText={
-              <Typography
-                className="u-primaryColor u-fz-tiny u-c-pointer"
-                onClick={this.copyLinkToClipboard}
-                variant="body1"
-              >
-                {t(`${documentType}.share.shareByLink.copy`)}
-              </Typography>
-            }
-            image={
-              <Circle backgroundColor="var(--silver)">
-                <Icon icon={LinkIcon} color="var(--charcoalGrey)" />
-              </Circle>
-            }
-            right={
-              <div ref={this.containerRef}>
-                <DropdownButton onClick={this.toggleMenu}>
-                  <Typography variant="body1">
-                    {t(
-                      `${documentType}.share.shareByLink.${
-                        hasReadOnlyPermissions ? 'ro' : 'rw'
-                      }`
-                    )}
-                  </Typography>
-                </DropdownButton>
-                {menuIsOpen && (
-                  <ActionMenu
-                    onClose={this.toggleMenu}
-                    popperOptions={{
-                      ...popperOptions,
-                      placement: 'bottom-end'
-                    }}
-                    containerElRef={this.containerRef}
-                    anchorElRef={this.containerRef}
-                  >
-                    <ActionMenuItem
-                      left={
-                        <Radio
-                          name="permissions"
-                          value="ro"
-                          className="u-w-1 u-h-1"
-                          checked={hasReadOnlyPermissions}
-                          readOnly
-                        />
-                      }
-                      onClick={() => {
-                        this.toggleMenu()
-                        this.updateLinkPermissions({ isReadOnly: true })
-                      }}
-                    >
-                      <>
-                        {t(`${documentType}.share.shareByLink.ro`)}
-                        <Typography
-                          className="u-mt-half"
-                          variant="caption"
-                          color="textSecondary"
-                        >
-                          {t(`${documentType}.share.shareByLink.desc.ro`)}
-                        </Typography>
-                      </>
-                    </ActionMenuItem>
-                    <ActionMenuItem
-                      left={
-                        <Radio
-                          name="permissions"
-                          value="rw"
-                          className="u-w-1 u-h-1"
-                          checked={!hasReadOnlyPermissions}
-                          readOnly
-                        />
-                      }
-                      onClick={() => {
-                        this.toggleMenu()
-                        this.updateLinkPermissions({ isReadOnly: false })
-                      }}
-                    >
-                      <>
-                        {t(`${documentType}.share.shareByLink.rw`)}
-                        <Typography
-                          className="u-mt-half"
-                          variant="caption"
-                          color="textSecondary"
-                        >
-                          {t(`${documentType}.share.shareByLink.desc.rw`)}
-                        </Typography>
-                      </>
-                    </ActionMenuItem>
-                    <hr />
-                    <ActionMenuItem
-                      left={<Icon icon={TrashIcon} color="var(--errorColor)" />}
-                      onClick={() => {
-                        this.toggleMenu()
-                        this.deleteShareLink()
-                      }}
-                    >
-                      <Typography className="u-error" variant="body1">
-                        {t(`${documentType}.share.shareByLink.deactivate`)}
-                      </Typography>
-                    </ActionMenuItem>
-                  </ActionMenu>
-                )}
-              </div>
-            }
+  return (
+    <div className="u-w-100">
+      <div className="u-flex u-flex-row u-flex-justify-between u-flex-items-center">
+        <Typography variant="body1">
+          {t(`${documentType}.share.shareByLink.subtitle`)}
+        </Typography>
+        {!checked && (
+          <Button
+            style={{ position: 'initial' }} // fix z-index bug on iOS when under a BottomDrawer due to relative position
+            theme="text"
+            className={styles['aligned-dropdown-button']}
+            onClick={async () => {
+              await createShareLink()
+              copyLinkToClipboard({ isAutomaticCopy: true })
+            }}
+            busy={loading}
+            label={t(`${documentType}.share.shareByLink.create`)}
           />
         )}
       </div>
-    )
-  }
+      {checked && (
+        <CompositeRow
+          className="u-ph-0"
+          primaryText={
+            <Typography
+              onClick={copyLinkToClipboard}
+              className="u-c-pointer"
+              variant="body1"
+            >
+              {t(`${documentType}.share.shareByLink.activated`)}
+            </Typography>
+          }
+          secondaryText={
+            <Typography
+              className="u-primaryColor u-fz-tiny u-c-pointer"
+              onClick={copyLinkToClipboard}
+              variant="body1"
+            >
+              {t(`${documentType}.share.shareByLink.copy`)}
+            </Typography>
+          }
+          image={
+            <Circle backgroundColor="var(--silver)">
+              <Icon icon={LinkIcon} color="var(--charcoalGrey)" />
+            </Circle>
+          }
+          right={
+            <div ref={containerRef}>
+              <DropdownButton onClick={toggleMenu}>
+                <Typography variant="body1">
+                  {t(
+                    `${documentType}.share.shareByLink.${
+                      hasReadOnlyPermissions ? 'ro' : 'rw'
+                    }`
+                  )}
+                </Typography>
+              </DropdownButton>
+              {menuIsOpen && (
+                <ActionMenu
+                  onClose={toggleMenu}
+                  popperOptions={{
+                    ...popperOptions,
+                    placement: 'bottom-end'
+                  }}
+                  containerElRef={containerRef}
+                  anchorElRef={containerRef}
+                >
+                  <ActionMenuItem
+                    left={
+                      <Radio
+                        name="permissions"
+                        value="ro"
+                        className="u-w-1 u-h-1"
+                        checked={hasReadOnlyPermissions}
+                        readOnly
+                      />
+                    }
+                    onClick={() => {
+                      toggleMenu()
+                      updateLinkPermissions({ isReadOnly: true })
+                    }}
+                  >
+                    <>
+                      {t(`${documentType}.share.shareByLink.ro`)}
+                      <Typography
+                        className="u-mt-half"
+                        variant="caption"
+                        color="textSecondary"
+                      >
+                        {t(`${documentType}.share.shareByLink.desc.ro`)}
+                      </Typography>
+                    </>
+                  </ActionMenuItem>
+                  <ActionMenuItem
+                    left={
+                      <Radio
+                        name="permissions"
+                        value="rw"
+                        className="u-w-1 u-h-1"
+                        checked={!hasReadOnlyPermissions}
+                        readOnly
+                      />
+                    }
+                    onClick={() => {
+                      toggleMenu()
+                      updateLinkPermissions({ isReadOnly: false })
+                    }}
+                  >
+                    <>
+                      {t(`${documentType}.share.shareByLink.rw`)}
+                      <Typography
+                        className="u-mt-half"
+                        variant="caption"
+                        color="textSecondary"
+                      >
+                        {t(`${documentType}.share.shareByLink.desc.rw`)}
+                      </Typography>
+                    </>
+                  </ActionMenuItem>
+                  <hr />
+                  <ActionMenuItem
+                    left={<Icon icon={TrashIcon} color="var(--errorColor)" />}
+                    onClick={() => {
+                      toggleMenu()
+                      deleteShareLink()
+                    }}
+                  >
+                    <Typography className="u-error" variant="body1">
+                      {t(`${documentType}.share.shareByLink.deactivate`)}
+                    </Typography>
+                  </ActionMenuItem>
+                </ActionMenu>
+              )}
+            </div>
+          }
+        />
+      )}
+    </div>
+  )
 }
 
 ShareByLink.propTypes = {
@@ -267,4 +251,4 @@ ShareByLink.propTypes = {
   onDisable: PropTypes.func.isRequired
 }
 
-export default translate()(ShareByLink)
+export default ShareByLink
