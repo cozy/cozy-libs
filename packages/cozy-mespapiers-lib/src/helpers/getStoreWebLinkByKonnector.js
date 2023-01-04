68c9b8c129ca29591f42be72025c57df2517d266
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-01-04 16:30:41 +0100

    feat(mespapiers): Use `redirectAfterInstall` to get Store redirection
    see Store feature for more details
    https://github.com/cozy/cozy-store/pull/842

--- packages/cozy-mespapiers-lib/src/helpers/getStoreWebLinkByKonnector.js
+++ packages/cozy-mespapiers-lib/src/helpers/getStoreWebLinkByKonnector.js
@@ -6,20 +6,38 @@ import log from 'cozy-logger'
  * @param {CozyClient} param.client - Instance of CozyClient
  * @param {string} [param.konnectorName] - Name of Connector
  * @param {string} [param.konnectorCategory] - Category of Connector
+ * @param {string} [param.redirectionPath] - Path to redirect from Store after connector installation
  * @returns {string} - Link of Store where the Connector is
  */
 export const getStoreWebLinkByKonnector = ({
   client,
   konnectorName,
-  konnectorCategory
+  konnectorCategory,
+  redirectionPath
 }) => {
   if (!konnectorName && !konnectorCategory) {
     log('error', 'konnectorName or konnectorCategory must be defined')
     return null
   }
 
+  const encodedAppLink = redirectionPath
+    ? encodeURIComponent(
+        generateWebLink({
+          slug: client.getInstanceOptions().app.slug,
+          cozyUrl: client.getStackClient().uri,
+          subDomainType: client.getInstanceOptions().subdomain,
+          pathname: '/',
+          hash: redirectionPath
+        })
+      )
+    : null
+
   const hash = konnectorName
-    ? `discover/${konnectorName}`
+    ? encodedAppLink
+      ? `discover/${konnectorName}?redirectAfterInstall=${encodedAppLink}`
+      : `discover/${konnectorName}`
+    : encodedAppLink
+    ? `discover?type=konnector&category=${konnectorCategory}&redirectAfterInstall=${encodedAppLink}`
     : `discover?type=konnector&category=${konnectorCategory}`
 
   const webLink = generateWebLink({
