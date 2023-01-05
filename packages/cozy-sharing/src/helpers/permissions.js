5133f894886a48870fb7176d8e29ccaaf6f911e7
Théo Poizat <hello@zatteo.com>
2023-01-05 10:03:48 +0100

    refactor: Move checkIsReadOnlyPermissions in a new permissions helper

---
+++ packages/cozy-sharing/src/helpers/permissions.js
@@ -0,0 +1,15 @@
+import get from 'lodash/get'
+import { models } from 'cozy-client'
+
+export const checkIsReadOnlyPermissions = permissions => {
+  const permissionCategories = get(
+    permissions,
+    '[0].attributes.permissions',
+    {}
+  )
+  return (
+    Object.values(permissionCategories).filter(permissionCategory =>
+      models.permission.isReadOnly(permissionCategory)
+    ).length > 0
+  )
+}
