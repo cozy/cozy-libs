4dd2c273884619d158ee2d2d2d1376c073b4a336
Merkur39 <merkkur39@gmail.com>
2023-01-09 13:25:14 +0100

    feat(harvest): Add Error component
    Resumption of the component used in the AccountModal component.
    Extraction of the methods `loadSelectedAccountId` &
    `fetchAccount` from the component AccountModal

---
+++ packages/cozy-harvest-lib/src/components/AccountModalWithoutTabs/Error.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { useClient } from 'cozy-client'
+import Infos from 'cozy-ui/transpiled/react/Infos'
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+
+import { loadSelectedAccountId } from '../../helpers/accounts'
+import withLocales from '../hoc/withLocales'
+
+const Error = ({ accountId, accountsAndTriggers, trigger, lastError }) => {
+  const { t } = useI18n()
+  const client = useClient()
+
+  const error = !trigger ? new Error('No matching trigger found') : lastError
+
+  const handleClick = () => {
+    loadSelectedAccountId(client, accountId, accountsAndTriggers)
+  }
+
+  return (
+    <Infos
+      actionButton={
+        <Button
+          label={t('modal.konnector.error.button')}
+          color="error"
+          onClick={handleClick}
+        />
+      }
+      title={t('modal.konnector.error.title')}
+      text={t('modal.konnector.error.description', error)}
+      icon="warning"
+      isImportant
+    />
+  )
+}
+
+export default withLocales(Error)
