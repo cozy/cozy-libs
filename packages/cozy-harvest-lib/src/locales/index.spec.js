6c3bdc6873e3f115ab84797068bcfc5a9e16ebe4
Merkur39 <merkkur39@gmail.com>
2023-01-18 12:50:53 +0100

    feat(harvest-lib): Update launchTrigger button locales

--- packages/cozy-harvest-lib/src/locales/index.spec.js
+++ packages/cozy-harvest-lib/src/locales/index.spec.js
@@ -3,6 +3,6 @@ import { getBoundT } from './index'
 describe('bound t', () => {
   it('should be possible to get a t bound to harvest locales', () => {
     const t = getBoundT('en')
-    expect(t('card.launchTrigger.button.label')).toBe('Run again now')
+    expect(t('card.launchTrigger.button.label')).toBe('Synchronize')
   })
 })
