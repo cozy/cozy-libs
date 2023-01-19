a0b63779fabc191a70d63ebb006626d5da97f33c
Théo Poizat <hello@zatteo.com>
2023-01-19 16:39:13 +0100

    fix: Realtime order issue caused invalid recipient status
    Sharing events were received in a good order,
    but were processed asynchronously. Resulting sharing events could then
    be added to the store in a bad order.

--- packages/cozy-sharing/src/helpers/sharings.js
+++ packages/cozy-sharing/src/helpers/sharings.js
@@ -16,26 +16,29 @@ export const getSharingObject = (internalSharing, sharing) => {
   }
 }
 
-export const createSharingInStore = (
+export const createSharingInStore = async ({
   client,
   doctype,
   dispatch,
   docsId,
   sharing
-) => {
+}) => {
   // TODO Check if we can getByIds to avoid query in map
-  docsId.map(async id => {
-    const file =
-      doctype === 'io.cozy.files'
-        ? await client.query(Q(doctype).getById(id))
-        : undefined
+  await Promise.all(
+    docsId.map(async id => {
+      const file =
+        doctype === 'io.cozy.files'
+          ? await client.query(Q(doctype).getById(id))
+          : undefined
 
-    const path =
-      file &&
-      (file.data.path || (await fetchFilesPaths(client, doctype, [file.data])))
+      const path =
+        file &&
+        (file.data.path ||
+          (await fetchFilesPaths(client, doctype, [file.data])))
 
-    dispatch(addSharing(sharing, path))
-  })
+      dispatch(addSharing(sharing, path))
+    })
+  )
 }
 
 export const updateSharingInStore = (dispatch, sharing) => {
