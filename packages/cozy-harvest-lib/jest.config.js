656f2267269718e180effbfd55aa33f5e1323615
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2023-01-27 18:27:32 +0100

    chore: Add Jest config for `*.styl` files in Harvest
    This is needed for tests that indirectly import components
    from Cozy UI that use `*.styl` files.

--- packages/cozy-harvest-lib/jest.config.js
+++ packages/cozy-harvest-lib/jest.config.js
@@ -15,6 +15,8 @@ module.exports = {
   moduleNameMapper: {
     '^cozy-logger$': 'cozy-logger/dist/index.js',
     '\\.(png|gif|jpe?g|svg|css)$': '<rootDir>/src/__mocks__/fileMock.js',
+    // identity-obj-proxy module is installed by cozy-scripts
+    '.styl$': 'identity-obj-proxy',
     // Force cozy-client resolving to use harvest's version of cozy-client
     // Can be removed when cozy-client's version in the workspace is > 14.4.0.
     // Since otherwise harvest tries to mock unexisting methods from
