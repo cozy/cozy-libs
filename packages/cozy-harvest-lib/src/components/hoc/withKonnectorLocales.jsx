e7ce1117b6bfe11cbc3347472f3b022e60d87e77
PolariTOON <36267812+PolariTOON@users.noreply.github.com>
2023-02-01 11:41:12 +0100

    fix: Make `withKonnectorLocales`' return value extends `React.Component`

--- packages/cozy-harvest-lib/src/components/hoc/withKonnectorLocales.jsx
+++ packages/cozy-harvest-lib/src/components/hoc/withKonnectorLocales.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import { extend } from 'cozy-ui/transpiled/react/I18n'
@@ -9,8 +9,8 @@ import { getDisplayName } from './utils'
  * HOC which ensures that a component get extended I18n with locales data from
  * konnector manifest.
  */
-export const withKonnectorLocales = Component => {
-  class WrappedComponent extends Component {
+export const withKonnectorLocales = WrappedComponent => {
+  class WrapperComponent extends Component {
     constructor(props, context) {
       super(props, context)
       const { konnector, lang } = this.props
@@ -20,21 +20,21 @@ export const withKonnectorLocales = Component => {
       }
     }
     render() {
-      return <Component {...this.props} />
+      return <WrappedComponent {...this.props} />
     }
   }
 
-  WrappedComponent.displayName = `withKonnectorLocales(${getDisplayName(
-    Component
+  WrapperComponent.displayName = `withKonnectorLocales(${getDisplayName(
+    WrappedComponent
   )}`
 
-  WrappedComponent.propTypes = {
-    ...Component.propTypes,
+  WrapperComponent.propTypes = {
+    ...WrappedComponent.propTypes,
     konnector: PropTypes.object.isRequired,
     lang: PropTypes.string.isRequired
   }
 
-  return WrappedComponent
+  return WrapperComponent
 }
 
 export default withKonnectorLocales
