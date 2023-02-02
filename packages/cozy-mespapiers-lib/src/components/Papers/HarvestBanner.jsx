3c29138a71325fe83718c45170495c134b75ff2e
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-02-02 15:40:47 +0100

    feat(mespapiers): Use identifier and accounts to get trigger instead of
    sourceAccount. The sourceAccount changes during a deco/reco of a
    connector. But in this case the cozyMetadata.sourceAccount of a file
    is not updated, so it is not a reliable source. You have to use the
    sourceAccountIdentifier coupled with the connector slug to get the
    right trigger. This is what we do here.

--- packages/cozy-mespapiers-lib/src/components/Papers/HarvestBanner.jsx
+++ packages/cozy-mespapiers-lib/src/components/Papers/HarvestBanner.jsx
@@ -1,45 +1,68 @@
 import React from 'react'
-import { useQuery } from 'cozy-client'
+import { useQuery, isQueryLoading } from 'cozy-client'
 import { LaunchTriggerCard } from 'cozy-harvest-lib'
 import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
 
 import {
   buildTriggersQueryByConnectorSlug,
-  buildConnectorsQueryById
+  buildConnectorsQueryById,
+  buildAccountsQueryByLoginAndSlug
 } from '../../helpers/queries'
 
 const HarvestBanner = ({ papers }) => {
-  const { createdByApp: connectorSlug, sourceAccount: connectorAccount } =
-    papers?.list?.[0]?.cozyMetadata || {}
+  const {
+    createdByApp: connectorSlug,
+    sourceAccountIdentifier: connectorAccountIdentifier
+  } = papers?.list?.[0]?.cozyMetadata || {}
+
+  const queryAccounts = buildAccountsQueryByLoginAndSlug({
+    login: connectorAccountIdentifier,
+    slug: connectorSlug,
+    enabled: Boolean(connectorSlug)
+  })
+  const { data: accounts, ...accountsQueryLeft } = useQuery(
+    queryAccounts.definition,
+    queryAccounts.options
+  )
+  const isAccountsLoading = isQueryLoading(accountsQueryLeft)
+  const account = accounts?.[0]
+
   const queryTriggers = buildTriggersQueryByConnectorSlug(
     connectorSlug,
     Boolean(connectorSlug)
   )
-  const { data: triggers } = useQuery(
+  const { data: triggers, ...triggersQueryLeft } = useQuery(
     queryTriggers.definition,
     queryTriggers.options
   )
+  const isTriggersLoading = isQueryLoading(triggersQueryLeft)
   const trigger = triggers?.find(
-    trigger => trigger.message.account === connectorAccount
+    trigger => trigger.message.account === account?._id
   )
 
   const queryKonnector = buildConnectorsQueryById(
     `io.cozy.konnectors/${connectorSlug}`
   )
-
-  const { data: konnectors } = useQuery(
+  const { data: konnectors, ...konnectorsQueryLeft } = useQuery(
     queryKonnector.definition,
     queryKonnector.options
   )
+  const isKonnectorsLoading = isQueryLoading(konnectorsQueryLeft)
   const konnector = konnectors?.[0]
 
-  if (!konnector) return null
+  if (
+    !konnector ||
+    isAccountsLoading ||
+    isTriggersLoading ||
+    isKonnectorsLoading
+  )
+    return null
 
   return (
     <>
       <LaunchTriggerCard
         flowProps={{ initialTrigger: trigger, konnector }}
-        konnectorRoot={`harvest/${connectorSlug}/accounts/${connectorAccount}`}
+        konnectorRoot={`harvest/${connectorSlug}`}
       />
       <Divider />
     </>
