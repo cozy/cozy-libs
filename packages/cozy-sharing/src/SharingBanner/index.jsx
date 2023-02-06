8cb57b0cb5d3ec03dbba003dead2f5e487b8d803
cballevre <celestin.ballevre@protonmail.com>
2023-02-06 09:07:11 +0100

    fix(SharingBanner): Add custom previewPath
    By default, previewPath is /preview but we needed to be /preview/ into cozy-notes. That's why, we can add previewPath to SharingBanner

--- packages/cozy-sharing/src/SharingBanner/index.jsx
+++ packages/cozy-sharing/src/SharingBanner/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import withLocales from '../withLocales'
 import { useSharingInfos } from './hooks/useSharingInfos'
 import { SharingBanner } from './components/SharingBanner'
 
-const Plugin = () => {
-  const sharingInfos = useSharingInfos()
-
+const Plugin = ({ previewPath }) => {
+  const sharingInfos = useSharingInfos(previewPath)
   return <SharingBanner sharingInfos={sharingInfos} />
+}
+
+Plugin.propTypes = {
+  previewPath: PropTypes.string
 }
 
 export const SharingBannerPlugin = withLocales(Plugin)
