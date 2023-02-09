f721b3f1e9c8bddc126c796fd8140892450698b9
cballevre <celestin.ballevre@protonmail.com>
2023-02-09 18:20:08 +0100

    fix(cozy-ci): Use token instead password into .transifexrc
    You can find more infos on .transifexrc and their config in [transifex cli code](https://github.com/transifex/cli/blob/devel/examples/exampleconf/.transifexrc)

--- packages/cozy-ci/transifex.sh
+++ packages/cozy-ci/transifex.sh
@@ -2,4 +2,4 @@
 
 curl -o- https://raw.githubusercontent.com/transifex/cli/master/install.sh | bash
 install -m0644 .transifexrc.tpl ~/.transifexrc
-echo "password = $TX_PASSWD" >> ~/.transifexrc
+echo "token = $TX_TOKEN" >> ~/.transifexrc
