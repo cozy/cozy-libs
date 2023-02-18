74c98a5514145e176047ebd2c233e3f01c7bda7f
Antonin Cezard <acezard@gmail.com>
2023-02-18 15:16:05 +0100

    fix: Update every lint error
    Some errors might be runtime so eslint was disabled on those few lines

--- packages/cozy-minilog/lib/web/index.js
+++ packages/cozy-minilog/lib/web/index.js
@@ -1,42 +1,53 @@
-var Minilog = require('../common/minilog.js');
+var Minilog = require('../common/minilog.js')
 
 var oldEnable = Minilog.enable,
-    oldDisable = Minilog.disable,
-    isChrome = (typeof navigator != 'undefined' && /chrome/i.test(navigator.userAgent)),
-    console = require('./console.js');
+  oldDisable = Minilog.disable,
+  isChrome =
+    typeof navigator != 'undefined' && /chrome/i.test(navigator.userAgent),
+  console = require('./console.js')
 
 // Use a more capable logging backend if on Chrome
-Minilog.defaultBackend = (isChrome ? console.minilog : console);
+Minilog.defaultBackend = isChrome ? console.minilog : console
 
 // apply enable inputs from localStorage and from the URL
-if(typeof window != 'undefined') {
+if (typeof window != 'undefined') {
   try {
-    Minilog.enable(JSON.parse(window.localStorage['minilogSettings']));
-  } catch(e) {}
-  if(window.location && window.location.search) {
-    var match = RegExp('[?&]minilog=([^&]*)').exec(window.location.search);
-    match && Minilog.enable(decodeURIComponent(match[1]));
+    Minilog.enable(JSON.parse(window.localStorage['minilogSettings']))
+  } catch (e) {
+    // ignore
+  }
+  if (window.location && window.location.search) {
+    var match = RegExp('[?&]minilog=([^&]*)').exec(window.location.search)
+    match && Minilog.enable(decodeURIComponent(match[1]))
   }
 }
 
 // Make enable also add to localStorage
-Minilog.enable = function() {
-  oldEnable.call(Minilog, true);
-  try { window.localStorage['minilogSettings'] = JSON.stringify(true); } catch(e) {}
-  return this;
-};
+Minilog.enable = function () {
+  oldEnable.call(Minilog, true)
+  try {
+    window.localStorage['minilogSettings'] = JSON.stringify(true)
+  } catch (e) {
+    // ignore
+  }
+  return this
+}
 
-Minilog.disable = function() {
-  oldDisable.call(Minilog);
-  try { delete window.localStorage.minilogSettings; } catch(e) {}
-  return this;
-};
+Minilog.disable = function () {
+  oldDisable.call(Minilog)
+  try {
+    delete window.localStorage.minilogSettings
+  } catch (e) {
+    // ignore
+  }
+  return this
+}
 
-exports = module.exports = Minilog;
+exports = module.exports = Minilog
 
 exports.backends = {
   array: require('./array.js'),
   browser: Minilog.defaultBackend,
   localStorage: require('./localstorage.js'),
   jQuery: require('./jquery_simple.js')
-};
+}
--- packages/cozy-minilog/test/lib/localstorage.js
+++ packages/cozy-minilog/test/lib/localstorage.js
@@ -1,39 +1,38 @@
-var db;
+var db
 
-function LocalStorage() {
-}
-db = LocalStorage;
+function LocalStorage() {}
+db = LocalStorage
 
 db.prototype.getItem = function (key) {
   if (key in this) {
-    return String(this[key]);
+    return String(this[key])
   }
-  return null;
-};
+  return null
+}
 
 db.prototype.setItem = function (key, val) {
-  this[key] = String(val);
-};
+  this[key] = String(val)
+}
 
 db.prototype.removeItem = function (key) {
-  delete this[key];
-};
+  delete this[key]
+}
 
 db.prototype.clear = function () {
-  var self = this;
+  var self = this
   Object.keys(self).forEach(function (key) {
-    self[key] = undefined;
-    delete self[key];
-  });
-};
+    self[key] = undefined
+    delete self[key]
+  })
+}
 
 db.prototype.key = function (i) {
-  i = i || 0;
-  return Object.keys(this)[i];
-};
+  i = i || 0
+  return Object.keys(this)[i]
+}
 
 db.prototype.__defineGetter__('length', function () {
-  return Object.keys(this).length;
-});
+  return Object.keys(this).length
+})
 
-module.exports = new LocalStorage();
+module.exports = new LocalStorage()
