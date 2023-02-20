3af83a66e90877d7067e9a6f22bf6d33d40333d7
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-02-20 13:48:03 +0100

    feat(harvest): Add create account wording in CannotConnectModal

--- packages/cozy-harvest-lib/src/components/AccountForm/CannotConnectModal.jsx
+++ packages/cozy-harvest-lib/src/components/AccountForm/CannotConnectModal.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Typography from 'cozy-ui/transpiled/react/Typography'
-import Link from 'cozy-ui/transpiled/react/Link'
 import { ConfirmDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import { useComponentsProps } from '../Providers/ComponentsPropsProvider'
+import Markdown from '../Markdown'
 
 const CannotConnectModal = ({ vendorName, vendorLink, onClose }) => {
   const { t } = useI18n()
@@ -20,12 +20,20 @@ const CannotConnectModal = ({ vendorName, vendorLink, onClose }) => {
           <Typography variant="h4" className="u-mb-1">
             {t('accountForm.cannotConnectModal.title')}
           </Typography>
-          <Typography className="u-mb-1">
-            {t('accountForm.cannotConnectModal.content')}
-            <Link href={vendorLink} target="_blank" rel="noopener noreferrer">
-              {vendorName}
-            </Link>
-          </Typography>
+          <Markdown
+            className="u-mb-1"
+            source={t('accountForm.cannotConnectModal.content', {
+              vendorLink,
+              vendorName
+            })}
+          />
+          <Markdown
+            className="u-mb-1"
+            source={t('accountForm.cannotConnectModal.noAccount', {
+              vendorLink,
+              vendorName
+            })}
+          />
           {ComponentsProps?.CannotConnectModal?.extraContent}
         </>
       }
