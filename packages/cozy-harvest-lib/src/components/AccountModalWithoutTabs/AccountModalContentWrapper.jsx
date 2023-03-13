1de2b07bcd7a3817c5896f8656deefdedd694773
Merkur39 <merkkur39@gmail.com>
2023-03-13 12:22:25 +0100

    fix: Konnector error alert display
    The `AccountModalContentWrapper` component is only used
    in the new version of the `AccountModal`, and in this version
    the error alert is handled in the `DataTab` component

--- packages/cozy-harvest-lib/src/components/AccountModalWithoutTabs/AccountModalContentWrapper.jsx
+++ packages/cozy-harvest-lib/src/components/AccountModalWithoutTabs/AccountModalContentWrapper.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { useOutletContext } from 'react-router-dom'
-import DialogContent from '@material-ui/core/DialogContent'
 
+import DialogContent from 'cozy-ui/transpiled/react/Dialog/DialogContent'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
 import FlowProvider from '../FlowProvider'
-import TriggerError from './TriggerError'
 
 const AccountModalContentWrapper = ({ children }) => {
   const { isMobile } = useBreakpoints()
@@ -20,28 +19,19 @@ const AccountModalContentWrapper = ({ children }) => {
   return (
     <DialogContent className={isMobile ? 'u-p-0' : 'u-pt-0'}>
       <FlowProvider initialTrigger={trigger} konnector={konnector}>
-        {({ flow }) => (
-          <>
-            <TriggerError
-              flow={flow}
-              konnector={konnector}
-              account={account}
-              trigger={trigger}
-              intentsApi={intentsApi}
-            />
-            {React.Children.map(children, child =>
-              React.isValidElement(child)
-                ? React.cloneElement(child, {
-                    flow,
-                    account,
-                    trigger,
-                    intentsApi,
-                    innerAccountModalOverrides
-                  })
-                : null
-            )}
-          </>
-        )}
+        {({ flow }) =>
+          React.Children.map(children, child =>
+            React.isValidElement(child)
+              ? React.cloneElement(child, {
+                  flow,
+                  account,
+                  trigger,
+                  intentsApi,
+                  innerAccountModalOverrides
+                })
+              : null
+          )
+        }
       </FlowProvider>
     </DialogContent>
   )
