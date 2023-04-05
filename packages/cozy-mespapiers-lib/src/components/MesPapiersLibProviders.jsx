542cf6eefd30708e6e88074b02cc921879648423
AlexisG <merkkur39@gmail.com>
2023-04-05 12:19:43 +0200

    fix: Remove Fab button on onboarding page
    As long as the user has not validated his onboarding,
    via the "Let's go!" button,
    he will not be able to access the features of the app

--- packages/cozy-mespapiers-lib/src/components/MesPapiersLibProviders.jsx
+++ packages/cozy-mespapiers-lib/src/components/MesPapiersLibProviders.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import { useQuery } from 'cozy-client'
 import { I18n, initTranslation } from 'cozy-ui/transpiled/react/I18n'
 
 import { ModalProvider } from './Contexts/ModalProvider'
@@ -13,10 +14,18 @@ import { MesPapiersLibLayout } from './MesPapiersLibLayout'
 import PapersFabWrapper from './PapersFab/PapersFabWrapper'
 import { FILES_DOCTYPE, CONTACTS_DOCTYPE } from '../doctypes'
 import { getComponents } from '../helpers/defaultComponent'
+import { getOnboardingStatus } from '../helpers/queries'
 
 export const MesPapiersLibProviders = ({ lang, components }) => {
   const polyglot = initTranslation(lang, lang => require(`../locales/${lang}`))
   const { PapersFab, Onboarding } = getComponents(components)
+
+  const { data: settingsData } = useQuery(
+    getOnboardingStatus.definition,
+    getOnboardingStatus.options
+  )
+
+  const isOnboarded = settingsData?.[0]?.onboarded
 
   return (
     <I18n lang={lang} polyglot={polyglot}>
@@ -28,7 +37,7 @@ export const MesPapiersLibProviders = ({ lang, components }) => {
                 <OnboardingProvider OnboardingComponent={Onboarding}>
                   <MesPapiersLibLayout />
                 </OnboardingProvider>
-                {PapersFab && (
+                {PapersFab && isOnboarded && (
                   <PapersFabWrapper>
                     <PapersFab />
                   </PapersFabWrapper>
