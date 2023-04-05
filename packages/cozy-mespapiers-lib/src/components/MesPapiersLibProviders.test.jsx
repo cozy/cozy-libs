9555d59700bfffacd674e224c13a5eb003fb345e
AlexisG <merkkur39@gmail.com>
2023-04-05 12:19:43 +0200

    fix: Remove Fab button on onboarding page
    As long as the user has not validated his onboarding,
    via the "Let's go!" button,
    he will not be able to access the features of the app

--- packages/cozy-mespapiers-lib/src/components/MesPapiersLibProviders.test.jsx
+++ packages/cozy-mespapiers-lib/src/components/MesPapiersLibProviders.test.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
+import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
+import React from 'react'
+
+import { useQuery } from 'cozy-client'
 
 import { MesPapiersLibProviders } from './MesPapiersLibProviders'
 import AppLike from '../../test/components/AppLike'
@@ -9,8 +12,12 @@ jest.mock('./PapersFab/PapersFabWrapper', () => () => (
   <div data-testid="PapersFabWrapper" />
 ))
 /* eslint-enable react/display-name */
+jest.mock('cozy-client/dist/hooks/useQuery', () => jest.fn())
 
-const setup = ({ components } = {}) => {
+const setup = ({ components, onboarded = true } = {}) => {
+  useQuery.mockReturnValue({
+    data: [{ onboarded }]
+  })
   return render(
     <AppLike>
       <MesPapiersLibProviders {...(components && { components: components })} />
@@ -19,15 +26,24 @@ const setup = ({ components } = {}) => {
 }
 
 describe('MesPapiersLibProviders', () => {
-  it('should display PapersFabWrapper & PapersFab by default', () => {
-    const { getByTestId } = setup()
+  describe('If onboarding is done', () => {
+    it('should display PapersFabWrapper & PapersFab', () => {
+      const { getByTestId } = setup()
 
-    expect(getByTestId('PapersFabWrapper'))
+      expect(getByTestId('PapersFabWrapper')).toBeInTheDocument()
+    })
+    it('should not display PapersFabWrapper & PapersFab', () => {
+      const { queryByTestId } = setup({ components: { PapersFab: null } })
+
+      expect(queryByTestId('PapersFabWrapper')).toBeNull()
+    })
   })
 
-  it('should not display PapersFabWrapper & PapersFab', () => {
-    const { queryByTestId } = setup({ components: { PapersFab: null } })
+  describe('If the onboarding is not finished', () => {
+    it('should not display PapersFabWrapper & PapersFab by default', () => {
+      const { queryByTestId } = setup({ onboarded: false })
 
-    expect(queryByTestId('PapersFabWrapper')).toBeNull()
+      expect(queryByTestId('PapersFabWrapper')).toBeNull()
+    })
   })
 })
