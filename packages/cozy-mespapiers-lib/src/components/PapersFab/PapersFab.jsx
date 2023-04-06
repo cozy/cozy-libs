65cad799a356e18be292ba01a7c4893aaa5c1869
AlexisG <merkkur39@gmail.com>
2023-04-06 12:50:15 +0200

    feat: Update PaperFab components
    Now, on the home page,
    the PaperFab directly opens the modal for creating a paper.
    But it must continue to open a menu when you are in a list of papers
    
    Also, the PaperFab is now in its extended variant
    when you are at the top of the page,
    and goes back to its circular variant
    when you go down (offset of 80px)

--- packages/cozy-mespapiers-lib/src/components/PapersFab/PapersFab.jsx
+++ packages/cozy-mespapiers-lib/src/components/PapersFab/PapersFab.jsx
@@ -1,37 +1,28 @@
-import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
 import Fab from 'cozy-ui/transpiled/react/Fab'
 import Icon from 'cozy-ui/transpiled/react/Icon'
-import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
-import { makeStyles } from 'cozy-ui/transpiled/react/styles'
 
 import withLocales from '../../locales/withLocales'
-
-const useStyles = makeStyles(() => ({
-  fab: {
-    position: 'fixed',
-    zIndex: 10,
-    bottom: '1rem',
-    right: isDesktop => (isDesktop ? '6rem' : '1rem')
-  }
-}))
+import { useScroll } from '../Hooks/useScroll'
 
 const PapersFab = ({ t, className, innerRef, onClick, a11y }) => {
-  const { isDesktop } = useBreakpoints()
-  const classes = useStyles(isDesktop)
+  const scroll = useScroll()
+  const isOnTop = scroll.scrollTop < 80
 
   return (
     <Fab
       color="primary"
-      className={cx(classes.fab, className)}
+      variant={isOnTop ? 'extended' : 'circular'}
+      className={className}
       innerRef={innerRef}
       onClick={onClick}
-      aria-label={t('Home.Fab.ariaLabel')}
+      aria-label={t('Home.Fab.createPaper')}
       {...a11y}
     >
-      <Icon icon="plus" />
+      <Icon icon="plus" {...(isOnTop && { className: 'u-mr-half' })} />
+      {isOnTop && t('common.add')}
     </Fab>
   )
 }
