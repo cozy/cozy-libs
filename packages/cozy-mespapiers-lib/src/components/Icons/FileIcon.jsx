1e35505096abc3fb2cee2991e0b3d48b9f4520aa
AlexisG <merkkur39@gmail.com>
2023-04-06 09:04:32 +0200

    feat: Add "Others" suggestion
    Improved FileIcon to accept an undefined icon

--- packages/cozy-mespapiers-lib/src/components/Icons/FileIcon.jsx
+++ packages/cozy-mespapiers-lib/src/components/Icons/FileIcon.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 
-import IconStack from 'cozy-ui/transpiled/react/IconStack'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import IconStack from 'cozy-ui/transpiled/react/IconStack'
 
 const FileIcon = ({ icon, faded }) => {
   return (
     <IconStack
       backgroundClassName={faded ? 'u-o-50' : ''}
       backgroundIcon={<Icon icon="file-duotone" color="#E049BF" size={32} />}
-      foregroundIcon={<Icon icon={icon} color="#E049BF" size={16} />}
+      {...(icon && {
+        foregroundIcon: <Icon icon={icon} color="#E049BF" size={16} />
+      })}
     />
   )
 }
