ad3d58efc2e8c95154806006772fe4145cf57a45
AlexisG <merkkur39@gmail.com>
2023-04-13 19:42:04 +0200

    feat: Sort category of papers
    Sorting of the qualification labels listed on the homepage
    based on the creation date of the files
    
    Conditioned by flag `mespapiers.v2-1-0.enabled`

--- packages/cozy-mespapiers-lib/src/components/Home/helpers.spec.js
+++ packages/cozy-mespapiers-lib/src/components/Home/helpers.spec.js
@@ -1,6 +1,11 @@
 import get from 'lodash/get'
+import MockDate from 'mockdate'
 
-import { filterPapersByThemeAndSearchValue, hasItemByLabel } from './helpers'
+import {
+  filterPapersByThemeAndSearchValue,
+  hasItemByLabel,
+  makePapersGroupByQualificationLabel
+} from './helpers'
 
 const locales = {
   items: {
@@ -155,5 +160,49 @@ describe('hasItemByLabel', () => {
     )
 
     expect(res).toBe(false)
+  })
+})
+
+describe('makePapersGroupByQualificationLabel', () => {
+  beforeEach(() => {
+    MockDate.set('2023-12-01:09:00.000000+01:00')
+  })
+  afterEach(() => {
+    MockDate.reset()
+  })
+  const fileList = [
+    {
+      _id: 'file01',
+      name: "Avis d'imposition 01",
+      metadata: { qualification: { label: 'tax_notice' } },
+      created_at: '2023-04-01:09:00.000000+01:00'
+    },
+    {
+      _id: 'file02',
+      name: 'Facture internet 01',
+      metadata: { qualification: { label: 'isp_invoice' } },
+      created_at: '2023-01-01:09:00.000000+01:00'
+    },
+    {
+      _id: 'file03',
+      name: "Avis d'imposition 02",
+      metadata: { qualification: { label: 'tax_notice' } },
+      created_at: '2023-02-01:09:00.000000+01:00'
+    },
+    {
+      _id: 'file04',
+      name: 'Facture internet 02',
+      metadata: { qualification: { label: 'isp_invoice' } },
+      created_at: '2023-06-01:09:00.000000+01:00'
+    }
+  ]
+
+  it('should return the correct object with files grouped by qualification label and sort by their created_at date', () => {
+    const res = makePapersGroupByQualificationLabel(fileList)
+
+    expect(res).toEqual({
+      isp_invoice: [fileList[1], fileList[3]],
+      tax_notice: [fileList[0], fileList[2]]
+    })
   })
 })
