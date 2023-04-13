1d7c981f3e57dadcca6d225c96c5d8ae98dd4398
AlexisG <merkkur39@gmail.com>
2023-04-13 19:42:04 +0200

    feat: Sort category of papers
    Sorting of the qualification labels listed on the homepage
    based on the creation date of the files
    
    Conditioned by flag `mespapiers.v2-1-0.enabled`

--- packages/cozy-mespapiers-lib/src/components/Home/helpers.js
+++ packages/cozy-mespapiers-lib/src/components/Home/helpers.js
@@ -1,6 +1,8 @@
 import Fuse from 'fuse.js'
 import groupBy from 'lodash/groupBy'
 
+import flag from 'cozy-flags'
+
 const fuse = new Fuse([], {
   findAllMatches: true,
   threshold: 0.3,
@@ -57,6 +59,57 @@ export const filterPapersByThemeAndSearchValue = ({
   return filteredFiles
 }
 
-export const makePapersGroupByQualificationLabel = papers => {
-  return groupBy(papers, 'metadata.qualification.label')
+/**
+ * Get the latest date from a list of files
+ * @param {import('cozy-client/types/types').IOCozyFile[]} files - list of files
+ * @returns {Date} - latest date
+ */
+const getLatestDate = files => {
+  return Math.max(...files.map(file => new Date(file.created_at)))
+}
+
+/**
+ * Compare function to sort categories by date
+ * @param {string} a - first category
+ * @param {string} b - second category
+ * @param {Object} papersGrouped - object with qualification label as key and files as value
+ *
+//  */
+const compareCategoryByDate = papersGrouped => (a, b) => {
+  const dateA = new Date(getLatestDate(papersGrouped[a]))
+  const dateB = new Date(getLatestDate(papersGrouped[b]))
+
+  return dateB - dateA
+}
+
+/**
+ * Group files by qualification label and sort them by date
+ *
+ * @param {import('cozy-client/types/types').IOCozyFile[]} files - list of files
+ * @returns {Object} - object with qualification label as key and files as value
+ */
+export const makePapersGroupByQualificationLabel = files => {
+  if (!flag('mespapiers.v2-1-0.enabled')) {
+    return groupBy(files, 'metadata.qualification.label')
+  }
+  const filesGrouped = files.reduce((acc, file) => {
+    const label = file.metadata.qualification.label
+    if (!acc[label]) {
+      acc[label] = []
+    }
+    acc[label].push(file)
+
+    return acc
+  }, {})
+
+  const sortedCategories = Object.keys(filesGrouped).sort(
+    compareCategoryByDate(filesGrouped)
+  )
+
+  const result = {}
+  for (let category of sortedCategories) {
+    result[category] = filesGrouped[category]
+  }
+
+  return result
 }
