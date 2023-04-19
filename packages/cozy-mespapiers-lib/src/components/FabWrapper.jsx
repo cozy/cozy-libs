040aafb70f223f18751502f1bfe35aed22a50e52
AlexisG <merkkur39@gmail.com>
2023-04-19 12:33:04 +0200

    feat(cozy-mespapiers-lib): Update style to FabWrapper
    Now, in Desktop mode, the position of the FabWrapper
    automatically adapts to the size of the application container.

--- packages/cozy-mespapiers-lib/src/components/FabWrapper.jsx
+++ packages/cozy-mespapiers-lib/src/components/FabWrapper.jsx
@@ -3,21 +3,24 @@ import React from 'react'
 import Box from 'cozy-ui/transpiled/react/Box'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
-const makeStyle = isDesktop => ({
+const makeStyle = (isDesktop, offsetRight) => ({
   display: 'flex',
   alignItems: 'center',
   position: 'fixed',
   bottom: '1rem',
-  right: isDesktop ? '6rem' : '1rem',
+  right: isDesktop ? `calc(6rem + ${offsetRight}px)` : '1rem',
   zIndex: 10
 })
 
 const FabWrapper = ({ children }) => {
   const { isDesktop } = useBreakpoints()
+  const mainElement = document.querySelector('[role=main]')
+  const offsetRight =
+    window.innerWidth - (mainElement?.getBoundingClientRect()?.right ?? 0)
 
   if (!children) return null
 
-  return <Box {...makeStyle(isDesktop)}>{children}</Box>
+  return <Box {...makeStyle(isDesktop, offsetRight)}>{children}</Box>
 }
 
 export default FabWrapper
