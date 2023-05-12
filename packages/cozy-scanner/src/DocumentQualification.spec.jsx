c76b5cff5e9f975ffb35af5d002ec5af7e31115a
AlexisG <merkkur39@gmail.com>
2023-05-12 13:03:01 +0200

    feat(cozy-scanner): Hide health theme
    On the condition of a flag (`hide.healthTheme.enabled`),
    we wish to be able to hide the Health theme.

--- packages/cozy-scanner/src/DocumentQualification.spec.jsx
+++ packages/cozy-scanner/src/DocumentQualification.spec.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-const MockDate = require('mockdate')
 import { render, fireEvent } from '@testing-library/react'
+import React from 'react'
+
+import flag from 'cozy-flags'
+import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme'
+
+import { DocumentQualification } from './DocumentQualification'
+
+const MockDate = require('mockdate')
 jest.mock('cozy-ui/transpiled/react/utils/color', () => ({
   getCssVariableValue: () => '#fff'
 }))
+jest.mock('cozy-flags')
 // Popper does not work well inside of jest as it heavily relies on DOM APIs (see https://github.com/popperjs/popper-core/issues/478).
 jest.mock('@material-ui/core/Popper', () => {
   return ({ children }) => children
 })
 
-import { DocumentQualification } from './DocumentQualification'
-import MuiCozyTheme from 'cozy-ui/transpiled/react/MuiCozyTheme'
-
 const MOCKED_DATE = '2019-01-01'
 
+const setup = ({
+  isFlag = false,
+  onDescribed = jest.fn(),
+  onFileNameChanged = jest.fn()
+} = {}) => {
+  flag.mockReturnValue(isFlag)
+  return render(
+    <MuiCozyTheme>
+      <DocumentQualification
+        allowEditFileName={true}
+        onDescribed={onDescribed}
+        onFileNameChanged={onFileNameChanged}
+        title="Edit"
+        t={text => text}
+      />
+    </MuiCozyTheme>
+  )
+}
 describe('DocumentQualification', () => {
   beforeAll(() => {
     MockDate.set(MOCKED_DATE)
@@ -25,18 +47,11 @@ describe('DocumentQualification', () => {
   it('Initial render + selection + filename', () => {
     const onDescribed = jest.fn()
     const onFileNameChanged = jest.fn()
-    const { queryByText, getByText, asFragment, getByLabelText } = render(
-      <MuiCozyTheme>
-        <DocumentQualification
-          allowEditFileName={true}
-          onDescribed={onDescribed}
-          onFileNameChanged={onFileNameChanged}
-          title="Edit"
-          // initialSelected={{ itemId: 1, categoryLabel: 'Label1' }}
-          t={text => text}
-        />
-      </MuiCozyTheme>
-    )
+    const { queryByText, getByText, asFragment, getByLabelText } = setup({
+      isFlag: false,
+      onDescribed,
+      onFileNameChanged
+    })
     expect(asFragment()).toMatchSnapshot()
     const undefinedItemNode = queryByText(/Scan.themes.undefined/i)
     const isSelected = undefinedItemNode.closest(
@@ -78,5 +93,11 @@ describe('DocumentQualification', () => {
     fireEvent.click(familyItemNode)
     fireEvent.click(queryByText('Scan.items.birth_certificate'))
     expect(inputFileName.value).toBe('Manual name')
+  })
+
+  it('Hide health theme', () => {
+    const { queryByText } = setup({ isFlag: true })
+
+    expect(queryByText('Scan.themes.health')).toBeNull()
   })
 })
