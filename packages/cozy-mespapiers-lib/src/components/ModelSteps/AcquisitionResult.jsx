b29684da07add0af7fce41c33c8d0b385e12c36f
AlexisG <merkkur39@gmail.com>
2023-05-15 21:06:11 +0200

    feat: Improve AcquisitionResult component WIP [skip-travis]

--- packages/cozy-mespapiers-lib/src/components/ModelSteps/AcquisitionResult.jsx
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/AcquisitionResult.jsx
@@ -1,19 +1,21 @@
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React, { memo, useCallback } from 'react'
+import React, { useCallback, useState, useRef } from 'react'
 
-import Avatar from 'cozy-ui/transpiled/react/Avatar'
-import Button, { ButtonLink } from 'cozy-ui/transpiled/react/Button'
+import Button from 'cozy-ui/transpiled/react/Buttons'
 import Card from 'cozy-ui/transpiled/react/Card'
 import DialogActions from 'cozy-ui/transpiled/react/DialogActions'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import useEventListener from 'cozy-ui/transpiled/react/hooks/useEventListener'
 import { makeStyles } from 'cozy-ui/transpiled/react/styles'
 
+import AcquisitionResultInfo from './AcquisitionResultInfo'
 import { isSameFile } from './helpers'
+import RotateImage from './widgets/RotateImage'
 import { PaperDefinitionsStepPropTypes } from '../../constants/PaperDefinitionsPropTypes'
 import { KEYS } from '../../constants/const'
 import { useFormData } from '../Hooks/useFormData'
@@ -21,25 +23,30 @@ import { useStepperDialog } from '../Hooks/useStepperDialog'
 
 const isPDF = file => file.type === 'application/pdf'
 
-const useStyles = makeStyles(() => ({
-  img: {
-    maxWidth: '100%',
-    maxHeight: 'inherit'
+const useStyles = makeStyles(theme => ({
+  typography: {
+    color: 'var(--successColor)',
+    marginLeft: '0.5rem'
   },
-  avatar: {
-    color: 'var(--paperBackgroundColor)',
-    backgroundColor: 'var(--successColor)',
-    marginBottom: '1rem'
+  iconRetry: {
+    border: `1px solid ${theme.palette.border.main}`,
+    borderRadius: 0,
+    '&.small': {
+      padding: '0 1rem'
+    }
   }
 }))
 
 const AcquisitionResult = ({ currentFile, setCurrentFile, currentStep }) => {
+  const imageRef = useRef(null)
   const styles = useStyles()
   const { t } = useI18n()
   const { isMobile } = useBreakpoints()
   const { nextStep } = useStepperDialog()
   const { setFormData, formData } = useFormData()
-  const { multipage } = currentStep
+  const [rotationImage, setRotationImage] = useState(0)
+
+  const { multipage, illustration, page = 'default' } = currentStep
 
   const changeSelectedFile = () => {
     const newData = formData.data.filter(
@@ -69,6 +76,10 @@ const AcquisitionResult = ({ currentFile, setCurrentFile, currentStep }) => {
     [onValid]
   )
 
+  const handleRotate = () => {
+    setRotationImage(prev => (prev - 90) % 360)
+  }
+
   useEventListener(window, 'keydown', handleKeyDown)
 
   return (
@@ -81,31 +92,56 @@ const AcquisitionResult = ({ currentFile, setCurrentFile, currentStep }) => {
           }
         )}
       >
-        <div className="u-flex u-flex-column u-flex-items-center u-mb-2">
-          <Avatar icon="check" size="xlarge" className={styles.avatar} />
-          <Typography variant="h5">{t('Acquisition.success')}</Typography>
+        <div className="u-flex u-flex-items-center u-flex-justify-center u-mb-1-half">
+          <Icon
+            icon="check-circle"
+            color="var(--successColor)"
+            aria-hidden="true"
+          />
+          <Typography variant="h4" className={styles.typography} role="status">
+            {t('Acquisition.success')}
+          </Typography>
         </div>
-        <Card className="u-ta-center u-p-1 u-pb-half">
+        <AcquisitionResultInfo
+          icon={illustration}
+          text={t(`Acquisition.tooltip.${page}`)}
+          className="u-mb-1"
+        />
+        <Card className="u-ta-center u-p-1">
           <div className="u-mah-5">
             {!isPDF(currentFile) ? (
-              <img
-                src={URL.createObjectURL(currentFile)}
-                className={styles.img}
+              <RotateImage
+                image={URL.createObjectURL(currentFile)}
+                rotation={rotationImage}
+                a11n={{ 'aria-hidden': true }}
+                ref={imageRef}
               />
             ) : (
               <>
-                <Icon icon="file-type-pdf" size={80} />
+                <Icon icon="file-type-pdf" size={80} aria-hidden="true" />
                 <Typography>{currentFile.name}</Typography>
               </>
             )}
           </div>
-          <Button
-            className="u-mt-half"
-            data-testid="retry-button"
-            label={t('Acquisition.retry')}
-            theme="text"
-            onClick={changeSelectedFile}
-          />
+          <div style={{ display: 'flex', gap: '1rem', marginTop: '1rem' }}>
+            <Button
+              data-testid="retry-button"
+              label={t('Acquisition.retry')}
+              fullWidth
+              variant="secondary"
+              onClick={changeSelectedFile}
+            />
+            <IconButton
+              data-testid="rotate-button"
+              classes={{ root: styles.iconRetry }}
+              size="small"
+              onClick={handleRotate}
+              aria-label={t('Acquisition.rotate')}
+              title={t('Acquisition.rotate')}
+            >
+              <Icon icon="rotate-left" />
+            </IconButton>
+          </div>
         </Card>
       </div>
       <DialogActions
@@ -118,16 +154,16 @@ const AcquisitionResult = ({ currentFile, setCurrentFile, currentStep }) => {
         <Button
           className="u-db"
           data-testid="next-button"
-          extension="full"
+          fullWidth
           label={t('common.next')}
           onClick={() => onValid(false)}
         />
         {multipage && (
-          <ButtonLink
+          <Button
             className="u-ml-0 u-mb-half"
             data-testid="repeat-button"
-            extension="full"
-            theme="secondary"
+            fullWidth
+            variant="secondary"
             icon="camera"
             label={t('Acquisition.repeat')}
             onClick={() => onValid(true)}
@@ -144,4 +180,4 @@ AcquisitionResult.propTypes = {
   currentStep: PaperDefinitionsStepPropTypes
 }
 
-export default memo(AcquisitionResult)
+export default AcquisitionResult
