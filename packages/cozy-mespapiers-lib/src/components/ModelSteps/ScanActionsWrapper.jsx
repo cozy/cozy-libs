c83b72b71eb9b6432059f09147d279790794c6c5
AlexisG <merkkur39@gmail.com>
2023-05-17 19:35:04 +0200

    feat(mespapiers): Add ScanFlagshipActions component
    Addition of actions on the Scan step,
    if the application is used with the flagship app

--- packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanActionsWrapper.jsx
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanActionsWrapper.jsx
@@ -1,12 +1,30 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-import { isMobile } from 'cozy-device-helper'
+import { isFlagshipApp, isMobile } from 'cozy-device-helper'
+import flag from 'cozy-flags'
+import { useWebviewIntent } from 'cozy-intent'
 
 import ScanDesktopActions from './ScanDesktopActions'
+import ScanFlagshipActions from './ScanFlagshipActions'
 import ScanMobileActions from './ScanMobileActions'
 
 const ScanActionsWrapper = props => {
+  const [isFlagshipScanAvailable, setIsFlagshipScanAvailable] = useState(false)
+  const webviewIntent = useWebviewIntent()
+
+  useEffect(() => {
+    const checkScanDocument = async () => {
+      const isAvailable = await webviewIntent.call('isScannerAvailable')
+      setIsFlagshipScanAvailable(isAvailable)
+    }
+    webviewIntent && checkScanDocument()
+  }, [webviewIntent])
+
+  if ((isFlagshipApp() || flag('flagship.debug')) && isFlagshipScanAvailable) {
+    return <ScanFlagshipActions {...props} />
+  }
+
   if (isMobile()) {
     return <ScanMobileActions {...props} />
   }
@@ -17,6 +35,7 @@ const ScanActionsWrapper = props => {
 ScanActionsWrapper.propTypes = {
   onChangeFile: PropTypes.func,
   openFilePickerModal: PropTypes.func,
+  onOpenFlagshipScan: PropTypes.func
 }
 
 export default ScanActionsWrapper
