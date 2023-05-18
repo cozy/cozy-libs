50b9ed4dc7800f1639b84b215906f0a640dac260
AlexisG <merkkur39@gmail.com>
2023-05-18 15:58:54 +0200

    feat(mespapiers): Add `ScanResultTitle` component

---
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanResultTitle.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+import { makeStyles } from 'cozy-ui/transpiled/react/styles'
+
+const useStyles = makeStyles(() => ({
+  typography: {
+    color: 'var(--successColor)',
+    marginLeft: '0.5rem'
+  }
+}))
+
+const ScanResultTitle = () => {
+  const styles = useStyles()
+  const { t } = useI18n()
+
+  return (
+    <div className="u-flex u-flex-items-center u-flex-justify-center u-mb-1-half">
+      <Icon
+        icon="check-circle"
+        color="var(--successColor)"
+        aria-hidden="true"
+      />
+      <Typography variant="h4" className={styles.typography} role="status">
+        {t('Acquisition.success')}
+      </Typography>
+    </div>
+  )
+}
+
+export default ScanResultTitle
