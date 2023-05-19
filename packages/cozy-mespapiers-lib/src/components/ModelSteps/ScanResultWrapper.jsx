9b1fd4851407ff3d8e7c3dbabd1c257436266b55
AlexisG <merkkur39@gmail.com>
2023-05-19 13:10:35 +0200

    feat(mespapiers): Split ScanResultWrapper component
    And use the new components

--- packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanResultWrapper.jsx
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanResultWrapper.jsx
@@ -1,73 +1,28 @@
 import cx from 'classnames'
 import PropTypes from 'prop-types'
-import React, { memo, useCallback } from 'react'
+import React, { memo } from 'react'
 
-import Avatar from 'cozy-ui/transpiled/react/Avatar'
-import Button, { ButtonLink } from 'cozy-ui/transpiled/react/Button'
-import Card from 'cozy-ui/transpiled/react/Card'
-import DialogActions from 'cozy-ui/transpiled/react/DialogActions'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
-import Icon from 'cozy-ui/transpiled/react/Icon'
-import Typography from 'cozy-ui/transpiled/react/Typography'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import useEventListener from 'cozy-ui/transpiled/react/hooks/useEventListener'
-import { makeStyles } from 'cozy-ui/transpiled/react/styles'
 
-import { isSameFile } from './helpers'
+import ScanResultActions from './ScanResultActions'
+import ScanResultCard from './ScanResultCard'
+import ScanResultInfo from './ScanResultInfo'
+import ScanResultTitle from './ScanResultTitle'
 import { PaperDefinitionsStepPropTypes } from '../../constants/PaperDefinitionsPropTypes'
 import { KEYS } from '../../constants/const'
-import { useFormData } from '../Hooks/useFormData'
 import { useStepperDialog } from '../Hooks/useStepperDialog'
 
-const isPDF = file => file.type === 'application/pdf'
-
-const useStyles = makeStyles(() => ({
-  img: {
-    maxWidth: '100%',
-    maxHeight: 'inherit'
-  },
-  avatar: {
-    color: 'var(--paperBackgroundColor)',
-    backgroundColor: 'var(--successColor)',
-    marginBottom: '1rem'
-  }
-}))
-
 const ScanResultWrapper = ({ currentFile, setCurrentFile, currentStep }) => {
-  const styles = useStyles()
+  const { page = 'default', illustration } = currentStep
   const { t } = useI18n()
   const { isMobile } = useBreakpoints()
   const { nextStep } = useStepperDialog()
-  const { setFormData, formData } = useFormData()
-  const { multipage } = currentStep
 
-  const changeSelectedFile = () => {
-    const newData = formData.data.filter(
-      data => !isSameFile(currentFile, data.file)
-    )
-
-    setFormData(prev => ({
-      ...prev,
-      data: newData
-    }))
-
-    setCurrentFile(null)
+  const handleKeyDown = ({ key }) => {
+    if (key === KEYS.ENTER) nextStep()
   }
-
-  const onValid = useCallback(
-    (repeat = false) => {
-      if (!repeat) nextStep()
-      else setCurrentFile(null)
-    },
-    [nextStep, setCurrentFile]
-  )
-
-  const handleKeyDown = useCallback(
-    ({ key }) => {
-      if (key === KEYS.ENTER) onValid()
-    },
-    [onValid]
-  )
 
   useEventListener(window, 'keydown', handleKeyDown)
 
@@ -81,59 +36,22 @@ const ScanResultWrapper = ({ currentFile, setCurrentFile, currentStep }) => {
           }
         )}
       >
-        <div className="u-flex u-flex-column u-flex-items-center u-mb-2">
-          <Avatar icon="check" size="xlarge" className={styles.avatar} />
-          <Typography variant="h5">{t('Acquisition.success')}</Typography>
-        </div>
-        <Card className="u-ta-center u-p-1 u-pb-half">
-          <div className="u-mah-5">
-            {!isPDF(currentFile) ? (
-              <img
-                src={URL.createObjectURL(currentFile)}
-                className={styles.img}
-              />
-            ) : (
-              <>
-                <Icon icon="file-type-pdf" size={80} />
-                <Typography>{currentFile.name}</Typography>
-              </>
-            )}
-          </div>
-          <Button
-            className="u-mt-half"
-            data-testid="retry-button"
-            label={t('Acquisition.retry')}
-            theme="text"
-            onClick={changeSelectedFile}
-          />
-        </Card>
-      </div>
-      <DialogActions
-        disableSpacing
-        className={cx('columnLayout u-mb-1-half u-mt-0 cozyDialogActions', {
-          'u-mh-1': !isMobile,
-          'u-mh-0': isMobile
-        })}
-      >
-        <Button
-          className="u-db"
-          data-testid="next-button"
-          extension="full"
-          label={t('common.next')}
-          onClick={() => onValid(false)}
+        <ScanResultTitle />
+        <ScanResultInfo
+          icon={illustration}
+          text={t(`Acquisition.tooltip.${page}`)}
+          className="u-mb-1"
         />
-        {multipage && (
-          <ButtonLink
-            className="u-ml-0 u-mb-half"
-            data-testid="repeat-button"
-            extension="full"
-            theme="secondary"
-            icon="camera"
-            label={t('Acquisition.repeat')}
-            onClick={() => onValid(true)}
-          />
-        )}
-      </DialogActions>
+        <ScanResultCard
+          currentFile={currentFile}
+          setCurrentFile={setCurrentFile}
+        />
+      </div>
+      <ScanResultActions
+        currentStep={currentStep}
+        onNextStep={nextStep}
+        onRepeatStep={() => setCurrentFile(null)}
+      />
     </>
   )
 }
