dc7919259a1084d21968a6dcbec068c7e93ce90c
Crash-- <quentin.valmori@gmail.com>
2023-05-19 12:20:40 +0200

    test: Add DialogProvider to AppLike

--- packages/cozy-harvest-lib/test/AppLike.jsx
+++ packages/cozy-harvest-lib/test/AppLike.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
 
 import CozyClient, { CozyProvider as CozyClientProvider } from 'cozy-client'
+import { useCozyDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import I18n from 'cozy-ui/transpiled/react/I18n'
 import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
+import DialogContext from '../src/components/DialogContext'
 import enLocale from '../src/locales/en.json'
 
 const defaultClient = new CozyClient()
@@ -18,15 +20,31 @@ const AppLike = ({
 }) => {
   const client = clientOption || defaultClient
   const store = storeOption || defaultClient.store
+
   return (
     <CozyClientProvider client={client}>
       <BreakpointsProvider>
-        <I18n lang="en" dictRequire={() => locale || enLocale}>
-          <ReduxProvider store={store}>{children}</ReduxProvider>
-        </I18n>
+        <DialogContextApp>
+          <I18n lang="en" dictRequire={() => locale || enLocale}>
+            <ReduxProvider store={store}>{children}</ReduxProvider>
+          </I18n>
+        </DialogContextApp>
       </BreakpointsProvider>
     </CozyClientProvider>
   )
 }
 
+const DialogContextApp = ({ children }) => {
+  const dialogContext = useCozyDialog({
+    size: 'l',
+    open: true,
+    onClose: null,
+    disableTitleAutoPadding: true
+  })
+  return (
+    <DialogContext.Provider value={dialogContext}>
+      {children}
+    </DialogContext.Provider>
+  )
+}
 export default AppLike
