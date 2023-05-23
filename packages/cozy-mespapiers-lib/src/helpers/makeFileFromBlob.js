6f68e820227e2d9b25f591687e804bcff0a75918
AlexisG <merkkur39@gmail.com>
2023-05-23 11:45:23 +0200

    refactor(mespapiers): Rename `makeFileWithBlob ` to `makeFileFromBlob`

--- packages/cozy-mespapiers-lib/src/helpers/makeFileFromBlob.js
+++ packages/cozy-mespapiers-lib/src/helpers/makeFileFromBlob.js
@@ -4,7 +4,7 @@
  * @param {string} attrs.name - File name
  * @returns {File}
  */
-export const makeFileWithBlob = (blobFile, { name } = {}) => {
+export const makeFileFromBlob = (blobFile, { name } = {}) => {
   const defaultName = `${name || 'temp'}.${blobFile.type.split('/')[1]}`
   const newFile = new File([blobFile], defaultName, { type: blobFile.type })
 
