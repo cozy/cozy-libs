4d8994c0b29d4098cafe35c3bdae6dc30003da31
AlexisG <merkkur39@gmail.com>
2023-05-23 11:22:08 +0200

    feat(mespapiers): Add RotateImage component

---
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/widgets/RotateImage.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+import React, { forwardRef } from 'react'
+
+import { useRotatedImage } from '../../Hooks/useRotatedImage'
+
+const RotateImage = forwardRef(
+  ({ image, onLoaded, rotation, a11n, ...props }, ref) => {
+    const src = useRotatedImage(image, rotation) ?? {}
+
+    return (
+      <img
+        ref={ref}
+        onLoad={onLoaded}
+        src={src}
+        style={{ maxWidth: '100%', maxHeight: 'inherit' }}
+        {...a11n}
+        {...props}
+      />
+    )
+  }
+)
+RotateImage.displayName = 'RotateImage'
+
+RotateImage.propTypes = {
+  image: PropTypes.string,
+  onLoaded: PropTypes.func,
+  rotation: PropTypes.number,
+  alt: PropTypes.string,
+  a11n: PropTypes.object
+}
+
+RotateImage.defaultProps = {
+  onLoaded: () => {},
+  rotation: 0,
+  alt: '',
+  a11n: {}
+}
+
+export default RotateImage
