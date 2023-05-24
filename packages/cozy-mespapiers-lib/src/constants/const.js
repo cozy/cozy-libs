adce299a1726fea8235bfbd96d20cb6a12d3dec8
AlexisG <merkkur39@gmail.com>
2023-05-24 17:06:06 +0200

    feat: Add Cozy app Android and iOS URLs

--- packages/cozy-mespapiers-lib/src/constants/const.js
+++ packages/cozy-mespapiers-lib/src/constants/const.js
@@ -3,4 +3,8 @@ export const KEYS = {
   ENTER: 'Enter'
 }
 
+export const IOS_APP_URL = 'https://apps.apple.com/fr/app/my-cozy/id1600636174'
+export const ANDROID_APP_URL =
+  'https://play.google.com/store/apps/details?id=io.cozy.flagship.mobile&hl'
+
 export const DEFAULT_MAX_FILES_DISPLAYED = 3
