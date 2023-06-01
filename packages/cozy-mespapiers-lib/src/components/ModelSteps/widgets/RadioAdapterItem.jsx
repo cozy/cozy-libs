a39b4c84402b2bd091744cdf843d2b0385b9da99
AlexisG <merkkur39@gmail.com>
2023-06-01 10:33:17 +0200

    refactor(mespapiers): Split RadioAdapter component

---
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/widgets/RadioAdapterItem.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemIcon'
+import Radio from 'cozy-ui/transpiled/react/Radios'
+
+import { useScannerI18n } from '../../Hooks/useScannerI18n'
+
+const RadioAdapterItem = ({ onClick, option, value }) => {
+  const scannerT = useScannerI18n()
+
+  const isChecked = inputValue => {
+    return inputValue === value
+  }
+
+  return (
+    <ListItem button onClick={onClick}>
+      <ListItemIcon>
+        <Radio value={option} checked={isChecked(option)} />
+      </ListItemIcon>
+      <ListItemText
+        primary={
+          <div value={option}>
+            {scannerT(`attributes.contractType.${option}`)}
+          </div>
+        }
+      />
+    </ListItem>
+  )
+}
+
+RadioAdapterItem.propTypes = {
+  onClick: PropTypes.func,
+  option: PropTypes.string,
+  value: PropTypes.string
+}
+
+export default RadioAdapterItem
