27502fee2768bf1adc95443b1d953af33684b543
AlexisG <merkkur39@gmail.com>
2023-06-09 16:17:16 +0200

    feat(mespapiers): Add StepperDialogTitle component

---
+++ packages/cozy-mespapiers-lib/src/components/StepperDialog/StepperDialogTitle.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import Typography from 'cozy-ui/transpiled/react/Typography'
+
+import { useScannerI18n } from '../Hooks/useScannerI18n'
+import { useStepperDialog } from '../Hooks/useStepperDialog'
+
+const StepperDialogTitle = () => {
+  const scannerT = useScannerI18n()
+  const {
+    allCurrentSteps,
+    currentStepIndex,
+    currentDefinition,
+    stepperDialogTitle
+  } = useStepperDialog()
+
+  return (
+    <>
+      {scannerT(`items.${stepperDialogTitle}`, {
+        country: currentDefinition.country
+      })}
+      <Typography variant="h6">
+        {`${currentStepIndex}/${allCurrentSteps.length}`}
+      </Typography>
+    </>
+  )
+}
+
+export default StepperDialogTitle
