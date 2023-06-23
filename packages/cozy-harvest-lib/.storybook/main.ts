02b56a20e810e689e5405c32e7a23d8038bd7d35
Antonin Cezard <acezard@gmail.com>
2023-06-23 15:12:54 +0200

    feat(harvest): Convert Storybook to TS
    Also remove default files except one

--- packages/cozy-harvest-lib/.storybook/main.ts
+++ packages/cozy-harvest-lib/.storybook/main.ts
@@ -1,5 +1,6 @@
-/** @type { import('@storybook/react-webpack5').StorybookConfig } */
-const config = {
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
@@ -14,4 +15,5 @@ const config = {
     autodocs: "tag",
   },
 };
+
 export default config;
