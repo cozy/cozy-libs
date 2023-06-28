fb5731849ffd792a4e7e55ac66cb5daf22188bb5
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-06-28 15:56:49 +0200

    feat(codemods): Add transform-ui-muicozytheme-imports

---
+++ packages/cozy-codemods/src/transforms/transform-ui-muicozytheme-imports.js
@@ -0,0 +1,49 @@
+const transformUiDeprecatedImports = (file, api) => {
+  const j = api.jscodeshift
+  const root = j(file.source)
+
+  const deprecatedComponentsPath = [
+    'react/MuiCozyTheme/Buttons',
+    'react/MuiCozyTheme/MuiBreadcrumbs',
+    'react/MuiCozyTheme/Dialog',
+    'react/MuiCozyTheme/Accordion',
+    'react/MuiCozyTheme/AccordionDetails',
+    'react/MuiCozyTheme/AccordionSummary',
+    'react/MuiCozyTheme/Divider',
+    'react/MuiCozyTheme/Grid',
+    'react/MuiCozyTheme/List',
+    'react/MuiCozyTheme/ListItem',
+    'react/MuiCozyTheme/ListItemIcon',
+    'react/MuiCozyTheme/ListItemSecondaryAction',
+    'react/MuiCozyTheme/ListSubheader',
+    'react/MuiCozyTheme/Menu',
+    'react/MuiCozyTheme/Switch',
+    'react/MuiCozyTheme/TextField'
+  ]
+
+  root.find(j.ImportDeclaration).forEach(nodePath => {
+    const absoluteImportPath = nodePath.value.source.value
+    const relativeImportPath = absoluteImportPath.replace(
+      'cozy-ui/transpiled/',
+      ''
+    )
+
+    const shouldBeTransformed = deprecatedComponentsPath.some(path =>
+      relativeImportPath.includes(path)
+    )
+
+    if (shouldBeTransformed) {
+      const newPath = absoluteImportPath
+        .replace('MuiCozyTheme/', '')
+        .replace('MuiBreadcrumbs', 'Breadcrumbs')
+        .replace('Buttons', 'Button')
+
+      nodePath.value.source.value = newPath
+      nodePath.value.source.raw = newPath
+    }
+  })
+
+  return root.toSource()
+}
+
+module.exports = transformUiDeprecatedImports
