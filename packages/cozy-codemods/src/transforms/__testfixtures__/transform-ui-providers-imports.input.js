ec35ec0db5f53defee7f4db4df6f815fe3d8eece
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-07-06 15:43:52 +0200

    feat(codemods): Add transform-ui-providers-imports

---
+++ packages/cozy-codemods/src/transforms/__testfixtures__/transform-ui-providers-imports.input.js
@@ -0,0 +1,10 @@
+import CozyTheme, { useCozyTheme } from "cozy-ui/transpiled/react/CozyTheme"
+import I18n, { useI18n, initTranslation, extend } from "cozy-ui/transpiled/react/I18n"
+import withLocales from "cozy-ui/transpiled/react/I18n/withLocales"
+import withOnlyLocales from "cozy-ui/transpiled/react/I18n/withOnlyLocales"
+import useBreakpoints, {
+  BreakpointsProvider
+} from "cozy-ui/transpiled/react/hooks/useBreakpoints"
+
+// should not be modified
+import useRealtime from "cozy-ui/transpiled/react/hooks/useRealtime"
