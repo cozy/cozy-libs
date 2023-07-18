fe0d94585e85138b3cd0ab7cc72cef461fcdc218
AlexisG <merkkur39@gmail.com>
2023-07-18 12:41:51 +0200

    feat(mespapiers): Create SubmitButton to handle form submission
    This component contains the button and
    all the submit logic during a paper creation.
    
    Until then, all this logic is present only in the Contact step,
    which required having this step last in the paper creation process.

---
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/widgets/SubmitButton.jsx
@@ -0,0 +1,80 @@
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
+
+import { useClient } from 'cozy-client'
+import Button from 'cozy-ui/transpiled/react/Buttons'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import useEventListener from 'cozy-ui/transpiled/react/hooks/useEventListener'
+
+import ConfirmReplaceFile from './ConfirmReplaceFile'
+import { KEYS } from '../../../constants/const'
+import { FILES_DOCTYPE } from '../../../doctypes'
+import { useFormData } from '../../Hooks/useFormData'
+
+const SubmitButton = ({ onClose, disabled }) => {
+  const [isBusy, setIsBusy] = useState(false)
+  const [confirmReplaceFileModal, setConfirmReplaceFileModal] = useState(false)
+  const { formSubmit, formData } = useFormData()
+  const client = useClient()
+  const { t } = useI18n()
+
+  const cozyFiles = formData.data.filter(d => d.file.from === 'cozy')
+  const isDisabled = disabled || isBusy
+
+  const submit = async () => {
+    setIsBusy(true)
+    await formSubmit()
+    onClose()
+  }
+
+  const handleReplace = async isFileReplaced => {
+    if (isFileReplaced) {
+      for (const { file } of cozyFiles) {
+        await client.destroy({ _id: file.id, _type: FILES_DOCTYPE })
+      }
+    }
+    submit()
+  }
+
+  const handleClick = () => {
+    if (cozyFiles.length > 0) {
+      setConfirmReplaceFileModal(true)
+    } else {
+      submit()
+    }
+  }
+
+  const handleKeyDown = ({ key }) => {
+    if (isDisabled) return
+    if (key === KEYS.ENTER) handleClick()
+  }
+
+  useEventListener(window, 'keydown', handleKeyDown)
+
+  return (
+    <>
+      <Button
+        fullWidth
+        label={t(!isBusy ? 'ContactStep.save' : 'ContactStep.onLoad')}
+        onClick={handleClick}
+        disabled={isDisabled}
+        busy={isBusy}
+        data-testid="ButtonSave"
+      />
+      {confirmReplaceFileModal && (
+        <ConfirmReplaceFile
+          onClose={() => setConfirmReplaceFileModal(false)}
+          onReplace={handleReplace}
+          cozyFilesCount={cozyFiles.length}
+        />
+      )}
+    </>
+  )
+}
+
+SubmitButton.propTypes = {
+  onClose: PropTypes.func.isRequired,
+  disabled: PropTypes.bool
+}
+
+export default SubmitButton
