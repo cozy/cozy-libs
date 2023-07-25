d407eade336800e5a5398a695abb7342792e58ae
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-07-25 15:20:32 +0200

    feat(mespapiers): Categorize and add contact reference on reminders

--- packages/cozy-mespapiers-lib/src/components/ModelSteps/NoteDialog.jsx
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/NoteDialog.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import { useClient } from 'cozy-client'
+import { Qualification } from 'cozy-client/dist/models/document'
+import { saveFileQualification, normalize } from 'cozy-client/dist/models/file'
 import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import IntentIframe from 'cozy-ui/transpiled/react/IntentIframe'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 
+import { FILES_DOCTYPE } from '../../doctypes'
+import { addContactReferenceToFile } from '../../helpers/createPdfAndSave'
+import { buildFileQueryById } from '../../helpers/queries'
+import { useFormData } from '../Hooks/useFormData'
 import { useStepperDialog } from '../Hooks/useStepperDialog'
 import StepperDialogTitle from '../StepperDialog/StepperDialogTitle'
 
@@ -12,6 +19,31 @@ const NoteDialog = ({ onClose, onBack }) => {
   const client = useClient()
   const { isMobile } = useBreakpoints()
   const { currentStepIndex } = useStepperDialog()
+  const { qualificationLabel } = useParams()
+  const { formData } = useFormData()
+
+  const handleTerminate = async ({ document }) => {
+    const qualification = Qualification.getByLabel(qualificationLabel)
+    const fileCollection = client.collection(FILES_DOCTYPE)
+
+    const { data } = await client.fetchQueryAndGetFromState(
+      buildFileQueryById(document.id)
+    )
+    const { data: fileCreated } = await saveFileQualification(
+      client,
+      data,
+      qualification
+    )
+    const normalizedFile = normalize(fileCreated)
+
+    await addContactReferenceToFile({
+      fileCreated: normalizedFile,
+      fileCollection,
+      contacts: formData.contacts
+    })
+
+    onClose()
+  }
 
   return (
     <Dialog
@@ -36,7 +68,7 @@ const NoteDialog = ({ onClose, onBack }) => {
           }}
           create={client.intents.create}
           onCancel={onClose}
-          onTerminate={onClose}
+          onTerminate={handleTerminate}
         />
       }
     />
