f94eaa010e284606dac3fb4f5c599f61e42ccf33
cballevre <celestin.ballevre@protonmail.com>
2023-07-27 16:10:51 +0200

    feat: Limit recipients by document

--- packages/cozy-sharing/src/helpers/recipients.js
+++ packages/cozy-sharing/src/helpers/recipients.js
@@ -1,3 +1,5 @@
+import flag from 'cozy-flags'
+
 export const filterAndReworkRecipients = (recipients, previousRecipients) => {
   return recipients
     .filter(recipient => recipient.status !== 'owner')
@@ -19,3 +21,27 @@ export const filterAndReworkRecipients = (recipients, previousRecipients) => {
 }
 
 export const FADE_IN_DURATION = 600
+
+/**
+ * @returns {boolean} the maximum recipients limit by document
+ */
+export const computeRecipientsLimit = () => {
+  const flagLimit = flag('sharing.recipients-limit')
+  if (flagLimit !== null) {
+    return flagLimit
+  }
+  return 100
+}
+
+/**
+ * @param {object[]} current List of current recipients
+ * @param {object[]} next List of next recipients
+ * @returns {boolean} whether total number of recipients has reach the limit
+ */
+export const hasReachRecipientsLimit = (current, next) => {
+  const limit = computeRecipientsLimit()
+  if (current.length + next.length > limit) {
+    return true
+  }
+  return false
+}
