c08d6c1583020b22ac6a35b25b317d59a8dbcbf3
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-08-01 10:34:34 +0200

    fix(mespapiers): Copy content on Note wasn't working on iOS Safari
    Like it said in the doc: Must be called in response to a user gesture event, like click or keyup.

--- packages/cozy-mespapiers-lib/src/components/Actions/Items/copyReminderContent.js
+++ packages/cozy-mespapiers-lib/src/components/Actions/Items/copyReminderContent.js
@@ -1,48 +1,43 @@
 import copy from 'copy-text-to-clipboard'
-import React, { forwardRef } from 'react'
+import React, { useRef } from 'react'
 
+import useFetchJSON from 'cozy-client/dist/hooks/useFetchJSON'
 import ActionsMenuItem from 'cozy-ui/transpiled/react/ActionsMenu/ActionsMenuItem'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
 import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
 import Alerter from 'cozy-ui/transpiled/react/deprecated/Alerter'
+import useEventListener from 'cozy-ui/transpiled/react/hooks/useEventListener'
 
 import withLocales from '../../../locales/withLocales'
 
 export const copyReminderContent = () => {
   return {
     name: 'copyReminderContent',
-    action: async (doc, { client }) => {
-      try {
-        const reminderContent = await client.stackClient.fetchJSON(
-          'GET',
-          `/notes/${doc._id}/text`
-        )
-        const hasCopied = copy(reminderContent)
+    Component: withLocales(({ doc, ...props }) => {
+      const { t } = useI18n()
+      const elRef = useRef()
+      const { data } = useFetchJSON('GET', `/notes/${doc._id}/text`)
+
+      useEventListener(elRef.current, 'click', () => {
+        const hasCopied = copy(data)
+
         if (hasCopied) {
           Alerter.success('action.copyReminderContent.success')
         } else {
           Alerter.error('action.copyReminderContent.error')
         }
-      } catch {
-        Alerter.error('action.copyReminderContent.error')
-      }
-    },
-    Component: withLocales(
-      // eslint-disable-next-line react/display-name
-      forwardRef((props, ref) => {
-        const { t } = useI18n()
-
-        return (
-          <ActionsMenuItem {...props} ref={ref}>
-            <ListItemIcon>
-              <Icon icon="copy" />
-            </ListItemIcon>
-            <ListItemText primary={t('action.copyReminderContent.text')} />
-          </ActionsMenuItem>
-        )
       })
-    )
+
+      return (
+        <ActionsMenuItem {...props} ref={elRef}>
+          <ListItemIcon>
+            <Icon icon="copy" />
+          </ListItemIcon>
+          <ListItemText primary={t('action.copyReminderContent.text')} />
+        </ActionsMenuItem>
+      )
+    })
   }
 }
