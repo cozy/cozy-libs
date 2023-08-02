d5dd69f38dcf167a2da8341dec3f93242480922d
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-08-02 12:11:34 +0200

    feat(mespapiers): Remove event listener on note content copy
    It was not working on desktop.
    
    After a lot of research and try, I didn't
    find a better way to have something working on mobile and desktop.
    I don't know why but fetchJSON approach is only working on desktop
    and useFetchJSON only on mobile. I don't think the cozy code is to blame, because with useFetchJSON or fetchJSON we get the content of the note as expected, but it's the copy-text-to-clipboard lib that doesn't seem to be working despite receiving the content.

--- packages/cozy-mespapiers-lib/src/components/Actions/Items/copyReminderContent.js
+++ packages/cozy-mespapiers-lib/src/components/Actions/Items/copyReminderContent.js
@@ -1,26 +1,27 @@
 import copy from 'copy-text-to-clipboard'
-import React, { useRef } from 'react'
+import React, { forwardRef } from 'react'
 
 import useFetchJSON from 'cozy-client/dist/hooks/useFetchJSON'
+import { isMobile } from 'cozy-device-helper'
+import log from 'cozy-logger'
 import ActionsMenuItem from 'cozy-ui/transpiled/react/ActionsMenu/ActionsMenuItem'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
 import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
 import Alerter from 'cozy-ui/transpiled/react/deprecated/Alerter'
-import useEventListener from 'cozy-ui/transpiled/react/hooks/useEventListener'
 
 import withLocales from '../../../locales/withLocales'
 
 export const copyReminderContent = () => {
   return {
     name: 'copyReminderContent',
-    Component: withLocales(({ doc, ...props }) => {
-      const { t } = useI18n()
-      const elRef = useRef()
-      const { data } = useFetchJSON('GET', `/notes/${doc._id}/text`)
+    action: async (doc, { client, noteContent }) => {
+      const data = isMobile()
+        ? noteContent
+        : await client.stackClient.fetchJSON('GET', `/notes/${doc._id}/text`)
 
-      useEventListener(elRef.current, 'click', () => {
+      try {
         const hasCopied = copy(data)
 
         if (hasCopied) {
@@ -28,16 +29,33 @@ export const copyReminderContent = () => {
         } else {
           Alerter.error('action.copyReminderContent.error')
         }
-      })
+      } catch (error) {
+        Alerter.error('action.copyReminderContent.error')
+        log(
+          'error',
+          `Error in 'copyReminderContent' when trying to copy to clipboard: ${error}`
+        )
+      }
+    },
+    Component: withLocales(
+      // eslint-disable-next-line react/display-name
+      forwardRef(({ doc, onClick, ...props }, ref) => {
+        const { t } = useI18n()
+        const { data } = useFetchJSON('GET', `/notes/${doc._id}/text`)
 
-      return (
-        <ActionsMenuItem {...props} ref={elRef}>
-          <ListItemIcon>
-            <Icon icon="copy" />
-          </ListItemIcon>
-          <ListItemText primary={t('action.copyReminderContent.text')} />
-        </ActionsMenuItem>
-      )
-    })
+        return (
+          <ActionsMenuItem
+            {...props}
+            ref={ref}
+            onClick={() => onClick({ noteContent: data })}
+          >
+            <ListItemIcon>
+              <Icon icon="copy" />
+            </ListItemIcon>
+            <ListItemText primary={t('action.copyReminderContent.text')} />
+          </ActionsMenuItem>
+        )
+      })
+    )
   }
 }
