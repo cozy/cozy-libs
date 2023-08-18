2438da5b24a1f54309e753173a67f85aa9691bfd
AlexisG <merkkur39@gmail.com>
2023-08-18 09:14:29 +0200

    feat(mespapiers): Set onboarded true if skipOnboarding query param

--- packages/cozy-mespapiers-lib/src/components/OnboardedGuardedRoute.jsx
+++ packages/cozy-mespapiers-lib/src/components/OnboardedGuardedRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   Navigate,
   Outlet,
@@ -6,16 +6,19 @@ import {
   useSearchParams
 } from 'react-router-dom'
 
-import { useQuery, hasQueryBeenLoaded } from 'cozy-client'
+import { useQuery, hasQueryBeenLoaded, useClient } from 'cozy-client'
+import log from 'cozy-logger'
 import Spinner from 'cozy-ui/transpiled/react/Spinner'
 
 import { useOnboarding } from './Hooks/useOnboarding'
+import { SETTINGS_DOCTYPE } from '../doctypes'
 import { getAppSettings } from '../helpers/queries'
 
 const OnboardedGuardedRoute = () => {
   const location = useLocation()
   const [searchParams] = useSearchParams()
   const { OnboardingComponent } = useOnboarding()
+  const client = useClient()
 
   const skipOnboarding = searchParams.get('skipOnboarding') !== null
   const isOnboardingPage = location.pathname === '/paper/onboarding'
@@ -24,6 +27,32 @@ const OnboardedGuardedRoute = () => {
     getAppSettings.definition,
     getAppSettings.options
   )
+
+  useEffect(() => {
+    const setOnboardedTrue = async () => {
+      try {
+        await client.save({
+          ...settingsData[0],
+          onboarded: true,
+          _type: SETTINGS_DOCTYPE
+        })
+      } catch (error) {
+        log(
+          'error',
+          'Error when saving settings in OnboardedGuardedRoute',
+          error
+        )
+      }
+    }
+
+    if (
+      skipOnboarding &&
+      settingsData?.length > 0 &&
+      !settingsData[0].onboarded
+    ) {
+      setOnboardedTrue()
+    }
+  }, [client, settingsData, skipOnboarding])
 
   if (!hasQueryBeenLoaded(settingsQuery)) {
     return (
