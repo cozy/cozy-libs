432a1dfb8a224cd69d3052f2f1e27fb1d044c279
Alexis G <merkkur39@gmail.com>
2023-08-24 17:06:18 +0200

    feat(mespapiers): Install konnector from intent
    This component will display an Intent modal allowing
    to install a konnector, instead of being redirected
    directly to cozy-store.

---
+++ packages/cozy-mespapiers-lib/src/components/InstallKonnectorFromIntent/InstallKonnectorFromIntent.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+
+import { useClient } from 'cozy-client'
+import IntentModal from 'cozy-ui/transpiled/react/deprecated/IntentModal'
+
+import { APPS_DOCTYPE } from '../../doctypes'
+
+const InstallKonnectorFromIntent = () => {
+  const navigate = useNavigate()
+  const [searchParams] = useSearchParams()
+  const client = useClient()
+  const konnectorSlug = searchParams.get('slug')
+  const konnectorCategory = searchParams.get('category')
+
+  const handleTerminate = ({ document }) => {
+    const redirectLink = `${searchParams.get('redirectAfterInstall')}${
+      document.slug
+    }`
+
+    return navigate(`${redirectLink}`, {
+      replace: true
+    })
+  }
+
+  const handleClose = () => {
+    return navigate('..')
+  }
+
+  return (
+    <IntentModal
+      action="INSTALL"
+      doctype={APPS_DOCTYPE}
+      mobileFullscreen
+      options={{
+        slug: konnectorSlug,
+        pageToDisplay: 'details',
+        category: konnectorCategory,
+        configure: false
+      }}
+      create={client.intents.create}
+      onComplete={handleTerminate}
+      dismissAction={handleClose}
+    />
+  )
+}
+
+export default InstallKonnectorFromIntent
