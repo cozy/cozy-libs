51e82f620eecb7d8a2c7d729c92be3038cb5772a
cballevre <celestin.ballevre@protonmail.com>
2023-09-20 09:14:23 +0200

    test(cozy-sharing): Migrate test

--- packages/cozy-sharing/test/AppLike.jsx
+++ packages/cozy-sharing/test/AppLike.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
 import { CozyProvider, createMockClient } from 'cozy-client'
-import { I18n } from 'cozy-ui/transpiled/react/I18n'
-import { BreakpointsProvider } from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
+import { BreakpointsProvider } from 'cozy-ui/transpiled/react/providers/Breakpoints'
+import CozyTheme from 'cozy-ui/transpiled/react/providers/CozyTheme'
+import { I18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import langEn from '../locales/en.json'
 
 const AppLike = ({ children, client }) => (
   <BreakpointsProvider>
-    <I18n lang="en" dictRequire={() => langEn}>
-      <CozyProvider client={client || createMockClient({})}>
-        {children}
-      </CozyProvider>
-    </I18n>
+    <CozyTheme>
+      <I18n lang="en" dictRequire={() => langEn}>
+        <CozyProvider client={client || createMockClient({})}>
+          {children}
+        </CozyProvider>
+      </I18n>
+    </CozyTheme>
   </BreakpointsProvider>
 )
 
