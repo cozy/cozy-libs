40e2b859d2b5ed5f0e5d7b9eb9796bc9d50dc9ac
Alexis G <merkkur39@gmail.com>
2023-10-25 17:52:24 +0200

    refactor: Rename `isOCRCompliant` to `isSomePaperStepsCompliantWithOCR`

--- packages/cozy-mespapiers-lib/src/helpers/isOCRActivated.js
+++ packages/cozy-mespapiers-lib/src/helpers/isOCRActivated.js
@@ -1,17 +1,16 @@
 import { isFlagshipApp } from 'cozy-device-helper'
 import flag from 'cozy-flags'
 
-import { isOCRCompliant } from './isOCRCompliant'
+import { isSomePaperStepsCompliantWithOCR } from './isSomePaperStepsCompliantWithOCR'
 
-const isOCRActivated = steps => {
+export const isOCRActivated = steps => {
   // TODO : Check whether the Flagship application has OCR capability
   if (
     isFlagshipApp() &&
-    isOCRCompliant(steps) &&
+    isSomePaperStepsCompliantWithOCR(steps) &&
     flag('mespapiers.ocr.enabled')
-  )
+  ) {
     return true
+  }
   return false
 }
-
-export { isOCRActivated }
--- packages/cozy-mespapiers-lib/src/helpers/isOCRCompliant.js
+++
@@ -1,7 +0,0 @@
-const isOCRCompliant = steps => {
-  return steps.some(
-    step => step.isDisplayed === 'ocr' || step.isDisplayed === 'all'
-  )
-}
-
-export { isOCRCompliant }
