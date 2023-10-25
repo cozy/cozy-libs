b8b12e0ccad6478d4e66f00d99c40d6d6c66d210
Alexis G <merkkur39@gmail.com>
2023-10-25 17:50:44 +0200

    feat: Add `FLAGSHIP_SCAN_TEMP_FILENAME` constant
    For the name of the file created following
    to the flagship scanner.
    It is a good discriminating element which will be used soon.

--- packages/cozy-mespapiers-lib/src/constants/const.js
+++ packages/cozy-mespapiers-lib/src/constants/const.js
@@ -8,3 +8,5 @@ export const ANDROID_APP_URL =
   'https://play.google.com/store/apps/details?id=io.cozy.flagship.mobile&hl'
 
 export const DEFAULT_MAX_FILES_DISPLAYED = 3
+
+export const FLAGSHIP_SCAN_TEMP_FILENAME = 'flagshipScanTemp.png'
