64da8a73ef5abca82e2a7089cbcdccb6695a58ff
cballevre <celestin.ballevre@protonmail.com>
2023-11-06 14:56:26 +0100

    feat: Allow to skip maintenance for konnector with a flag

--- packages/cozy-harvest-lib/src/components/hooks/useMaintenanceStatus.jsx
+++ packages/cozy-harvest-lib/src/components/hooks/useMaintenanceStatus.jsx
@@ -1,13 +1,28 @@
 import { useQuery } from 'cozy-client'
+import flag from 'cozy-flags'
 
 import { buildAppsRegistryBySlug } from '../../helpers/queries'
 
 const useMaintenanceStatus = slug => {
+  const skipMaintenanceList = flag('harvest.skip-maintenance-for.list') ?? []
+  const skipMaintenance = skipMaintenanceList.includes(slug)
+
   const registryQuery = buildAppsRegistryBySlug(slug)
-  const registryResult = useQuery(
-    registryQuery.definition,
-    registryQuery.options
-  )
+  const registryResult = useQuery(registryQuery.definition, {
+    ...registryQuery.options,
+    enabled: !skipMaintenance
+  })
+
+  if (skipMaintenance) {
+    return {
+      data: {
+        isInMaintenance: false,
+        messages: {}
+      },
+      fetchStatus: 'loaded',
+      lastError: null
+    }
+  }
 
   return {
     data: {
