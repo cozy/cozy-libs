b1aa84a5965f707f8cdfb58a9e3e332dad9fd128
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-11-27 11:15:42 +0100

    feat(mespapiers): Center Onboarding verticaly

--- packages/cozy-mespapiers-lib/src/components/Onboarding/Onboarding.jsx
+++ packages/cozy-mespapiers-lib/src/components/Onboarding/Onboarding.jsx
@@ -1,10 +1,8 @@
-import cx from 'classnames'
 import React from 'react'
 
 import { Q, useClient } from 'cozy-client'
 import Empty from 'cozy-ui/transpiled/react/Empty'
 import Button from 'cozy-ui/transpiled/react/deprecated/Button'
-import useBreakpoints from 'cozy-ui/transpiled/react/providers/Breakpoints'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import HomeCloud from '../../assets/icons/HomeCloud.svg'
@@ -13,7 +11,6 @@ import { SETTINGS_DOCTYPE } from '../../doctypes'
 const Onboarding = () => {
   const { t } = useI18n()
   const client = useClient()
-  const { isDesktop } = useBreakpoints()
 
   const onClick = async () => {
     const { data } = await client.query(Q(SETTINGS_DOCTYPE))
@@ -23,20 +20,17 @@ const Onboarding = () => {
 
   return (
     <Empty
-      className={cx('u-p-1 u-flex-grow-1', {
-        'u-flex-justify-start': isDesktop
-      })}
       icon={HomeCloud}
       iconSize="large"
       title={t('Home.Empty.title')}
       text={t('Home.Empty.text')}
-      layout={false}
+      centered
     >
       <Button
-        theme="primary"
-        onClick={onClick}
-        label={t('Onboarding.cta')}
         className="u-mb-1"
+        theme="primary"
+        label={t('Onboarding.cta')}
+        onClick={onClick}
       />
     </Empty>
   )
