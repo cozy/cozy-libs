b32efa67f4cbec954c2688e46f2bac7460b89bcc
cballevre <celestin.ballevre@protonmail.com>
2023-12-04 16:41:39 +0100

    feat: Add ErrorAlert

---
+++ packages/cozy-harvest-lib/src/components/cards/ErrorAlert.stories.jsx
@@ -0,0 +1,50 @@
+import { ErrorAlert } from './ErrorAlert'
+import { KonnectorJobError } from '../../helpers/konnectors'
+
+const meta = {
+  component: ErrorAlert,
+  args: {
+    label: 'Sync. il y a 2 mois',
+    error: new KonnectorJobError('LOGIN_FAILED'),
+    konnectorRoot: '/konnector/dummy',
+    trigger: {},
+    historyAction: () => {},
+    flow: {
+      konnector: {
+        name: 'Dummy',
+        type: 'konnector',
+        slug: 'dummy',
+        locales: {
+          en: {
+            description: 'Konnector used for debug purpose',
+            fields: {
+              date: {
+                label: 'Date'
+              },
+              error: {
+                label: 'Intended error'
+              }
+            }
+          }
+        }
+      }
+    },
+    account: {},
+    intentsApi: {},
+    isRunnable: true
+  }
+}
+
+export default meta
+
+export const Default = {
+  name: 'Default',
+  args: {}
+}
+
+export const NotRunnable = {
+  name: 'Not runnable',
+  args: {
+    isRunnable: false
+  }
+}
