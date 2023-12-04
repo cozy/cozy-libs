b56537a7fa365104d88f70f9b3af02c3d3ccf003
JF-Cozy <julien.ficheux@cozycloud.cc>
2023-12-04 16:02:43 +0100

    feat: Add tooltip on forward Fab

--- packages/cozy-mespapiers-lib/src/components/ForwardFab/ForwardFab.jsx
+++ packages/cozy-mespapiers-lib/src/components/ForwardFab/ForwardFab.jsx
@@ -2,24 +2,65 @@ import cx from 'classnames'
 import PropTypes from 'prop-types'
 import React from 'react'
 
+import { useClient, Q, useQuery } from 'cozy-client'
+import ClickAwayListener from 'cozy-ui/transpiled/react/ClickAwayListener'
 import Fab from 'cozy-ui/transpiled/react/Fab'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import Tooltip from 'cozy-ui/transpiled/react/Tooltip'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
+import { SETTINGS_DOCTYPE } from '../../doctypes'
+import { getAppSettings } from '../../helpers/queries'
 import withLocales from '../../locales/withLocales'
+import { usePapersCreated } from '../Contexts/PapersCreatedProvider'
+
+const styles = { tooltip: { whiteSpace: 'pre-line' } }
 
 const ForwardFab = ({ className, innerRef, onClick }) => {
   const { t } = useI18n()
+  const client = useClient()
+  const { countPaperCreatedByMesPapiers } = usePapersCreated()
+
+  const { data: settings } = useQuery(
+    getAppSettings.definition,
+    getAppSettings.options
+  )
+
+  const hideForwardFabTooltip = settings?.[0]?.hideForwardFabTooltip
+
+  const open = countPaperCreatedByMesPapiers === 1 && !hideForwardFabTooltip
+
+  const hideTooltip = async () => {
+    if (!open) return null
+
+    const { data } = await client.query(Q(SETTINGS_DOCTYPE))
+    const settings = data?.[0] || {}
+    await client.save({
+      ...settings,
+      _type: SETTINGS_DOCTYPE,
+      hideForwardFabTooltip: true
+    })
+  }
 
   return (
-    <Fab
-      className={cx('u-mr-half', className)}
-      innerRef={innerRef}
-      onClick={onClick}
-      aria-label={t('Home.Fab.forwardPaper')}
-    >
-      <Icon icon="paperplane" />
-    </Fab>
+    <ClickAwayListener onClickAway={hideTooltip}>
+      <Tooltip
+        open={open}
+        title={<div style={styles.tooltip}>{t('Home.Fab.tooltip')}</div>}
+      >
+        <Fab
+          className={cx('u-mr-half', className)}
+          innerRef={innerRef}
+          onClick={() => {
+            hideTooltip()
+            onClick()
+          }}
+          aria-label={t('Home.Fab.forwardPaper')}
+        >
+          <Icon icon="paperplane" />
+        </Fab>
+      </Tooltip>
+    </ClickAwayListener>
   )
 }
 
