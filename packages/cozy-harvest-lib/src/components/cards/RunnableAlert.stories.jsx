82492696ab0dcd91ceda0a694de3048706544ff5
cballevre <celestin.ballevre@protonmail.com>
2023-12-04 16:42:49 +0100

    feat: Add RunnableAlert

---
+++ packages/cozy-harvest-lib/src/components/cards/RunnableAlert.stories.jsx
@@ -0,0 +1,61 @@
+import { RunnableAlert } from './RunnableAlert'
+
+const meta = {
+  component: RunnableAlert,
+  args: {
+    label: 'Sync. il y a 2 mois',
+    isRunning: false,
+    konnectorSlug: 'dummy',
+    konnectorRoot: '/konnector/dummy',
+    trigger: {},
+    error: undefined,
+    historyAction: () => {},
+    flow: {
+      konnector: {
+        name: 'Dummy',
+        type: 'konnector',
+        slug: 'dummy',
+        locales: {
+          en: {
+            description: 'Konnector used for debug purpose',
+            fields: {
+              date: {
+                label: 'Date'
+              },
+              error: {
+                label: 'Intended error'
+              }
+            }
+          }
+        }
+      }
+    },
+    account: {},
+    intentsApi: {},
+    isRunnable: true
+  }
+}
+
+export default meta
+
+export const Default = {
+  name: 'Default',
+  args: {
+    label: 'Sync. il y a 2 mois'
+  }
+}
+
+export const Running = {
+  name: 'Running',
+  args: {
+    label: 'Date de sync. inconnue',
+    isRunning: true
+  }
+}
+
+export const NotRunnable = {
+  name: 'Not runnable',
+  args: {
+    isRunnable: false
+  }
+}
