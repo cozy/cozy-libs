6de683aba8dd7cb035e1155fc5aee949a626d96a
cballevre <celestin.ballevre@protonmail.com>
2023-12-04 16:43:22 +0100

    feat: Add TriggerAlert

---
+++ packages/cozy-harvest-lib/src/components/cards/TriggerAlert.stories.jsx
@@ -0,0 +1,52 @@
+import { TriggerAlert } from './TriggerAlert'
+import { KonnectorJobError } from '../../helpers/konnectors'
+
+const meta = {
+  component: TriggerAlert,
+  args: {
+    trigger: {},
+    isInMaintenance: false,
+    isInError: false,
+    isRunnable: true,
+    isRunning: false,
+    expectingTriggerLaunch: false,
+    withMaintenanceDescription: false,
+    error: new KonnectorJobError('LOGIN_FAILED'),
+    maintenanceMessages: {
+      en: {
+        long_message:
+          "We're currently working on this service. Please try again later."
+      }
+    },
+    account: {},
+    konnectorRoot: '/konnector/dummy',
+    historyAction: () => {},
+    intentsApi: {},
+    flow: {
+      konnector: {
+        name: 'Dummy',
+        type: 'konnector',
+        slug: 'dummy',
+        locales: {
+          en: {
+            description: 'Konnector used for debug purpose',
+            fields: {
+              date: {
+                label: 'Date'
+              },
+              error: {
+                label: 'Intended error'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+export default meta
+
+export const Default = {
+  name: 'Default'
+}
--- packages/cozy-harvest-lib/src/components/cards/helpers.js
+++ packages/cozy-harvest-lib/src/components/cards/helpers.js
@@ -19,42 +19,23 @@ const getDifferenceInMillisecondes = date => {
  * @param {function} options.t - i18n function
  * @param {import('cozy-client/types/types').IOCozyKonnector} options.konnector - Associated Connector
  * @param {object} options.trigger - Associated trigger
- * @param {object} options.running - If the connector is running
- * @param {object} options.expectingTriggerLaunch - If the trigger is waiting to be launched
- * @param {object} options.lastSuccessDate - The last date when the trigger was successfully executed
- * @param {object} options.isKonnectorRunnable - If the konnector is runnable
- * @param {object} options.isInMaintenance - If the konnector is in maintenance
+ * @param {object} options.isRunning - If the connector is running
  * @returns {string}
  */
-export const makeLabel = ({
-  t,
-  konnector,
-  trigger,
-  running,
-  expectingTriggerLaunch,
-  isInMaintenance,
-  isKonnectorRunnable
-}) => {
+export const makeLabel = ({ t, konnector, trigger, isRunning }) => {
   const lastSuccessDate = getLastSuccessDate(trigger)
-  if (!isKonnectorRunnable) {
-    return t('accountForm.notClientSide', { name: konnector.name })
-  }
-  const mantenanceSuffix = isInMaintenance
-    ? ` · ${t('konnectorBlock.inMaintenance')}`
-    : ''
-
-  if (running || expectingTriggerLaunch) {
-    return `${t('card.launchTrigger.lastSync.syncing')}${mantenanceSuffix}`
+  if (isRunning) {
+    return t('card.launchTrigger.lastSync.syncing')
   }
 
   if (lastSuccessDate) {
     if (getDifferenceInMillisecondes(lastSuccessDate) < DEFAULT_TIME) {
-      return `${t('card.launchTrigger.lastSync.justNow')}${mantenanceSuffix}`
+      return t('card.launchTrigger.lastSync.justNow')
     }
 
-    return `${t('card.launchTrigger.lastSync.afterSomeTimes', {
+    return t('card.launchTrigger.lastSync.afterSomeTimes', {
       times: formatLocallyDistanceToNow(lastSuccessDate)
-    })}${mantenanceSuffix}`
+    })
   }
 
   if (isDisconnected(konnector, trigger)) {
