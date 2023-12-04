f31525f627747f0a7079b335c059074b07cc327f
cballevre <celestin.ballevre@protonmail.com>
2023-12-04 16:42:49 +0100

    feat: Add RunnableAlert

---
+++ packages/cozy-harvest-lib/src/components/cards/RunnableAlert.jsx
@@ -0,0 +1,116 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import Spinner from 'cozy-ui/transpiled/react/Spinner'
+
+import { LaunchButton } from './LaunchButton'
+import { LaunchTriggerAlertMenu } from './LaunchTriggerAlertMenu'
+import { TriggerAlertTemplate } from './TriggerAlertTemplate'
+import { UnrunnableAlert } from './UnrunnableAlert'
+import { launchAction, connectAction, configureAction } from './actions'
+import { isDisconnected } from '../../helpers/konnectors'
+import KonnectorIcon from '../KonnectorIcon'
+
+/**
+ * This component allows users to synchronise their connector. It also warns the user when the connector is running.
+ *
+ * @component
+ * @param {Object} props - The component props.
+ * @param {string} props.label - The label of the alert.
+ * @param {boolean} props.isRunning - Indicates if the alert is currently running.
+ * @param {string} props.konnectorSlug - The slug of the konnector.
+ * @param {string} props.konnectorRoot - The root of the konnector.
+ * @param {Object} props.trigger - The trigger object.
+ * @param {Object} props.error - The error object.
+ * @param {Function} props.historyAction - The history action function.
+ * @param {Object} props.flow - The flow object.
+ * @param {Object} props.account - The account object.
+ * @param {Object} props.intentsApi - The intents API object.
+ * @param {boolean} props.isRunnable - Indicates if the alert is runnable.
+ * @returns {JSX.Element} The RunnableAlert component.
+ */
+function RunnableAlert({
+  label,
+  isRunning,
+  konnectorSlug,
+  konnectorRoot,
+  trigger,
+  error,
+  historyAction,
+  flow,
+  account,
+  intentsApi,
+  isRunnable
+}) {
+  const { konnector } = flow
+  const isKonnectorDisconnected = isDisconnected(konnector, trigger)
+
+  const actions = [launchAction, connectAction, configureAction]
+  const options = {
+    flow,
+    account,
+    intentsApi,
+    error,
+    historyAction,
+    konnectorRoot,
+    trigger,
+    isRunning,
+    isDisconnected: isKonnectorDisconnected
+  }
+
+  if (!isRunnable) {
+    return <UnrunnableAlert konnectorName={konnector.name} />
+  }
+
+  return (
+    <TriggerAlertTemplate
+      label={label}
+      color="var(--paperBackgroundColor)"
+      icon={
+        isRunning ? (
+          <Spinner className="u-flex" noMargin />
+        ) : (
+          <KonnectorIcon
+            className="u-w-1 u-h-1"
+            konnector={{ slug: konnectorSlug }}
+          />
+        )
+      }
+      menu={
+        <LaunchTriggerAlertMenu
+          actions={actions}
+          options={options}
+          disabled={isRunning}
+        />
+      }
+      button={
+        <LaunchButton
+          konnectorRoot={konnectorRoot}
+          trigger={trigger}
+          isRunning={isRunning}
+          error={error}
+          historyAction={historyAction}
+          flow={flow}
+          account={account}
+          intentsApi={intentsApi}
+        />
+      }
+    />
+  )
+}
+
+RunnableAlert.propTypes = {
+  label: PropTypes.string.isRequired,
+  isRunning: PropTypes.bool.isRequired,
+  konnectorSlug: PropTypes.string.isRequired,
+  konnectorRoot: PropTypes.string.isRequired,
+  trigger: PropTypes.object.isRequired,
+  error: PropTypes.object,
+  historyAction: PropTypes.func.isRequired,
+  flow: PropTypes.object.isRequired,
+  account: PropTypes.object.isRequired,
+  intentsApi: PropTypes.object,
+  isRunnable: PropTypes.bool.isRequired
+}
+
+export { RunnableAlert }
