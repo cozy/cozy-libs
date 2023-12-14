dce29929edde4296db31be0978ce6eebe92e4ccb
Alexis G <merkkur39@gmail.com>
2023-12-14 15:30:45 +0100

    feat(mespapiers): Add the SelectPaperFormat modal to the process

--- packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanResult/OcrProcessingDialog.jsx
+++ packages/cozy-mespapiers-lib/src/components/ModelSteps/ScanResult/OcrProcessingDialog.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useWebviewIntent } from 'cozy-intent'
 import { Dialog } from 'cozy-ui/transpiled/react/CozyDialogs'
 import Empty from 'cozy-ui/transpiled/react/Empty'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
+import SelectPaperFormat from './SelectPaperFormat'
 import OcrProcessingIcon from '../../../assets/icons/OcrProcessing.svg'
 import { useFormData } from '../../Hooks/useFormData'
 import { useStepperDialog } from '../../Hooks/useStepperDialog'
@@ -21,31 +22,49 @@ const OcrProcessingDialog = ({ onBack, rotatedFile }) => {
   const { setFormData, formData } = useFormData()
   const webviewIntent = useWebviewIntent()
   const { currentDefinition, nextStep } = useStepperDialog()
+  const [multipleFormat, setMultipleFormat] = useState({
+    enabled: false,
+    ocr: null
+  })
   const { ocrAttributes } = currentDefinition
 
   useEffect(() => {
     const init = async () => {
       const fileSides = getFormDataFilesForOcr(formData, rotatedFile)
       const ocrFromFlagship = await getOcrFromFlagship(fileSides, webviewIntent)
-      const attributesFound = getAttributesFromOcr(
-        ocrFromFlagship,
-        ocrAttributes[0]
-      )
+      const fileVersions = ocrAttributes.map(attr => attr.version)
 
-      const metadataFromOcr = makeMetadataFromOcr(attributesFound)
-      setFormData(prev => ({
-        ...prev,
-        metadata: {
-          ...prev.metadata,
-          ...metadataFromOcr
-        }
-      }))
+      // If paper has multiple formats, we need to display a dialog to let user confirm the right format
+      if (fileVersions.length > 1) {
+        setMultipleFormat({ enabled: true, ocr: ocrFromFlagship })
+      } else {
+        // If paper has no format, we can go to next step
+        const attributesFound = getAttributesFromOcr(
+          ocrFromFlagship,
+          ocrAttributes[0]
+        )
 
-      nextStep()
+        const metadataFromOcr = makeMetadataFromOcr(attributesFound)
+        setFormData(prev => ({
+          ...prev,
+          metadata: {
+            ...prev.metadata,
+            ...metadataFromOcr
+          }
+        }))
+
+        nextStep()
+      }
     }
     init()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+  if (multipleFormat.enabled) {
+    return (
+      <SelectPaperFormat onBack={onBack} ocrFromFlagship={multipleFormat.ocr} />
+    )
+  }
 
   return (
     <Dialog
