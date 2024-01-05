496947f5b88bfc15f1d1b48a82cf42d0e55f17da
Alexis G <merkkur39@gmail.com>
2024-01-05 16:06:41 +0100

    fix: Fix types of cozy-logger
    related https://github.com/cozy/cozy-libs/pull/2379

--- packages/cozy-logger/index.d.ts
+++ packages/cozy-logger/index.d.ts
@@ -8,13 +8,10 @@ declare module 'cozy-logger' {
 
   namespace log {
     export const addFilter: (filter: unknown) => number
-
     export const namespace: (
       namespace: string
     ) => (type: string, message: string, label?: string, ns?: string) => void
-
     export const setNoRetry: (obj: unknown) => unknown
-
     export const setLevel: (lvl: unknown) => void
   }
 
