41e4ffb48089c302f5c2fb34a726330acc977429
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-01-16 09:44:16 +0100

    feat(mespapiers): Upgrade cozy-ui 100.2.0 to 101.1.1
    BREAKING CHANGE: you must have `cozy-ui >= 101.1.1`

--- packages/cozy-mespapiers-lib/test/jestLib/setup.js
+++ packages/cozy-mespapiers-lib/test/jestLib/setup.js
@@ -7,13 +7,6 @@ global.requestAnimationFrame = cb => {
   setTimeout(cb, 0)
 }
 
-/* Fix error: "Material-UI: The color provided to augmentColor(color) is invalid.
-The color object needs to have a `main` property or a `500` property."*/
-jest.mock('cozy-ui/transpiled/react/utils/color', () => ({
-  getCssVariableValue: () => '#fff',
-  getInvertedCssVariableValue: () => '#000'
-}))
-
 // Fix error "Cannot use import statement outside a module"
 jest.mock('flexsearch/dist/module/lang/latin/balance', () => ({
   encode: jest.fn()
