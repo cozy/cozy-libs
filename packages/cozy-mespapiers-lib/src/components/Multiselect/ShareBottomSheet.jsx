7175f9f9634cd37b25a3dcd985362659f69b87c0
Théo Poizat <hello@zatteo.com>
2024-01-25 18:08:06 +0100

    feat: Add ShareBottomSheet

---
+++ packages/cozy-mespapiers-lib/src/components/Multiselect/ShareBottomSheet.jsx
@@ -0,0 +1,72 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useClient } from 'cozy-client'
+import BottomSheet, {
+  BottomSheetItem
+} from 'cozy-ui/transpiled/react/BottomSheet'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import List from 'cozy-ui/transpiled/react/List'
+import ListItem from 'cozy-ui/transpiled/react/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
+
+import { makeZipFolder } from '../Actions/utils'
+import { useFileSharing } from '../Contexts/FileSharingProvider'
+
+const ShareBottomSheet = ({ onClose, fileId, docs }) => {
+  const { t, f } = useI18n()
+  const navigate = useNavigate()
+  const client = useClient()
+  const { shareFiles } = useFileSharing()
+
+  const shareAsAttachment = async () => {
+    const idsToShare = fileId ? [fileId] : docs.map(doc => doc._id)
+    await shareFiles(idsToShare)
+  }
+
+  const shareByLink = async () => {
+    let fileToForward
+
+    if (fileId) {
+      fileToForward = { _id: fileId }
+    } else if (docs.length === 1) {
+      fileToForward = docs[0]
+    } else {
+      fileToForward = await makeZipFolder({ client, docs, t, f })
+    }
+
+    navigate(`../forward/${fileToForward._id}`)
+  }
+
+  return (
+    <BottomSheet backdrop onClose={onClose}>
+      <BottomSheetItem disableGutters>
+        <List>
+          <ListItem button onClick={shareAsAttachment}>
+            <ListItemIcon>
+              <Icon icon="attachment" />
+            </ListItemIcon>
+            <ListItemText primary={t('ShareBottomSheet.attachment')} />
+          </ListItem>
+          <ListItem button onClick={shareByLink}>
+            <ListItemIcon>
+              <Icon icon="link" />
+            </ListItemIcon>
+            <ListItemText primary={t('ShareBottomSheet.link')} />
+          </ListItem>
+        </List>
+      </BottomSheetItem>
+    </BottomSheet>
+  )
+}
+
+ShareBottomSheet.propTypes = {
+  onClose: PropTypes.func,
+  fileId: PropTypes.string,
+  docs: PropTypes.array
+}
+
+export default ShareBottomSheet
