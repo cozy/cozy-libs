ade6e66b87bd2006e0cd420c302bc76ca83aae36
Théo Poizat <hello@zatteo.com>
2024-01-26 13:12:37 +0100

    test: Refactor MultiSelectionProvider tests to use renderHook
    By leveraging renderHook, we can :
    - remove TestComponent which was a test  layer build to expose
    MultiSelectionContext to tests
    - more easily simulate rerender
    
    Only the first test "isMultiSelectionActive" has been changed to take
    into account that isMultiSelectionActive is calculated from current
    location.

--- packages/cozy-mespapiers-lib/src/components/Contexts/MultiSelectionProvider.spec.jsx
+++ packages/cozy-mespapiers-lib/src/components/Contexts/MultiSelectionProvider.spec.jsx
@@ -1,78 +1,27 @@
-import { fireEvent, render } from '@testing-library/react'
-import React, { useContext } from 'react'
+import { renderHook, act } from '@testing-library/react-hooks'
+import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 import flag from 'cozy-flags'
 
-import MultiSelectionContext, {
-  MultiSelectionProvider
-} from './MultiSelectionProvider'
+import { MultiSelectionProvider } from './MultiSelectionProvider'
+import { useMultiSelection } from '../Hooks/useMultiSelection'
 
 jest.mock('cozy-flags')
+jest.mock('react-router-dom')
 
-const setup = ({ filesToAdd, removeFile }) => {
+const setup = () => {
   flag.mockReturnValue(true)
-  const TestComponent = () => {
-    const {
-      isMultiSelectionActive,
-      addMultiSelectionFile,
-      removeMultiSelectionFile,
-      removeAllMultiSelectionFiles,
-      allMultiSelectionFiles,
-      changeCurrentMultiSelectionFile,
-      removeCurrentMultiSelectionFile,
-      removeAllCurrentMultiSelectionFiles,
-      confirmCurrentMultiSelectionFiles,
-      currentMultiSelectionFiles
-    } = useContext(MultiSelectionContext)
 
-    return (
-      <>
-        <div data-testid="multiSelectionFilesState">
-          {JSON.stringify(allMultiSelectionFiles)}
-        </div>
-        <div data-testid="currentMultiSelectionFilesState">
-          {JSON.stringify(currentMultiSelectionFiles)}
-        </div>
-        <div data-testid="isActive">
-          {JSON.stringify(isMultiSelectionActive)}
-        </div>
-        <button
-          data-testid="addMultiSelectionBtn"
-          onClick={() => addMultiSelectionFile(filesToAdd.shift())}
-        />
-        <button
-          data-testid="removeMultiSelectionFileBtn"
-          onClick={() => removeMultiSelectionFile(removeFile)}
-        />
-        <button
-          data-testid="removeAllMultiSelectionBtn"
-          onClick={() => removeAllMultiSelectionFiles()}
-        />
-        <button
-          data-testid="changeCurrentMultiSelectionBtn"
-          onClick={() => changeCurrentMultiSelectionFile(filesToAdd.shift())}
-        />
-        <button
-          data-testid="removeCurrentMultiSelectionFileBtn"
-          onClick={() => removeCurrentMultiSelectionFile(removeFile)}
-        />
-        <button
-          data-testid="removeAllCurrentMultiSelectionBtn"
-          onClick={() => removeAllCurrentMultiSelectionFiles()}
-        />
-        <button
-          data-testid="confirmCurrentMultiSelectionFiles"
-          onClick={() => confirmCurrentMultiSelectionFiles()}
-        />
-      </>
-    )
-  }
+  useLocation.mockReturnValue({ pathname: '/files/multiselect' })
 
-  return render(
-    <MultiSelectionProvider>
-      <TestComponent />
-    </MultiSelectionProvider>
+  const wrapper = ({ children }) => (
+    <MultiSelectionProvider>{children}</MultiSelectionProvider>
   )
+
+  return renderHook(() => useMultiSelection(), {
+    wrapper
+  })
 }
 
 const fileMock01 = { _id: '01', name: 'file01' }
@@ -81,271 +30,226 @@ const fileMock02 = { _id: '02', name: 'file02' }
 describe('MultiSelectionProvider', () => {
   describe('isMultiSelectionActive', () => {
     it('should remove all files to its state if isMultiSelectionActive is set to false', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02]
+      const { result, rerender } = setup()
+
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock01)
       })
 
-      const setActiveBtn = getByTestId('setActiveBtn')
-      const addMultiSelectionBtn = getByTestId('addMultiSelectionBtn')
-      const multiSelectionFilesState = getByTestId('multiSelectionFilesState')
-
-      // isMultiSelectionActive => true
-      fireEvent.click(setActiveBtn)
-
-      fireEvent.click(addMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
-
-      fireEvent.click(addMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"},{"_id":"02","name":"file02"}]'
-      )
+      expect(result.current.allMultiSelectionFiles).toEqual([fileMock01])
 
       // isMultiSelectionActive => false
-      fireEvent.click(setActiveBtn)
-      expect(multiSelectionFilesState.textContent).toBe('[]')
+      useLocation.mockReturnValue({ pathname: '/paper' })
+
+      rerender()
+
+      expect(result.current.allMultiSelectionFiles).toEqual([])
     })
   })
 
   describe('addMultiSelectionFile', () => {
     it('should add file to its state', () => {
-      const { getByTestId } = setup({ filesToAdd: [fileMock01] })
+      const { result } = setup()
 
-      const addMultiSelectionBtn = getByTestId('addMultiSelectionBtn')
-      const multiSelectionFilesState = getByTestId('multiSelectionFilesState')
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock01)
+      })
 
-      fireEvent.click(addMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
+      expect(result.current.allMultiSelectionFiles).toEqual([fileMock01])
     })
 
     it('should add a second file to its state', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02]
+      const { result } = setup()
+
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock01)
       })
 
-      const addMultiSelectionBtn = getByTestId('addMultiSelectionBtn')
-      const multiSelectionFilesState = getByTestId('multiSelectionFilesState')
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock02)
+      })
 
-      fireEvent.click(addMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
-
-      fireEvent.click(addMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"},{"_id":"02","name":"file02"}]'
-      )
+      expect(result.current.allMultiSelectionFiles).toEqual([
+        fileMock01,
+        fileMock02
+      ])
     })
 
     it('should add a second file to its state even if it is the same file', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock01]
+      const { result } = setup()
+
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock01)
       })
 
-      const addMultiSelectionBtn = getByTestId('addMultiSelectionBtn')
-      const multiSelectionFilesState = getByTestId('multiSelectionFilesState')
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock01)
+      })
 
-      fireEvent.click(addMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
-
-      fireEvent.click(addMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"},{"_id":"01","name":"file01"}]'
-      )
+      expect(result.current.allMultiSelectionFiles).toEqual([
+        fileMock01,
+        fileMock01
+      ])
     })
   })
 
   describe('removeMultiSelectionFile', () => {
     it('should remove specific file to its state', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02],
-        removeFile: 0
+      const { result } = setup()
+
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock01)
       })
 
-      const addMultiSelectionBtn = getByTestId('addMultiSelectionBtn')
-      const removeMultiSelectionFileBtn = getByTestId(
-        'removeMultiSelectionFileBtn'
-      )
-      const multiSelectionFilesState = getByTestId('multiSelectionFilesState')
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock02)
+      })
 
-      fireEvent.click(addMultiSelectionBtn)
-      fireEvent.click(addMultiSelectionBtn)
-      fireEvent.click(removeMultiSelectionFileBtn)
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"02","name":"file02"}]'
-      )
+      act(() => {
+        result.current.removeMultiSelectionFile(0)
+      })
+
+      expect(result.current.allMultiSelectionFiles).toEqual([fileMock02])
     })
   })
 
   describe('removeAllMultiSelectionFiles', () => {
     it('should remove all files to its state', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02]
+      const { result } = setup()
+
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock01)
       })
 
-      const addMultiSelectionBtn = getByTestId('addMultiSelectionBtn')
-      const removeAllMultiSelectionBtn = getByTestId(
-        'removeAllMultiSelectionBtn'
-      )
-      const multiSelectionFilesState = getByTestId('multiSelectionFilesState')
+      act(() => {
+        result.current.addMultiSelectionFile(fileMock02)
+      })
 
-      fireEvent.click(addMultiSelectionBtn)
-      fireEvent.click(addMultiSelectionBtn)
-      fireEvent.click(removeAllMultiSelectionBtn)
-      expect(multiSelectionFilesState.textContent).toBe('[]')
+      act(() => {
+        result.current.removeAllMultiSelectionFiles(fileMock01)
+      })
+
+      expect(result.current.allMultiSelectionFiles).toEqual([])
     })
   })
 
   describe('changeCurrentMultiSelectionFile', () => {
     it('should add file to its state', () => {
-      const { getByTestId } = setup({ filesToAdd: [fileMock01] })
+      const { result } = setup()
 
-      const changeCurrentMultiSelectionBtn = getByTestId(
-        'changeCurrentMultiSelectionBtn'
-      )
-      const currentMultiSelectionFilesState = getByTestId(
-        'currentMultiSelectionFilesState'
-      )
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock01)
+      })
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
+      expect(result.current.currentMultiSelectionFiles).toEqual([fileMock01])
     })
 
     it('should add a second file to its state', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02]
+      const { result } = setup()
+
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock01)
       })
 
-      const changeCurrentMultiSelectionBtn = getByTestId(
-        'changeCurrentMultiSelectionBtn'
-      )
-      const currentMultiSelectionFilesState = getByTestId(
-        'currentMultiSelectionFilesState'
-      )
+      expect(result.current.currentMultiSelectionFiles).toEqual([fileMock01])
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock02)
+      })
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"},{"_id":"02","name":"file02"}]'
-      )
+      expect(result.current.currentMultiSelectionFiles).toEqual([
+        fileMock01,
+        fileMock02
+      ])
     })
 
     it('should remove the file from its state if it was already present', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock01]
+      const { result } = setup()
+
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock01)
       })
 
-      const changeCurrentMultiSelectionBtn = getByTestId(
-        'changeCurrentMultiSelectionBtn'
-      )
-      const currentMultiSelectionFilesState = getByTestId(
-        'currentMultiSelectionFilesState'
-      )
+      expect(result.current.currentMultiSelectionFiles).toEqual([fileMock01])
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock01)
+      })
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe('[]')
+      expect(result.current.currentMultiSelectionFiles).toEqual([])
     })
   })
 
   describe('removeCurrentMultiSelectionFile', () => {
     it('should remove specific file to its state', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02],
-        removeFile: fileMock01
+      const { result } = setup()
+
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock01)
       })
 
-      const changeCurrentMultiSelectionBtn = getByTestId(
-        'changeCurrentMultiSelectionBtn'
-      )
-      const removeCurrentMultiSelectionFileBtn = getByTestId(
-        'removeCurrentMultiSelectionFileBtn'
-      )
-      const currentMultiSelectionFilesState = getByTestId(
-        'currentMultiSelectionFilesState'
-      )
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock02)
+      })
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      fireEvent.click(removeCurrentMultiSelectionFileBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe(
-        '[{"_id":"02","name":"file02"}]'
-      )
+      act(() => {
+        result.current.removeCurrentMultiSelectionFile(fileMock01)
+      })
+
+      expect(result.current.currentMultiSelectionFiles).toEqual([fileMock02])
     })
   })
 
   describe('removeAllCurrentMultiSelectionFiles', () => {
     it('should remove all files to its state', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02]
+      const { result } = setup()
+
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock01)
       })
 
-      const changeCurrentMultiSelectionBtn = getByTestId(
-        'changeCurrentMultiSelectionBtn'
-      )
-      const removeAllCurrentMultiSelectionBtn = getByTestId(
-        'removeAllCurrentMultiSelectionBtn'
-      )
-      const currentMultiSelectionFilesState = getByTestId(
-        'currentMultiSelectionFilesState'
-      )
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock02)
+      })
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      fireEvent.click(removeAllCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe('[]')
+      act(() => {
+        result.current.removeAllCurrentMultiSelectionFiles()
+      })
+
+      expect(result.current.currentMultiSelectionFiles).toEqual([])
     })
   })
 
   describe('confirmCurrentMultiSelectionFiles', () => {
     it('should move all files in currentMultiSelectionFilesState to multiSelectionFilesState', () => {
-      const { getByTestId } = setup({
-        filesToAdd: [fileMock01, fileMock02]
+      const { result } = setup()
+
+      // add first file to current selection
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock01)
       })
+      expect(result.current.currentMultiSelectionFiles).toEqual([fileMock01])
+      expect(result.current.allMultiSelectionFiles).toEqual([])
 
-      const changeCurrentMultiSelectionBtn = getByTestId(
-        'changeCurrentMultiSelectionBtn'
-      )
-      const confirmCurrentMultiSelectionBtn = getByTestId(
-        'confirmCurrentMultiSelectionFiles'
-      )
-      const currentMultiSelectionFilesState = getByTestId(
-        'currentMultiSelectionFilesState'
-      )
-      const multiSelectionFilesState = getByTestId('multiSelectionFilesState')
+      // add second file to current selection
+      act(() => {
+        result.current.changeCurrentMultiSelectionFile(fileMock02)
+      })
+      expect(result.current.currentMultiSelectionFiles).toEqual([
+        fileMock01,
+        fileMock02
+      ])
+      expect(result.current.allMultiSelectionFiles).toEqual([])
 
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"}]'
-      )
-      expect(multiSelectionFilesState.textContent).toBe('[]')
-
-      fireEvent.click(changeCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"},{"_id":"02","name":"file02"}]'
-      )
-      expect(multiSelectionFilesState.textContent).toBe('[]')
-
-      fireEvent.click(confirmCurrentMultiSelectionBtn)
-      expect(currentMultiSelectionFilesState.textContent).toBe('[]')
-      expect(multiSelectionFilesState.textContent).toBe(
-        '[{"_id":"01","name":"file01"},{"_id":"02","name":"file02"}]'
-      )
+      // confirm current selection
+      act(() => {
+        result.current.confirmCurrentMultiSelectionFiles(fileMock02)
+      })
+      expect(result.current.currentMultiSelectionFiles).toEqual([])
+      expect(result.current.allMultiSelectionFiles).toEqual([
+        fileMock01,
+        fileMock02
+      ])
     })
   })
 })
