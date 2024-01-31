d1f7d513e99b26c85050a52db190ccb8af6cf105
Théo Poizat <hello@zatteo.com>
2024-01-31 15:39:46 +0100

    fix: Use copyToClipboard instead of copy-text-to-clipboard package

--- packages/cozy-mespapiers-lib/src/components/Actions/Items/copyReminderContent.js
+++ packages/cozy-mespapiers-lib/src/components/Actions/Items/copyReminderContent.js
@@ -1,24 +1,20 @@
-import copy from 'copy-text-to-clipboard'
 import React, { forwardRef } from 'react'
 
 import useFetchJSON from 'cozy-client/dist/hooks/useFetchJSON'
 import { isMobile } from 'cozy-device-helper'
-import minilog from 'cozy-minilog'
 import ActionsMenuItem from 'cozy-ui/transpiled/react/ActionsMenu/ActionsMenuItem'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
 import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
-import Alerter from 'cozy-ui/transpiled/react/deprecated/Alerter'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
+import { copyToClipboard } from '../../../helpers/copyToClipboard'
 import withLocales from '../../../locales/withLocales'
-
-const log = minilog('copyReminderContent')
 
 export const copyReminderContent = () => {
   return {
     name: 'copyReminderContent',
-    action: async (docs, { client, noteContent }) => {
+    action: async (docs, { client, noteContent, t, showAlert }) => {
       const data = isMobile()
         ? noteContent
         : await client.stackClient.fetchJSON(
@@ -26,21 +22,7 @@ export const copyReminderContent = () => {
             `/notes/${docs[0]._id}/text`
           )
 
-      try {
-        const hasCopied = copy(data)
-
-        if (hasCopied) {
-          Alerter.success('action.copyReminderContent.success')
-        } else {
-          Alerter.error('action.copyReminderContent.error')
-        }
-      } catch (error) {
-        Alerter.error('action.copyReminderContent.error')
-        log.error(
-          'Error in "copyReminderContent" when trying to copy to clipboard',
-          error
-        )
-      }
+      copyToClipboard(data, { t, showAlert })
     },
     Component: withLocales(
       // eslint-disable-next-line react/display-name
