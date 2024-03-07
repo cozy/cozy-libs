4504029ff456c929326a6d17435d7b3a934a497c
cballevre <celestin.ballevre@protonmail.com>
2024-03-07 08:35:48 +0100

    feat(sharing): Add storybook

---
+++ packages/cozy-sharing/.storybook/main.js
@@ -0,0 +1,18 @@
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: true
+  },
+  staticDirs: ['./public'],
+};
+
+export default config;
