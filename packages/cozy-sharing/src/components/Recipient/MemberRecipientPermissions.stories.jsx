98ecd6137e304d1dec24b7302f894fd260ce6e6f
cballevre <celestin.ballevre@protonmail.com>
2024-03-11 12:13:31 +0100

    refactor(MemberRecipientPermissions): Use ActionsMenu instead old ones

--- packages/cozy-sharing/src/components/Recipient/MemberRecipientPermissions.stories.jsx
+++ packages/cozy-sharing/src/components/Recipient/MemberRecipientPermissions.stories.jsx
@@ -2,12 +2,39 @@ import MemberRecipientPermissions from './MemberRecipientPermissions'
 
 const meta = {
   component: MemberRecipientPermissions,
-  args: {}
+  args: {
+    onRevoke: () => {},
+    onRevokeSelf: () => {},
+    type: 'two-way',
+    status: 'ready'
+  },
+  argTypes: {
+    status: {
+      control: 'select',
+      options: ['ready', 'mail-not-sent', 'pending', 'seen', 'owner'],
+      defaultValue: 'ready'
+    },
+    type: {
+      control: 'select',
+      options: ['one-way', 'two-way'],
+      defaultValue: 'two-way'
+    }
+  }
 }
 
 export default meta
 
-export const Default = {
-  name: 'Default',
-  args: {}
+export const Owner = {
+  name: 'Owner',
+  args: {
+    isOwner: true
+  }
+}
+
+export const Self = {
+  name: 'Self',
+  args: {
+    isOwner: false,
+    instance: 'http://alice.cozy.localhost:8080'
+  }
 }
---
+++ packages/cozy-sharing/src/components/Recipient/actions/revokeMember.js
@@ -0,0 +1,39 @@
+import React, { forwardRef } from 'react'
+
+import ActionsMenuItem from 'cozy-ui/transpiled/react/ActionsMenu/ActionsMenuItem'
+import Icon from 'cozy-ui/transpiled/react/Icon'
+import TrashIcon from 'cozy-ui/transpiled/react/Icons/Trash'
+import ListItemIcon from 'cozy-ui/transpiled/react/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+
+const revokeMember = ({ t, isOwner, handleRevocation }) => {
+  const revokeType = isOwner ? 'revoke' : 'revokeSelf'
+  const title = t(`RevokeMemberItem.${revokeType}.title`)
+  const desc = t(`RevokeMemberItem.${revokeType}.desc`)
+
+  const icon = TrashIcon
+
+  return {
+    name: 'revokeMember',
+    label: title,
+    icon,
+    action: () => {
+      handleRevocation()
+    },
+    Component: forwardRef(function RevokeMemberItem(props, ref) {
+      return (
+        <ActionsMenuItem {...props} ref={ref}>
+          <ListItemIcon>
+            <Icon icon={icon} color="var(--errorColor)" />
+          </ListItemIcon>
+          <ListItemText
+            primary={<Typography color="error">{title}</Typography>}
+            secondary={desc}
+          />
+        </ActionsMenuItem>
+      )
+    })
+  }
+}
+export { revokeMember }
