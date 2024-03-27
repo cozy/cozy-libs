ca4a46acdf3bd951595a6ffacca9d3f8c386619f
cballevre <celestin.ballevre@protonmail.com>
2024-03-27 15:47:29 +0100

    fix: Use sourceAccountIdentifier and konnector slug to show viewer modal
    Parameter of useDataCardFiles change but we didn't update ViewerModal properly. I chose to search this param with a query to io.cozy.accounts as we had already this information without changing route path.  This is a temporary fix and I think we need to refactor the viewer part to have an abstraction to account in this modal

--- packages/cozy-harvest-lib/src/datacards/ViewerModal.jsx
+++ packages/cozy-harvest-lib/src/datacards/ViewerModal.jsx
@@ -1,16 +1,54 @@
 import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
 
+import { models, useQuery } from 'cozy-client'
 import Viewer from 'cozy-ui/transpiled/react/Viewer'
 import Overlay from 'cozy-ui/transpiled/react/deprecated/Overlay'
 
 import { useDataCardFiles } from './useDataCardFiles'
 import { MountPointContext } from '../components/MountPointContext'
+import { buildAccountQueryById } from '../helpers/queries'
 
 export const ViewerModal = () => {
   const { accountId, folderToSaveId, fileIndex } = useParams()
+
+  const buildAccountQuery = buildAccountQueryById(accountId)
+  const accountResult = useQuery(
+    buildAccountQuery.definition,
+    buildAccountQuery.options
+  )
+
+  const sourceAccountIdentifier = models.account.getAccountName(
+    accountResult.data
+  )
+  const konnectorSlug = accountResult.data?.account_type
+
+  if (!sourceAccountIdentifier || !konnectorSlug) return <Overlay />
+
+  return (
+    <ViewerModalContent
+      sourceAccountIdentifier={sourceAccountIdentifier}
+      folderToSaveId={folderToSaveId}
+      konnectorSlug={konnectorSlug}
+      accountId={accountId}
+      fileIndex={fileIndex}
+    />
+  )
+}
+
+const ViewerModalContent = ({
+  sourceAccountIdentifier,
+  folderToSaveId,
+  konnectorSlug,
+  accountId,
+  fileIndex
+}) => {
   const { pushHistory, replaceHistory } = useContext(MountPointContext)
-  const { data, fetchStatus } = useDataCardFiles(accountId, folderToSaveId)
+  const { data, fetchStatus } = useDataCardFiles(
+    sourceAccountIdentifier,
+    folderToSaveId,
+    konnectorSlug
+  )
   const handleCloseViewer = () => replaceHistory(`/accounts/${accountId}`)
   const handleFileChange = (_file, newIndex) =>
     pushHistory(`/viewer/${accountId}/${folderToSaveId}/${newIndex}`)
