28042700499ff1458d0ee6e74c5884f9900d2463
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-03-28 15:05:49 +0100

    feat: Add vehicle registration license number on file name

--- packages/cozy-mespapiers-lib/src/helpers/buildFilename.spec.js
+++ packages/cozy-mespapiers-lib/src/helpers/buildFilename.spec.js
@@ -8,7 +8,7 @@ describe('buildFilename', () => {
   it('should replace "/" by "_" in qualificationName', () => {
     const paperName = buildFilename({
       contacts: [],
-      qualificationName: 'passport/test'
+      qualification: { name: 'passport/test' }
     })
 
     expect(paperName).toEqual('passport_test.pdf')
@@ -17,41 +17,41 @@ describe('buildFilename', () => {
   it.each`
     opts | result
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   formatedDate: '2022.01.01',
   contacts: mockOneContact,
   filenameModel: ['labelGivenByUser', 'contactName', 'featureDate'],
   metadata: { labelGivenByUser: 'Mon fichier' }
 }} | ${'Mon fichier - Bernard Chabert - 2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   formatedDate: '2022.01.01',
   contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate'],
   metadata: { labelGivenByUser: 'Mon fichier' }
 }} | ${'Mon fichier - 2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   formatedDate: '2022.01.01',
   contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate'],
   metadata: { labelGivenByUser: '' }
 }} | ${'2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   formatedDate: '2022.01.01',
   contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate']
 }} | ${'2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   formatedDate: '2022.01.01',
   contacts: [],
   filenameModel: ['labelGivenByUser', 'featureDate'],
   metadata: {}
 }} | ${'2022.01.01.pdf'}
     ${{
-  qualificationName: 'fidelity_card',
+  qualification: { name: 'fidelity_card' },
   formatedDate: '2022.01.01',
   contacts: [],
   filenameModel: ['labelGivenByUser', 'page'],
@@ -59,48 +59,65 @@ describe('buildFilename', () => {
   pageName: 'front'
 }} | ${'Mon fichier - front.pdf'}
     ${{
-  qualificationName: 'fidelity_card',
+  qualification: { name: 'fidelity_card' },
   formatedDate: '2022.01.01',
   contacts: [],
   filenameModel: ['labelGivenByUser', 'page'],
   metadata: { labelGivenByUser: 'Mon fichier' }
 }} | ${'Mon fichier.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   contacts: []
 }} | ${'passport.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   contacts: [],
   pageName: 'front'
 }} | ${'passport - front.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   contacts: mockOneContact
 }} | ${'passport - Bernard Chabert.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   contacts: [],
   formatedDate: '2022.01.01'
 }} | ${'passport - 2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   pageName: 'front',
   contacts: mockOneContact
 }} | ${'passport - front - Bernard Chabert.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   pageName: 'front',
   contacts: [],
   formatedDate: '2022.01.01'
 }} | ${'passport - front - 2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport' },
   contacts: mockOneContact,
   formatedDate: '2022.01.01'
 }} | ${'passport - Bernard Chabert - 2022.01.01.pdf'}
     ${{
-  qualificationName: 'passport',
+  qualification: { name: 'passport', label: 'vehicle_registration' },
+  metadata: { vehicle: { licenseNumber: '123456' } },
+  contacts: mockOneContact,
+  formatedDate: '2022.01.01'
+}} | ${'passport - 123456 - Bernard Chabert - 2022.01.01.pdf'}
+    ${{
+  qualification: { name: 'passport', label: 'vehicle_registration' },
+  contacts: mockOneContact,
+  formatedDate: '2022.01.01'
+}} | ${'passport - Bernard Chabert - 2022.01.01.pdf'}
+    ${{
+  qualification: { name: 'passport' },
+  metadata: { vehicle: { licenseNumber: '123456' } },
+  contacts: mockOneContact,
+  formatedDate: '2022.01.01'
+}} | ${'passport - Bernard Chabert - 2022.01.01.pdf'}
+    ${{
+  qualification: { name: 'passport' },
   pageName: 'front',
   contacts: mockOneContact,
   formatedDate: '2022.01.01'
