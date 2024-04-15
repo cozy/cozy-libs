1c0e7e1c6afd1cef23d1e59206295fe27590bcfd
AlexisG <merkkur39@gmail.com>
2024-04-15 14:53:38 +0200

    docs(mespapiers): Fix and add jsdoc around paperDefinition

--- packages/cozy-mespapiers-lib/src/types.js
+++ packages/cozy-mespapiers-lib/src/types.js
@@ -8,21 +8,53 @@
  */
 
 /**
- * @typedef {Object} PapersDefinitionsStepAttributes
- * @property {string} name - Name of the attribute.
- * @property {string} type - Type of the attribute.
+ * @typedef {Object} PaperDefinitionStepAttributeOptionsTextFieldAttrs
+ * @property {string} label - Label of the inuput.
  */
 
 /**
- * @typedef {Object} PapersDefinitionsAcquisitionSteps
- * @property {number} occurrence - Number of occurrence for this step.
+ * @typedef {Object} PaperDefinitionStepAttributeOptions
+ * @property {string} value - Value of the input.
+ * @property {string} label - Label of the inuput.
+ * @property {boolean} [checked] - If the input is checked by default.
+ * @property {PaperDefinitionStepAttributeOptionsTextFieldAttrs} [textFieldAttrs] - If the input displays a text field.
+ */
+
+/**
+ * @typedef {'radio'|'date'|'text'|'number'|'contact'} PaperDefinitionStepAttributeType
+ */
+
+/**
+ * @typedef {Object} PaperDefinitionStepAttribute
+ * @property {string} name - Name of the input/metadata.
+ * @property {PaperDefinitionStepAttributeType} type - Type of the input.
+ * @property {string} inputLabel - Label of the inuput.
+ * @property {boolean} [required] - If the input is required.
+ * @property {number} [minLength] - Minimum length of the input.
+ * @property {number} [maxLength] - Maximum length of the input.
+ * @property {string} [mask] - Mask of the input. (eg: *: all characters, 9: number, a: letter)
+ * @property {string} [maskPlaceholder] - Placeholder of the mask.
+ * @property {PaperDefinitionStepAttributeOptions[]} [options] - Array of options.
+ */
+
+/**
+ * @typedef {'scan'|'information'|'contact'} PaperDefinitionAcquisitionStepModel
+ */
+
+/**
+ * @typedef {Object} PaperDefinitionAcquisitionStep
+ * @property {PaperDefinitionAcquisitionStepModel} model - Model of the step.
  * @property {string} illustration - Name of the illustration.
- * @property {string} text - Text of the step.
- * @property {PapersDefinitionsStepAttributes[]} attributes - Array of the attributes.
+ * @property {string} text - Content text of the step.
+ * @property {PaperDefinitionStepAttribute[]} attributes - Array of step attributes.
+ * @property {string} [tooltip] - Tooltip of the step.
+ * @property {boolean} [multipage] - On Scan model, if the step is multipage.
+ * @property {boolean} [multiple] - On Contact model, if the choice of contacts is multiple.
+ * @property {'all'|'ocr'} [isDisplayed] - If the step is displayed only via ocr or always.
  */
 
 /**
- * @typedef {object} PapersDefinitionsKonnectorCriteria
+ * @typedef {object} PaperDefinitionKonnectorCriteria
  * @property {string} [name] - Name of the konnector
  * @property {string} [category] - Category of the konnector
  * @property {string} [qualificationLabel] - Qualification label of the konnector
@@ -32,10 +64,12 @@
  * @typedef {Object} PaperDefinition
  * @property {string} label - Label of Paper.
  * @property {string} icon - Icon of Paper.
- * @property {number} placeholderIndex - Position on the Placeholder list.
- * @property {PapersDefinitionsAcquisitionSteps[]} acquisitionSteps - Array of acquisition steps.
- * @property {string} featureDate - Reference the attribute "name" to be used as main date.
  * @property {number} maxDisplay - Number of document beyond which a "see more" button is displayed.
+ * @property {PaperDefinitionAcquisitionStep[]} acquisitionSteps - Array of acquisition steps.
+ * @property {number} [placeholderIndex] - Position on the Placeholder list.
+ * @property {object[]} [ocrAttributes] - Array of ocr attributes.
+ * @property {string} [featureDate] - Reference the attribute "name" to be used as main date.
+ * @property {PaperDefinitionKonnectorCriteria} [konnectorCriteria] - Criteria for linking paper to a konnector.
  */
 
 export default {}
