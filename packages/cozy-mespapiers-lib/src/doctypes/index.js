108c63b99b04f179f668a9c1fc9f8954c0b735a5
JF-Cozy <julien.ficheux@cozycloud.cc>
2024-05-29 15:33:13 +0200

    feat(mespapiers): Some change after PR review

--- packages/cozy-mespapiers-lib/src/doctypes/index.js
+++ packages/cozy-mespapiers-lib/src/doctypes/index.js
@@ -9,17 +9,3 @@ export const TRIGGERS_DOCTYPE = 'io.cozy.triggers'
 export const KONNECTORS_DOCTYPE = 'io.cozy.konnectors'
 export const ACCOUNTS_DOCTYPE = 'io.cozy.accounts'
 export const APPS_REGISTRY_DOCTYPE = 'io.cozy.apps_registry'
-
-// the documents schema, necessary for CozyClient
-export default {
-  contacts: {
-    doctype: CONTACTS_DOCTYPE,
-    attributes: {},
-    relationships: {}
-  },
-  files: {
-    doctype: FILES_DOCTYPE,
-    attributes: {},
-    relationships: {}
-  }
-}
