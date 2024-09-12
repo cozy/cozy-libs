d6543349a7f4b7248d971ab52f1a7dacf41225ac
Ldoppea <yannick.chiron@gmail.com>
2024-09-12 18:12:16 +0200

    feat(cozy-viewer): Use new `downloadFile()` method from cozy-client
    In cozy/cozy-client#1518 we implemented a new `downloadFile()` that
    allow to download files as before in a browser, but will download files
    through cozy-intent when hosted in the Flagship app
    
    This commit will replace the old way to download files with the new one
    using `downloadFile()`
    
    BREAKING CHANGE: `downloadFile()` method has been removed from
    `cozy-viewer/src/helpers.js`. Use the one from
    `cozy-client/dist/models/file` instead

--- packages/cozy-viewer/src/NoViewer/DownloadButton.jsx
+++ packages/cozy-viewer/src/NoViewer/DownloadButton.jsx
@@ -2,18 +2,19 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 import { withClient } from 'cozy-client'
+import { downloadFile } from 'cozy-client/dist/models/file'
+import { useWebviewIntent } from 'cozy-intent'
 import Button from 'cozy-ui/transpiled/react/deprecated/Button'
 import { FileDoctype } from 'cozy-ui/transpiled/react/proptypes'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
-import { downloadFile } from '../helpers'
-
 const DownloadButton = ({ client, file, url }) => {
   const { t } = useI18n()
+  const webviewIntent = useWebviewIntent()
 
   return (
     <Button
-      onClick={() => downloadFile({ client, file, url })}
+      onClick={() => downloadFile({ client, file, url, webviewIntent })}
       label={t('Viewer.download')}
     />
   )
