3caa37d72d2db45740d994124d5f7fa0d940bff4
cballevre <celestin.ballevre@protonmail.com>
2024-11-07 16:47:14 +0100

    refactor(RedirectToAccountFormButton): Remove adaptive router

--- packages/cozy-harvest-lib/src/components/RedirectToAccountFormButton.jsx
+++ packages/cozy-harvest-lib/src/components/RedirectToAccountFormButton.jsx
@@ -1,18 +1,20 @@
-import React, { useContext, useCallback } from 'react'
+import React, { useCallback } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { triggers as triggersModel } from 'cozy-client/dist/models/trigger'
 import Button from 'cozy-ui/transpiled/react/Buttons'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
-import { MountPointContext } from './MountPointContext'
-
 const RedirectToAccountFormButton = ({ trigger }) => {
   const { t } = useI18n()
+  const navigate = useNavigate()
+
   const accountId = triggersModel.getAccountId(trigger)
-  const { replaceHistory } = useContext(MountPointContext)
+
   const handleClick = useCallback(() => {
-    replaceHistory(`/accounts/${accountId}/edit?reconnect`)
-  }, [accountId, replaceHistory])
+    navigate(`../accounts/${accountId}/edit?reconnect`, { replace: true })
+  }, [accountId, navigate])
+
   return (
     <Button
       className="u-ml-0"
