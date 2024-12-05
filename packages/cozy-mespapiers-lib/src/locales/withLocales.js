04d61311d67f55e189a2ccac8a288a352c0ac0ed
Théo Poizat <hello@zatteo.com>
2024-12-05 11:39:14 +0100

    feat: Remove cozy-mespapiers-lib

--- packages/cozy-mespapiers-lib/src/locales/withLocales.js
+++
@@ -1,8 +0,0 @@
-import I18n from 'cozy-ui/transpiled/react/providers/I18n'
-import withOnlyLocales from 'cozy-ui/transpiled/react/providers/I18n/withOnlyLocales'
-
-const dictRequire = lang => require(`./${lang}.json`)
-
-export const i18nContextTypes = I18n.childContextTypes
-
-export default withOnlyLocales(dictRequire)
