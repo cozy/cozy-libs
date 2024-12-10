804d8bc6ffcbdac2234234b4cf1ab46303f87985
Théo Poizat <hello@zatteo.com>
2024-12-10 14:52:53 +0100

    feat: Remove cozy-authentication
    This library was used for our Cordova apps that have been replaced
    by the flagship app in React Native.
    It is not used anymore.

--- packages/cozy-authentication/src/index.js
+++
@@ -1,7 +0,0 @@
-export { default as MobileRouter } from './MobileRouter'
-
-import Authentication from './Authentication'
-import Revoked from './Revoked'
-
-export { Authentication }
-export { Revoked }
