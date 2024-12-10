554094e56ad6f97decbce0675de27e695e837101
Théo Poizat <hello@zatteo.com>
2024-12-10 14:52:53 +0100

    feat: Remove cozy-authentication
    This library was used for our Cordova apps that have been replaced
    by the flagship app in React Native.
    It is not used anymore.

--- packages/cozy-authentication/scripts/get-translations.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -euxo pipefail
-
-DRIVE_DIR=$1
-JQ_REQ='{ onboarding: .mobile.onboarding } + { revoked: .mobile.revoked } | { mobile:  . }'
-
-cat $DRIVE_DIR/locales/en.json | jq "$JQ_REQ" > src/locales/en.json
-cat $DRIVE_DIR/locales/fr.json | jq "$JQ_REQ" > src/locales/fr.json
--- packages/cozy-authentication/src/withLocales.jsx
+++
@@ -1,8 +0,0 @@
-import withLocales from 'cozy-ui/transpiled/react/I18n/withLocales'
-
-const locales = {
-  en: require(`./locales/en.json`),
-  fr: require(`./locales/fr.json`)
-}
-
-export default withLocales(locales)
