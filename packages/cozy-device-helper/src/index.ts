ae6a50cd8031431646886a16effdf310c89a6e37
Théo Poizat <hello@zatteo.com>
2024-12-13 17:24:29 +0100

    feat: Remove cordova methods from cozy-device-helper

--- packages/cozy-device-helper/src/index.ts
+++ packages/cozy-device-helper/src/index.ts
@@ -1,24 +1,5 @@
-export {
-  getPlatform,
-  isIOSApp,
-  isAndroidApp,
-  isWebApp,
-  isMobileApp,
-  isAndroid,
-  isIOS,
-  isMobile
-} from './platform'
-export { getDeviceName } from './device'
-export { checkApp, startApp } from './apps'
-export {
-  hasDevicePlugin,
-  hasInAppBrowserPlugin,
-  hasSafariPlugin,
-  hasNetworkInformationPlugin
-} from './plugins'
-export { isCordova } from './cordova'
+export { isAndroid, isIOS, isMobile } from './platform'
 
-export { nativeLinkOpen } from './link'
 export { openDeeplinkOrRedirect } from './deeplink'
 
 export {
