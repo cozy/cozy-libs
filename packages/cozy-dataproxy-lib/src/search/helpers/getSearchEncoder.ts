17dd7697dec36830d37a732767c91b5a899a55fb
Paul Tran-Van <paul.tranvan@gmail.com>
2024-12-13 16:35:19 +0100

    fix: Imprecise search results
    Sometimes a search could return surprising results, with no clear match.
    For instance a "test" search would return a file with the name
    "dist.pdf".
    This is because we used the "balance" encoder, which performs a
    "literal" transformation on text, e.g. "d" matches "t", "i" matches "e",
    and so on. Thus, "dist" is transformed into "test", hence the matching.
    
    This is not wanted in our case, and makes the index bigger, so we
    disable it.

--- packages/cozy-dataproxy-lib/src/search/helpers/getSearchEncoder.ts
+++ packages/cozy-dataproxy-lib/src/search/helpers/getSearchEncoder.ts
@@ -1,7 +1,7 @@
 import FlexSearch from 'flexsearch'
 // @ts-expect-error module/lang/latin/balance is not described by Flexsearch types but exists
-import { encode as encode_balance } from 'flexsearch/dist/module/lang/latin/balance'
+import { encode as encode_simple } from 'flexsearch/dist/module/lang/latin/simple'
 
 export const getSearchEncoder = (): FlexSearch.Encoders => {
-  return encode_balance as FlexSearch.Encoders
+  return encode_simple as FlexSearch.Encoders
 }
