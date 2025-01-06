0bc40a02d17dd00f3c033c046ec12a9e2a67dbe3
Théo Poizat <hello@zatteo.com>
2025-01-06 12:10:14 +0100

    test: Remove enzyme from cozy-procedures
    We want to remove enzyme which is deprecated. Because we do not know if
    we want to keep cozy-procedures which has been unmainted for the last
    4 years, I removed them without doing the work of migrating them to
    testing-library.

--- packages/cozy-procedures/jestSetup.js
+++
@@ -1,7 +0,0 @@
-import { configure, mount, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-configure({ adapter: new Adapter() })
-
-global.mount = mount
-global.shallow = shallow
