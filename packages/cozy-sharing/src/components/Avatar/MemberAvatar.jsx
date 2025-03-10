f1c91b0eca443a277e68cd278db0a94271ddccd4
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-03-10 16:29:30 +0100

    feat(Sharing): Remove image avatar from stack

--- packages/cozy-sharing/src/components/Avatar/MemberAvatar.jsx
+++ packages/cozy-sharing/src/components/Avatar/MemberAvatar.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 
-import { useClient } from 'cozy-client'
 import Avatar from 'cozy-ui/transpiled/react/legacy/Avatar'
 
 import logger from '../../logger'
 import { getDisplayName, getInitials } from '../../models'
 
 const MemberAvatar = ({ recipient, ...rest }) => {
-  const client = useClient()
-
   // There are cases when due to apparent memory leaks, the recipient does not exist
   // This will trigger an unhanded error in the Avatar component and crash the app
   // At the moment, we are not sure where is the root cause of this cascading undefined props
@@ -21,21 +18,23 @@ const MemberAvatar = ({ recipient, ...rest }) => {
 
   /**
    * avatarPath is always the same for a recipient, but image
-   * can be different since the stack generate it on the fly.
+   * can be different since the stack generate it on the fly
+   * because they can be customized by the user.
    * It can be "gray" during the first load depending of the
    * status' sharing, but can become active (from the realtime)
    * so we need a way to "refresh" the image. Passing the
    * status in the url force the refresh of the image when the
    * status changes
    */
-  const image =
-    recipient.avatarPath && recipient.status
-      ? `${client.options.uri}${recipient.avatarPath}?v=${recipient.status}`
-      : null
+  // we comment this part for now because it's not compliant with Twake theme
+  // const image =
+  //   recipient.avatarPath && recipient.status
+  //     ? `${client.options.uri}${recipient.avatarPath}?v=${recipient.status}`
+  //     : null
 
   return (
     <Avatar
-      image={image}
+      // image={image}
       text={getInitials(recipient)}
       textId={getDisplayName(recipient)}
       disabled={
