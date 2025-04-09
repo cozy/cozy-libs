96dcb70db8b79f258a14a7d639f9449620188500
Théo Poizat <hello@zatteo.com>
2025-04-09 11:02:15 +0200

    feat(Search): Update mobile search bar in Twake theme

--- packages/cozy-search/src/components/AssistantMobile.jsx
+++ packages/cozy-search/src/components/AssistantMobile.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import MagnifierIcon from 'cozy-ui/transpiled/react/Icons/Magnifier'
 import SearchBar from 'cozy-ui/transpiled/react/SearchBar'
+import { isTwakeTheme } from 'cozy-ui/transpiled/react/helpers/isTwakeTheme'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 import useExtendI18n from 'cozy-ui/transpiled/react/providers/I18n/useExtendI18n'
 
-import { AssistantIcon } from './AssistantIcon/AssistantIcon'
 import { locales } from '../locales'
+import { AssistantIcon } from './AssistantIcon/AssistantIcon'
+import { isAssistantEnabled } from './helpers'
 
 export const AssistantMobile = () => {
   const { t } = useI18n()
@@ -19,10 +22,16 @@ export const AssistantMobile = () => {
     <SearchBar
       size="medium"
       icon={
-        <Icon className="u-ml-1 u-mr-half" icon={AssistantIcon} size={24} />
+        <Icon
+          className="u-ml-1 u-mr-half"
+          icon={isTwakeTheme() ? MagnifierIcon : AssistantIcon}
+          size={isTwakeTheme() ? 16 : 24}
+        />
       }
       type="button"
-      label={t('assistant.search.placeholder')}
+      label={
+        isAssistantEnabled() ? t('assistant.search.placeholder') : undefined // fallback on SearchBar default
+      }
       onClick={() => navigate('connected/search')} // FIXME this route is related to home app
     />
   )
