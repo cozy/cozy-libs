95df658e1129914c028b07709b3f12de4c379831
Bruno Michel <bruno@cozycloud.cc>
2025-04-24 14:45:42 +0200

    feat: Update cozy-mjml for Twake

--- packages/cozy-mjml/src/components/MJFooter.js
+++ packages/cozy-mjml/src/components/MJFooter.js
@@ -14,17 +14,10 @@ class MJFooter extends core.BodyComponent {
       return `
         <mj-section padding="16px 0">
           <mj-column vertical-align="middle">
-            <mj-button href="${instance}" background-color="#fff" font-size="14px" color="#297ef2" text-decoration="none" font-weight="bold" padding="0" inner-padding="4px 16px" vertical-align="middle" border-radius="16px">
-              <img width="24" height="24" src="https://files.cozycloud.cc/cozy-mjml/cozy-logo-round.png" style="vertical-align:middle;" />&nbsp;
+            <mj-button href="${instance}" background-color="#fff" font-size="14px" color="#0a84ff" text-decoration="none" font-weight="bold" padding="0" inner-padding="4px 16px" vertical-align="middle" border-radius="16px">
+              <img width="24" height="24" src="https://files.cozycloud.cc/cozy-mjml/twakelogo.png" style="vertical-align:middle;" />&nbsp;
               ${hostname}
             </mj-button>
-          </mj-column>
-        </mj-section>`
-    } else {
-      return `
-        <mj-section padding="16px 0 8px">
-          <mj-column align="center">
-            <mj-image padding="0" width="32px" src="https://files.cozycloud.cc/cozy-mjml/cozy-logo-round.png"></mj-image>
           </mj-column>
         </mj-section>`
     }
@@ -32,10 +25,10 @@ class MJFooter extends core.BodyComponent {
 
   cozySignature(locale) {
     // The default locale is 'en'
-    let text = 'Cozy Cloud hosts your personal cloud, Cozy'
+    let text = 'Twake Workplace hosts your personal cloud'
     let more = 'Hosted in France • Respectful of your privacy • Secure'
     if (locale == 'fr') {
-      text = 'Cozy Cloud héberge votre domicile numérique, Cozy'
+      text = 'Twake Workplace héberge votre domicile numérique'
       more = 'Hébergé en France • Respectueux de votre vie privée • Sécurisé'
     }
     return `
@@ -48,23 +41,25 @@ class MJFooter extends core.BodyComponent {
   }
 
   cozyHelp(locale) {
-    let blog = "Cozy Cloud's blog"
+    let blog = "Twake Workplace's blog"
     let help = 'Help &amp; support'
+    let link = 'https://help.cozy.io/'
     if (locale == 'fr') {
-      blog = 'Blog de Cozy Cloud'
+      blog = 'Blog de Twake Workplace'
       help = 'Aide et support'
+      link = 'https://support.cozy.io/'
     }
     return `
       <mj-section padding="0">
         <mj-group>
           <mj-column width="49.5%" padding="0">
-            <mj-button href="https://blog.cozy.io" mj-class="primary-link" font-size="12px" align="right">${blog}</mj-button>
+            <mj-button href="https://blog.cozy.io/${locale}/" mj-class="primary-link" font-size="12px" align="right">${blog}</mj-button>
           </mj-column>
           <mj-column width="1%" padding="0">
             <mj-text align="center" padding="5px 0" color="#95999d" font-size="16px">|</mj-text>
           </mj-column>
           <mj-column width="49.5%" padding="0">
-            <mj-button href="https://support.cozy.io" mj-class="primary-link" font-size="12px" align="left">${help}</mj-button>
+            <mj-button href="${link}" mj-class="primary-link" font-size="12px" align="left">${help}</mj-button>
           </mj-column>
         </mj-group>
       </mj-section>`
@@ -81,13 +76,13 @@ class MJFooter extends core.BodyComponent {
       <mj-section padding-bottom="5px">
         <mj-group>
           <mj-column width="50%">
-            <mj-image align="right" alt="Appstore" padding="4px" width="130px" src="https://files.cozycloud.cc/cozy-mjml/appstore-${locale}.png" href="https://itunes.apple.com/${itunesLang}/developer/cozy-cloud/id1131616091?mt=8"></mj-image>
+            <mj-image align="right" alt="Appstore" padding="4px" width="130px" src="https://files.cozycloud.cc/cozy-mjml/twake-appstore-${locale}.png" href="https://itunes.apple.com/${itunesLang}/developer/cozy-cloud/id1131616091?mt=8"></mj-image>
           </mj-column>
           <mj-column width="50%">
-            <mj-image align="left" alt="Play Store" padding="4px" width="130px" src="https://files.cozycloud.cc/cozy-mjml/playstore-${locale}.png" href="https://play.google.com/store/apps/developer?id=Cozy+Cloud${playParams}"></mj-image>
+            <mj-image align="left" alt="Play Store" padding="4px" width="130px" src="https://files.cozycloud.cc/cozy-mjml/twake-playstore-${locale}.png" href="https://play.google.com/store/apps/developer?id=Cozy+Cloud${playParams}"></mj-image>
           </mj-column>
           <mj-column width="100%">
-            <mj-image align="center" alt="Cozy Desktop" padding="4px" width="130px" src="https://files.cozycloud.cc/cozy-mjml/desktop-${locale}.png" href="https://cozy.io/${locale}/download/#desktop"></mj-image>
+            <mj-image align="center" alt="Cozy Desktop" padding="4px" width="130px" src="https://files.cozycloud.cc/cozy-mjml/twake-desktop-${locale}.png" href="https://cozy.io/${locale}/download/#desktop"></mj-image>
           </mj-column>
         </mj-group>
       </mj-section>`
