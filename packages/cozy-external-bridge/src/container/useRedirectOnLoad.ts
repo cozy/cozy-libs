9b03927605fba958078e1dfa8a1b9193223acb4b
Théo Poizat <hello@zatteo.com>
2025-05-22 08:49:43 +0200

    feat: Add log for useRedirectOnLoad

--- packages/cozy-external-bridge/src/container/useRedirectOnLoad.ts
+++ packages/cozy-external-bridge/src/container/useRedirectOnLoad.ts
@@ -1,8 +1,12 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
+import Minilog from 'cozy-minilog'
+
 import { BRIDGE_ROUTE_PREFIX } from './constants'
 import { getIframe } from './helpers'
+
+const log = Minilog('🌉 [Container bridge]')
 
 // When we load the container app, we want to forward
 // the relevant part of the URL to the iframe
@@ -18,6 +22,8 @@ export const useRedirectOnLoad = (): void => {
       destUrl.hash = location.hash
       destUrl.search = location.search
       iframe.src = destUrl.toString()
+
+      log.debug('Redirecting iframe to', destUrl.toString())
     }
   }, [])
 }
