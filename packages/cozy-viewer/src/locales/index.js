ef1dfa8243f561f1ab0c31807af2438c31f42a52
Théo Poizat <hello@zatteo.com>
2025-09-23 15:55:26 +0200

    feat(cozy-viewer): Add ru and vi locales

--- packages/cozy-viewer/src/locales/index.js
+++ packages/cozy-viewer/src/locales/index.js
@@ -2,7 +2,9 @@ import { getI18n } from 'cozy-ui/transpiled/react/providers/I18n/helpers'
 
 import en from './en.json'
 import fr from './fr.json'
+import ru from './ru.json'
+import vi from './vi.json'
 
-export const locales = { en, fr }
+export const locales = { en, fr, ru, vi }
 
 export const getViewerI18n = () => getI18n(undefined, lang => locales[lang])
