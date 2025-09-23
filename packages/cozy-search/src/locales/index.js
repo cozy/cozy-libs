aa7766717698176b7fbb16802230d02fe084edc3
Théo Poizat <hello@zatteo.com>
2025-09-23 15:54:31 +0200

    feat(cozy-search): Add ru and vi locales

--- packages/cozy-search/src/locales/index.js
+++ packages/cozy-search/src/locales/index.js
@@ -1,4 +1,6 @@
 import en from './en.json'
 import fr from './fr.json'
+import ru from './ru.json'
+import vi from './vi.json'
 
-export const locales = { en, fr }
+export const locales = { en, fr, ru, vi }
