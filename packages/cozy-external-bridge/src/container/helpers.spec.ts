c9c7ef08d10a46ec9250e34d2204c24fabff19cb
Théo Poizat <hello@zatteo.com>
2025-11-07 11:11:57 +0100

    test: Add tests to handleParentOriginRequest

--- packages/cozy-external-bridge/src/container/helpers.spec.ts
+++ packages/cozy-external-bridge/src/container/helpers.spec.ts
@@ -1,4 +1,4 @@
-import { extractUrl } from './helpers'
+import { extractUrl, handleParentOriginRequest } from './helpers'
 
 describe('extractUrl', () => {
   it('should extract pathname, hash, and search from a URL starting with http', () => {
@@ -11,5 +11,47 @@ describe('extractUrl', () => {
     const url = '/path?query=123#fragment'
     const expected = '/path?query=123#fragment'
     expect(extractUrl(url)).toBe(expected)
+  })
+})
+
+describe('handleParentOriginRequest', () => {
+  it('should return undefined when event.origin is different from url origin', () => {
+    const mockPostMessage = jest.fn()
+    const url = 'http://expected-origin.com'
+    const eventOrigin = 'http://different-origin.com'
+    const mockEvent = {
+      origin: eventOrigin,
+      data: 'requestParentOrigin',
+      source: {
+        postMessage: mockPostMessage
+      }
+    } as unknown as MessageEvent
+
+    const result = handleParentOriginRequest(mockEvent, url)
+
+    expect(result).toBeUndefined()
+    expect(mockPostMessage).not.toHaveBeenCalled()
+  })
+
+  it('should successfully execute postMessage when origins match', () => {
+    const mockPostMessage = jest.fn()
+    const url = 'http://expected-origin.com'
+    const eventOrigin = 'http://expected-origin.com'
+    const mockEvent = {
+      origin: eventOrigin,
+      data: 'requestParentOrigin',
+      source: {
+        postMessage: mockPostMessage
+      }
+    } as unknown as MessageEvent
+
+    const result = handleParentOriginRequest(mockEvent, url)
+
+    expect(result).toBeUndefined()
+    expect(mockPostMessage).toHaveBeenCalledWith(
+      'answerParentOrigin',
+      eventOrigin
+    )
+    expect(mockPostMessage).toHaveBeenCalledTimes(1)
   })
 })
